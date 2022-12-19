2bfd79790585f6b314ad2888975eae07f861149e
prxt <prxt@6529.io>
2022-12-19 20:17:04 +0200

    Fix user page, make latest activity images clickable
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- pages/[user].tsx
+++ pages/[user].tsx
@@ -58,16 +58,17 @@ export async function getServerSideProps(req: any, res: any, resolvedUrl: any) {
     };
   }
   const nftRequest = await fetch(`${process.env.API_ENDPOINT}/api/ens/${user}`);
-  const response = await nftRequest.json();
-
-  const userDisplay = response.display
-    ? response.display
-    : formatAddress(response.wallet);
+  let userDisplay = formatAddress(user);
+  const responseText = await nftRequest.text();
+  if (responseText) {
+    const response = await nftRequest.json();
+    userDisplay = response.display ? response.display : userDisplay;
+  }
 
   return {
     props: {
       title: userDisplay,
-      url: response.display ? response.display : response.wallet,
+      url: userDisplay.includes(".eth") ? userDisplay : user,
     },
   };
 }
