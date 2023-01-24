0b737020a41e3cccb3de50ab18d4d8042944d16f
prxt <prxt@6529.io>
2023-01-24 13:07:08 +0200

    Introduce BASE_ENDPOINT env variable

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
     ETHERSCAN_API_KEY: process.env.ETHERSCAN_API_KEY,
     ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY,
     API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
+    BASE_ENDPOINT: process.env.REACT_APP_BASE_ENDPOINT,
     ACTIVATE_API_PASSWORD:
       process.env.REACT_APP_ACTIVATE_API_PASSWORD &&
       process.env.REACT_APP_ACTIVATE_API_PASSWORD === "true"
