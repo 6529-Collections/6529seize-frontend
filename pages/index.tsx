83d61e9960801e847392b6962479b0a67ba0de6e
prxt <prxt@6529.io>
2023-02-09 10:01:41 +0200

    Test layout shift
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,6 @@ import { Transaction } from "ethers";
 import { MEMES_CONTRACT } from "../constants";
 import { DBResponse } from "../entities/IDBResponse";
 import { NFT, MemesExtendedData } from "../entities/INFT";
-import Leaderboard from "../components/leaderboard/Leaderboard";
-import LatestActivity from "../components/latest-activity/LatestActivity";
 
 import dynamic from "next/dynamic";
 import { getDateDisplay, numberWithCommas } from "../helpers/Helpers";
@@ -23,7 +21,20 @@ const NFTImage = dynamic(() => import("../components/nft-image/NFTImage"), {
   ssr: false,
 });
 
+const Leaderboard = dynamic(
+  () => import("../components/leaderboard/Leaderboard"),
+  { ssr: false }
+);
+
+const LatestActivity = dynamic(
+  () => import("../components/latest-activity/LatestActivity"),
+  { ssr: false }
+);
+
 export default function Home() {
+  const [isHeaderLoaded, setIsHeaderLoaded] = useState(false);
+  const [isNftImageLoaded, setIsNftImageLoaded] = useState(false);
+
   const [nft, setNFT] = useState<NFT>();
   const [nftExtended, setnftExtended] = useState<MemesExtendedData>();
   const { address, connector, isConnected } = useAccount();
@@ -98,9 +109,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Header />
+        <Header
+          onLoad={() => {
+            setIsHeaderLoaded(true);
+          }}
+        />
 
-        {nft && nftExtended && (
+        {isHeaderLoaded && nft && nftExtended && (
           <>
             <Container className={`pt-4 ${styles.mainContainer}`}>
               <Row>
@@ -123,6 +138,9 @@ export default function Home() {
                           animation={true}
                           height={650}
                           balance={nftBalance}
+                          onLoad={() => {
+                            setIsNftImageLoaded(true);
+                          }}
                         />
                       ) : (
                         <a href={`/the-memes/${nft.id}`}>
@@ -131,205 +149,214 @@ export default function Home() {
                             animation={true}
                             height={650}
                             balance={nftBalance}
+                            onLoad={() => {
+                              setIsNftImageLoaded(true);
+                            }}
                           />
                         </a>
                       )}
                     </Row>
                   </Container>
                 </Col>
-                <Col
-                  className="pt-3 pb-3"
-                  xs={{ span: 12 }}
-                  sm={{ span: 12 }}
-                  md={{ span: 6 }}
-                  lg={{ span: 6 }}>
-                  <Container>
-                    <Row>
-                      <Col>
-                        <u>
-                          <h3>
-                            <a href={`/the-memes/${nft.id}`}>
-                              Card {nft.id} - {nft.name}
-                            </a>
-                          </h3>
-                        </u>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                        <Table bordered={false}>
-                          <tbody>
-                            <tr>
-                              <td>Edition Size</td>
-                              <td>{nft.supply}</td>
-                            </tr>
-                            <tr>
-                              <td>Collection</td>
-                              <td>{nft.collection}</td>
-                            </tr>
-                            <tr>
-                              <td>Season</td>
-                              <td>{nftExtended.season}</td>
-                            </tr>
-                            <tr>
-                              <td>Meme</td>
-                              <td>{nftExtended.meme_name}</td>
-                            </tr>
-                            <tr>
-                              <td>Artist</td>
-                              <td>{nft.artist}</td>
-                            </tr>
-                            <tr>
-                              <td>Mint Date</td>
-                              <td>{printMintDate(nft)}</td>
-                            </tr>
-                            <tr>
-                              <td>File Type</td>
-                              <td>
-                                {nft.animation
-                                  ? nft.metadata.animation_details?.format
-                                  : nft.metadata.image_details.format}
-                              </td>
-                            </tr>
-                            <tr>
-                              <td>Dimensions</td>
-                              <td>
-                                {nft.metadata.image_details.width} x{" "}
-                                {nft.metadata.image_details.height}
-                              </td>
-                            </tr>
-                          </tbody>
-                        </Table>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                        <h3>Minting Approach</h3>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                        <a
-                          href={
-                            `https://github.com/6529-Collections/thememecards/tree/main/card` +
-                            nft.id
-                          }
-                          target="_blank"
-                          rel="noreferrer">
-                          Distribution Plan
-                        </a>
-                      </Col>
-                    </Row>
-                    {/* <Row>
-                      <Col>
-                        <a
-                          href={
-                            `https://github.com/6529-Collections/thememecards/tree/main/card` +
-                            nft.id
-                          }
-                          target="_blank"
-                          rel="noreferrer">
-                          Allowlist
-                        </a>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                        <a
-                          href={
-                            `https://github.com/6529-Collections/thememecards/tree/main/card` +
-                            nft.id
-                          }
-                          target="_blank"
-                          rel="noreferrer">
-                          Randomization
-                        </a>
-                      </Col>
-                    </Row> */}
-                    <Row className="pt-3">
-                      <Col>
-                        Mint price:{" "}
-                        {nft.mint_price > 0 ? `${nft.mint_price} ETH` : `N/A`}
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                        Floor Price:{" "}
-                        {nft.floor_price > 0
-                          ? `${numberWithCommas(
-                              Math.round(nft.floor_price * 100) / 100
-                            )} ETH`
-                          : `N/A`}
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                        Market Cap:{" "}
-                        {nft.market_cap > 0
-                          ? `${numberWithCommas(
-                              Math.round(nft.market_cap * 100) / 100
-                            )} ETH`
-                          : `N/A`}
-                      </Col>
-                    </Row>
-                    <Row className="pt-3">
-                      <Col>
-                        <a
-                          href={`https://opensea.io/assets/ethereum/${MEMES_CONTRACT}/${nft.id}`}
-                          target="_blank"
-                          rel="noreferrer">
-                          <Image
-                            className={styles.marketplace}
-                            src="/opensea.png"
-                            alt="opensea"
-                            width={40}
-                            height={40}
-                          />
-                        </a>
-                        {/* <a
-                          href={`https://looksrare.org/collections/${MEMES_CONTRACT}/${nft.id}`}
-                          target="_blank"
-                          rel="noreferrer">
-                          <Image
-                        className={styles.marketplace}
-                        src="/looksrare.png"
-                        alt="looksrare"
-                        width={40}
-                        height={40}
-                      />
-                        </a> */}
-                        <a
-                          href={`https://x2y2.io/eth/${MEMES_CONTRACT}/${nft.id}`}
-                          target="_blank"
-                          rel="noreferrer">
-                          <Image
-                            className={styles.marketplace}
-                            src="/x2y2.png"
-                            alt="x2y2"
-                            width={40}
-                            height={40}
-                          />
-                        </a>
-                      </Col>
-                    </Row>
-                  </Container>
-                </Col>
+                {isNftImageLoaded && (
+                  <Col
+                    className="pt-3 pb-3"
+                    xs={{ span: 12 }}
+                    sm={{ span: 12 }}
+                    md={{ span: 6 }}
+                    lg={{ span: 6 }}>
+                    <Container>
+                      <Row>
+                        <Col>
+                          <u>
+                            <h3>
+                              <a href={`/the-memes/${nft.id}`}>
+                                Card {nft.id} - {nft.name}
+                              </a>
+                            </h3>
+                          </u>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <Table bordered={false}>
+                            <tbody>
+                              <tr>
+                                <td>Edition Size</td>
+                                <td>{nft.supply}</td>
+                              </tr>
+                              <tr>
+                                <td>Collection</td>
+                                <td>{nft.collection}</td>
+                              </tr>
+                              <tr>
+                                <td>Season</td>
+                                <td>{nftExtended.season}</td>
+                              </tr>
+                              <tr>
+                                <td>Meme</td>
+                                <td>{nftExtended.meme_name}</td>
+                              </tr>
+                              <tr>
+                                <td>Artist</td>
+                                <td>{nft.artist}</td>
+                              </tr>
+                              <tr>
+                                <td>Mint Date</td>
+                                <td>{printMintDate(nft)}</td>
+                              </tr>
+                              <tr>
+                                <td>File Type</td>
+                                <td>
+                                  {nft.animation
+                                    ? nft.metadata.animation_details?.format
+                                    : nft.metadata.image_details.format}
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>Dimensions</td>
+                                <td>
+                                  {nft.metadata.image_details.width} x{" "}
+                                  {nft.metadata.image_details.height}
+                                </td>
+                              </tr>
+                            </tbody>
+                          </Table>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <h3>Minting Approach</h3>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <a
+                            href={
+                              `https://github.com/6529-Collections/thememecards/tree/main/card` +
+                              nft.id
+                            }
+                            target="_blank"
+                            rel="noreferrer">
+                            Distribution Plan
+                          </a>
+                        </Col>
+                      </Row>
+                      {/* <Row>
+                        <Col>
+                          <a
+                            href={
+                              `https://github.com/6529-Collections/thememecards/tree/main/card` +
+                              nft.id
+                            }
+                            target="_blank"
+                            rel="noreferrer">
+                            Allowlist
+                          </a>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <a
+                            href={
+                              `https://github.com/6529-Collections/thememecards/tree/main/card` +
+                              nft.id
+                            }
+                            target="_blank"
+                            rel="noreferrer">
+                            Randomization
+                          </a>
+                        </Col>
+                      </Row> */}
+                      <Row className="pt-3">
+                        <Col>
+                          Mint price:{" "}
+                          {nft.mint_price > 0 ? `${nft.mint_price} ETH` : `N/A`}
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          Floor Price:{" "}
+                          {nft.floor_price > 0
+                            ? `${numberWithCommas(
+                                Math.round(nft.floor_price * 100) / 100
+                              )} ETH`
+                            : `N/A`}
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          Market Cap:{" "}
+                          {nft.market_cap > 0
+                            ? `${numberWithCommas(
+                                Math.round(nft.market_cap * 100) / 100
+                              )} ETH`
+                            : `N/A`}
+                        </Col>
+                      </Row>
+                      <Row className="pt-3">
+                        <Col>
+                          <a
+                            href={`https://opensea.io/assets/ethereum/${MEMES_CONTRACT}/${nft.id}`}
+                            target="_blank"
+                            rel="noreferrer">
+                            <Image
+                              className={styles.marketplace}
+                              src="/opensea.png"
+                              alt="opensea"
+                              width={40}
+                              height={40}
+                            />
+                          </a>
+                          {/* <a
+                            href={`https://looksrare.org/collections/${MEMES_CONTRACT}/${nft.id}`}
+                            target="_blank"
+                            rel="noreferrer">
+                            <Image
+                          className={styles.marketplace}
+                          src="/looksrare.png"
+                          alt="looksrare"
+                          width={40}
+                          height={40}
+                        />
+                          </a> */}
+                          <a
+                            href={`https://x2y2.io/eth/${MEMES_CONTRACT}/${nft.id}`}
+                            target="_blank"
+                            rel="noreferrer">
+                            <Image
+                              className={styles.marketplace}
+                              src="/x2y2.png"
+                              alt="x2y2"
+                              width={40}
+                              height={40}
+                            />
+                          </a>
+                        </Col>
+                      </Row>
+                    </Container>
+                  </Col>
+                )}
               </Row>
             </Container>
-            <Container className={styles.mainContainer}>
-              <Row>
-                <Col xs={12} sm={12} md={12} lg={12}>
-                  <Leaderboard page={1} pageSize={10} showMore={false} />
-                </Col>
-              </Row>
-            </Container>
-            <Container className={styles.mainContainer}>
-              <Row>
-                <Col xs={12} sm={12} md={12} lg={12}>
-                  <LatestActivity page={1} pageSize={12} showMore={false} />
-                </Col>
-              </Row>
-            </Container>
+            {isNftImageLoaded && (
+              <>
+                <Container className={styles.mainContainer}>
+                  <Row>
+                    <Col xs={12} sm={12} md={12} lg={12}>
+                      <Leaderboard page={1} pageSize={10} showMore={false} />
+                    </Col>
+                  </Row>
+                </Container>
+                <Container className={styles.mainContainer}>
+                  <Row>
+                    <Col xs={12} sm={12} md={12} lg={12}>
+                      <LatestActivity page={1} pageSize={12} showMore={false} />
+                    </Col>
+                  </Row>
+                </Container>
+              </>
+            )}
           </>
         )}
       </main>
