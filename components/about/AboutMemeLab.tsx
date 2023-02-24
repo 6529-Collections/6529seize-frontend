7b7044606f53af058485f9907178f71b7f0623c7
prxt <prxt@6529.io>
2023-02-24 11:25:24 +0200

    Fix ssr for about section, align in community table, test sourcemaps

--- components/about/AboutMemeLab.tsx
+++ components/about/AboutMemeLab.tsx
@@ -1,26 +1,9 @@
 import { Col, Container, Row } from "react-bootstrap";
-import Head from "next/head";
-import { AboutSection } from "../../pages/about/[section]";
 import Image from "next/image";
 
 export default function AboutMemeLab() {
   return (
     <>
-      <Head>
-        <title>About - MemeLab | 6529 SEIZE</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="About - MemeLab | 6529 SEIZE" />
-        <meta
-          property="og:url"
-          content={`${process.env.BASE_ENDPOINT}/about/${AboutSection.MEME_LAB}`}
-        />
-        <meta property="og:title" content={`About - MemeLab`} />
-        <meta property="og:description" content="6529 SEIZE" />
-        <meta
-          property="og:image"
-          content={`${process.env.BASE_ENDPOINT}/Seize_Logo_Glasses_2.png`}
-        />
-      </Head>
       <Container>
         <Row>
           <Col className="text-center pt-2 pb-4">
--- components/about/AboutMemesCalendar.tsx
+++ components/about/AboutMemesCalendar.tsx
@@ -1,29 +1,9 @@
 import styles from "./About.module.scss";
 import { Col, Container, Row, Table } from "react-bootstrap";
-import Head from "next/head";
-import { AboutSection } from "../../pages/about/[section]";
 
 export default function AboutMemesCalendar() {
   return (
     <>
-      <Head>
-        <title>About - Memes Calendar | 6529 SEIZE</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="About - Memes Calendar | 6529 SEIZE"
-        />
-        <meta
-          property="og:url"
-          content={`${process.env.BASE_ENDPOINT}/about/${AboutSection.MEMES_CALENDAR}`}
-        />
-        <meta property="og:title" content={`About - Memes Calendar`} />
-        <meta property="og:description" content="6529 SEIZE" />
-        <meta
-          property="og:image"
-          content={`${process.env.BASE_ENDPOINT}/Seize_Logo_Glasses_2.png`}
-        />
-      </Head>
       <Container>
         <Row>
           <Col className="text-center">
