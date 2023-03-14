dc5f8c71b6e3ff66734d0149be1946247067660b
prxt <prxt@6529.io>
2023-03-14 12:32:58 +0200

    Add GDRC1 in about
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

---
+++ components/about/AboutGDRC1.tsx
@@ -0,0 +1,37 @@
+import { Col, Container, Row } from "react-bootstrap";
+import styles from "./About.module.scss";
+
+interface Props {
+  html: string;
+}
+
+export default function AboutGDRC1(props: Props) {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col className="pt-2 pb-2">
+            We support{" "}
+            <a
+              href="https://digitalrightscharter.org/"
+              target="_blank"
+              rel="noreferrer">
+              The Global Digital Rights Charter 1
+            </a>
+            .
+            <br />
+            <br />
+            Full text of the GDRC 1 is below.
+          </Col>
+        </Row>
+        <Row className="pt-3 pb-3">
+          <Col
+            className={styles.aboutGDRC1Container}
+            dangerouslySetInnerHTML={{
+              __html: props.html,
+            }}></Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
