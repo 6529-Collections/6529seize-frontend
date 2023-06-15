4c0ed749804e8e10427e530bc70f78ed3b3a049b
Ragne <ragneinfo@gmail.com>
2023-06-15 15:40:12 +0300

    changes

--- components/allowlist-tool/builder/token-pools/AllowlistToolBuilderTokenPoolsAdd.tsx
+++ components/allowlist-tool/builder/token-pools/AllowlistToolBuilderTokenPoolsAdd.tsx
@@ -163,7 +163,23 @@ export default function AllowlistToolBuilderTokenPoolsAdd() {
         </div>
         <div className="tw-flex-1">
           <label className="tw-block tw-text-sm tw-font-normal tw-leading-5 tw-text-neutral-100">
-            Token ID(s)
+            <label className="tw-flex tw-items-center tw-gap-x-2 tw-text-sm tw-font-normal tw-leading-5 tw-text-neutral-100">
+              <span>Token ID(s)</span>
+              <svg
+                className="tw-h-5 tw-w-5 tw-text-neutral-400"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M12 16V12M12 8H12.01M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </label>
           </label>
           <div className="tw-mt-2">
             <input
