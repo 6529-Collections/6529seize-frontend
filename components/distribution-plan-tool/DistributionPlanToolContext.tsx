49fe2f2e4eb0d2eae4027343d5e3a4dddceefd67
Simo <info@analyticsflow.ee>
2023-07-04 11:11:02 +0300

    wip

--- components/distribution-plan-tool/DistributionPlanToolContext.tsx
+++ components/distribution-plan-tool/DistributionPlanToolContext.tsx
@@ -299,9 +299,8 @@ export default function DistributionPlanToolContextWrapper({
     setDistributionPlan(distributionPlan);
     await initState(distributionPlan.id);
     if (step === DistributionPlanToolStep.CREATE_PLAN) {
-      setStep(DistributionPlanToolStep.BUILD_PHASES);
+      setStep(DistributionPlanToolStep.CREATE_SNAPSHOTS);
     }
-    // setStep(DistributionPlanToolStep.CREATE_PHASES);
   };
 
   return (
