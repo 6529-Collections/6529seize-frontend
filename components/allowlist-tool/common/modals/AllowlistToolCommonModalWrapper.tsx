80d4855cebbdd2409184692e5cdf36401bccea73
Ragne <ragneinfo@gmail.com>
2023-07-10 18:45:57 +0300

    wip

--- components/allowlist-tool/common/modals/AllowlistToolCommonModalWrapper.tsx
+++ components/allowlist-tool/common/modals/AllowlistToolCommonModalWrapper.tsx
@@ -52,7 +52,7 @@ export default function AllowlistToolCommonModalWrapper({
                   ref={modalRef}
                   className={`tw-relative tw-w-full tw-transform tw-rounded-lg tw-bg-neutral-900 tw-pb-4 tw-pt-5 sm:tw-pb-6 tw-text-left tw-shadow-xl tw-transition-all sm:tw-my-8 sm:tw-w-full  ${modalSizeClass}`}
                 >
-                  <div className="tw-px-4 sm:tw-px-6 tw-flex tw-justify-between tw-items-center">
+                  <div className="tw-px-4 sm:tw-pl-6 sm:tw-pr-4 tw-flex tw-justify-between tw-items-center">
                     <p className="tw-max-w-sm tw-text-lg tw-text-white tw-font-medium tw-mb-0">
                       {title}
                     </p>
--- components/distribution-plan-tool/build-phases/build-phase/form/component-config/snapshots-table/FinalizeSnapshotsTable.tsx
+++ components/distribution-plan-tool/build-phases/build-phase/form/component-config/snapshots-table/FinalizeSnapshotsTable.tsx
@@ -105,89 +105,85 @@ export default function FinalizeSnapshotsTable({
   }, [groupSnapshots, snapshots]);
   return (
     <div className="tw-mt-6 tw-flow-root">
-      <div className="-tw-mx-4 -tw-my-2 tw-overflow-x-auto sm:-tw-mx-6 lg:-tw-mx-8">
-        <div className="tw-inline-block tw-min-w-full tw-py-2 tw-align-middle sm:tw-px-6 lg:tw-px-8">
-          <div className="tw-overflow-hidden tw-shadow tw-ring-1 tw-ring-white/10 tw-rounded-lg">
-            <table className="tw-min-w-full tw-divide-y tw-divide-neutral-700/60">
-              <thead className="tw-bg-neutral-800/60">
-                <th
-                  scope="col"
-                  className="tw-py-3 tw-pl-4 tw-pr-3 tw-whitespace-nowrap tw-text-left tw-text-[0.6875rem] tw-leading-[1.125rem] tw-font-medium tw-text-neutral-400 tw-uppercase tw-tracking-[0.25px] sm:tw-pl-6"
-                >
-                  Name
-                </th>
-                <th
-                  scope="col"
-                  className="tw-px-3 tw-py-3 tw-whitespace-nowrap tw-text-left tw-text-[0.6875rem] tw-leading-[1.125rem] tw-font-medium tw-text-neutral-400 tw-uppercase tw-tracking-[0.25px]"
-                >
-                  Type
-                </th>
-                <th
-                  scope="col"
-                  className="tw-px-3 tw-py-3 tw-whitespace-nowrap tw-text-left tw-text-[0.6875rem] tw-leading-[1.125rem] tw-font-medium tw-text-neutral-400 tw-uppercase tw-tracking-[0.25px]"
-                >
-                  Exclude components
-                </th>
-                <th
-                  scope="col"
-                  className="tw-px-3 tw-py-3 tw-whitespace-nowrap tw-text-left tw-text-[0.6875rem] tw-leading-[1.125rem] tw-font-medium tw-text-neutral-400 tw-uppercase tw-tracking-[0.25px]"
-                >
-                  Top holders
-                </th>
-                <th
-                  scope="col"
-                  className="tw-px-3 tw-py-3 tw-whitespace-nowrap tw-text-left tw-text-[0.6875rem] tw-leading-[1.125rem] tw-font-medium tw-text-neutral-400 tw-uppercase tw-tracking-[0.25px]"
-                ></th>
-              </thead>
-              <tbody className="tw-bg-neutral-900 tw-divide-y tw-divide-neutral-700/40">
-                <AllowlistToolAnimationWrapper mode="wait" initial={false}>
-                  {rows.map((row) => (
-                    <DistributionPlanTableRowWrapper key={row.groupSnapshotId}>
-                      <td className="tw-whitespace-nowrap tw-py-4 tw-pl-4 tw-pr-3 tw-text-xs tw-font-medium tw-text-white sm:tw-pl-6">
-                        {row.snapshotName}
-                      </td>
-                      <td className="tw-whitespace-nowrap tw-px-3 tw-py-4 tw-text-xs tw-font-normal tw-text-neutral-300">
-                        {row.snapshotType}
-                      </td>
-                      <td className="tw-whitespace-nowrap tw-px-3 tw-py-4 tw-text-xs tw-font-normal tw-text-neutral-300">
-                        {row.excludeComponentWinners}
-                      </td>
-                      <td className="tw-whitespace-nowrap tw-px-3 tw-py-4 tw-text-xs tw-font-normal tw-text-neutral-300">
-                        {row.topHoldersFilter}
-                      </td>
-                      <td className="tw-whitespace-nowrap tw-px-3 tw-py-4 tw-text-xs tw-font-normal tw-text-neutral-300">
-                        <button
-                          type="button"
-                          title="Delete"
-                          onClick={(e) => {
-                            e.preventDefault();
-                            onRemoveGroupSnapshot(row.groupSnapshotId);
-                          }}
-                          className="tw-rounded-full tw-group tw-flex tw-items-center tw-justify-center tw-p-2 tw-text-xs tw-font-medium tw-border-none tw-ring-1 tw-ring-inset tw-text-neutral-400 tw-bg-neutral-400/10 tw-ring-neutral-400/20"
-                        >
-                          <svg
-                            className="tw-h-4 tw-w-4 group-hover:tw-text-error tw-transition tw-duration-300 tw-ease-out"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M9 3H15M3 6H21M19 6L18.2987 16.5193C18.1935 18.0975 18.1409 18.8867 17.8 19.485C17.4999 20.0118 17.0472 20.4353 16.5017 20.6997C15.882 21 15.0911 21 13.5093 21H10.4907C8.90891 21 8.11803 21 7.49834 20.6997C6.95276 20.4353 6.50009 20.0118 6.19998 19.485C5.85911 18.8867 5.8065 18.0975 5.70129 16.5193L5 6M10 10.5V15.5M14 10.5V15.5"
-                              stroke="currentColor"
-                              strokeWidth="2"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                            />
-                          </svg>
-                        </button>
-                      </td>
-                    </DistributionPlanTableRowWrapper>
-                  ))}
-                </AllowlistToolAnimationWrapper>
-              </tbody>
-            </table>
-          </div>
-        </div>
+      <div className="tw-overflow-x-auto tw-ring-1 tw-ring-white/10 tw-rounded-lg">
+        <table className="tw-min-w-full tw-divide-y tw-divide-neutral-700/60">
+          <thead className="tw-bg-neutral-800/60">
+            <th
+              scope="col"
+              className="tw-py-3 tw-pl-4 tw-pr-3 tw-whitespace-nowrap tw-text-left tw-text-[0.6875rem] tw-leading-[1.125rem] tw-font-medium tw-text-neutral-400 tw-uppercase tw-tracking-[0.25px] sm:tw-pl-6"
+            >
+              Name
+            </th>
+            <th
+              scope="col"
+              className="tw-px-3 tw-py-3 tw-whitespace-nowrap tw-text-left tw-text-[0.6875rem] tw-leading-[1.125rem] tw-font-medium tw-text-neutral-400 tw-uppercase tw-tracking-[0.25px]"
+            >
+              Type
+            </th>
+            <th
+              scope="col"
+              className="tw-px-3 tw-py-3 tw-whitespace-nowrap tw-text-left tw-text-[0.6875rem] tw-leading-[1.125rem] tw-font-medium tw-text-neutral-400 tw-uppercase tw-tracking-[0.25px]"
+            >
+              Exclude components
+            </th>
+            <th
+              scope="col"
+              className="tw-px-3 tw-py-3 tw-whitespace-nowrap tw-text-left tw-text-[0.6875rem] tw-leading-[1.125rem] tw-font-medium tw-text-neutral-400 tw-uppercase tw-tracking-[0.25px]"
+            >
+              Top holders
+            </th>
+            <th
+              scope="col"
+              className="tw-px-3 tw-py-3 tw-whitespace-nowrap tw-text-left tw-text-[0.6875rem] tw-leading-[1.125rem] tw-font-medium tw-text-neutral-400 tw-uppercase tw-tracking-[0.25px]"
+            ></th>
+          </thead>
+          <tbody className="tw-bg-neutral-900 tw-divide-y tw-divide-neutral-700/40">
+            <AllowlistToolAnimationWrapper mode="wait" initial={false}>
+              {rows.map((row) => (
+                <DistributionPlanTableRowWrapper key={row.groupSnapshotId}>
+                  <td className="tw-whitespace-nowrap tw-py-4 tw-pl-4 tw-pr-3 tw-text-xs tw-font-medium tw-text-white sm:tw-pl-6">
+                    {row.snapshotName}
+                  </td>
+                  <td className="tw-whitespace-nowrap tw-px-3 tw-py-4 tw-text-xs tw-font-normal tw-text-neutral-300">
+                    {row.snapshotType}
+                  </td>
+                  <td className="tw-whitespace-nowrap tw-px-3 tw-py-4 tw-text-xs tw-font-normal tw-text-neutral-300">
+                    {row.excludeComponentWinners}
+                  </td>
+                  <td className="tw-whitespace-nowrap tw-px-3 tw-py-4 tw-text-xs tw-font-normal tw-text-neutral-300">
+                    {row.topHoldersFilter}
+                  </td>
+                  <td className="tw-whitespace-nowrap tw-px-3 tw-py-4 tw-text-xs tw-font-normal tw-text-neutral-300">
+                    <button
+                      type="button"
+                      title="Delete"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        onRemoveGroupSnapshot(row.groupSnapshotId);
+                      }}
+                      className="tw-rounded-full tw-group tw-flex tw-items-center tw-justify-center tw-p-2 tw-text-xs tw-font-medium tw-border-none tw-ring-1 tw-ring-inset tw-text-neutral-400 tw-bg-neutral-400/10 tw-ring-neutral-400/20"
+                    >
+                      <svg
+                        className="tw-h-4 tw-w-4 group-hover:tw-text-error tw-transition tw-duration-300 tw-ease-out"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M9 3H15M3 6H21M19 6L18.2987 16.5193C18.1935 18.0975 18.1409 18.8867 17.8 19.485C17.4999 20.0118 17.0472 20.4353 16.5017 20.6997C15.882 21 15.0911 21 13.5093 21H10.4907C8.90891 21 8.11803 21 7.49834 20.6997C6.95276 20.4353 6.50009 20.0118 6.19998 19.485C5.85911 18.8867 5.8065 18.0975 5.70129 16.5193L5 6M10 10.5V15.5M14 10.5V15.5"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                    </button>
+                  </td>
+                </DistributionPlanTableRowWrapper>
+              ))}
+            </AllowlistToolAnimationWrapper>
+          </tbody>
+        </table>
       </div>
     </div>
   );
