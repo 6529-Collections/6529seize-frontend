05024b23d6274eeb550692128a2db0b91d7bf8dc
Ragne <ragneinfo@gmail.com>
2023-07-10 15:24:24 +0300

    wip

--- components/distribution-plan-tool/common/DistributionPlanTableWrapper.tsx
+++ components/distribution-plan-tool/common/DistributionPlanTableWrapper.tsx
@@ -4,7 +4,7 @@ export default function DistributionPlanTableWrapper({
   children: React.ReactNode;
 }) {
   return (
-    <div className="tw-mt-6 tw-flow-root">
+    <div className="tw-flow-root">
       <div className="-tw-mx-4 -tw-my-2 tw-overflow-x-auto sm:-tw-mx-6 lg:-tw-mx-8">
         <div className="tw-inline-block tw-min-w-full tw-py-2 tw-align-middle sm:tw-px-6 lg:tw-px-8">
           <div className="tw-overflow-hidden tw-shadow tw-ring-1 tw-ring-white/10 tw-rounded-lg">
