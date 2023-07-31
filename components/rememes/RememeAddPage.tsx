13c098f6086f623dfc8e852e9ab39a920fe0c422
prxt <prxt@6529.io>
2023-07-31 15:18:40 +0300

    WIP
    Signed-off-by: prxt <prxt@6529.io>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- components/rememes/RememeAddPage.tsx
+++ components/rememes/RememeAddPage.tsx
@@ -2,26 +2,68 @@ import styles from "./Rememes.module.scss";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import { MEMES_CONTRACT } from "../../constants";
+import { MEMES_CONTRACT, OPENSEA_STORE_FRONT_CONTRACT } from "../../constants";
 import { NFT } from "../../entities/INFT";
-import { fetchAllPages, postData } from "../../services/6529api";
+import { fetchAllPages, fetchUrl, postData } from "../../services/6529api";
 import RememeAddComponent from "./RememeAddComponent";
 import { useAccount, useSignMessage } from "wagmi";
+import { useWeb3Modal } from "@web3modal/react";
+import { DBResponse } from "../../entities/IDBResponse";
+import { ConsolidatedTDHMetrics } from "../../entities/ITDH";
+import { areEqualAddresses, numberWithCommas } from "../../helpers/Helpers";
 
-export interface AddRememe {
-  contract: string;
-  token_ids: string[];
-  references: number[];
-}
+export const TDH_THRESHOLD = 1;
+export const MOD_TDH_THRESHOLD = 100000;
 
 export default function RememeAddPage() {
   const accountResolution = useAccount();
+  const web3modal = useWeb3Modal();
 
   const signMessage = useSignMessage();
   const [memes, setMemes] = useState<NFT[]>([]);
   const [memesLoaded, setMemesLoaded] = useState(false);
+  const [userTDH, setUserTDH] = useState<ConsolidatedTDHMetrics>();
 
-  const [addRememe, setAddRememe] = useState<AddRememe>();
+  const [addRememe, setAddRememe] = useState<any>();
+  const [verificationErrors, setVerificationErrors] = useState<string[]>([]);
+  const [signErrors, setSignErrors] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (addRememe && userTDH) {
+      const errors: string[] = [];
+      if (TDH_THRESHOLD > userTDH.boosted_tdh) {
+        errors.push(
+          `Error: You need at least ${numberWithCommas(
+            TDH_THRESHOLD
+          )} TDH before you can add Rememes`
+        );
+      }
+      if (
+        !areEqualAddresses(
+          addRememe.contract.address,
+          OPENSEA_STORE_FRONT_CONTRACT
+        ) &&
+        !areEqualAddresses(
+          addRememe.contract.contractDeployer,
+          accountResolution.address
+        ) &&
+        MOD_TDH_THRESHOLD > userTDH.boosted_tdh
+      ) {
+        errors.push(
+          `Error: You need at least ${numberWithCommas(
+            MOD_TDH_THRESHOLD
+          )} TDH before you can add Rememes for which you are not deployer`
+        );
+      }
+      setVerificationErrors(errors);
+    }
+  }, [addRememe, userTDH]);
+
+  useEffect(() => {
+    if (signMessage.isError) {
+      setSignErrors([`Error: ${signMessage.error?.message.split(".")[0]}`]);
+    }
+  }, [signMessage.isError]);
 
   useEffect(() => {
     fetchAllPages(
@@ -31,6 +73,21 @@ export default function RememeAddPage() {
       setMemesLoaded(true);
     });
   }, []);
+
+  useEffect(() => {
+    async function fetchTdh() {
+      const url = `${process.env.API_ENDPOINT}/api/consolidated_owner_metrics/?wallet=${accountResolution.address}`;
+      return fetchUrl(url).then((response: DBResponse) => {
+        if (response && response.data.length == 1) {
+          setUserTDH(response.data[0]);
+        }
+      });
+    }
+
+    if (accountResolution.isConnected) {
+      fetchTdh();
+    }
+  }, [accountResolution.isConnected]);
 
   useEffect(() => {
     if (signMessage.isSuccess && signMessage.data) {
@@ -70,6 +127,8 @@ export default function RememeAddPage() {
                         memes={memes}
                         verifiedRememe={(r) => {
                           setAddRememe(r);
+                          setVerificationErrors([]);
+                          setSignErrors([]);
                           signMessage.reset();
                         }}
                       />
@@ -81,23 +140,47 @@ export default function RememeAddPage() {
             <Row className="pt-2">
               <Col className="d-flex justify-content-between align-items-center">
                 <span>
-                  {signMessage.isError &&
-                    !signMessage.isLoading &&
-                    `Error: ${signMessage.error?.message.split(".")[0]}`}
+                  {verificationErrors.length > 0 && (
+                    <ul>
+                      {verificationErrors.map((ve, index) => (
+                        <li key={`ve-${index}`}>{ve}</li>
+                      ))}
+                    </ul>
+                  )}
+                  {signErrors.length > 0 && (
+                    <ul>
+                      {signErrors.map((se, index) => (
+                        <li key={`se-${index}`}>{se}</li>
+                      ))}
+                    </ul>
+                  )}
                 </span>
-                <Button
-                  className="seize-btn"
-                  disabled={!addRememe}
-                  onClick={() => {
-                    if (addRememe) {
-                      signMessage.signMessage({
-                        message: JSON.stringify(addRememe),
-                        // message: `Adding ReMeme...\n\nContract: ${addRememe.contract}\n\nToken IDs: ${addRememe.tokenIds}\n\nMeme References: ${addRememe.references}`,
-                      });
+                {accountResolution.isConnected ? (
+                  <Button
+                    className="seize-btn"
+                    disabled={
+                      !addRememe ||
+                      !addRememe.valid ||
+                      !userTDH ||
+                      verificationErrors.length > 0
                     }
-                  }}>
-                  Submit
-                </Button>
+                    onClick={() => {
+                      if (addRememe) {
+                        signMessage.signMessage({
+                          message: JSON.stringify(addRememe),
+                        });
+                      }
+                    }}>
+                    Add Rememe
+                  </Button>
+                ) : (
+                  <Button
+                    className="seize-btn btn-white"
+                    disabled={web3modal.isOpen}
+                    onClick={() => web3modal.open()}>
+                    {web3modal.isOpen ? `Connecting...` : `Connect Wallet`}
+                  </Button>
+                )}
               </Col>
             </Row>
           </Container>
