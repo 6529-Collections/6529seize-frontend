15a0148f4a66c49818d4c69d62e02489eba5e54c
Ragne <ragneinfo@gmail.com>
2023-07-31 21:50:12 +0300

    wip

--- components/allowlist-tool/icons/AllowlistToolCsvIcon.tsx
+++ components/allowlist-tool/icons/AllowlistToolCsvIcon.tsx
@@ -1,7 +1,7 @@
 export default function AllowlistToolCsvIcon() {
   return (
     <svg
-      className="tw-h-auto tw-w-auto tw-text-neutral-300 group-hover:tw-text-white tw-transition tw-duration-300 tw-ease-out"
+      className="tw-h-auto tw-w-auto tw-text-[#76bc99] tw-transition tw-duration-300 tw-ease-out"
       viewBox="0 0 71 80"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
