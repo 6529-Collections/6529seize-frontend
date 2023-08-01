6d77105a379431f1d8083f0d78edd5b37572f195
Ragne <ragneinfo@gmail.com>
2023-08-01 22:04:11 +0300

    wip

--- components/distribution-plan-tool/create-snapshots/form/CreateSnapshotFormSearchCollectionMemesModal.tsx
+++ components/distribution-plan-tool/create-snapshots/form/CreateSnapshotFormSearchCollectionMemesModal.tsx
@@ -78,46 +78,50 @@ export default function CreateSnapshotFormSearchCollectionMemesModal({
   };
 
   return (
-    <div>
-      <fieldset className="tw-mx-8">
-        <legend className="tw-text-base tw-font-semibold tw-leading-6 tw-text-white">
-          Seasons
-        </legend>
-        <div className="tw-mt-4 tw-divide-y tw-divide-white tw-border-b tw-border-t tw-border-white">
-          {options.map((option) => (
-            <div
-              key={option.value}
-              className="tw-relative tw-flex tw-items-start tw-py-4"
-            >
-              <div className="tw-min-w-0 tw-flex-1 tw-text-sm tw-leading-6">
-                <label
-                  htmlFor={`option-${option.value}`}
-                  className="tw-select-none tw-font-medium tw-text-white"
-                >
-                  {option.value} ({option.tokenIds})
-                </label>
+    <div className="tw-px-8">
+      <div className="tw-mt-6">
+        <fieldset>
+          <legend className="tw-text-base tw-font-semibold tw-leading-6 tw-text-white">
+            Seasons
+          </legend>
+          <div className="tw-mt-4 tw-divide-y tw-divide-solid tw-divide-neutral-800 tw-divide-x-0 tw-border-solid tw-border-x-0 tw-border-b tw-border-t tw-border-neutral-800">
+            {options.map((option) => (
+              <div
+                key={option.value}
+                className="tw-relative tw-flex tw-items-start tw-py-4"
+              >
+                <div className="tw-min-w-0 tw-flex-1 tw-text-sm tw-leading-6">
+                  <label
+                    htmlFor={`option-${option.value}`}
+                    className="tw-select-none tw-font-medium tw-text-white"
+                  >
+                    {option.value} ({option.tokenIds})
+                  </label>
+                </div>
+                <div className="tw-ml-3 tw-flex tw-h-6 tw-w-auto tw-items-center">
+                  <input
+                    id={`option-${option.value}`}
+                    name={`person-${option.value}`}
+                    type="checkbox"
+                    checked={selected.includes(option.value)}
+                    onChange={() => handleSelect(option.value)}
+                    className="tw-form-checkbox tw-h-4 tw-w-4 tw-bg-neutral-800 tw-rounded tw-border-solid tw-border-gray-600 tw-text-primary-500 focus:tw-ring-primary-500"
+                  />
+                </div>
               </div>
-              <div className="tw-ml-3 tw-flex tw-h-6 tw-items-center">
-                <input
-                  id={`option-${option.value}`}
-                  name={`person-${option.value}`}
-                  type="checkbox"
-                  checked={selected.includes(option.value)}
-                  onChange={() => handleSelect(option.value)}
-                  className="tw-form-checkbox tw-h-4 tw-w-4 tw-rounded tw-border-gray-300 tw-text-indigo-600 tw-focus:tw-ring-indigo-600"
-                />
-              </div>
-            </div>
-          ))}
-        </div>
-      </fieldset>
-      <button
-        type="button"
-        onClick={onDone}
-        className="tw-relative tw-bg-primary-500 tw-px-4 tw-py-3 tw-text-sm tw-font-medium tw-text-white tw-border tw-border-solid tw-border-primary-500 tw-rounded-lg hover:tw-bg-primary-600 hover:tw-border-primary-600 tw-transition tw-duration-300 tw-ease-out"
-      >
-        Select
-      </button>
+            ))}
+          </div>
+        </fieldset>
+      </div>
+      <div className="tw-mt-8 tw-flex tw-justify-end">
+        <button
+          type="button"
+          onClick={onDone}
+          className="tw-relative tw-bg-primary-500 tw-px-4 tw-py-3 tw-text-sm tw-font-medium tw-text-white tw-border tw-border-solid tw-border-primary-500 tw-rounded-lg hover:tw-bg-primary-600 hover:tw-border-primary-600 tw-transition tw-duration-300 tw-ease-out"
+        >
+          Select
+        </button>
+      </div>
     </div>
   );
 }
