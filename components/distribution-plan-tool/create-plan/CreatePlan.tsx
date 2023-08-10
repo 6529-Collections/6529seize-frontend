4582cbc17ca55c8b29478ca82885ca3dbad443b8
Simo <info@analyticsflow.ee>
2023-08-10 17:07:36 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/distribution-plan-tool/create-plan/CreatePlan.tsx
+++ components/distribution-plan-tool/create-plan/CreatePlan.tsx
@@ -12,25 +12,25 @@ import {
 } from "../../allowlist-tool/allowlist-tool.types";
 
 export default function CreatePlan() {
-  // const { setState } = useContext(DistributionPlanToolContext);
-  // useEffect(() => {
-  //   const fetchAllowlist = async () => {
-  //     const url = `${process.env.ALLOWLIST_API_ENDPOINT}/allowlists/3e97d0f2-c39f-436c-9478-d18867b8f9e8`;
-  //     const response = await fetch(url, {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //     });
-  //     const data: AllowlistToolResponse<AllowlistDescription> =
-  //       await response.json();
-  //     if ("error" in data) {
-  //       return;
-  //     }
+  const { setState } = useContext(DistributionPlanToolContext);
+  useEffect(() => {
+    const fetchAllowlist = async () => {
+      const url = `${process.env.ALLOWLIST_API_ENDPOINT}/allowlists/6692c846-b458-42c1-8c31-3b4208ee1138`;
+      const response = await fetch(url, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data: AllowlistToolResponse<AllowlistDescription> =
+        await response.json();
+      if ("error" in data) {
+        return;
+      }
 
-  //     setState(data);
-  //   };
-  //   fetchAllowlist();
-  // }, []);
+      setState(data);
+    };
+    fetchAllowlist();
+  }, []);
 
   return (
     <div>
