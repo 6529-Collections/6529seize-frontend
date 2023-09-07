54492f65688b7b8cb60aa2200abbdfe9f66de346
Ragne <ragneinfo@gmail.com>
2023-09-07 08:35:15 +0300

    wip
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/distribution-plan-tool/create-custom-snapshots/form/CreateCustomSnapshotFormAddWalletsModal.tsx
+++ components/distribution-plan-tool/create-custom-snapshots/form/CreateCustomSnapshotFormAddWalletsModal.tsx
@@ -30,42 +30,39 @@ export default function CreateCustomSnapshotFormAddWalletsModal({
             Add wallets
           </p>
           <div className="tw-mt-6 tw-flex tw-gap-x-4">
-            <div className="tw-flex tw-gap-x-4">
-              <div className="tw-relative">
-                <div className="tw-flex tw-justify-between tw-items-center">
-                  <label className="tw-block tw-text-sm tw-font-normal tw-leading-5 tw-text-neutral-100">
-                    Wallet no.
-                  </label>
-                </div>
-                <div className="tw-mt-1.5">
-                  <input
-                    type="text"
-                    name="owner"
-                    autoComplete="off"
-                    onChange={(e) => setManualWallet(e.target.value ?? "")}
-                    onKeyDown={(e) => {
-                      if (e.key === "Enter") {
-                        e.preventDefault();
-                      }
-                    }}
-                    className="tw-form-input tw-block tw-w-full tw-rounded-lg tw-border-0 tw-py-3 tw-pl-3 tw-pr-20 tw-bg-neutral-700/40 tw-text-white tw-font-light tw-caret-primary-400 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-neutral-700/40
+            <div className="tw-flex tw-justify-between tw-w-full tw-gap-x-4">
+              <div className="tw-flex tw-gap-x-4">
+                <div className="tw-w-80 tw-relative">
+                  <div className="tw-flex tw-justify-between tw-items-center">
+                    <label className="tw-block tw-text-sm tw-font-normal tw-leading-5 tw-text-neutral-100">
+                      Wallet no.
+                    </label>
+                  </div>
+                  <div className="tw-mt-1.5">
+                    <input
+                      type="text"
+                      name="owner"
+                      autoComplete="off"
+                      onChange={(e) => setManualWallet(e.target.value ?? "")}
+                      onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                          e.preventDefault();
+                        }
+                      }}
+                      className="tw-form-input tw-block tw-w-full tw-rounded-lg tw-border-0 tw-py-3 tw-pl-3 tw-pr-3 tw-bg-neutral-700/40 tw-text-white tw-font-light tw-caret-primary-400 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-neutral-700/40
               hover:tw-ring-neutral-700 placeholder:tw-text-neutral-500 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset focus:tw-ring-primary-400 tw-text-base sm:tw-leading-6 tw-transition tw-duration-300 tw-ease-out"
-                  />
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="tw-self-end">
-                <button
-                  onClick={addManualWallet}
-                  type="button"
-                  className="tw-cursor-pointer tw-bg-transparent hover:tw-bg-neutral-800/80 tw-px-4 tw-py-3 tw-text-sm tw-font-medium tw-text-white tw-border-2 tw-border-solid tw-border-neutral-700 tw-rounded-lg tw-transition tw-duration-300 tw-ease-out"
-                >
-                  Add
-                </button>
-              </div>
-              <div className="tw-mt-9">
-                <span className="tw-text-sm tw-text-neutral-400 tw-font-normal">
-                  or
-                </span>
+                <div className="tw-self-end">
+                  <button
+                    onClick={addManualWallet}
+                    type="button"
+                    className="tw-cursor-pointer tw-bg-transparent hover:tw-bg-neutral-800/80 tw-px-4 tw-py-3 tw-text-sm tw-font-medium tw-text-white tw-border-2 tw-border-solid tw-border-neutral-700 tw-rounded-lg tw-transition tw-duration-300 tw-ease-out"
+                  >
+                    Add
+                  </button>
+                </div>
               </div>
               <div className="tw-mt-10">
                 <CreateCustomSnapshotFormUpload
