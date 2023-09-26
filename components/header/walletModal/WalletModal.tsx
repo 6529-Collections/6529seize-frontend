99a82863b7d73e58eb8d98dbc227f03f51845a4e
prxt <prxt@6529.io>
2023-09-26 15:41:47 +0300

    WIP
    Signed-off-by: prxt <prxt@6529.io>
    
    Signed-off-by: Analytics Flow info@analyticsflow.ee

--- components/header/walletModal/WalletModal.tsx
+++ components/header/walletModal/WalletModal.tsx
@@ -1,4 +1,4 @@
-import { Modal } from "react-bootstrap";
+import { Col, Container, Modal, Row } from "react-bootstrap";
 import styles from "./WalletModal.module.scss";
 import { disconnect } from "@wagmi/core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -17,15 +17,26 @@ interface Props {
 
 export default function WalletModal(props: Props) {
   const [ens, setEns] = useState<ENS>();
+  const [copied, setIsCopied] = useState<boolean>(false);
 
   useEffect(() => {
     const url = `${process.env.API_ENDPOINT}/api/user/${props.wallet}`;
     fetchUrl(url).then((response: ENS) => {
       if (!isEmptyObject(response)) {
         setEns(response);
+      } else {
+        setEns(undefined);
       }
     });
   }, [props.wallet]);
+
+  function copy() {
+    navigator.clipboard.writeText(props.wallet);
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  }
 
   return (
     <Modal show={props.show} onHide={() => props.onHide()}>
@@ -77,78 +88,80 @@ export default function WalletModal(props: Props) {
           )}
         </span>
       </Modal.Body>
-      {ens ? (
-        <Modal.Footer
-          className={`${styles.footer} d-flex justify-content-between`}>
-          <span
-            className={`${styles.footerBtnContainer} d-flex flex-column align-items-center gap-2`}>
-            <span
-              className={styles.footerBtn}
-              onClick={() =>
-                (window.location.href = ens?.display
-                  ? `/${ens.display}`
-                  : `/${props.wallet as string}`)
-              }>
-              <FontAwesomeIcon className={styles.footerIcon} icon="user" />
-            </span>
-            <span className="font-smaller">
-              <b>View Profile</b>
-            </span>
-          </span>
-          <span
-            className={`${styles.footerBtnContainer} d-flex flex-column align-items-center gap-2`}>
-            <span
-              className={styles.footerBtn}
-              onClick={() =>
-                (window.location.href = ens?.display
-                  ? `/${ens.display}/settings`
-                  : `/${props.wallet as string}/settings`)
-              }>
-              <FontAwesomeIcon className={styles.footerIcon} icon="gear" />
-            </span>
-            <span className="font-smaller">
-              <b>Profile Settings</b>
-            </span>
-          </span>
-          <span
-            className={`${styles.footerBtnContainer} d-flex flex-column align-items-center gap-2`}>
-            <span
-              className={styles.footerBtn}
+      <Modal.Footer className={styles.footer}>
+        <Container>
+          <Row className="d-flex justify-content-between">
+            <Col
+              xs={3}
+              className={`${styles.footerBtnContainer} d-flex flex-column align-items-center gap-2`}
+              onClick={() => copy()}>
+              <span className={styles.footerBtn}>
+                <FontAwesomeIcon className={styles.footerIcon} icon="copy" />
+              </span>
+              <span className="font-smaller no-wrap">
+                <b>{copied ? `Copied` : `Copy Address`}</b>
+              </span>
+            </Col>
+            {ens && (
+              <>
+                <Col
+                  xs={3}
+                  className={`${styles.footerBtnContainer} d-flex flex-column align-items-center gap-2`}
+                  onClick={() =>
+                    (window.location.href = ens?.display
+                      ? `/${ens.display}`
+                      : `/${props.wallet as string}`)
+                  }>
+                  <span className={styles.footerBtn}>
+                    <FontAwesomeIcon
+                      className={styles.footerIcon}
+                      icon="user"
+                    />
+                  </span>
+                  <span className="font-smaller no-wrap">
+                    <b>View Profile</b>
+                  </span>
+                </Col>
+                <Col
+                  xs={3}
+                  className={`${styles.footerBtnContainer} d-flex flex-column align-items-center gap-2`}
+                  onClick={() =>
+                    (window.location.href = ens?.display
+                      ? `/${ens.display}/settings`
+                      : `/${props.wallet as string}/settings`)
+                  }>
+                  <span className={styles.footerBtn}>
+                    <FontAwesomeIcon
+                      className={styles.footerIcon}
+                      icon="gear"
+                    />
+                  </span>
+                  <span className="font-smaller no-wrap">
+                    <b>Profile Settings</b>
+                  </span>
+                </Col>
+              </>
+            )}
+            <Col
+              xs={3}
+              className={`${styles.footerBtnContainer} d-flex flex-column align-items-center gap-2`}
               onClick={() => {
                 disconnect();
                 props.onHide();
               }}>
-              <FontAwesomeIcon
-                className={styles.footerIcon}
-                icon="arrow-right-from-bracket"
-              />
-            </span>
-            <span className="font-smaller">
-              <b>Disconnect</b>
-            </span>
-          </span>
-        </Modal.Footer>
-      ) : (
-        <Modal.Footer className={`${styles.footer} d-flex justify-content-end`}>
-          <span
-            className={`${styles.footerBtnContainer} d-flex flex-column align-items-center gap-2`}>
-            <span
-              className={styles.footerBtn}
-              onClick={() => {
-                disconnect();
-                props.onHide();
-              }}>
-              <FontAwesomeIcon
-                className={styles.footerIcon}
-                icon="arrow-right-from-bracket"
-              />
-            </span>
-            <span className="font-smaller">
-              <b>Disconnect</b>
-            </span>
-          </span>
-        </Modal.Footer>
-      )}
+              <span className={styles.footerBtn}>
+                <FontAwesomeIcon
+                  className={styles.footerIcon}
+                  icon="arrow-right-from-bracket"
+                />
+              </span>
+              <span className="font-smaller no-wrap">
+                <b>Disconnect</b>
+              </span>
+            </Col>
+          </Row>
+        </Container>
+      </Modal.Footer>
     </Modal>
   );
 }
