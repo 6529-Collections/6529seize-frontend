e8ccf812ec66c34164b48e1ae569672002a72645
Simo <info@analyticsflow.ee>
2023-09-29 10:22:48 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- components/distribution-plan-tool/build-phases/build-phase/form/component-config/SnapshotSelectTokenIds.tsx
+++ components/distribution-plan-tool/build-phases/build-phase/form/component-config/SnapshotSelectTokenIds.tsx
@@ -65,7 +65,7 @@ export default function SnapshotSelectTokenIds({
               value={formValues.tokenIds}
               onChange={handleChange}
               autoComplete="off"
-              placeholder="Token ID(s)"
+              placeholder="Example: 1,3,54-78"
               className="tw-form-input tw-block tw-w-full tw-rounded-lg tw-border-0 tw-py-3 tw-px-3 tw-bg-neutral-700/40 tw-text-white tw-font-light tw-caret-primary-400 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-neutral-700/40 placeholder:tw-text-neutral-500 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset focus:tw-ring-primary-400 hover:tw-ring-neutral-700 tw-text-base sm:tw-leading-6 tw-transition tw-duration-300 tw-ease-out"
             />
           </div>
