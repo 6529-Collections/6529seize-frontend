fb6687b2be048d7517879fb4394d7ebe7e3e62af
Ragne <ragneinfo@gmail.com>
2023-10-01 16:47:23 +0300

    wip
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- components/distribution-plan-tool/connect/distributipn-plan-tool-connect.tsx
+++ components/distribution-plan-tool/connect/distributipn-plan-tool-connect.tsx
@@ -28,8 +28,8 @@ export default function DistributionPlanToolConnect() {
   }, [address]);
 
   return (
-    <div className="tw-pt-8 tw-pb-12 tw-max-w-[65.625rem] min-[1101px]:tw-max-w-[960px] min-[1200px]:tw-max-w-[1150px] tw-min-[1300px]:max-w-[1250px] tw-min-[1400px]:max-w-[1350px] tw-mx-auto tw-px-14">
-      <div className="tw-hidden">
+    <div className="xl:tw-max-w-2xl xl:tw-mx-auto lg:tw-px-8 xl:tw-px-0">
+      <div>
         {(() => {
           switch (authStatus) {
             case DistributionPlanAuth.NOT_CONNECTED:
@@ -41,33 +41,6 @@ export default function DistributionPlanToolConnect() {
               return null;
           }
         })()}
-      </div>
-      <div className="tw-mt-6 tw-max-w-lg">
-        <h1 className="tw-uppercase tw-text-white">
-          Sign in With Your Seize Account
-        </h1>
-        <div className="tw-mt-8">
-          <p className="tw-text-justify tw-mb-0 tw-mt-2 tw-text-base tw-font-light tw-text-neutral-300">
-            You can sign in to use tool with your Seize (eth) account using
-            Metamask or any other wallet.
-          </p>
-          <p className="tw-text-justify tw-mb-0 tw-mt-2 tw-text-base tw-font-light tw-text-neutral-300">
-            There is no cost or gas to sign in.
-          </p>
-          <p className="tw-text-justify tw-mb-0 tw-mt-2 tw-text-base tw-font-light tw-text-neutral-300">
-            You can use your any address in your consolidated account - we
-            recommeng connecting to Seize with the hot address in your
-            consolidated account.
-          </p>
-        </div>
-        <div className="tw-mt-8">
-          <button
-            type="submit"
-            className="tw-group tw-flex tw-gap-x-3 tw-items-center tw-justify-center tw-bg-primary-500 tw-px-6 tw-py-3.5 tw-font-medium tw-text-sm tw-text-white tw-border tw-border-primary-500 tw-rounded-lg hover:tw-bg-primary-600 hover:tw-border-primary-600 tw-transition tw-duration-300 tw-ease-out"
-          >
-            Sign in
-          </button>
-        </div>
       </div>
     </div>
   );
