4a9a262b51cc8216f3bd72bb5ae45c3cf5d852e9
Ragne <ragneinfo@gmail.com>
2023-10-01 16:47:23 +0300

    wip
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow info@analyticsflow.ee

--- components/distribution-plan-tool/connect/distribution-plan-tool-not-connected.tsx
+++ components/distribution-plan-tool/connect/distribution-plan-tool-not-connected.tsx
@@ -1,8 +1,22 @@
 export default function DistributionPlanToolNotConnected() {
   return (
-    <div className="tw-text-center tw-flex tw-flex-col tw-items-center">
+    <div className="tw-flex tw-flex-col">
       <h1 className="tw-uppercase tw-text-white">Not connected</h1>
-      <p className="tw-m-0 tw-text-base tw-font-light tw-text-neutral-300">
+      <div className="tw-mb-6 lg:tw-max-w-2xl">
+        <p className="tw-text-justify tw-mb-0 tw-mt-2 tw-text-base tw-leading-[1.6] tw-font-normal tw-text-neutral-300">
+          You can sign in to use tool with your Seize (eth) account using
+          Metamask or any other wallet.
+        </p>
+        <p className="tw-text-justify tw-mb-0 tw-mt-2 tw-text-base tw-leading-[1.6] tw-font-normal tw-text-neutral-300">
+          There is no cost or gas to sign in.
+        </p>
+        <p className="tw-text-justify tw-mb-0 tw-mt-2 tw-text-base tw-leading-[1.6] tw-font-normal tw-text-neutral-300">
+          You can use your any address in your consolidated account - we
+          recommeng connecting to Seize with the hot address in your
+          consolidated account.
+        </p>
+      </div>
+      <p className="tw-mb-0 tw-text-sm tw-uppercase tw-tracking-wide tw-font-bold tw-text-white">
         To continue with the distribution plan tool, please connect your wallet.
       </p>
     </div>
--- pages/distribution-plan-tool/index.tsx
+++ pages/distribution-plan-tool/index.tsx
@@ -4,35 +4,36 @@ import DistributionPlanToolConnect from "../../components/distribution-plan-tool
 export default function DistributionPlanTool() {
   return (
     <DistributionPlanToolWrapper>
-      <div className="tw-flex tw-h-full tw-min-h-screen tw-bg-neutral-900">
-        <div className="tw-pt-16 tw-space-y-8 tw-pb-12 tw-max-w-[65.625rem] min-[1101px]:tw-max-w-[960px] min-[1200px]:tw-max-w-[1150px] tw-min-[1300px]:max-w-[1250px] tw-min-[1400px]:max-w-[1350px] tw-mx-auto tw-px-14">
-          <div className="tw-max-w-lg">
-            <div className="tw-flex tw-flex-col">
-              <p className="tw-text-white tw-font-semibold tw-mb-0">Allowlist Builder Tool</p>
-              <p className="tw-text-justify tw-mb-0 tw-mt-4 tw-block tw-font-light tw-text-base tw-text-neutral-400">
-                The Memes and the Allowlist Research Institute have built
-                probably the worlds deepest expertise in allowlist management
-                across 150 NFT drops and counting.
-              </p>
-              <p className="tw-text-justify tw-mb-0 tw-mt-4 tw-block tw-font-light tw-text-base tw-text-neutral-400">
-                This tool was built for The Memes team to manage the memes drops
-                and we are making it available for use for any member of the
-                memes community.
-              </p>
-              <p className="tw-text-justify tw-mb-0 tw-mt-4 tw-block tw-font-light tw-text-base tw-text-neutral-400">
-                As the tool can use computational resources, we have some mild
-                anti-spam measures in place to protect system performance. We
-                will reevaulate them over time.
-              </p>
-              <p className="tw-text-justify tw-mb-0 tw-mt-4 tw-block tw-font-light tw-text-base tw-text-neutral-400">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
-                ipsum dolor sit.
-              </p>
+      <div className="tw-flex tw-h-full tw-min-h-screen tw-overflow-x-hidden tw-bg-neutral-900">
+        <div className="tw-flex tw-flex-wrap tw-h-full tw-w-full lg:tw-min-h-screen tw-px-8 xl:tw-px-14 tw-space-y-8 lg:tw-space-y-0">
+          <div className="lg:tw-w-1/2 xl:tw-w-[40%] 2xl:tw-w-1/2 tw-pt-8 md:tw-pt-14">
+            <div className="2xl:tw-max-w-xl tw-mx-auto xl:tw-pr-16 2xl:tw-pr-0">
+              <div className="tw-flex tw-flex-col">
+                <p className="tw-text-white tw-font-semibold tw-mb-0 tw-text-base md:tw-text-lg">
+                  Allowlist Builder Tool
+                </p>
+                <p className="tw-text-justify tw-mb-0 tw-mt-2 md:tw-mt-4 tw-block tw-font-light tw-text-base tw-leading-[1.6] tw-text-neutral-400">
+                  The Memes and the Allowlist Research Institute have built
+                  probably the worlds deepest expertise in allowlist management
+                  across 150 NFT drops and counting.
+                </p>
+                <p className="tw-text-justify tw-mb-0 tw-mt-2 md:tw-mt-4 tw-block tw-font-light tw-text-base tw-leading-[1.6] tw-text-neutral-400">
+                  This tool was built for The Memes team to manage the memes
+                  drops and we are making it available for use for any member of
+                  the memes community.
+                </p>
+                <p className="tw-text-justify tw-mb-0 tw-mt-2 md:tw-mt-4 tw-block tw-font-light tw-text-base tw-leading-[1.6] tw-text-neutral-400">
+                  As the tool can use computational resources, we have some mild
+                  anti-spam measures in place to protect system performance. We
+                  will reevaulate them over time.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="tw-w-1/2 tw-border-l tw-border-solid tw-border-r-0 tw-border-t-0 tw-border-b-0 tw-border-neutral-600">
-          <DistributionPlanToolConnect />
+
+          <div className="lg:tw-w-1/2 xl:tw-w-[60%] 2xl:tw-w-1/2 tw-pt-8 lg:tw-pt-12 tw-pb-12 md:tw-pb-0 tw-border-l-0 lg:tw-border-l tw-border-solid tw-border-r-0 tw-border-t-1 lg:tw-border-t-0 tw-border-b-0 tw-border-neutral-700">
+            <DistributionPlanToolConnect />
+          </div>
         </div>
       </div>
     </DistributionPlanToolWrapper>
