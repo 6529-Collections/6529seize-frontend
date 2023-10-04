c2653a8c9fb85bd53514d5843a2a118e9cc0e132
Ragne <ragneinfo@gmail.com>
2023-10-04 09:23:48 +0300

    wip
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/common/input/CommonInput.tsx
+++ components/common/input/CommonInput.tsx
@@ -26,7 +26,9 @@ export default function CommonInput({
       value={value}
       autoComplete={autoComplete}
       placeholder={placeholder}
-      className="tw-form-input tw-block tw-w-full tw-rounded-lg tw-border-0 tw-py-3 tw-px-3 tw-bg-neutral-700/40 tw-text-white tw-font-light tw-caret-primary-400 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-neutral-700/40 placeholder:tw-text-neutral-500 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset focus:tw-ring-primary-400 hover:tw-ring-neutral-700 tw-text-base sm:tw-leading-6 tw-transition tw-duration-300 tw-ease-out"
+      className={` ${
+        disabled ? "tw-text-white/50 tw-bg-neutral-700/80" : "tw-text-white tw-bg-neutral-700/40"
+      } tw-form-input tw-block tw-w-full tw-rounded-lg tw-border-0 tw-py-3 tw-px-3 tw-font-light tw-caret-primary-400 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-neutral-700/40 placeholder:tw-text-neutral-500 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset focus:tw-ring-primary-400 hover:tw-ring-neutral-700 tw-text-base sm:tw-leading-6 tw-transition tw-duration-300 tw-ease-out`}
     />
   );
 }
