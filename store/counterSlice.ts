e0b7a772a658e4ef42cf0a5fffaad7066995a582
Simo <info@analyticsflow.ee>
2023-10-10 15:40:26 +0300

    redux init
    Signed-off-by: Simo <info@analyticsflow.ee>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- store/counterSlice.ts
+++ store/counterSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { HYDRATE } from "next-redux-wrapper";
 import type { AppState } from "./store";
 
@@ -14,10 +14,10 @@ export const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
-    incrementCounter(state, action) {
+    incrementCounter(state, action: PayloadAction<number>) {
       state.value += action.payload;
     },
-    decrementCounter(state, action) {
+    decrementCounter(state, action: PayloadAction<number>) {
       state.value -= action.payload;
     },
   },
@@ -36,3 +36,27 @@ export const counterSlice = createSlice({
 export const { incrementCounter, decrementCounter } = counterSlice.actions;
 export const selectCounter = (state: AppState) => state.counter.value;
 export default counterSlice.reducer;
+
+
+// import { useDispatch, useSelector } from "react-redux";
+// import {
+//   selectCounter,
+//   incrementCounter,
+//   decrementCounter,
+// } from "../../store/counterSlice";
+
+// usage;
+//   const counter = useSelector(selectCounter);
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     if (foo) {
+//       dispatch(incrementCounter(1));
+//     } else {
+//       dispatch(decrementCounter(1));
+//     }
+//   }, [foo]);
+
+//   useEffect(() => {
+//     console.log("counter", counter);
+//   }, [counter]);
