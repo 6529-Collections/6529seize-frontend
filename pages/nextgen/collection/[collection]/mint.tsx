1cdd8be19d51c5e05b711a6af5eeff115fbc544f
prxt <prxt@6529.io>
2023-10-11 15:08:25 +0300

    WIP
    Signed-off-by: prxt <prxt@6529.io>

--- pages/nextgen/collection/[collection]/mint.tsx
+++ pages/nextgen/collection/[collection]/mint.tsx
@@ -20,21 +20,17 @@ const NextGenMintComponent = dynamic(
   }
 );
 
-interface Props {
-  collection: number;
-  name: string;
-}
-
 export default function NextGenCollectionMint(props: any) {
-  const pagenameFull = `${props.name} | 6529 SEIZE`;
+  const pageProps = props.pageProps;
+  const pagenameFull = `${pageProps.name} | 6529 SEIZE`;
   const [connectedWallets, setConnectedWallets] = useState<string[]>([]);
 
   const [breadcrumbs, setBreadcrumbs] = useState<Crumb[]>([
     { display: "Home", href: "/" },
     { display: "NextGen", href: "/nextgen" },
     {
-      display: `Collection #${props.collection}`,
-      href: `/nextgen/collection/${props.collection}`,
+      display: `Collection #${pageProps.collection}`,
+      href: `/nextgen/collection/${pageProps.collection}`,
     },
     { display: `Mint` },
   ]);
@@ -47,22 +43,22 @@ export default function NextGenCollectionMint(props: any) {
         <meta name="description" content={pagenameFull} />
         <meta
           property="og:url"
-          content={`${process.env.BASE_ENDPOINT}/nextgen/collection/${props.id}`}
+          content={`${process.env.BASE_ENDPOINT}/nextgen/collection/${pageProps.id}`}
         />
-        <meta property="og:title" content={props.name} />
-        <meta property="og:image" content={props.image} />
+        <meta property="og:title" content={pageProps.name} />
+        <meta property="og:image" content={pageProps.image} />
         <meta property="og:description" content="6529 SEIZE" />
         <meta name="twitter:card" content={pagenameFull} />
-        <meta name="twitter:image:alt" content={props.name} />
-        <meta name="twitter:title" content={props.name} />
+        <meta name="twitter:image:alt" content={pageProps.name} />
+        <meta name="twitter:title" content={pageProps.name} />
         <meta name="twitter:description" content="6529 SEIZE" />
-        <meta name="twitter:image" content={props.image} />
+        <meta name="twitter:image" content={pageProps.image} />
       </Head>
 
       <main className={styles.main}>
         <Header onSetWallets={(wallets) => setConnectedWallets(wallets)} />
         <Breadcrumb breadcrumbs={breadcrumbs} />
-        <NextGenMintComponent collection={props.collection} />
+        <NextGenMintComponent collection={pageProps.collection} />
       </main>
     </>
   );
@@ -72,7 +68,7 @@ export async function getServerSideProps(req: any, res: any, resolvedUrl: any) {
   const collection = req.query.collection;
 
   let name = `Mint NextGen Collection #${collection}`;
-  const props: Props = {
+  const props = {
     collection,
     name,
   };
