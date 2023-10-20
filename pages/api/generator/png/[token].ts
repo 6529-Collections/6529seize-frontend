e684847b5defa579c1ce495aba7256a4e3157dc9
prxt <prxt@6529.io>
2023-10-20 10:22:53 +0300

    Console log
    Signed-off-by: prxt <prxt@6529.io>

--- pages/api/generator/png/[token].ts
+++ pages/api/generator/png/[token].ts
@@ -29,6 +29,7 @@ export default async function handler(
         .send({ error: "An error occurred while capturing the canvas." });
     }
   } catch (error) {
+    console.log("i am error", error);
     res
       .status(500)
       .send({ error: "An error occurred while processing the page." });
