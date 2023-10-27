f008526e775afa1f643557a67516cc88e9687cbf
Simo <info@analyticsflow.ee>
2023-10-27 09:52:29 +0300

    bugfix: hide username is available after changing username
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/user/settings/UserSettingsUsername.tsx
+++ components/user/settings/UserSettingsUsername.tsx
@@ -55,7 +55,7 @@ export default function UserSettingsUsername({
       }
     };
     getUsernameAvailability();
-  }, [debouncedUsername]);
+  }, [debouncedUsername, originalUsername]);
 
   return (
     <div>
