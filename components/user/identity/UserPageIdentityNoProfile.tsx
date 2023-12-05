84b9102163f34c1c62da3d8c6faa0702c6ed75bc
Ragne <ragneinfo@gmail.com>
2023-12-05 12:46:05 +0200

    alerts
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/user/identity/UserPageIdentityNoProfile.tsx
+++ components/user/identity/UserPageIdentityNoProfile.tsx
@@ -26,23 +26,65 @@ export default function UserPageIdentityNoProfile({
 
   if (isMyProfile) {
     return (
-      <div className="tw-mt-6 tw-text-base tw-text-neutral-300 tw-font-normal">
-        You have not set up your profile yet, to set up your profile click{" "}
-        <span
-          onClick={goToSettings}
-          className="tw-cursor-pointer tw-font-semibold tw-text-neutral-100 tw-underline hover:tw-text-primary-300 tw-transition tw-duration-300 tw-ease-out"
-        >
-          here.
-        </span>
+      <div className="tw-mt-6 tw-inline-flex tw-items-center tw-rounded-lg tw-bg-primary-400/5 tw-border tw-border-solid tw-border-primary-400/20 tw-px-4 tw-py-3">
+        <div className="tw-flex tw-items-center">
+          <svg
+            className="tw-flex-shrink- tw-self-center tw-w-5 tw-h-5 tw-text-primary-300"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 16V12M12 8H12.01M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <div className="tw-ml-3 tw-self-center">
+            <h3 className="tw-text-sm tw-mb-0 tw-font-semibold tw-text-primary-300">
+              You have not set up your profile yet, to set up your profile click{" "}
+              <span
+                onClick={goToSettings}
+                className="tw-cursor-pointer tw-tracking-wider tw-font-bold tw-underline tw-text-white tw-transition tw-duration-300 tw-ease-out"
+              >
+                here
+              </span>
+            </h3>
+          </div>
+        </div>
       </div>
     );
   }
 
   return (
-    <div className="tw-mt-6 tw-text-base tw-text-neutral-300 tw-font-normal">
-      Address{" "}
-      <span className="tw-font-semibold tw-text-neutral-100">{user}</span> have
-      not set up profile yet.
+    <div className="tw-mt-6 tw-inline-flex tw-items-center tw-rounded-lg tw-bg-white/5 tw-border tw-border-solid tw-border-white/10 tw-px-4 tw-py-3">
+      <div className="tw-flex tw-items-center">
+        <svg
+          className="tw-flex-shrink- tw-self-center tw-w-5 tw-h-5 tw-text-neutral-400"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M12 16V12M12 8H12.01M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+        <div className="tw-ml-3 tw-self-center">
+          <h3 className="tw-text-sm tw-mb-0 tw-font-semibold tw-text-neutral-400">
+            Address
+            <span className="tw-px-1.5 tw-font-semibold tw-text-neutral-100">
+              {user}
+            </span>
+            have not set up profile yet.
+          </h3>
+        </div>
+      </div>
     </div>
   );
 }
--- components/user/identity/header/UserPageIdentityHeaderCICRateWrapper.tsx
+++ components/user/identity/header/UserPageIdentityHeaderCICRateWrapper.tsx
@@ -26,11 +26,59 @@ export default function UserPageIdentityHeaderCICRateWrapper({
   useEffect(() => setIAmConnected(!!address), [address]);
 
   if (!iAmConnected) {
-    return <div>Please connect to rate</div>;
+    return (
+      <div className="tw-mt-6 tw-inline-flex tw-items-center tw-rounded-lg tw-bg-primary-400/5 tw-border tw-border-solid tw-border-primary-400/20 tw-px-4 tw-py-3">
+        <div className="tw-flex tw-items-center">
+          <svg
+            className="tw-flex-shrink- tw-self-center tw-w-5 tw-h-5 tw-text-primary-300"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 16V12M12 8H12.01M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <div className="tw-ml-3 tw-self-center">
+            <h3 className="tw-text-sm tw-mb-0 tw-font-semibold tw-text-primary-300">
+              Please connect to rate
+            </h3>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   if (!iHaveProfile) {
-    return <div>Please make profile for rate</div>;
+    return (
+      <div className="tw-mt-6 tw-inline-flex tw-items-center tw-rounded-lg tw-bg-primary-400/5 tw-border tw-border-solid tw-border-primary-400/20 tw-px-4 tw-py-3">
+        <div className="tw-flex tw-items-center">
+          <svg
+            className="tw-flex-shrink- tw-self-center tw-w-5 tw-h-5 tw-text-primary-300"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 16V12M12 8H12.01M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <div className="tw-ml-3 tw-self-center">
+            <h3 className="tw-text-sm tw-mb-0 tw-font-semibold tw-text-primary-300">
+              Please make profile to rate
+            </h3>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   if (isMyProfile) {
