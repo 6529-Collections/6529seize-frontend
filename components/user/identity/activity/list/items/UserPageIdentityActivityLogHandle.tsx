410380ac34c241284d7c65ea8a9d34fd96d943d2
Simo <info@analyticsflow.ee>
2023-12-13 13:57:24 +0200

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/user/identity/activity/list/items/UserPageIdentityActivityLogHandle.tsx
+++ components/user/identity/activity/list/items/UserPageIdentityActivityLogHandle.tsx
@@ -16,47 +16,37 @@ export default function UserPageIdentityActivityLogHandle({
     <li className="tw-py-4">
       <div className="tw-flex tw-items-center tw-justify-between tw-gap-x-3">
         <div className="tw-inline-flex tw-items-center tw-space-x-2">
-          {/* <svg
-            className="tw-flex-shrink-0 tw-h-5 tw-w-5 tw-text-iron-100"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M16 7.99999V13C16 13.7956 16.3161 14.5587 16.8787 15.1213C17.4413 15.6839 18.2043 16 19 16C19.7956 16 20.5587 15.6839 21.1213 15.1213C21.6839 14.5587 22 13.7956 22 13V12C21.9999 9.74302 21.2362 7.55247 19.8333 5.78452C18.4303 4.01658 16.4705 2.77521 14.2726 2.26229C12.0747 1.74936 9.76793 1.99503 7.72734 2.95936C5.68676 3.92368 4.03239 5.54995 3.03325 7.57371C2.03411 9.59748 1.74896 11.8997 2.22416 14.1061C2.69936 16.3125 3.90697 18.2932 5.65062 19.7263C7.39428 21.1593 9.57143 21.9603 11.8281 21.9991C14.0847 22.0379 16.2881 21.3122 18.08 19.94M16 12C16 14.2091 14.2091 16 12 16C9.79086 16 8 14.2091 8 12C8 9.79085 9.79086 7.99999 12 7.99999C14.2091 7.99999 16 9.79085 16 12Z"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg> */}
           <div className="tw-inline-flex tw-space-x-1.5">
             <span className="tw-truncate tw-max-w-[12rem] tw-text-sm tw-font-semibold tw-text-iron-100">
               {profile?.profile?.handle}
             </span>
             <span className="tw-text-sm tw-text-iron-400 tw-font-semibold">
-              changed
+              {isAdded ? "created" : "changed"}
             </span>
             <span className="tw-text-sm tw-text-iron-300 tw-font-medium">
               handle
             </span>
-            <span className="tw-whitespace-nowrap tw-text-sm tw-font-semibold tw-text-iron-100">
-              {log.contents.old_value}
-            </span>
-            <svg
-              className="tw-flex-shrink-0 tw-h-5 tw-w-5 tw-text-iron-400"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M4 12H20M20 12L14 6M20 12L14 18"
-                stroke="currentcOLOR"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+            {!isAdded && (
+              <>
+                <span className="tw-text-sm tw-font-semibold tw-text-neutral-100">
+                  {log.contents.old_value}
+                </span>
+                <svg
+                  className="tw-flex-shrink-0 tw-h-5 tw-w-5 tw-text-neutral-400"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M4 12H20M20 12L14 6M20 12L14 18"
+                    stroke="currentcOLOR"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </>
+            )}
 
             <span className="tw-whitespace-nowrap tw-text-sm tw-font-semibold tw-text-iron-100">
               {log.contents.new_value}
--- components/user/identity/activity/list/items/UserPageIdentityActivityLogPrimaryWallet.tsx
+++ components/user/identity/activity/list/items/UserPageIdentityActivityLogPrimaryWallet.tsx
@@ -48,44 +48,45 @@ export default function UserPageIdentityActivityLogPrimaryWallet({
     <li className="tw-py-4">
       <div className="tw-flex tw-items-center tw-justify-between tw-gap-x-3">
         <div className="tw-inline-flex tw-items-center tw-space-x-2">
-          {/* <div className="tw-flex tw-items-center tw-flex-shrink-0 tw-h-5 tw-w-5 tw-text-iron-100">
-            <EthereumIcon />
-          </div> */}
           <div className="tw-inline-flex tw-space-x-1.5">
             <span className="tw-truncate tw-max-w-[12rem] tw-text-sm tw-font-semibold tw-text-iron-100">
               {profile?.profile?.handle}
             </span>
             <span className="tw-text-sm tw-text-iron-400 tw-font-semibold">
-              changed
+              {isAdded ? "added" : "changed"}
             </span>
             <span className="tw-text-sm tw-text-iron-300 tw-font-medium">
               primary wallet
             </span>
-            <span className="tw-whitespace-nowrap tw-group tw-inline-flex tw-text-sm tw-font-semibold tw-text-iron-100">
-              {oldTitle}
-              <Tippy content="Copy" theme="dark" placement="top">
-                <button
-                  onClick={handleCopyOld}
-                  className="tw-hidden group-hover:tw-block tw-mx-1 tw-h-4 tw-w-4 tw-bg-transparent tw-cursor-pointer tw-text-sm tw-font-semibold tw-text-white tw-border-0 focus:tw-outline-none tw-transition tw-duration-300 tw-ease-out"
+            {!isAdded && (
+              <>
+                <span className="tw-whitespace-nowrap tw-group tw-inline-flex tw-text-sm tw-font-semibold tw-text-iron-100">
+                  {oldTitle}
+                  <Tippy content="Copy" theme="dark" placement="top">
+                    <button
+                      onClick={handleCopyOld}
+                      className="tw-hidden group-hover:tw-block tw-mx-1 tw-h-4 tw-w-4 tw-bg-transparent tw-cursor-pointer tw-text-sm tw-font-semibold tw-text-white tw-border-0 focus:tw-outline-none tw-transition tw-duration-300 tw-ease-out"
+                    >
+                      <CopyIcon />
+                    </button>
+                  </Tippy>
+                </span>
+                <svg
+                  className="tw-h-5 tw-w-5 tw-text-iron-400"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <CopyIcon />
-                </button>
-              </Tippy>
-            </span>
-            <svg
-              className="tw-h-5 tw-w-5 tw-text-iron-400"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M4 12H20M20 12L14 6M20 12L14 18"
-                stroke="currentcOLOR"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+                  <path
+                    d="M4 12H20M20 12L14 6M20 12L14 18"
+                    stroke="currentcOLOR"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </>
+            )}
             <span className="tw-group tw-inline-flex  tw-text-sm tw-font-semibold tw-text-iron-100">
               {newTitle}
               <Tippy content="Copy" theme="dark" placement="top">
