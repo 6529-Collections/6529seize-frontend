d727bcd00704d98edec94c3700e96190dd026855
Ragne <ragneinfo@gmail.com>
2023-12-19 18:27:28 +0200

    ui fixes
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/profile-activity/list/items/ProfileActivityLogRate.tsx
+++ components/profile-activity/list/items/ProfileActivityLogRate.tsx
@@ -81,19 +81,19 @@ export default function ProfileActivityLogRate({
           {log.target_profile_handle}
         </span>
       </button>
-      <span className={`${getTotalRatingClass()} tw-text-sm tw-font-semibold`}>
+      <span className={`${getTotalRatingClass()} tw-whitespace-nowrap tw-text-sm tw-font-semibold`}>
         (total {newRatingStr})
       </span>
       {isSystemAdjustment && (
-        <span className="tw-inline-flex tw-items-center tw-gap-x-1.5 tw-rounded-md tw-px-2 tw-py-1 tw-text-xs tw-font-medium tw-text-white tw-ring-1 tw-ring-inset tw-ring-gray-700">
+        <span className="tw-whitespace-nowrap tw-inline-flex tw-items-center tw-gap-x-1.5 tw-rounded-md tw-px-2 tw-py-1 tw-text-xs tw-font-medium tw-text-iron-300 tw-ring-1 tw-ring-inset tw-ring-iron-700">
           <svg
-            className="tw-h-1.5 tw-w-1.5 tw-fill-red"
+            className="tw-flex-shrink-0 tw-h-1.5 tw-w-1.5 tw-fill-red"
             viewBox="0 0 6 6"
             aria-hidden="true"
           >
             <circle cx="3" cy="3" r="3" />
           </svg>
-          System Adjustment
+         <span> System Adjustment</span>
         </span>
       )}
     </>
