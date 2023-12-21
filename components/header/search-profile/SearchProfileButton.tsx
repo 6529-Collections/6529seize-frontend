dff8ed50e1863a7a59e37e9886d3909c5444cfee
Simo <info@analyticsflow.ee>
2023-12-21 12:47:45 +0200

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/header/search-profile/SearchProfileButton.tsx
+++ components/header/search-profile/SearchProfileButton.tsx
@@ -2,13 +2,21 @@ import { useState } from "react";
 import CommonAnimationWrapper from "../../utils/animation/CommonAnimationWrapper";
 import CommonAnimationOpacity from "../../utils/animation/CommonAnimationOpacity";
 import SearchProfileModal from "./SearchProfileModal";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  weight: ["300", "400", "500", "600", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function SearchProfileButton() {
   const [isSearchProfileOpen, setIsSearchProfileOpen] =
     useState<boolean>(false);
 
   return (
-    <div className="tailwind-scope">
+    <div className={`tailwind-scope ${inter.className}`}>
       <button
         type="button"
         onClick={() => setIsSearchProfileOpen(true)}
