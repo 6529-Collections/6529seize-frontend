e20d74ef8e67f10050f0719ac6dde49f33ac6732
Simo <info@analyticsflow.ee>
2024-01-04 20:11:09 +0200

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- pages/index.tsx
+++ pages/index.tsx
@@ -387,17 +387,22 @@ export default function Home({
               </Row>
             </Container>
             <div
-              className={`tailwind-scope tw-relative tw-px-6 min-[1100px]:tw-max-w-[960px] min-[1200px]:tw-max-w-[1150px] min-[1300px]:tw-max-w-[1250px] min-[1400px]:tw-max-w-[1350px] min-[1500px]:tw-max-w-[1450px] min-[1600px]:tw-max-w-[1550px] min-[1800px]:tw-max-w-[1750px] min-[2000px]:tw-max-w-[1950px] tw-mx-auto ${inter.className}`}
+              className={`tailwind-scope tw-relative tw-px-2 min-[1100px]:tw-max-w-[960px] min-[1200px]:tw-max-w-[1150px] min-[1300px]:tw-max-w-[1250px] min-[1400px]:tw-max-w-[1350px] min-[1500px]:tw-max-w-[1450px] min-[1600px]:tw-max-w-[1550px] min-[1800px]:tw-max-w-[1750px] min-[2000px]:tw-max-w-[1950px] tw-mx-auto ${inter.className}`}
             >
               <div className="tw-mt-6">
-                <h1 className="tw-block tw-uppercase tw-text-iron-50 tw-float-none tw-pb-0 tw-mb-0">
-                  Community Activity
+                <h1 className="tw-block tw-uppercase tw-float-none tw-pb-0 tw-mb-0">
+                  Community Activity{" "}
+                  <a href="/community-activity">
+                    <span className={styles.viewAllLink}>VIEW ALL</span>
+                  </a>
                 </h1>
               </div>
-              <ProfileActivityLogs
-                initialParams={INITIAL_ACTIVITY_LOGS_PARAMS}
-                withFilters={true}
-              />
+              <div className="tw-px-2">
+                <ProfileActivityLogs
+                  initialParams={INITIAL_ACTIVITY_LOGS_PARAMS}
+                  withFilters={true}
+                />
+              </div>
             </div>
             {isNftImageLoaded && (
               <Container className={styles.mainContainer}>
