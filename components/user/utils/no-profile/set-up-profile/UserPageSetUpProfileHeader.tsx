9a738bdb0fe708430502e2d338940577251c638d
Simo <info@analyticsflow.ee>
2024-01-09 14:31:31 +0200

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/user/utils/no-profile/set-up-profile/UserPageSetUpProfileHeader.tsx
+++ components/user/utils/no-profile/set-up-profile/UserPageSetUpProfileHeader.tsx
@@ -4,11 +4,8 @@ export default function UserPageSetUpProfileHeader() {
       <div className="tw-min-w-0 tw-flex-1">
         <div className="tw-flex tw-flex-col">
           <h2 className="tw-mb-1 tw-text-base tw-font-semibold tw-text-iron-50 sm:tw-text-lg">
-            Ooops! You don&apos;t have a profile yet.
+            Set up your profile in 30 seconds or less
           </h2>
-          <p className="tw-font-normal tw-text-iron-400 tw-text-sm tw-mb-0">
-            First lets set up your profile.
-          </p>
         </div>
       </div>
     </div>
