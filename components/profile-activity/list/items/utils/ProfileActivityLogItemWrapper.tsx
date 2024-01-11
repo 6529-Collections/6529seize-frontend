b5a49bfb3d732a0280ee809d8aa20090b8b6de78
analyticsflowee <49652774+analyticsflowee@users.noreply.github.com>
2024-01-11 15:43:19 +0200

    Search redirect and more (#238)
    * lot of smaller improvements
    
    Signed-off-by: Simo <info@analyticsflow.ee>
    
    ---------
    
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    Signed-off-by: Simo <info@analyticsflow.ee>
    Co-authored-by: Ragne <ragneinfo@gmail.com>

--- components/profile-activity/list/items/utils/ProfileActivityLogItemWrapper.tsx
+++ components/profile-activity/list/items/utils/ProfileActivityLogItemWrapper.tsx
@@ -1,9 +1,11 @@
 import {
   ProfileActivityLog,
   ProfileActivityLogType,
+  RateMatter,
 } from "../../../../../entities/IProfile";
 import ProfileActivityLogItemTimeAgo from "./ProfileActivityLogItemTimeAgo";
-import ProfileActivityLogItemHandle from "./ProfileActivityLogItemHandle";
+import { UserPageTabType } from "../../../../user/layout/UserPageTabs";
+import CommonProfileLink from "../../../../user/utils/CommonProfileLink";
 
 export default function ProfileActivityLogItemWrapper({
   log,
@@ -16,12 +18,26 @@ export default function ProfileActivityLogItemWrapper({
 }) {
   const isArchived = log.type === ProfileActivityLogType.PROFILE_ARCHIVED;
 
+  const handleOrWallet = log?.profile_handle ?? "";
+  const isCurrentUser =
+    user?.toLowerCase() === log?.profile_handle?.toLowerCase();
+
+  const tabTarget =
+    log.type === ProfileActivityLogType.RATING_EDIT &&
+    log.contents.rating_matter === RateMatter.REP
+      ? UserPageTabType.REP
+      : UserPageTabType.IDENTITY;
+
   return (
     <tr className="tw-flex tw-items-center tw-justify-between">
       <td className="tw-py-2.5">
         <span className="tw-space-x-1 tw-inline-flex tw-items-center">
           {!isArchived && (
-            <ProfileActivityLogItemHandle log={log} user={user} />
+            <CommonProfileLink
+              handleOrWallet={handleOrWallet}
+              isCurrentUser={isCurrentUser}
+              tabTarget={tabTarget}
+            />
           )}
           {children}
         </span>
--- components/user/identity/statements/utils/UserPageIdentityAddStatementsInput.tsx
+++ components/user/identity/statements/utils/UserPageIdentityAddStatementsInput.tsx
@@ -46,7 +46,7 @@ export default function UserPageIdentityAddStatementsContactInput({
           value={value}
           onChange={handleInputChange}
           placeholder={STATEMENT_INPUT_PLACEHOLDER[activeType]}
-          className="tw-form-input tw-appearance-none tw-block tw-w-full tw-rounded-lg tw-border-0 tw-py-3 tw-pl-10 tw-pr-3 tw-bg-iron-900 tw-text-white tw-font-medium tw-caret-primary-300 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-iron-700 placeholder:tw-text-iron-400 focus:tw-outline-none focus:tw-bg-transparent focus:tw-ring-1 focus:tw-ring-inset hover:tw-ring-neutral-700 focus:tw-ring-primary-300 tw-text-base sm:tw-leading-6 tw-transition tw-duration-300 tw-ease-out"
+          className="tw-form-input tw-appearance-none tw-block tw-w-full tw-rounded-lg tw-border-0 tw-py-3 tw-pl-10 tw-pr-3 tw-bg-iron-900 tw-text-iron-50 tw-font-normal tw-caret-primary-400 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-iron-700 placeholder:tw-text-iron-400 focus:tw-outline-none focus:tw-bg-transparent focus:tw-ring-1 focus:tw-ring-inset hover:tw-ring-neutral-600 focus:tw-ring-primary-400 tw-text-base sm:tw-leading-6 tw-transition tw-duration-300 tw-ease-out"
         />
       </div>
     </>
