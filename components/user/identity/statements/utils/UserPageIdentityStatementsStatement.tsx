d2ece043b831750a6243b407b0ca797326828b49
Simo <info@analyticsflow.ee>
2024-01-17 15:32:22 +0200

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/user/identity/statements/utils/UserPageIdentityStatementsStatement.tsx
+++ components/user/identity/statements/utils/UserPageIdentityStatementsStatement.tsx
@@ -59,6 +59,7 @@ export default function UserPageIdentityStatementsStatement({
           <a
             href={statement.statement_value}
             target="_blank"
+            rel="noopener noreferrer"
             className={`${
               isTouchScreen ? "tw-block" : "tw-hidden group-hover:tw-block"
             } tw-p-2  tw-bg-transparent tw-cursor-pointer tw-text-sm tw-font-semibold tw-text-iron-200 tw-border-0 focus:tw-outline-none tw-transition tw-duration-300 tw-ease-out`}
