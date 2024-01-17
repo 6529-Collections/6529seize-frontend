04e2c1e42945ff058267b18b8d8a42c7651ef4bc
Ragne <ragneinfo@gmail.com>
2024-01-17 11:50:23 +0200

    ui fixes
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/user/identity/statements/about/UserPageIdentityStatementsAbout.tsx
+++ components/user/identity/statements/about/UserPageIdentityStatementsAbout.tsx
@@ -46,38 +46,53 @@ export default function UserPageIdentityStatementsAbout({
   };
 
   return (
-    <div className="tw-col-span-full tw-pb-6 tw-mb-6 tw-border-b tw-border-solid tw-border-t-0 tw-border-x-0  tw-border-white/10">
+    <div className="tw-col-span-full tw-pb-6 tw-mb-6 tw-border-b tw-border-solid tw-border-t-0 tw-border-x-0  tw-border-iron-800">
       <div className="tw-max-w-full sm:tw-max-w-prose">
-        <div className="tw-inline-flex tw-w-full tw-items-center">
+        <div>
           {isMyProfile && (
-            <button
-              onClick={toggleView}
-              className="tw-bg-transparent tw-border-none tw-text-iron-500 hover:tw-text-iron-200 tw-transition tw-duration-300 tw-ease-out"
-            >
+            <>
               {view === AboutStatementView.STATEMENT && (
-                <PencilIcon size={PencilIconSize.SMALL} />
-              )}
-              {view === AboutStatementView.EDIT && (
-                <svg
-                  className="tw-h-6 tw-w-6"
-                  aria-hidden="true"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
+                <button
+                  onClick={toggleView}
+                  className="tw-p-0 tw-group tw-bg-transparent tw-inline-flex tw-items-center tw-border-none tw-text-iron-400 tw-transition tw-duration-300 tw-ease-out"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
+                  <span className="tw-block tw-text-base tw-font-medium tw-text-iron-50 group-hover:tw-text-iron-400 tw-transition tw-duration-300 tw-ease-out">
+                    About
+                  </span>
+                  <div className="tw-p-2 -tw-mt-0.5">
+                    <PencilIcon size={PencilIconSize.SMALL} />
+                  </div>
+                </button>
               )}
-            </button>
+
+              {view === AboutStatementView.EDIT && (
+                <button
+                  onClick={toggleView}
+                  className="tw-p-0 tw-group tw-bg-transparent tw-inline-flex tw-items-center tw-border-none tw-text-iron-400 tw-transition tw-duration-300 tw-ease-out"
+                >
+                  <span className="tw-block tw-text-base tw-font-medium tw-text-iron-50 group-hover:tw-text-iron-400 tw-transition tw-duration-300 tw-ease-out">
+                    About
+                  </span>
+                  <div className="tw-p-2 -tw-mt-0.5">
+                    <svg
+                      className="tw-flex-shrink-0 tw-h-6 tw-w-6"
+                      aria-hidden="true"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </div>
+                </button>
+              )}
+            </>
           )}
-          <span className="tw-block tw-text-base tw-font-medium tw-text-iron-50">
-            About
-          </span>
         </div>
         {view === AboutStatementView.STATEMENT && (
           <UserPageIdentityStatementsAboutStatement statement={statement} />
