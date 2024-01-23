32d51a2e71fa890faf25ce7a868413494206772d
prxt <prxt@6529.io>
2024-01-23 16:47:12 +0200

    WIP
    Signed-off-by: prxt <prxt@6529.io>

--- components/communityStats/CommunityStats.tsx
+++ components/communityStats/CommunityStats.tsx
@@ -16,6 +16,7 @@ import {
 } from "chart.js";
 import { numberWithCommas } from "../../helpers/Helpers";
 import CommunityStatsDaysSelector from "./CommunityStatsDaysSelector";
+import CommunityStatsGraphs from "./CommunityStatsGraphs";
 
 ChartJS.register(
   CategoryScale,
@@ -56,190 +57,12 @@ const GRAPH_OPTIONS = {
 };
 
 export default function CommunityStats() {
-  const page = 1;
-
-  const [tdhHistory, setTdhHistory] = useState<GlobalTDHHistory[]>([]);
   const [latestHistory, setLatestHistory] = useState<GlobalTDHHistory>();
-  const [tdhLabels, setTdhLabels] = useState<Date[]>([]);
-
-  const [pageSize, setPageSize] = useState(30);
-  const [tdhHistoryLoaded, setTdhHistoryLoaded] = useState(false);
-
-  useEffect(() => {
-    setTdhHistoryLoaded(false);
-  }, [pageSize]);
 
   function getEstimatedDaysUntil(x: number) {
     const diff = x - latestHistory!.total_boosted_tdh;
     const days = diff / latestHistory!.net_boosted_tdh;
     return Math.round(days);
-  }
-
-  useEffect(() => {
-    let url = `${process.env.API_ENDPOINT}/api/tdh_global_history?page_size=${pageSize}&page=${page}`;
-    fetchUrl(url).then((response: DBResponse) => {
-      const tdhH = response.data.reverse();
-      setTdhHistory(tdhH);
-      setTdhLabels(tdhH.map((t) => t.date));
-      setLatestHistory(tdhH[tdhH.length - 1]);
-      setTdhHistoryLoaded(true);
-    });
-  }, [pageSize]);
-
-  function printTotalTDH() {
-    const data = {
-      labels: tdhLabels,
-      datasets: [
-        {
-          label: "Total Boosted TDH",
-          data: tdhLabels.map(
-            (l) => tdhHistory.find((t) => t.date === l)?.total_boosted_tdh
-          ),
-          borderColor: "#00DC21",
-          backgroundColor: "#00DC21",
-        },
-        {
-          label: "Total Unboosted TDH",
-          data: tdhLabels.map(
-            (l) => tdhHistory.find((t) => t.date === l)?.total_tdh
-          ),
-          borderColor: "#1861FF",
-          backgroundColor: "#1861FF",
-        },
-        {
-          label: "Total Unweighted TDH",
-          data: tdhLabels.map(
-            (l) => tdhHistory.find((t) => t.date === l)?.total_tdh__raw
-          ),
-          borderColor: "#e55137",
-          backgroundColor: "#e55137",
-        },
-      ],
-    };
-
-    return (
-      <>
-        {/* <Bar data={data} options={GRAPH_OPTIONS} /> */}
-        <Line data={data} options={GRAPH_OPTIONS} />
-      </>
-    );
-  }
-
-  function printNetTDH() {
-    const data = {
-      labels: tdhLabels,
-      datasets: [
-        {
-          label: "Net Boosted TDH",
-          data: tdhLabels.map(
-            (l) => tdhHistory.find((t) => t.date === l)?.net_boosted_tdh
-          ),
-          borderColor: "#00DC21",
-          backgroundColor: "#00DC21",
-        },
-        {
-          label: "Net Unboosted TDH",
-          data: tdhLabels.map(
-            (l) => tdhHistory.find((t) => t.date === l)?.net_tdh
-          ),
-          borderColor: "#1861FF",
-          backgroundColor: "#1861FF",
-        },
-        {
-          label: "Net Unweighted TDH",
-          data: tdhLabels.map(
-            (l) => tdhHistory.find((t) => t.date === l)?.net_tdh__raw
-          ),
-          borderColor: "#e55137",
-          backgroundColor: "#e55137",
-        },
-      ],
-    };
-
-    return (
-      <>
-        {/* <Bar data={data} options={GRAPH_OPTIONS} /> */}
-        <Line data={data} options={GRAPH_OPTIONS} />
-      </>
-    );
-  }
-
-  function printCreatedTDH() {
-    const data = {
-      labels: tdhLabels,
-      datasets: [
-        {
-          label: "Created Boosted TDH",
-          data: tdhLabels.map(
-            (l) => tdhHistory.find((t) => t.date === l)?.created_boosted_tdh
-          ),
-          borderColor: "#00DC21",
-          backgroundColor: "#00DC21",
-        },
-        {
-          label: "Created Unboosted TDH",
-          data: tdhLabels.map(
-            (l) => tdhHistory.find((t) => t.date === l)?.created_tdh
-          ),
-          borderColor: "#1861FF",
-          backgroundColor: "#1861FF",
-        },
-        {
-          label: "Created Unweighted TDH",
-          data: tdhLabels.map(
-            (l) => tdhHistory.find((t) => t.date === l)?.created_tdh__raw
-          ),
-          borderColor: "#e55137",
-          backgroundColor: "#e55137",
-        },
-      ],
-    };
-
-    return (
-      <>
-        {/* <Bar data={data} options={GRAPH_OPTIONS} /> */}
-        <Line data={data} options={GRAPH_OPTIONS} />
-      </>
-    );
-  }
-
-  function printDestroyedTDH() {
-    const data = {
-      labels: tdhLabels,
-      datasets: [
-        {
-          label: "Destroyed Boosted TDH",
-          data: tdhLabels.map(
-            (l) => tdhHistory.find((t) => t.date === l)?.destroyed_boosted_tdh
-          ),
-          borderColor: "#00DC21",
-          backgroundColor: "#00DC21",
-        },
-        {
-          label: "Destroyed Unboosted TDH",
-          data: tdhLabels.map(
-            (l) => tdhHistory.find((t) => t.date === l)?.destroyed_tdh
-          ),
-          borderColor: "#1861FF",
-          backgroundColor: "#1861FF",
-        },
-        {
-          label: "Destroyed Unweighted TDH",
-          data: tdhLabels.map(
-            (l) => tdhHistory.find((t) => t.date === l)?.destroyed_tdh__raw
-          ),
-          borderColor: "#e55137",
-          backgroundColor: "#e55137",
-        },
-      ],
-    };
-
-    return (
-      <>
-        {/* <Bar data={data} options={GRAPH_OPTIONS} /> */}
-        <Line data={data} options={GRAPH_OPTIONS} />
-      </>
-    );
   }
 
   return (
@@ -248,154 +71,75 @@ export default function CommunityStats() {
         <Col sm={12} md={8} className="pt-4">
           <h1 className="mb-0">COMMUNITY STATS</h1>
         </Col>
-        {/* <Col
-          sm={12}
-          md={4}
-          className="d-flex align-items-center justify-content-end gap-2 pt-4">
-          <span
-            className={`${styles.timeSelectionSpan} ${
-              pageSize == 7 ? styles.timeSelectionSpanSelected : ""
-            }`}
-            onClick={() => setPageSize(7)}>
-            7D
-          </span>
-          <span
-            className={`${styles.timeSelectionSpan} ${
-              pageSize == 30 ? styles.timeSelectionSpanSelected : ""
-            }`}
-            onClick={() => setPageSize(30)}>
-            1M
-          </span>
-        </Col> */}
       </Row>
-      {tdhHistory.length > 0 && (
-        <>
-          <Row className="pt-4 pb-4 font-bolder">
-            <Col sm={12} md={{ span: 10, offset: 1 }}>
-              <Container className="no-padding">
-                <Row>
-                  <Col
-                    sm={12}
-                    md={6}
-                    lg={4}
-                    className="d-flex flex-column gap-2">
-                    <Table>
-                      <tbody>
-                        <tr>
-                          <td>Community TDH</td>
-                          <td className="text-right">
-                            {numberWithCommas(latestHistory!.total_boosted_tdh)}
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>Daily Change</td>
-                          <td className="text-right">
-                            {numberWithCommas(latestHistory!.net_boosted_tdh)}
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>Daily % Change</td>
-                          <td className="text-right">
-                            {(
-                              (latestHistory!.net_boosted_tdh /
-                                latestHistory!.total_boosted_tdh) *
-                              100
-                            ).toFixed(2)}
-                            %
-                          </td>
-                        </tr>
-                      </tbody>
-                    </Table>
-                  </Col>
-                  <Col sm={12} md={6} lg={{ span: 4, offset: 4 }}>
-                    <Table>
-                      <tbody>
-                        <tr>
-                          <td>Estimated days until 250M</td>
-                          <td className="text-right">
-                            {numberWithCommas(getEstimatedDaysUntil(250000000))}
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>Estimated days until 500M</td>
-                          <td className="text-right">
-                            {numberWithCommas(getEstimatedDaysUntil(500000000))}
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>Estimated days until 1B</td>
-                          <td className="text-right">
-                            {numberWithCommas(
-                              getEstimatedDaysUntil(1000000000)
-                            )}
-                          </td>
-                        </tr>
-                      </tbody>
-                    </Table>
-                  </Col>
-                </Row>
-              </Container>
-            </Col>
-          </Row>
-          <Row className="pt-3">
-            <Col>
-              <CommunityStatsDaysSelector
-                loaded={tdhHistoryLoaded}
-                page_size={pageSize}
-                setPageSize={setPageSize}
-              />
-            </Col>
-          </Row>
-          <Row className="pt-4 pb-4">
-            <Col sm={12} md={{ span: 10, offset: 1 }}>
-              <Container className="no-padding">
-                <Row>
-                  <h2 className="mb-0 font-color">Total TDH</h2>
-                </Row>
-                <Row className="pt-4">
-                  <Col>{printTotalTDH()}</Col>
-                </Row>
-              </Container>
-            </Col>
-          </Row>
-          <Row className="pt-4 pb-4">
-            <Col sm={12} md={{ span: 10, offset: 1 }}>
-              <Container className="no-padding">
-                <Row>
-                  <h2 className="mb-0 font-color">Net TDH Daily Change</h2>
-                </Row>
-                <Row className="pt-4">
-                  <Col>{printNetTDH()}</Col>
-                </Row>
-              </Container>
-            </Col>
-          </Row>
-          <Row className="pt-4 pb-4">
-            <Col sm={12} md={{ span: 10, offset: 1 }}>
-              <Container className="no-padding">
-                <Row>
-                  <h2 className="mb-0 font-color">Created TDH Daily Change</h2>
-                </Row>
-                <Row className="pt-4">
-                  <Col>{printCreatedTDH()}</Col>
-                </Row>
-              </Container>
-            </Col>
-          </Row>
-          <Row className="pt-4 pb-4">
-            <Col sm={12} md={{ span: 10, offset: 1 }}>
-              <Container className="no-padding">
-                <Row>
-                  <h2 className="mb-0 font-color">Destroyed TDH Change</h2>
-                </Row>
-                <Row className="pt-4">
-                  <Col>{printDestroyedTDH()}</Col>
-                </Row>
-              </Container>
-            </Col>
-          </Row>
-        </>
+      {latestHistory && (
+        <Row className="pt-4 pb-4 font-bolder">
+          <Col sm={12} md={{ span: 10, offset: 1 }}>
+            <Container className="no-padding">
+              <Row>
+                <Col sm={12} md={6} lg={4} className="d-flex flex-column gap-2">
+                  <Table>
+                    <tbody>
+                      <tr>
+                        <td>Community TDH</td>
+                        <td className="text-right">
+                          {numberWithCommas(latestHistory!.total_boosted_tdh)}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Daily Change</td>
+                        <td className="text-right">
+                          {numberWithCommas(latestHistory!.net_boosted_tdh)}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Daily % Change</td>
+                        <td className="text-right">
+                          {(
+                            (latestHistory!.net_boosted_tdh /
+                              latestHistory!.total_boosted_tdh) *
+                            100
+                          ).toFixed(2)}
+                          %
+                        </td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                </Col>
+                <Col sm={12} md={6} lg={{ span: 4, offset: 4 }}>
+                  <Table>
+                    <tbody>
+                      <tr>
+                        <td>Estimated days until 250M</td>
+                        <td className="text-right">
+                          {numberWithCommas(getEstimatedDaysUntil(250000000))}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Estimated days until 500M</td>
+                        <td className="text-right">
+                          {numberWithCommas(getEstimatedDaysUntil(500000000))}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Estimated days until 1B</td>
+                        <td className="text-right">
+                          {numberWithCommas(getEstimatedDaysUntil(1000000000))}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                </Col>
+              </Row>
+            </Container>
+          </Col>
+        </Row>
       )}
+      <Row>
+        <Col>
+          <CommunityStatsGraphs setLatestHistory={setLatestHistory} />
+        </Col>
+      </Row>
     </Container>
   );
 }
---
+++ components/communityStats/CommunityStatsGraphs.tsx
@@ -0,0 +1,345 @@
+import { Container, Row, Col } from "react-bootstrap";
+import CommunityStatsDaysSelector from "./CommunityStatsDaysSelector";
+import { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import { DBResponse } from "../../entities/IDBResponse";
+import { GlobalTDHHistory, TDHHistory } from "../../entities/ITDH";
+import { fetchUrl } from "../../services/6529api";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+  BarElement,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+  BarElement
+);
+
+const GRAPH_OPTIONS = {
+  scales: {
+    x: {
+      grid: {
+        color: "rgb(45, 45, 45)",
+      },
+      ticks: {
+        color: "rgb(154, 154, 154)",
+      },
+    },
+    y: {
+      grid: {
+        color: "rgb(45, 45, 45)",
+      },
+      ticks: {
+        color: "rgb(154, 154, 154)",
+      },
+    },
+  },
+  plugins: {
+    legend: {
+      labels: {
+        color: "rgb(200, 200, 200)",
+      },
+    },
+  },
+};
+
+export default function CommunityStatsGraphs(
+  props: Readonly<{
+    wallet?: string;
+    setLatestHistory?: (history: TDHHistory) => void;
+  }>
+) {
+  const [tdhHistory, setTdhHistory] = useState<
+    TDHHistory[] | GlobalTDHHistory[]
+  >([]);
+  const [tdhLabels, setTdhLabels] = useState<Date[]>([]);
+  const [tdhHistoryLoaded, setTdhHistoryLoaded] = useState(false);
+
+  const [pageSize, setPageSize] = useState(30);
+
+  useEffect(() => {
+    setTdhHistoryLoaded(false);
+  }, [pageSize]);
+
+  useEffect(() => {
+    if (tdhHistoryLoaded) {
+      return;
+    }
+
+    let url = `${process.env.API_ENDPOINT}/api/${
+      props.wallet
+        ? `tdh_history?wallet=${props.wallet}`
+        : "tdh_global_history?"
+    }&page_size=${pageSize}&page=1`;
+    fetchUrl(url).then((response: DBResponse) => {
+      const tdhH = response.data.reverse();
+      setTdhHistory(tdhH);
+      if (props.setLatestHistory) {
+        props.setLatestHistory(tdhH[tdhH.length - 1]);
+      }
+      setTdhLabels(tdhH.map((t) => t.date));
+      setTdhHistoryLoaded(true);
+    });
+  }, [pageSize, tdhHistoryLoaded]);
+
+  function printTotalTDH() {
+    const data = {
+      labels: tdhLabels,
+      datasets: [
+        {
+          label: "Total Boosted TDH",
+          data: tdhLabels.map((l) => {
+            const a = tdhHistory.find((t) => t.date === l);
+            if (!a) {
+              return 0;
+            }
+            if ((a as any).total_boosted_tdh) {
+              return (a as any).total_boosted_tdh;
+            }
+            return (a as any).boosted_tdh;
+          }),
+          borderColor: "#00DC21",
+          backgroundColor: "#00DC21",
+        },
+        {
+          label: "Total Unboosted TDH",
+          data: tdhLabels.map((l) => {
+            {
+              const a = tdhHistory.find((t) => t.date === l);
+              if (!a) {
+                return 0;
+              }
+              if ((a as any).total_tdh) {
+                return (a as any).total_tdh;
+              }
+              return (a as any).tdh;
+            }
+          }),
+          borderColor: "#1861FF",
+          backgroundColor: "#1861FF",
+        },
+        {
+          label: "Total Unweighted TDH",
+          data: tdhLabels.map((l) => {
+            const a = tdhHistory.find((t) => t.date === l);
+            if (!a) {
+              return 0;
+            }
+            if ((a as any).total_tdh__raw) {
+              return (a as any).total_tdh__raw;
+            }
+            return (a as any).tdh__raw;
+          }),
+          borderColor: "#e55137",
+          backgroundColor: "#e55137",
+        },
+      ],
+    };
+
+    return (
+      <>
+        {/* <Bar data={data} options={GRAPH_OPTIONS} /> */}
+        <Line data={data} options={GRAPH_OPTIONS} />
+      </>
+    );
+  }
+
+  function printNetTDH() {
+    const data = {
+      labels: tdhLabels,
+      datasets: [
+        {
+          label: "Net Boosted TDH",
+          data: tdhLabels.map(
+            (l) => tdhHistory.find((t) => t.date === l)?.net_boosted_tdh
+          ),
+          borderColor: "#00DC21",
+          backgroundColor: "#00DC21",
+        },
+        {
+          label: "Net Unboosted TDH",
+          data: tdhLabels.map(
+            (l) => tdhHistory.find((t) => t.date === l)?.net_tdh
+          ),
+          borderColor: "#1861FF",
+          backgroundColor: "#1861FF",
+        },
+        {
+          label: "Net Unweighted TDH",
+          data: tdhLabels.map(
+            (l) => tdhHistory.find((t) => t.date === l)?.net_tdh__raw
+          ),
+          borderColor: "#e55137",
+          backgroundColor: "#e55137",
+        },
+      ],
+    };
+
+    return (
+      <>
+        {/* <Bar data={data} options={GRAPH_OPTIONS} /> */}
+        <Line data={data} options={GRAPH_OPTIONS} />
+      </>
+    );
+  }
+
+  function printCreatedTDH() {
+    const data = {
+      labels: tdhLabels,
+      datasets: [
+        {
+          label: "Created Boosted TDH",
+          data: tdhLabels.map(
+            (l) => tdhHistory.find((t) => t.date === l)?.created_boosted_tdh
+          ),
+          borderColor: "#00DC21",
+          backgroundColor: "#00DC21",
+        },
+        {
+          label: "Created Unboosted TDH",
+          data: tdhLabels.map(
+            (l) => tdhHistory.find((t) => t.date === l)?.created_tdh
+          ),
+          borderColor: "#1861FF",
+          backgroundColor: "#1861FF",
+        },
+        {
+          label: "Created Unweighted TDH",
+          data: tdhLabels.map(
+            (l) => tdhHistory.find((t) => t.date === l)?.created_tdh__raw
+          ),
+          borderColor: "#e55137",
+          backgroundColor: "#e55137",
+        },
+      ],
+    };
+
+    return (
+      <>
+        {/* <Bar data={data} options={GRAPH_OPTIONS} /> */}
+        <Line data={data} options={GRAPH_OPTIONS} />
+      </>
+    );
+  }
+
+  function printDestroyedTDH() {
+    const data = {
+      labels: tdhLabels,
+      datasets: [
+        {
+          label: "Destroyed Boosted TDH",
+          data: tdhLabels.map(
+            (l) => tdhHistory.find((t) => t.date === l)?.destroyed_boosted_tdh
+          ),
+          borderColor: "#00DC21",
+          backgroundColor: "#00DC21",
+        },
+        {
+          label: "Destroyed Unboosted TDH",
+          data: tdhLabels.map(
+            (l) => tdhHistory.find((t) => t.date === l)?.destroyed_tdh
+          ),
+          borderColor: "#1861FF",
+          backgroundColor: "#1861FF",
+        },
+        {
+          label: "Destroyed Unweighted TDH",
+          data: tdhLabels.map(
+            (l) => tdhHistory.find((t) => t.date === l)?.destroyed_tdh__raw
+          ),
+          borderColor: "#e55137",
+          backgroundColor: "#e55137",
+        },
+      ],
+    };
+
+    return (
+      <>
+        {/* <Bar data={data} options={GRAPH_OPTIONS} /> */}
+        <Line data={data} options={GRAPH_OPTIONS} />
+      </>
+    );
+  }
+
+  if (!tdhHistory.length && tdhHistoryLoaded) {
+    return <></>;
+  }
+
+  return (
+    <Container>
+      <Row className="pt-3">
+        <Col className="d-flex align-items-center">
+          <h3 className="mb-0">Graphs</h3>
+        </Col>
+        <Col>
+          <CommunityStatsDaysSelector
+            loaded={tdhHistoryLoaded}
+            page_size={pageSize}
+            setPageSize={setPageSize}
+          />
+        </Col>
+      </Row>
+      <Row className="pt-4 pb-4">
+        <Col sm={12} md={{ span: 10, offset: 1 }}>
+          <Container className="no-padding">
+            <Row>
+              <h2 className="mb-0 font-color">Total TDH</h2>
+            </Row>
+            <Row className="pt-4">
+              <Col>{tdhHistory.length > 0 && <>{printTotalTDH()}</>}</Col>
+            </Row>
+          </Container>
+        </Col>
+      </Row>
+      <Row className="pt-4 pb-4">
+        <Col sm={12} md={{ span: 10, offset: 1 }}>
+          <Container className="no-padding">
+            <Row>
+              <h2 className="mb-0 font-color">Net TDH Daily Change</h2>
+            </Row>
+            <Row className="pt-4">
+              <Col>{tdhHistory.length > 0 && <>{printNetTDH()}</>}</Col>
+            </Row>
+          </Container>
+        </Col>
+      </Row>
+      <Row className="pt-4 pb-4">
+        <Col sm={12} md={{ span: 10, offset: 1 }}>
+          <Container className="no-padding">
+            <Row>
+              <h2 className="mb-0 font-color">Created TDH Daily Change</h2>
+            </Row>
+            <Row className="pt-4">
+              <Col>{tdhHistory.length > 0 && <>{printCreatedTDH()}</>}</Col>
+            </Row>
+          </Container>
+        </Col>
+      </Row>
+      {tdhHistory.some((t) => t.destroyed_boosted_tdh > 0) && (
+        <Row className="pt-4 pb-4">
+          <Col sm={12} md={{ span: 10, offset: 1 }}>
+            <Container className="no-padding">
+              <Row>
+                <h2 className="mb-0 font-color">Destroyed TDH Daily Change</h2>
+              </Row>
+              <Row className="pt-4">
+                <Col>{tdhHistory.length > 0 && <>{printDestroyedTDH()}</>}</Col>
+              </Row>
+            </Container>
+          </Col>
+        </Row>
+      )}
+    </Container>
+  );
+}
