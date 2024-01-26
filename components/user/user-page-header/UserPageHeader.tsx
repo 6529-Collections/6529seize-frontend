ba2f3ab7fafdbd6ab5691e0ff92ea4943c897e45
analyticsflowee <49652774+analyticsflowee@users.noreply.github.com>
2024-01-26 11:20:36 +0200

    wip (#253)
    * error close icon
    
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    ---------
    
    Signed-off-by: Simo <info@analyticsflow.ee>
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    Co-authored-by: Ragne <ragneinfo@gmail.com>

--- components/user/user-page-header/UserPageHeader.tsx
+++ components/user/user-page-header/UserPageHeader.tsx
@@ -98,38 +98,32 @@ export default function UserPageHeader({
           canEdit={canEdit}
         />
         <div className="tw-relative tw-px-6 min-[1100px]:tw-max-w-[960px] min-[1200px]:tw-max-w-[1150px] min-[1300px]:tw-max-w-[1250px] min-[1400px]:tw-max-w-[1350px] min-[1500px]:tw-max-w-[1450px] min-[1600px]:tw-max-w-[1550px] min-[1800px]:tw-max-w-[1750px] min-[2000px]:tw-max-w-[1950px] tw-mx-auto">
-          <div className="tw-w-full tw-flex tw-gap-x-6 tw-flex-wrap tw-items-start">
-            <div>
-              <div className="-tw-mt-20 sm:-tw-mt-24">
-                <UserPageHeaderPfpWrapper profile={profile} canEdit={canEdit}>
-                  <UserPageHeaderPfp
-                    canEdit={canEdit}
-                    profile={profile}
-                    defaultBanner1={
-                      profile.profile?.banner_1 ?? DEFAULT_BANNER_1
-                    }
-                    defaultBanner2={
-                      profile.profile?.banner_2 ?? DEFAULT_BANNER_2
-                    }
-                  />
-                </UserPageHeaderPfpWrapper>
-              </div>
-              <UserPageHeaderName
-                profile={profile}
-                canEdit={canEdit}
-                mainAddress={mainAddress}
-              />
-
-              <UserPageHeaderLevel level={profile.level} />
-              {showAbout && (
-                <UserPageHeaderAbout
-                  profile={profile}
-                  statement={aboutStatement}
+          <div className="tw-flex tw-flex-col">
+            <div className="-tw-mt-20 sm:-tw-mt-24 tw-w-min">
+              <UserPageHeaderPfpWrapper profile={profile} canEdit={canEdit}>
+                <UserPageHeaderPfp
                   canEdit={canEdit}
+                  profile={profile}
+                  defaultBanner1={profile.profile?.banner_1 ?? DEFAULT_BANNER_1}
+                  defaultBanner2={profile.profile?.banner_2 ?? DEFAULT_BANNER_2}
                 />
-              )}
-              <UserPageHeaderStats profile={profile} />
+              </UserPageHeaderPfpWrapper>
             </div>
+            <UserPageHeaderName
+              profile={profile}
+              canEdit={canEdit}
+              mainAddress={mainAddress}
+            />
+
+            <UserPageHeaderLevel level={profile.level} />
+            {showAbout && (
+              <UserPageHeaderAbout
+                profile={profile}
+                statement={aboutStatement}
+                canEdit={canEdit}
+              />
+            )}
+            <UserPageHeaderStats profile={profile} />
           </div>
         </div>
       </section>
