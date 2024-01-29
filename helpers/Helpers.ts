6379a8b6295dc855affa89c2c1f5ca4df3c9665b
analyticsflowee <49652774+analyticsflowee@users.noreply.github.com>
2024-01-29 17:01:53 +0200

    fixed profile rededirect path (#256)
    * fixed profile rededirect path
    
    Signed-off-by: Simo <info@analyticsflow.ee>
    
    ---------
    
    Signed-off-by: Simo <info@analyticsflow.ee>
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- helpers/Helpers.ts
+++ helpers/Helpers.ts
@@ -578,5 +578,5 @@ export const getProfileTargetRoute = ({
   if (router.route.includes("[user]")) {
     return router.route.replace("[user]", handleOrWallet);
   }
-  return `${handleOrWallet}/${USER_PAGE_TAB_META[defaultPath].route}`;
+  return `/${handleOrWallet}/${USER_PAGE_TAB_META[defaultPath].route}`;
 };
