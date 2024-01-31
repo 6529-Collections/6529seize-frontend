2e4ac4369d6d7971d35160753526b5915c0e1bb5
Simo <info@analyticsflow.ee>
2024-01-31 17:51:41 +0200

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/user/identity/statements/UserPageIdentityStatements.tsx
+++ components/user/identity/statements/UserPageIdentityStatements.tsx
@@ -142,7 +142,7 @@ export default function UserPageIdentityStatements({
                     placement="top"
                   >
                     <div
-                      tabindex="0"
+                      tabIndex={0}
                       className="tw-rounded-full tw-h-10 tw-w-10 tw-inline-flex tw-items-center tw-justify-center focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset focus:tw-ring-primary-400"
                     >
                       <svg
@@ -155,9 +155,9 @@ export default function UserPageIdentityStatements({
                         <path
                           d="M12 16V12M12 8H12.01M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
                           stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                         />
                       </svg>
                     </div>
