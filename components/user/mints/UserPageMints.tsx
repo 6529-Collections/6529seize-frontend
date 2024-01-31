24549042b1ff2aa4fb6d36e5039dd9c8a4a8cce3
Simo <info@analyticsflow.ee>
2024-01-31 17:58:33 +0200

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/user/mints/UserPageMints.tsx
+++ components/user/mints/UserPageMints.tsx
@@ -173,108 +173,119 @@ export default function UserPageMints({
             NextGen - Pebbles
           </h2>
         </div>
-        <p className="tw-font-normal tw-text-iron-400 tw-text-sm sm:tw-text-base tw-mb-0">
-          Congratulations! You are eligible to mint Pebbles in{" "}
-          <span className="tw-pl-1 tw-font-semibold tw-text-iron-200">
-            {phaseNames}.
-          </span>
-        </p>
-        <div className="tw-mt-6 sm:tw-mt-8 tw-flex tw-flex-col">
-          <span className="tw-text-base sm:tw-text-lg tw-font-semibold tw-text-iron-50">
-            Minting Page
-          </span>
-          <div className="tw-mt-1">
-            <a
-              href="www.seize.io/nextgen/pebbles/mint"
-              className="tw-group tw-inline-flex tw-items-center tw-text-primary-300 hover:tw-text-primary-400 tw-font-medium tw-text-base tw-duration-300 tw-transition tw-ease-out"
-              target="_blank"
-            >
-              <span>seize.io/nextgen/pebbles/mint</span>
-              <svg
-                className="tw-h-6 tw-w-6 tw-ml-2 tw-text-primary-300 group-hover:tw-text-primary-400 tw-duration-400 tw-transition tw-ease-out group-hover:-tw-translate-y-0.5 group-hover:tw-translate-x-0.5"
-                viewBox="0 0 24 24"
-                fill="none"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7 17L17 7M17 7H7M17 7V17"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </a>
-          </div>
-        </div>
-        <UserPageMintsPhases phases={phases} />
-
-        <div className="tw-mt-7 sm:tw-mt-9 tw-flex tw-flex-col">
-          <span className="tw-text-base sm:tw-text-lg tw-font-semibold tw-text-iron-50">
-            Your Minting Addresses
-          </span>
-          <p className="tw-mb-0 tw-mt-1 tw-text-iron-400 tw-text-sm tw-font-medium">
-            You can mint from any of the following addresses
-          </p>
-          <div className="tw-mt-3 tw-flex tw-flex-col tw-gap-y-1.5 ">
-            {delegations
-              .filter((wallet) => wallet.data?.address)
-              .flatMap((wallet) => {
-                const res: string[] = [];
-                const address = wallet.data?.address;
-                if (address) {
-                  res.push(address);
-                }
-                const delegation = wallet.data?.delegation;
-                if (
-                  delegation &&
-                  !VALID_DELEGATION_ADDRESSES.includes(delegation)
-                ) {
-                  res.push(delegation);
-                }
-                return res;
-              })
-              .map((wallet) => (
-                <div key={wallet} className="tw-inline-flex sm:tw-items-center">
+        {phases.length ? (
+          <>
+            <p className="tw-font-normal tw-text-iron-400 tw-text-sm sm:tw-text-base tw-mb-0">
+              Congratulations! You are eligible to mint Pebbles in{" "}
+              <span className="tw-pl-1 tw-font-semibold tw-text-iron-200">
+                {phaseNames}.
+              </span>
+            </p>
+            <div className="tw-mt-6 sm:tw-mt-8 tw-flex tw-flex-col">
+              <span className="tw-text-base sm:tw-text-lg tw-font-semibold tw-text-iron-50">
+                Minting Page
+              </span>
+              <div className="tw-mt-1">
+                <a
+                  href="www.seize.io/nextgen/pebbles/mint"
+                  className="tw-group tw-inline-flex tw-items-center tw-text-primary-300 hover:tw-text-primary-400 tw-font-medium tw-text-base tw-duration-300 tw-transition tw-ease-out"
+                  target="_blank"
+                >
+                  <span>seize.io/nextgen/pebbles/mint</span>
                   <svg
-                    className="tw-flex-shrink-0 tw-h-5 tw-w-5 tw-mr-2 tw-text-iron-300"
+                    className="tw-h-6 tw-w-6 tw-ml-2 tw-text-primary-300 group-hover:tw-text-primary-400 tw-duration-400 tw-transition tw-ease-out group-hover:-tw-translate-y-0.5 group-hover:tw-translate-x-0.5"
                     viewBox="0 0 24 24"
                     fill="none"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      d="M20 9.5V7.2C20 6.0799 20 5.51984 19.782 5.09202C19.5903 4.7157 19.2843 4.40974 18.908 4.21799C18.4802 4 17.9201 4 16.8 4H5.2C4.0799 4 3.51984 4 3.09202 4.21799C2.7157 4.40973 2.40973 4.71569 2.21799 5.09202C2 5.51984 2 6.0799 2 7.2V16.8C2 17.9201 2 18.4802 2.21799 18.908C2.40973 19.2843 2.71569 19.5903 3.09202 19.782C3.51984 20 4.07989 20 5.2 20L16.8 20C17.9201 20 18.4802 20 18.908 19.782C19.2843 19.5903 19.5903 19.2843 19.782 18.908C20 18.4802 20 17.9201 20 16.8V14.5M15 12C15 11.5353 15 11.303 15.0384 11.1098C15.1962 10.3164 15.8164 9.69624 16.6098 9.53843C16.803 9.5 17.0353 9.5 17.5 9.5H19.5C19.9647 9.5 20.197 9.5 20.3902 9.53843C21.1836 9.69624 21.8038 10.3164 21.9616 11.1098C22 11.303 22 11.5353 22 12C22 12.4647 22 12.697 21.9616 12.8902C21.8038 13.6836 21.1836 14.3038 20.3902 14.4616C20.197 14.5 19.9647 14.5 19.5 14.5H17.5C17.0353 14.5 16.803 14.5 16.6098 14.4616C15.8164 14.3038 15.1962 13.6836 15.0384 12.8902C15 12.697 15 12.4647 15 12Z"
+                      d="M7 17L17 7M17 7H7M17 7V17"
                       stroke="currentColor"
                       strokeWidth="2"
                       strokeLinecap="round"
                       strokeLinejoin="round"
                     />
                   </svg>
+                </a>
+              </div>
+            </div>
+            <UserPageMintsPhases phases={phases} />
 
-                  <span className="tw-text-iron-300 tw-break-all tw-font-medium tw-text-base">
-                    {wallet}
-                  </span>
-                </div>
-              ))}
-          </div>
-        </div>
-        <div className="tw-mt-6">
-          <p className="tw-mb-0 tw-text-iron-400 tw-text-sm tw-font-normal">
-            To change your minting address, change your minting delegate for All
-            NFTs or The Memes in the{" "}
-            <a
-              href="https://seize.io/delegation/delegation-center"
-              rel="noopener noreferrer"
-              className="tw-group tw-inline-flex tw-items-center tw-text-iron-200 hover:tw-text-primary-300 tw-font-medium tw-text-sm tw-duration-300 tw-transition tw-ease-out"
-              target="_blank"
-            >
-              Delegation Center
-            </a>{" "}
-            at any time before the mint.
+            <div className="tw-mt-7 sm:tw-mt-9 tw-flex tw-flex-col">
+              <span className="tw-text-base sm:tw-text-lg tw-font-semibold tw-text-iron-50">
+                Your Minting Addresses
+              </span>
+              <p className="tw-mb-0 tw-mt-1 tw-text-iron-400 tw-text-sm tw-font-medium">
+                You can mint from any of the following addresses
+              </p>
+              <div className="tw-mt-3 tw-flex tw-flex-col tw-gap-y-1.5 ">
+                {delegations
+                  .filter((wallet) => wallet.data?.address)
+                  .flatMap((wallet) => {
+                    const res: string[] = [];
+                    const address = wallet.data?.address;
+                    if (address) {
+                      res.push(address);
+                    }
+                    const delegation = wallet.data?.delegation;
+                    if (
+                      delegation &&
+                      !VALID_DELEGATION_ADDRESSES.includes(delegation)
+                    ) {
+                      res.push(delegation);
+                    }
+                    return res;
+                  })
+                  .map((wallet) => (
+                    <div
+                      key={wallet}
+                      className="tw-inline-flex sm:tw-items-center"
+                    >
+                      <svg
+                        className="tw-flex-shrink-0 tw-h-5 tw-w-5 tw-mr-2 tw-text-iron-300"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M20 9.5V7.2C20 6.0799 20 5.51984 19.782 5.09202C19.5903 4.7157 19.2843 4.40974 18.908 4.21799C18.4802 4 17.9201 4 16.8 4H5.2C4.0799 4 3.51984 4 3.09202 4.21799C2.7157 4.40973 2.40973 4.71569 2.21799 5.09202C2 5.51984 2 6.0799 2 7.2V16.8C2 17.9201 2 18.4802 2.21799 18.908C2.40973 19.2843 2.71569 19.5903 3.09202 19.782C3.51984 20 4.07989 20 5.2 20L16.8 20C17.9201 20 18.4802 20 18.908 19.782C19.2843 19.5903 19.5903 19.2843 19.782 18.908C20 18.4802 20 17.9201 20 16.8V14.5M15 12C15 11.5353 15 11.303 15.0384 11.1098C15.1962 10.3164 15.8164 9.69624 16.6098 9.53843C16.803 9.5 17.0353 9.5 17.5 9.5H19.5C19.9647 9.5 20.197 9.5 20.3902 9.53843C21.1836 9.69624 21.8038 10.3164 21.9616 11.1098C22 11.303 22 11.5353 22 12C22 12.4647 22 12.697 21.9616 12.8902C21.8038 13.6836 21.1836 14.3038 20.3902 14.4616C20.197 14.5 19.9647 14.5 19.5 14.5H17.5C17.0353 14.5 16.803 14.5 16.6098 14.4616C15.8164 14.3038 15.1962 13.6836 15.0384 12.8902C15 12.697 15 12.4647 15 12Z"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+
+                      <span className="tw-text-iron-300 tw-break-all tw-font-medium tw-text-base">
+                        {wallet}
+                      </span>
+                    </div>
+                  ))}
+              </div>
+            </div>
+            <div className="tw-mt-6">
+              <p className="tw-mb-0 tw-text-iron-400 tw-text-sm tw-font-normal">
+                To change your minting address, change your minting delegate for
+                All NFTs or The Memes in the{" "}
+                <a
+                  href="https://seize.io/delegation/delegation-center"
+                  rel="noopener noreferrer"
+                  className="tw-group tw-inline-flex tw-items-center tw-text-iron-200 hover:tw-text-primary-300 tw-font-medium tw-text-sm tw-duration-300 tw-transition tw-ease-out"
+                  target="_blank"
+                >
+                  Delegation Center
+                </a>{" "}
+                at any time before the mint.
+              </p>
+            </div>
+          </>
+        ) : (
+          <p className="tw-font-normal tw-text-iron-400 tw-text-sm sm:tw-text-base tw-mb-0">
+            You are not eligible to mint Pebbles at this time.
           </p>
-        </div>
+        )}
         <div className="tw-mt-8 tw-pt-8 md:tw-mt-10 md:tw-pt-10 tw-border-t tw-border-solid tw-border-x-0 tw-border-b tw-mb-0 tw-text-iron-400 tw-text-sm tw-font-normal">
           <div className="md:tw-max-w-5xl">
             <span className="tw-text-iron-300">Note:</span> The Your Mints tab
