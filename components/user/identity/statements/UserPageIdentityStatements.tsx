e154b62fa12bce627b802cd9cca9002fdeb79cd8
analyticsflowee <49652774+analyticsflowee@users.noreply.github.com>
2024-02-01 12:31:38 +0200

    user page identity statements fixes (#261)
    * user page identity statements fixes
    
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    ---------
    
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    Signed-off-by: Simo <info@analyticsflow.ee>
    Co-authored-by: Ragne <ragneinfo@gmail.com>

--- components/user/identity/statements/UserPageIdentityStatements.tsx
+++ components/user/identity/statements/UserPageIdentityStatements.tsx
@@ -14,6 +14,7 @@ import UserPageIdentityStatementsContacts from "./contacts/UserPageIdentityState
 import UserPageIdentityStatementsSocialMediaVerificationPosts from "./social-media-verification-posts/UserPageIdentityStatementsSocialMediaVerificationPosts";
 import { QueryKey } from "../../../react-query-wrapper/ReactQueryWrapper";
 import UserPageIdentityStatementsNFTAccounts from "./nft-accounts/UserPageIdentityStatementsNFTAccounts";
+import Tippy from "@tippyjs/react";
 
 export default function UserPageIdentityStatements({
   profile,
@@ -83,54 +84,84 @@ export default function UserPageIdentityStatements({
       <div>
         <UserPageIdentityAddStatementsHeader profile={profile} />
         <div className="tw-mt-2 lg:tw-mt-4 tw-bg-iron-900 tw-border tw-border-iron-800 tw-border-solid tw-rounded-xl tw-scroll-py-3 tw-overflow-auto">
-          <div className="tw-px-4 tw-py-6 lg:tw-p-8 tw-mx-auto tw-grid tw-grid-cols-1 xl:tw-gap-x-8 xl:tw-gap-y-2 lg:tw-mx-0 xl:tw-grid-cols-5">
+          <div className="tw-relative tw-px-4 tw-py-6 lg:tw-p-8 tw-mx-auto tw-grid tw-grid-cols-1 xl:tw-gap-x-8 xl:tw-gap-y-2 lg:tw-mx-0 xl:tw-grid-cols-5">
             <div className="tw-col-span-full tw-space-y-6 md:tw-space-y-8">
-              <div className="tw-grid tw-grid-cols-1 tw-gap-x-8 tw-gap-y-8 xl:tw-gap-y-6 xl:tw-grid-cols-6">
-                <div className="tw-col-span-2">
+              <div className="tw-grid tw-grid-cols-1 tw-gap-x-8 tw-gap-y-8 xl:tw-grid-cols-9">
+                <div className="tw-col-span-3">
                   <UserPageIdentityStatementsConsolidatedAddresses
                     profile={profile}
                   />
                 </div>
-                <div className="tw-col-span-2">
+                <div className="tw-col-span-3">
                   <UserPageIdentityStatementsSocialMediaAccounts
                     statements={socialMediaAccounts}
                     profile={profile}
                     loading={isLoading}
                   />
                 </div>
-                <div className="tw-col-span-2">
+                <div className="tw-col-span-3">
                   <UserPageIdentityStatementsNFTAccounts
                     statements={nftAccounts}
                     profile={profile}
                     loading={isLoading}
                   />
                 </div>
-              </div>
-              <div className="tw-grid tw-grid-cols-1 tw-gap-x-8 tw-gap-y-8 xl:tw-gap-y-6 xl:tw-grid-cols-6">
-                <div className="tw-col-span-2">
+
+                <div className="tw-col-span-3">
                   <UserPageIdentityStatementsContacts
                     statements={contacts}
                     profile={profile}
                     loading={isLoading}
                   />
                 </div>
-                <div className="tw-col-span-2">
+                <div className="tw-col-span-3">
                   <UserPageIdentityStatementsSocialMediaVerificationPosts
                     statements={socialMediaVerificationPosts}
                     profile={profile}
                     loading={isLoading}
                   />
                 </div>
-                <div className="tw-col-span-2">
-                  <ul className="tw-pl-4 tw-list-disc tw-text-iron-500 tw-text-sm tw-font-normal tw-space-y-1">
-                    <li>All statements are optional.</li>
-                    <li>All statements are fully and permanently public.</li>
-                    <li>
-                      Seize does not connect to social media accounts or verify
-                      posts.
-                    </li>
-                    <li>The community will rate the accuracy of statements.</li>
-                  </ul>
+                <div className="tw-absolute tw-right-2 lg:tw-right-4 tw-top-2 xl:tw-top-4">
+                  <Tippy
+                    content={
+                      <ul className="tw-pl-4 tw-list-disc tw-text-iron-300 tw-font-normal tw-space-y-1">
+                        <li>All statements are optional.</li>
+                        <li>
+                          All statements are fully and permanently public.
+                        </li>
+                        <li>
+                          Seize does not connect to social media accounts or
+                          verify posts.
+                        </li>
+                        <li>
+                          The community will rate the accuracy of statements.
+                        </li>
+                      </ul>
+                    }
+                    theme="dark"
+                    placement="top"
+                  >
+                    <div
+                      tabIndex={0}
+                      className="tw-rounded-full tw-h-10 tw-w-10 tw-inline-flex tw-items-center tw-justify-center focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset focus:tw-ring-primary-400"
+                    >
+                      <svg
+                        className="tw-flex-shrink-0 tw-w-5 tw-h-5 tw-text-iron-400"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M12 16V12M12 8H12.01M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                    </div>
+                  </Tippy>
                 </div>
               </div>
             </div>
