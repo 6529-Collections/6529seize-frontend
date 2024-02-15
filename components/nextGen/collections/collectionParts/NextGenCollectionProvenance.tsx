46d6d3a2656acba6347476e8dd33bcc978a706ed
prxt <prxt@6529.io>
2024-02-15 09:16:00 +0200

    WIP
    Signed-off-by: prxt <prxt@6529.io>

--- components/nextGen/collections/collectionParts/NextGenCollectionProvenance.tsx
+++ components/nextGen/collections/collectionParts/NextGenCollectionProvenance.tsx
@@ -47,18 +47,13 @@ export default function NextGenCollectionProvenance(props: Readonly<Props>) {
 
   return (
     <Container className="no-padding" ref={scrollTarget}>
-      {/* <Row>
-          <Col>
-            <h3 className="mb-0">Provenance</h3>
-          </Col>
-        </Row>
-        <hr /> */}
       <Row className={`pt-2 ${styles.logsScrollContainer}`}>
         <Col>
           <Table bordered={false} className={styles.logsTable}>
             <tbody>
               {logs.map((log) => (
                 <NextGenCollectionProvenanceRow
+                  collection={props.collection}
                   log={log}
                   key={`${log.block}-${log.transaction}`}
                 />
@@ -90,17 +85,51 @@ export default function NextGenCollectionProvenance(props: Readonly<Props>) {
 
 export function NextGenCollectionProvenanceRow(
   props: Readonly<{
+    collection: NextGenCollection;
     log: NextGenLog;
   }>
 ) {
   const log = props.log;
+
+  function printParsedLog() {
+    const escapedCollectionName = props.collection.name.replace(
+      /[-\/\\^$*+?.()|[\]{}]/g,
+      "\\$&"
+    );
+    const pattern = new RegExp("(" + escapedCollectionName + " #(\\d+))");
+    const match = log.log.match(pattern);
+
+    try {
+      if (match && match[1] && match[2] && typeof match.index === "number") {
+        const startIndex = match.index;
+        const endIndex = startIndex + match[1].length;
+        const beforeMatch = log.log.substring(0, startIndex);
+        const afterMatch = log.log.substring(endIndex);
+        const normalisedTokenId = parseInt(match[2], 10); // Ensure base 10 is used for parsing
+        const tokenId = props.collection.id * 10000000000 + normalisedTokenId;
+
+        return (
+          <>
+            {beforeMatch}
+            <a href={`/nextgen/token/${tokenId}`}>{match[1]}</a>
+            {afterMatch}
+          </>
+        );
+      }
+    } catch (e) {
+      console.error("Error processing log:", e);
+    }
+
+    return <>{log.log}</>;
+  }
+
   return (
     <tr>
       <td className="align-middle text-center">
         {getDateDisplay(new Date(log.block_timestamp * 1000))}
       </td>
       <td className={styles.collectionProvenance}>
-        <span className="d-flex">{log.log}</span>
+        <span className="d-flex">{printParsedLog()}</span>
       </td>
       <td className="text-right">
         <a
