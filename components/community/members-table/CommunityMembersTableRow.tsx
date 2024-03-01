c538678b590a6cde57f88a4513958b1d11cf8017
Simo <info@analyticsflow.ee>
2024-03-01 16:28:40 +0200

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/community/members-table/CommunityMembersTableRow.tsx
+++ components/community/members-table/CommunityMembersTableRow.tsx
@@ -42,13 +42,11 @@ export default function CommunityMembersTableRow({
           <div className="tw-h-8 tw-w-8 tw-rounded-lg tw-ring-1 tw-ring-white/10 tw-bg-iron-800"></div>
         )}
       </td>
-      <td
-        className={`tw-group tw-pr-4 sm:tw-pr-6 tw-whitespace-nowrap tw-group tw-py-3 tw-text-sm sm:tw-text-base tw-font-medium ${textColorClass}`}
-      >
+      <td className="tw-group tw-pr-4 sm:tw-pr-6 tw-whitespace-nowrap tw-group tw-py-3 tw-text-sm sm:tw-text-base tw-font-medium">
         <div className="tw-truncate tw-max-w-[12rem] sm:tw-max-w-xs">
           <Link
             href={path}
-            className="tw-no-underline group-hover:tw-underline group-hover:tw-text-iron-500 tw-transition tw-duration-300 tw-ease-out"
+            className={`tw-no-underline group-hover:tw-underline  group-hover:tw-text-iron-500 tw-transition tw-duration-300 tw-ease-out ${textColorClass}`}
           >
             {member.display}
           </Link>
