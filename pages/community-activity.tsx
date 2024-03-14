ec38b3c5559682393fa19074a2cc2db7bf7788ce
Simo <info@analyticsflow.ee>
2024-03-14 20:26:10 +0200

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- pages/community-activity.tsx
+++ pages/community-activity.tsx
@@ -25,6 +25,7 @@ import {
   selectActiveCurationFilterId,
   setActiveCurationFilterId,
 } from "../store/curationFilterSlice";
+import CommunityCurationFiltersSidebarToggleButton from "../components/curation/sidebar/CommunityCurationFiltersSidebarToggleButton";
 
 const Header = dynamic(() => import("../components/header/Header"), {
   ssr: false,
@@ -163,43 +164,11 @@ export default function CommunityActivityPage({
           }}
           transition={{ duration: 0.3, ease: "easeInOut" }}
         >
-          <button
-            className="tw-fixed tw-mt-2 tw-bg-iron-950 tw-border tw-border-l-0 tw-border-solid tw-border-neutral-600 tw-p-2 tw-rounded-r-lg tw-text-iron-400 hover:tw-text-iron-50 focus:tw-outline-none tw-transition tw-duration-300 tw-ease-out"
+          <CommunityCurationFiltersSidebarToggleButton
             ref={openButtonRef}
-            onClick={() => setOpen(!open)}
-          >
-            {open ? (
-              <svg
-                className="tw-h-6 tw-w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                className="tw-h-6 tw-w-6"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"
-                />
-              </svg>
-            )}
-          </button>
+            open={open}
+            setOpen={setOpen}
+          />
         </motion.div>
 
         <div className="tailwind-scope tw-bg-iron-950 tw-min-h-screen tw-mt-6 lg:tw-mt-8 tw-pb-16 lg:tw-pb-20 tw-px-6 min-[992px]:tw-px-3 min-[992px]:tw-max-w-[960px] max-[1100px]:tw-max-w-[950px] min-[1200px]:tw-max-w-[1050px] min-[1300px]:tw-max-w-[1150px] min-[1400px]:tw-max-w-[1250px] min-[1500px]:tw-max-w-[1280px] tw-mx-auto">
