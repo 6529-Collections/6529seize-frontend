b21b0cd071f1689066ad42c2d52a87385ba37000
Ragne <ragneinfo@gmail.com>
2024-03-26 15:59:24 +0200

    drops ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/drops/create/utils/metadata/CreateDropMetadataItem.tsx
+++ components/drops/create/utils/metadata/CreateDropMetadataItem.tsx
@@ -8,16 +8,28 @@ export default function CreateDropMetadataItem({
   readonly onMetadataRemove: (data_key: string) => void;
 }) {
   return (
-    <div className="tw-p-1 tw-bg-neutral-400 tw-rounded-md tw-justify-between tw-inline-flex tw-space-x-2">
-      <div>
+    <button
+      onClick={() => onMetadataRemove(data_key)}
+      type="button"
+      className="tw-border-0 tw-group tw-inline-flex tw-items-center tw-justify-between tw-gap-x-2 tw-rounded-lg tw-px-3 tw-py-1.5 tw-text-sm tw-font-normal tw-text-iron-200 tw-bg-iron-400/10 tw-ring-1 tw-ring-inset tw-ring-iron-700 hover:tw-ring-iron-600 tw-transition tw-duration-300 tw-ease-out"
+    >
+      <span>
         {data_key}: {data_value}
-      </div>
-      <button
-        className="tw-text-red"
-        onClick={() => onMetadataRemove(data_key)}
+      </span>
+      <svg
+        className="-tw-mr-0.5 tw-w-5 tw-h-5 tw-text-red group-hover:tw-scale-110 tw-transition tw-duration-300 tw-ease-out"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        X
-      </button>
-    </div>
+        <path
+          d="M18 6L6 18M6 6L18 18"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </button>
   );
 }
--- components/drops/create/utils/select-file/CreateDropSelectFile.tsx
+++ components/drops/create/utils/select-file/CreateDropSelectFile.tsx
@@ -11,16 +11,38 @@ export default function CreateDropSelectFile({
   readonly onFileChange: (file: File | null) => void;
 }) {
   return (
-    <div className="tw-inline-flex tw-space-x-4 tw-items-center">
-      <div className="tw-inline-flex tw-space-x-2">
+    <div className="tw-flex tw-space-x-4 tw-items-center">
+      <div className="tw-flex tw-items-center tw-space-x-6">
         <CreateDropSelectFileImage onFileChange={onFileChange} />
         <CreateDropSelectFileVideo onFileChange={onFileChange} />
         <CreateDropSelectFileGLB onFileChange={onFileChange} />
         <CreateDropSelectFileAudio onFileChange={onFileChange} />
       </div>
       {file && (
-        <div>
-          {file.name} <button onClick={() => onFileChange(null)}>X</button>
+        <div className="tw-flex tw-items-center tw-gap-x-2">
+          <p className="tw-mb-0 tw-text-sm tw-font-normal tw-text-iron-50">
+            {file.name}
+          </p>
+          <button
+            onClick={() => onFileChange(null)}
+            type="button"
+            className="tw-h-8 tw-w-8 tw-flex tw-items-center tw-justify-center tw-bg-transparent tw-border-0 tw-rounded-full hover:tw-bg-iron-800"
+          >
+            <svg
+              className="tw-w-5 tw-h-5 tw-text-red"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M18 6L6 18M6 6L18 18"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
         </div>
       )}
     </div>
