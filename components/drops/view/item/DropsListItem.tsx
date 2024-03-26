8f0861747fb847bc4486557254f4a322dc2b0e77
Ragne <ragneinfo@gmail.com>
2024-03-26 13:38:57 +0200

    drops ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow info@analyticsflow.ee

--- components/drops/view/item/DropsListItem.tsx
+++ components/drops/view/item/DropsListItem.tsx
@@ -1,17 +1,21 @@
 import { DropFull } from "../../../../entities/IDrop";
-import CommonTimeAgo from "../../../utils/CommonTimeAgo";
+import { getTimeAgo } from "../../../../helpers/Helpers";
 import DropPfp from "../../create/utils/DropPfp";
 import DropListItemContent from "./content/DropListItemContent";
 
 export default function DropsListItem({ drop }: { readonly drop: DropFull }) {
   return (
-    <div className="tw-border-solid tw-border-[1px] tw-border-neutral-600 tw-rounded-md tw-p-4">
-      <div className="tw-inline-flex tw-justify-between tw-w-full">
-        <div className="tw-inline-flex tw-items-center">
+    <div className="tw-border-solid tw-border tw-border-iron-800 tw-rounded-lg tw-bg-iron-900 tw-p-6">
+      <div className="tw-flex tw-justify-between tw-w-full">
+        <div className="tw-inline-flex tw-items-center tw-gap-x-3">
           <DropPfp pfpUrl={drop.author.pfp} />
-          <div>{drop.author.handle}</div>
+          <p className="tw-mb-0 tw-text-base tw-font-semibold">
+            {drop.author.handle}
+          </p>
         </div>
-        <CommonTimeAgo timestamp={drop.created_at} />
+        <span className="tw-whitespace-nowrap tw-font-normal tw-text-sm tw-text-iron-500">
+          {getTimeAgo(drop.created_at)}
+        </span>
       </div>
       <DropListItemContent drop={drop} />
       <div className="tw-border-t tw-inline-flex tw-space-x-2">
