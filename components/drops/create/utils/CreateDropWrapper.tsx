7c62f34548acf9a5df041a375180b5f534c7b642
Simo <info@analyticsflow.ee>
2024-03-27 21:08:00 +0200

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/drops/create/utils/CreateDropWrapper.tsx
+++ components/drops/create/utils/CreateDropWrapper.tsx
@@ -47,7 +47,7 @@ export default function CreateDropWrapper({
     }
   }, [breakpoint]);
   const [viewType, setViewType] = useState<CreateDropViewType>(
-    CreateDropViewType.FULL
+    CreateDropViewType.COMPACT
   );
 
   const [editorState, setEditorState] = useState<EditorState | null>(null);
