0394f7113c33d1224c2f2ce6ab6d4b1e10a7421d
Ragne <ragneinfo@gmail.com>
2024-03-28 12:06:09 +0200

    drops ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/drops/view/item/discussion/DropListItemDiscussion.tsx
+++ components/drops/view/item/discussion/DropListItemDiscussion.tsx
@@ -2,8 +2,8 @@ import DropListItemDiscussionInput from "./DropListItemDiscussionInput";
 
 export default function DropListItemDiscussion() {
   return (
-    <div className="tw-w-full tw-pt-4 ">
-      WIP {/* <DropListItemDiscussionInput /> */}
+    <div className="tw-w-full tw-py-5 tw-px-4 sm:tw-px-5 tw-border-t tw-border-solid tw-border-b-0 tw-border-x-0 tw-border-iron-700">
+       <DropListItemDiscussionInput /> 
     </div>
   );
 }
