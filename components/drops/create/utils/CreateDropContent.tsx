e7ee5227423d84b53732749ebd8035c97b3216e0
Simo <info@analyticsflow.ee>
2024-03-28 13:39:42 +0200

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/drops/create/utils/CreateDropContent.tsx
+++ components/drops/create/utils/CreateDropContent.tsx
@@ -31,7 +31,7 @@ import { TableCellNode, TableNode, TableRowNode } from "@lexical/table";
 import { CodeHighlightNode, CodeNode } from "@lexical/code";
 import { AutoLinkNode, LinkNode } from "@lexical/link";
 import NFTPlugin from "../lexical/plugins/nft/NFTPlugin";
-import { NftNode } from "../lexical/nodes/NftNode";
+// import { NftNode } from "../lexical/nodes/NftNode";
 
 export default function CreateDropContent({
   viewType,
@@ -57,7 +57,7 @@ export default function CreateDropContent({
     nodes: [
       MentionNode,
       HashtagNode,
-      NftNode,
+      // NftNode,
       RootNode,
       HeadingNode,
       ListNode,
@@ -119,7 +119,7 @@ export default function CreateDropContent({
             <OnChangePlugin onChange={onEditorStateChange} />
             <NewMentionsPlugin onSelect={onMentionedUserAdded} />
             <NewHashtagsPlugin onSelect={onHashtagAdded} />
-            <NFTPlugin />
+            {/* <NFTPlugin /> */}
             {viewType === CreateDropViewType.COMPACT && <OneLinerPlugin />}
             <MaxLengthPlugin maxLength={25000} />
             {viewType === CreateDropViewType.COMPACT && (
