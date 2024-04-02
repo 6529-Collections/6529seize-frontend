d664b5b749276398465b7153c29982c3b24bc49d
prxt6529 <117092621+prxt6529@users.noreply.github.com>
2024-04-02 15:07:31 +0300

    Schema Rework follow up changes - NFT Tdh table (#350)
    Signed-off-by: prxt <prxt@6529.io>

--- pages/meme-lab/[id]/distribution.tsx
+++ pages/meme-lab/[id]/distribution.tsx
@@ -1,10 +1,12 @@
-import Head from "next/head";
 import styles from "../../../styles/Home.module.scss";
 
 import dynamic from "next/dynamic";
 import { MEMELAB_CONTRACT } from "../../../constants";
-import { fetchUrl } from "../../../services/6529api";
 import HeaderPlaceholder from "../../../components/header/HeaderPlaceholder";
+import {
+  SharedHead,
+  getSharedServerSideProps,
+} from "../../../components/the-memes/MemeShared";
 
 const Header = dynamic(() => import("../../../components/header/Header"), {
   ssr: false,
@@ -20,28 +22,14 @@ const DistributionComponent = dynamic(
 
 export default function MemeLabDistributionPage(props: any) {
   const pageProps = props.pageProps;
-  const pagenameFull = `${pageProps.name} | 6529 SEIZE`;
 
   return (
     <>
-      <Head>
-        <title>{pagenameFull}</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content={pagenameFull} />
-        <meta
-          property="og:url"
-          content={`${process.env.BASE_ENDPOINT}/meme-lab/${pageProps.id}/distribution`}
-        />
-        <meta property="og:title" content={pageProps.name} />
-        <meta property="og:image" content={pageProps.image} />
-        <meta property="og:description" content="6529 SEIZE" />
-        <meta name="twitter:card" content={pagenameFull} />
-        <meta name="twitter:image:alt" content={pageProps.name} />
-        <meta name="twitter:title" content={pageProps.name} />
-        <meta name="twitter:description" content="6529 SEIZE" />
-        <meta name="twitter:image" content={pageProps.image} />
-      </Head>
-
+      <SharedHead
+        props={pageProps}
+        contract={MEMELAB_CONTRACT}
+        isDistribution={true}
+      />
       <main className={styles.main}>
         <Header />
         <DistributionComponent
@@ -56,25 +44,5 @@ export default function MemeLabDistributionPage(props: any) {
 }
 
 export async function getServerSideProps(req: any, res: any, resolvedUrl: any) {
-  const id = req.query.id;
-  const response = await fetchUrl(
-    `${process.env.API_ENDPOINT}/api/nfts_memelab?contract=${MEMELAB_CONTRACT}&id=${id}`
-  );
-  let name = `Meme Lab Card #${id} Distribution`;
-  let image = `${process.env.BASE_ENDPOINT}/Seize_Logo_Glasses_2.png`;
-  if (response && response.data && response.data.length > 0) {
-    name = `${response.data[0].name} | ${name}`;
-    image = response.data[0].thumbnail
-      ? response.data[0].thumbnail
-      : response.data[0].image
-      ? response.data[0].image
-      : image;
-  }
-  return {
-    props: {
-      id: id,
-      name: name,
-      image: image,
-    },
-  };
+  return getSharedServerSideProps(req, MEMELAB_CONTRACT);
 }
