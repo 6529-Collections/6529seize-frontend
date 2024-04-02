6af7ec2a0fbe24837634a8e2be0b7047685c03db
prxt6529 <117092621+prxt6529@users.noreply.github.com>
2024-04-02 15:07:31 +0300

    Schema Rework follow up changes - NFT Tdh table (#350)
    Signed-off-by: prxt <prxt@6529.io>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- pages/the-memes/[id]/index.tsx
+++ pages/the-memes/[id]/index.tsx
@@ -1,11 +1,13 @@
-import Head from "next/head";
 import styles from "../../../styles/Home.module.scss";
 
 import dynamic from "next/dynamic";
-import { MEMES_CONTRACT } from "../../../constants";
-import { fetchUrl } from "../../../services/6529api";
 import HeaderPlaceholder from "../../../components/header/HeaderPlaceholder";
-import { useState } from "react";
+import {
+  SharedHead,
+  SharedHeadProps,
+  getSharedServerSideProps,
+} from "../../../components/the-memes/MemeShared";
+import { MEMES_CONTRACT } from "../../../constants";
 
 const Header = dynamic(() => import("../../../components/header/Header"), {
   ssr: false,
@@ -20,58 +22,19 @@ const MemePageComponent = dynamic(
 );
 
 export default function MemePage(props: any) {
-  const pageProps = props.pageProps;
-  const pagenameFull = `${pageProps.name} | 6529 SEIZE`;
-  const [connectedWallets, setConnectedWallets] = useState<string[]>([]);
+  const pageProps: SharedHeadProps = props.pageProps;
 
   return (
     <>
-      <Head>
-        <title>{pagenameFull}</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content={pagenameFull} />
-        <meta
-          property="og:url"
-          content={`${process.env.BASE_ENDPOINT}/the-memes/${pageProps.id}`}
-        />
-        <meta property="og:title" content={pageProps.name} />
-        <meta property="og:image" content={pageProps.image} />
-        <meta property="og:description" content="6529 SEIZE" />
-        <meta name="twitter:card" content={pagenameFull} />
-        <meta name="twitter:image:alt" content={pageProps.name} />
-        <meta name="twitter:title" content={pageProps.name} />
-        <meta name="twitter:description" content="6529 SEIZE" />
-        <meta name="twitter:image" content={pageProps.image} />
-      </Head>
-
+      <SharedHead props={pageProps} contract={MEMES_CONTRACT} />
       <main className={styles.main}>
-        <Header onSetWallets={(wallets) => setConnectedWallets(wallets)} />
-        <MemePageComponent wallets={connectedWallets} />
+        <Header />
+        <MemePageComponent />
       </main>
     </>
   );
 }
 
 export async function getServerSideProps(req: any, res: any, resolvedUrl: any) {
-  const id = req.query.id;
-  const response = await fetchUrl(
-    `${process.env.API_ENDPOINT}/api/nfts?contract=${MEMES_CONTRACT}&id=${id}`
-  );
-  let name = `Meme Card #${id}`;
-  let image = `${process.env.BASE_ENDPOINT}/Seize_Logo_Glasses_2.png`;
-  if (response && response.data && response.data.length > 0) {
-    name = `${response.data[0].name} | ${name}`;
-    image = response.data[0].thumbnail
-      ? response.data[0].thumbnail
-      : response.data[0].image
-      ? response.data[0].image
-      : image;
-  }
-  return {
-    props: {
-      id: id,
-      name: name,
-      image: image,
-    },
-  };
+  return getSharedServerSideProps(req, MEMES_CONTRACT);
 }
