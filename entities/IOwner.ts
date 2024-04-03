fcf4d41e8f9dcba93a1582c7a7344099ac0937b5
prxt6529 <117092621+prxt6529@users.noreply.github.com>
2024-04-03 11:04:47 +0300

    NFTOwners routes (#351)
    Signed-off-by: prxt <prxt@6529.io>
    
    Signed-off-by: Analytics Flow info@analyticsflow.ee

--- entities/IOwner.ts
+++ entities/IOwner.ts
@@ -1,20 +1,5 @@
-export interface OwnerLite {
-  readonly token_id: number;
-  readonly contract: string;
-  balance: number;
-}
-
-export interface Owner {
-  created_at: Date;
-  wallet: `0x${string}`;
-  wallet_display: string | undefined;
+export interface NftOwner {
   token_id: number;
   contract: string;
   balance: number;
-}
-
-export interface OwnerRank extends Owner {
-  tdh: number;
-  tdh__raw: number;
-  tdh_rank: number;
 }
