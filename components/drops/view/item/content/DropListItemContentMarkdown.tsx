9627ba370841bdb422f756238ba13c1635f727b1
Ragne <ragneinfo@gmail.com>
2024-04-09 12:44:44 +0300

    drops ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/drops/view/item/content/DropListItemContentMarkdown.tsx
+++ components/drops/view/item/content/DropListItemContentMarkdown.tsx
@@ -74,7 +74,7 @@ const DropListItemContentMarkdown = React.memo(
   ({ drop }: { readonly drop: DropFull }) => {
     const dropContent = drop.content?.slice(0, 1000);
     return (
-      <div>
+      <div className="tw-relative">
         <Markdown
           remarkPlugins={[remarkGfm]}
           className="tw-w-full"
@@ -123,7 +123,16 @@ const DropListItemContentMarkdown = React.memo(
         >
           {dropContent}
         </Markdown>
-        <div className=" tw-bg-iron-200">More</div>
+        <div className="tw-bg-gradient-to-t tw-from-iron-900 tw-h-48 tw-absolute tw-inset-x-0 tw-bottom-0">
+          <div className="tw-h-full tw-flex tw-flex-col tw-items-center tw-justify-end">
+            <button
+              type="button"
+              className="tw-relative tw-shadow tw-text-xs tw-font-semibold tw-inline-flex tw-items-center tw-rounded-lg tw-bg-iron-700 tw-px-2 tw-py-1.5 tw-text-iron-200 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset focus:tw-ring-primary-400 tw-border-0 tw-ring-1 tw-ring-inset tw-ring-iron-700 hover:tw-ring-iron-600 focus:tw-z-10 tw-transition tw-duration-300 tw-ease-out"
+            >
+              Show more
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
