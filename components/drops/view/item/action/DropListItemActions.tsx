c1813661943a43fc017a2a229b63f70ad5056614
Ragne <ragneinfo@gmail.com>
2024-04-09 13:52:41 +0300

    drops ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- components/drops/view/item/action/DropListItemActions.tsx
+++ components/drops/view/item/action/DropListItemActions.tsx
@@ -17,7 +17,7 @@ export default function DropListItemActions({
   };
 
   return (
-    <div className="tw-mt-4 sm:tw-ml-12 tw-border-t tw-flex tw-items-center tw-justify-between lg:tw-gap-x-8">
+    <div className="tw-relative tw-mt-4 sm:tw-ml-12 tw-border-t tw-flex tw-items-center tw-justify-between lg:tw-gap-x-8">
       <DropListItemActionsItemWrapper
         state={RepActionExpandable.DISCUSSION}
         activeState={state}
