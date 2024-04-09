6d445e5d05f62b14a422e28ce77e00d17ed7e6a1
Simo <info@analyticsflow.ee>
2024-04-09 16:45:40 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>
    
    Signed-off-by: Analytics Flow info@analyticsflow.ee

--- components/drops/create/utils/metadata/CreateDropMetadataItem.tsx
+++ components/drops/create/utils/metadata/CreateDropMetadataItem.tsx
@@ -8,28 +8,37 @@ export default function CreateDropMetadataItem({
   readonly onMetadataRemove: (data_key: string) => void;
 }) {
   return (
-    <button
-      onClick={() => onMetadataRemove(data_key)}
-      type="button"
-      className="tw-border-0 tw-group tw-inline-flex tw-items-center tw-justify-between tw-gap-x-2 tw-rounded-lg tw-px-3 tw-py-1 tw-text-sm tw-font-normal tw-text-iron-200 tw-bg-iron-400/10 tw-ring-1 tw-ring-inset tw-ring-iron-700 hover:tw-ring-iron-600 tw-transition tw-duration-300 tw-ease-out"
-    >
-      <span>
-        <b> {data_key}</b>: {data_value}
-      </span>
-      <svg
-        className="-tw-mr-0.5 tw-w-6 tw-h-6 tw-shrink-0 tw-text-red group-hover:tw-scale-110 tw-transition tw-duration-300 tw-ease-out"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M18 6L6 18M6 6L18 18"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    </button>
+    <div className="tw-mt-2 tw-w-full tw-px-4 tw-py-2 tw-ring-1 tw-ring-inset tw-ring-iron-700 hover:tw-ring-iron-600 tw-rounded-lg tw-transition tw-duration-300 tw-ease-out">
+      <div className="tw-flex tw-items-center tw-gap-x-3">
+        <div className="tw-flex tw-items-center tw-justify-between tw-w-full tw-gap-x-1 tw-truncate">
+          <p className="tw-mb-0 tw-max-w-xl tw-text-sm tw-font-medium tw-text-iron-50 tw-truncate">
+            <b> {data_key}</b>: {data_value}
+          </p>
+          <div className="tw-h-8 tw-w-8 tw-flex tw-items-center tw-justify-center tw-border-0 tw-rounded-full tw-bg-transparent hover:tw-bg-iron-800 tw-transition tw-duration-300 tw-ease-out">
+            <button
+              onClick={() => onMetadataRemove(data_key)}
+              aria-label="Remove file"
+              className="tw-group tw-bg-transparent tw-border-none"
+            >
+              <svg
+                className="tw-w-5 tw-h-5 tw-text-red group-hover:tw-scale-110 tw-transition tw-duration-300 tw-ease-out"
+                viewBox="0 0 24 24"
+                fill="none"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M18 6L6 18M6 6L18 18"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
