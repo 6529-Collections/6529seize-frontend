d12ab387f08fdabeccc9cac6038c0e8601abeead
Simo <info@analyticsflow.ee>
2024-04-09 12:20:12 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>
    
    Signed-off-by: Analytics Flow info@analyticsflow.ee

--- components/drops/view/item/content/DropListItemContentMarkdown.tsx
+++ components/drops/view/item/content/DropListItemContentMarkdown.tsx
@@ -72,54 +72,62 @@ const customRenderer = ({
 
 const DropListItemContentMarkdown = React.memo(
   ({ drop }: { readonly drop: DropFull }) => {
+    const dropContent = drop.content?.slice(0, 1000);
     return (
-      <Markdown
-        remarkPlugins={[remarkGfm]}
-        className="tw-w-full"
-        components={{
-          h5: (params) => (
-            <h5 className="tw-text-iron-50">
-              {customRenderer({ content: params.children, drop })}
-            </h5>
-          ),
-          h4: (params) => (
-            <h4 className="tw-text-iron-50">
-              {customRenderer({ content: params.children, drop })}
-            </h4>
-          ),
-          h3: (params) => (
-            <h3 className="tw-text-iron-50">
-              {customRenderer({ content: params.children, drop })}
-            </h3>
-          ),
-          h2: (params) => (
-            <h2 className="tw-text-iron-50">
-              {customRenderer({ content: params.children, drop })}
-            </h2>
-          ),
-          h1: (params) => (
-            <h1 className="tw-text-iron-50">
-              {customRenderer({ content: params.children, drop })}
-            </h1>
-          ),
-          p: (params) => (
-            <p className="last:tw-mb-0 tw-text-md tw-leading-6 tw-text-iron-50 tw-font-normal">
-              {customRenderer({ content: params.children, drop })}
-            </p>
-          ),
-          li: (params) => (
-            <li className="tw-text-iron-50">
-              {customRenderer({ content: params.children, drop })}
-            </li>
-          ),
-        }}
-      >
-        {drop.content}
-      </Markdown>
+      <div>
+        <Markdown
+          remarkPlugins={[remarkGfm]}
+          className="tw-w-full"
+          components={{
+            h5: (params) => (
+              <h5 className="tw-text-iron-50">
+                {customRenderer({ content: params.children, drop })}
+              </h5>
+            ),
+            h4: (params) => (
+              <h4 className="tw-text-iron-50">
+                {customRenderer({ content: params.children, drop })}
+              </h4>
+            ),
+            h3: (params) => (
+              <h3 className="tw-text-iron-50">
+                {customRenderer({ content: params.children, drop })}
+              </h3>
+            ),
+            h2: (params) => (
+              <h2 className="tw-text-iron-50">
+                {customRenderer({ content: params.children, drop })}
+              </h2>
+            ),
+            h1: (params) => (
+              <h1 className="tw-text-iron-50">
+                {customRenderer({ content: params.children, drop })}
+              </h1>
+            ),
+            p: (params) => (
+              <p className="last:tw-mb-0 tw-text-md tw-leading-6 tw-text-iron-50 tw-font-normal">
+                {customRenderer({ content: params.children, drop })}
+              </p>
+            ),
+            li: (params) => (
+              <li className="tw-text-iron-50">
+                {customRenderer({ content: params.children, drop })}
+              </li>
+            ),
+            code: (params) => (
+              <code className="tw-text-iron-50 tw-whitespace-pre-wrap">
+                {customRenderer({ content: params.children, drop })}
+              </code>
+            ),
+          }}
+        >
+          {dropContent}
+        </Markdown>
+        <div className=" tw-bg-iron-200">More</div>
+      </div>
     );
   }
 );
-
 
 DropListItemContentMarkdown.displayName = "DropListItemContentMarkdown";
 export default DropListItemContentMarkdown;
