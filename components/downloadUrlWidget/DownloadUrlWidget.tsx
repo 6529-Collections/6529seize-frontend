85aa497be26a650ef45d80173837af448bdba716
prxt6529 <117092621+prxt6529@users.noreply.github.com>
2024-04-15 16:01:27 +0300

    Meme subscriptions (#341)
    Signed-off-by: prxt <prxt@6529.io>

--- components/downloadUrlWidget/DownloadUrlWidget.tsx
+++ components/downloadUrlWidget/DownloadUrlWidget.tsx
@@ -1,9 +1,10 @@
 import styles from "./DownloadUrlWidget.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import useDownloader from "react-use-downloader";
-import { API_AUTH_COOKIE } from "../../constants";
+import { API_AUTH_COOKIE, WALLET_AUTH_COOKIE } from "../../constants";
 import Cookies from "js-cookie";
-import DotLoader from "../dotLoader/DotLoader";
+import { Spinner } from "../dotLoader/DotLoader";
+import { Button, Modal } from "react-bootstrap";
 
 interface Props {
   preview: string;
@@ -13,27 +14,59 @@ interface Props {
 
 export default function DownloadUrlWidget(props: Readonly<Props>) {
   const apiAuth = Cookies.get(API_AUTH_COOKIE);
-  let headers = {};
+  let headers: any = {};
   if (apiAuth) {
-    headers = { "x-6529-auth": apiAuth };
+    headers["x-6529-auth"] = apiAuth;
   }
-  const { size, elapsed, percentage, download, cancel, error, isInProgress } =
-    useDownloader({ headers });
 
-  function startDownload() {
+  const allowlistAuth = Cookies.get(WALLET_AUTH_COOKIE);
+  if (allowlistAuth) {
+    headers["Authorization"] = `Bearer ${allowlistAuth}`;
+  }
+  const { download, isInProgress } = useDownloader({ headers });
+
+  async function startDownload() {
     if (!isInProgress) {
-      download(props.url, `${props.name}`);
+      await download(props.url, `${props.name}`);
     }
   }
 
   return (
-    <span
+    <button
       className={styles.downloadUrlWidget}
-      onClick={() => startDownload()}
-      aria-disabled={isInProgress}>
-      <FontAwesomeIcon icon="download" />
-      {isInProgress ? `Downloading` : `Download`} {props.preview}
-      {isInProgress && <DotLoader />}
-    </span>
+      onClick={() => {
+        startDownload();
+      }}
+      disabled={isInProgress}>
+      {isInProgress ? <Spinner /> : <FontAwesomeIcon icon="download" />}
+      {isInProgress ? `Downloading` : props.preview}
+    </button>
+  );
+}
+
+function DownloadUrlWidgetConfirm(
+  props: Readonly<{
+    show: boolean;
+    confirm_info: string;
+    handleClose(): void;
+    download(): void;
+  }>
+) {
+  return (
+    <Modal show={props.show} onHide={props.handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Confirm Download Info</Modal.Title>
+      </Modal.Header>
+      <hr className="mb-0 mt-0" />
+      <Modal.Body>{props.confirm_info}</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={props.handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={props.download}>
+          Looks good
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
