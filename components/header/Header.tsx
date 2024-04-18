1caa661b3a13a6314b1d03389f6c5213a8dfc057
Brook Riggio <11095+brookr@users.noreply.github.com>
2024-04-18 01:51:56 -0700

    Adds page on Nakamoto Threshold (#381)
    Signed-off-by: brookr <11095+brookr@users.noreply.github.com>

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -523,6 +523,13 @@ export default function Header(props: Readonly<Props>) {
                 </Row>
                 <Row className="pt-3">
                   <Col>
+                    <Link href={`/about/${AboutSection.NAKAMOTO_THRESHOLD}`}>
+                      <h3>Nakamoto Threshold</h3>
+                    </Link>
+                  </Col>
+                </Row>
+                <Row className="pt-3">
+                  <Col>
                     <Link href={`/about/${AboutSection.LICENSE}`}>
                       <h3>License</h3>
                     </Link>
@@ -869,6 +876,12 @@ export default function Header(props: Readonly<Props>) {
                                 link={{
                                   name: "Minting",
                                   path: `/about/${AboutSection.MINTING}`,
+                                }}
+                              />
+                              <HeaderDesktopLink
+                                link={{
+                                  name: "Nakamoto Threshold",
+                                  path: `/about/${AboutSection.NAKAMOTO_THRESHOLD}`,
                                 }}
                               />
                               <HeaderDesktopLink
