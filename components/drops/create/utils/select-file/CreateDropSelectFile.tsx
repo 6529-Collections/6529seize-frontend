43cb6587571bbebd7b732092760ccbb94a66cf2c
Ragne <ragneinfo@gmail.com>
2024-04-18 15:39:00 +0300

    drops ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow info@analyticsflow.ee

--- components/drops/create/utils/select-file/CreateDropSelectFile.tsx
+++ components/drops/create/utils/select-file/CreateDropSelectFile.tsx
@@ -23,37 +23,44 @@ export default function CreateDropSelectFile({
       </div>
 
       {file && (
-        <div className="tw-mt-2 tw-w-full tw-px-4 tw-py-2 tw-ring-1 tw-ring-inset tw-ring-iron-700 hover:tw-ring-iron-600 tw-rounded-lg tw-transition tw-duration-300 tw-ease-out">
-          <div className="tw-flex tw-items-center tw-gap-x-3">
-            <CreateDropSelectedFileIcon file={file} />
-            <div className="tw-flex tw-items-center tw-justify-between tw-w-full tw-gap-x-1 tw-truncate">
-              <p className="tw-mb-0 tw-max-w-xl tw-text-sm tw-font-medium tw-text-iron-50 tw-truncate">
-                {file.name}
-              </p>
-              <div className="tw-h-8 tw-w-8 tw-flex tw-items-center tw-justify-center tw-border-0 tw-rounded-full tw-bg-transparent hover:tw-bg-iron-800 tw-transition tw-duration-300 tw-ease-out">
-                <button
-                  onClick={() => onFileChange(null)}
-                  aria-label="Remove file"
-                  className="tw-group tw-bg-transparent tw-border-none"
+        <div className="tw-mt-2 tw-w-full tw-px-2 tw-py-2 tw-ring-1 tw-ring-inset tw-ring-iron-700  tw-bg-iron-800 tw-rounded-lg tw-transition tw-duration-300 tw-ease-out">
+          <div className="tw-flex tw-items-center tw-justify-between tw-w-full tw-gap-x-1 tw-truncate">
+            <p className="tw-mb-0 tw-max-w-xl tw-text-sm tw-font-medium tw-text-iron-50 tw-truncate">
+              {file.name}
+            </p>
+            <div className="tw-h-8 tw-w-8 tw-flex tw-items-center tw-justify-center tw-border-0 tw-rounded-full tw-bg-transparent hover:tw-bg-iron-800 tw-transition tw-duration-300 tw-ease-out">
+              <button
+                onClick={() => onFileChange(null)}
+                aria-label="Remove file"
+                className="tw-group tw-bg-transparent tw-border-none"
+              >
+                <svg
+                  className="tw-w-5 tw-h-5 tw-text-red group-hover:tw-scale-110 tw-transition tw-duration-300 tw-ease-out"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <svg
-                    className="tw-w-5 tw-h-5 tw-text-red group-hover:tw-scale-110 tw-transition tw-duration-300 tw-ease-out"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M18 6L6 18M6 6L18 18"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </button>
-              </div>
+                  <path
+                    d="M18 6L6 18M6 6L18 18"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </button>
             </div>
+          </div>
+          <div className="tw-mt-2 tw-flex tw-gap-x-3">
+            <div className="tw-h-full tw-w-full">
+              <img
+                src="https://d3lqz0a4bldqgf.cloudfront.net/drops/author_7c6c885e-87b2-11ee-9661-02424e2c14ad/9aecc1ce-14ba-4266-8644-3aca20639bc6.gif"
+                alt=""
+                className="tw-w-full tw-h-full tw-object-center tw-object-contain"
+              />
+            </div>
+            {/*   <CreateDropSelectedFileIcon file={file} /> */}
           </div>
         </div>
       )}
