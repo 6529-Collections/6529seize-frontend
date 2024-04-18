88475b41ae1080d6c14bf7aa9de982ce1706b36d
Simo <info@analyticsflow.ee>
2024-04-18 10:59:51 +0300

    search modal image as memo
    Signed-off-by: Simo <info@analyticsflow.ee>

---
+++ components/header/header-search/HeaderSearchModalItemMedia.tsx
@@ -0,0 +1,24 @@
+import { memo } from "react";
+import { NFTSearchResult } from "./HeaderSearchModalItem";
+import Image from "next/image";
+const HeaderSearchModalItemMedia = memo(
+  ({ nft }: { readonly nft: NFTSearchResult }) => {
+    const imgSrc = nft.icon_url ?? nft.thumbnail_url ?? nft.image_url;
+    if (!imgSrc) {
+      return <></>;
+    }
+    return (
+      <Image
+        priority
+        loading="eager"
+        width={0}
+        height={25}
+        style={{ height: "25px", width: "auto" }}
+        src={imgSrc}
+        alt={nft.name ?? `#${nft.id}`}
+      />
+    );
+  }
+);
+
+export default HeaderSearchModalItemMedia;
