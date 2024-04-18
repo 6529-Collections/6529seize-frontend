4e2f4c1f334056c2edce0c8b941d6be4a4b3cba9
prxt6529 <117092621+prxt6529@users.noreply.github.com>
2024-04-18 09:33:11 +0300

    Show airdrop address in subscriptions (#379)
    Signed-off-by: prxt <prxt@6529.io>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- components/delegation/NewDelegation.tsx
+++ components/delegation/NewDelegation.tsx
@@ -30,6 +30,10 @@ interface Props {
     collection: DelegationCollection;
   };
   ens: string | null | undefined;
+  collection_query?: string;
+  setCollectionQuery?(collection: string): any;
+  use_case_query?: number;
+  setUseCaseQuery?(useCase: number): any;
   onHide(): any;
   onSetToast(toast: any): any;
 }
@@ -44,9 +48,11 @@ export default function NewDelegationComponent(props: Readonly<Props>) {
   const [newDelegationToken, setNewDelegationToken] = useState<
     number | undefined
   >(undefined);
-  const [newDelegationUseCase, setNewDelegationUseCase] = useState<number>(0);
+  const [newDelegationUseCase, setNewDelegationUseCase] = useState<number>(
+    props.use_case_query ?? 0
+  );
   const [newDelegationCollection, setNewDelegationCollection] =
-    useState<string>("0");
+    useState<string>(props.collection_query ?? "0");
 
   const [newDelegationToAddress, setNewDelegationToAddress] = useState("");
 
@@ -178,7 +184,12 @@ export default function NewDelegationComponent(props: Readonly<Props>) {
             </Form.Group>
             <DelegationFormCollectionFormGroup
               collection={newDelegationCollection}
-              setCollection={setNewDelegationCollection}
+              setCollection={(c: string) => {
+                setNewDelegationCollection(c);
+                if (props.setCollectionQuery) {
+                  props.setCollectionQuery(c);
+                }
+              }}
               subdelegation={props.subdelegation}
             />
             <DelegationFormDelegateAddressFormGroup
@@ -198,6 +209,9 @@ export default function NewDelegationComponent(props: Readonly<Props>) {
                   onChange={(e) => {
                     const newCase = parseInt(e.target.value);
                     setNewDelegationUseCase(newCase);
+                    if (props.setUseCaseQuery) {
+                      props.setUseCaseQuery(newCase);
+                    }
                     clearErrors();
                   }}>
                   <option value={0} disabled>
