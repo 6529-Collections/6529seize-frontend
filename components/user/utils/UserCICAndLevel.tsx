0ac3ebb87cb2c0b3eb0e66e712ca84615bbf9dbf
Ragne <ragneinfo@gmail.com>
2024-04-23 17:38:41 +0300

    drops ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- components/user/utils/UserCICAndLevel.tsx
+++ components/user/utils/UserCICAndLevel.tsx
@@ -9,6 +9,14 @@ export default function UserCICAndLevel({
   readonly cicType: CICType;
 }) {
   return (
+    /*  <div className="tw-relative">
+      <div className="tw-flex tw-items-center tw-justify-center tw-h-4 tw-w-4 tw-text-[9px] tw-leading-3 tw-font-bold tw-rounded-full tw-ring-2 tw-ring-iron-300 tw-text-iron-300">
+        {level}
+      </div>
+      <span
+        className={`tw-flex-shrink-0 tw-absolute -tw-right-1 -tw-top-[0.1875rem] tw-block tw-h-2 tw-w-2 tw-rounded-full ${CIC_COLOR[cicType]}`}
+      ></span>
+    </div> */
     <div className="tw-relative">
       <div className="tw-flex tw-items-center tw-justify-center tw-h-5 tw-w-5 tw-text-[0.625rem] tw-leading-3 tw-font-bold tw-rounded-full tw-ring-2 tw-ring-iron-300 tw-text-iron-300">
         {level}
