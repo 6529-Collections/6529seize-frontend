391dd98fd9217cae9aa0cddd6c4f787a127591a5
Ragne <ragneinfo@gmail.com>
2024-05-02 14:43:32 +0300

    drops steps ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>

---
+++ components/waves/WavesOutcome.tsx
@@ -0,0 +1,28 @@
+import WavesOutcome6529ActionsOptions from "./WavesOutcome6529ActionsOptions";
+import WavesOutcomeAppsOptions from "./WavesOutcomeAppsOptions";
+import WavesOutcomeGeneralDataOptions from "./WavesOutcomeGeneralDataOptions";
+import WavesOutcomeManualOptions from "./WavesOutcomeManualOptions";
+import WavesOutcomeOnChainOptions from "./WavesOutcomeOnChainOptions";
+import WavesOutcomeSocialMediaOptions from "./WavesOutcomeSocialMediaOptions";
+import WavesOutcomeTabs from "./WavesOutcomeTabs";
+
+export default function WavesOutcome() {
+  return (
+    <div className="tw-max-w-[40rem] tw-mx-auto tw-w-full">
+      <p className="tw-mb-0 tw-text-2xl tw-font-bold tw-text-iron-50">
+        Actions
+      </p>
+      <div className="tw-mt-6">
+        <WavesOutcomeTabs />
+      </div>
+      <div className="tw-mt-6">
+        <WavesOutcomeManualOptions />
+        {/*   <WavesOutcome6529ActionsOptions /> */}
+        {/*  <WavesOutcomeSocialMediaOptions /> */}
+        {/* <WavesOutcomeOnChainOptions /> */}
+        {/* <WavesOutcomeGeneralDataOptions /> */}
+        {/*  <WavesOutcomeAppsOptions /> */}
+      </div>
+    </div>
+  );
+}
---
+++ components/waves/WavesOutcomeOnChainOptions.tsx
@@ -0,0 +1,50 @@
+export default function WavesOutcomeOnChainOptions() {
+  return (
+    <div className="tw-grid tw-grid-cols-3 tw-gap-x-4 tw-gap-y-4">
+      <div className="tw-relative tw-block tw-cursor-pointer tw-rounded-lg tw-ring-1 tw-ring-inset tw-ring-iron-700 tw-bg-[#232329] tw-opacity-60 tw-px-6 tw-py-5 tw-shadow-sm focus:tw-outline-none sm:tw-flex sm:tw-items-center tw-gap-x-3">
+        <input
+          type="radio"
+          className="tw-h-5 tw-w-5 tw-border-iron-600 tw-text-primary-500 focus:tw-ring-primary-500"
+        />
+        <span className="tw-flex tw-items-center">
+          <span className="tw-flex tw-flex-col tw-text-sm">
+            <span className="tw-font-semibold tw-text-iron-600">Ethereum</span>
+          </span>
+        </span>
+      </div>
+      <div className="tw-relative tw-block tw-cursor-pointer tw-rounded-lg tw-ring-1 tw-ring-inset tw-ring-iron-700 tw-bg-[#232329] tw-opacity-60 tw-px-6 tw-py-5 tw-shadow-sm focus:tw-outline-none sm:tw-flex sm:tw-items-center tw-gap-x-3">
+        <input
+          type="radio"
+          className="tw-h-5 tw-w-5 tw-border-iron-600 tw-text-primary-500 focus:tw-ring-primary-500"
+        />
+        <span className="tw-flex tw-items-center">
+          <span className="tw-flex tw-flex-col tw-text-sm">
+            <span className="tw-font-semibold tw-text-iron-600">Base</span>
+          </span>
+        </span>
+      </div>
+      <div className="tw-relative tw-block tw-cursor-pointer tw-rounded-lg tw-ring-1 tw-ring-inset tw-ring-iron-700 tw-bg-[#232329] tw-opacity-60 tw-px-6 tw-py-5 tw-shadow-sm focus:tw-outline-none sm:tw-flex sm:tw-items-center tw-gap-x-3">
+        <input
+          type="radio"
+          className="tw-h-5 tw-w-5 tw-border-iron-600 tw-text-primary-500 focus:tw-ring-primary-500"
+        />
+        <span className="tw-flex tw-items-center">
+          <span className="tw-flex tw-flex-col tw-text-sm">
+            <span className="tw-font-semibold tw-text-iron-600">BTC</span>
+          </span>
+        </span>
+      </div>
+      <div className="tw-relative tw-block tw-cursor-pointer tw-rounded-lg tw-ring-1 tw-ring-inset tw-ring-iron-700 tw-bg-[#232329] tw-opacity-60 tw-px-6 tw-py-5 tw-shadow-sm focus:tw-outline-none sm:tw-flex sm:tw-items-center tw-gap-x-3">
+        <input
+          type="radio"
+          className="tw-h-5 tw-w-5 tw-border-iron-600 tw-text-primary-500 focus:tw-ring-primary-500"
+        />
+        <span className="tw-flex tw-items-center">
+          <span className="tw-flex tw-flex-col tw-text-sm">
+            <span className="tw-font-semibold tw-text-iron-600">Solana</span>
+          </span>
+        </span>
+      </div>
+    </div>
+  );
+}
