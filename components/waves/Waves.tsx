d9f21a478e45534592b1b7bd4f830023caf8e85e
Ragne <ragneinfo@gmail.com>
2024-05-02 14:43:32 +0300

    drops steps ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- components/waves/Waves.tsx
+++ components/waves/Waves.tsx
@@ -3,6 +3,8 @@ import WavesMainSteps from "./WavesMainSteps";
 import WavesOverview from "./WavesOverview";
 import WavesRating from "./WavesRating";
 import WavesVisibility from "./WavesVisibility";
+import WavesPeriod from "./WavesPeriod";
+import WavesOutcome from "./WavesOutcome";
 
 export default function Waves() {
   return (
@@ -10,10 +12,12 @@ export default function Waves() {
       <div className="tw-grid tw-grid-cols-12 tw-min-h-screen tw-w-full">
         <WavesMainSteps />
         <div className="tw-relative tw-bg-[#1C1D20] tw-w-full tw-rounded-l-[40px] tw-p-8 tw-col-span-9">
-          <WavesOverview />
+          {/*    <WavesOverview /> */}
           {/*   <WavesVisibility /> */}
           {/*  <WavesDrops /> */}
           {/*  <WavesRating /> */}
+          {/*  <WavesPeriod /> */}
+          <WavesOutcome />
         </div>
       </div>
     </div>
