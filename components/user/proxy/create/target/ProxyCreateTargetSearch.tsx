d74cd2d9357b781dd0b8875acdc2f085a1d67ddb
Ragne <ragneinfo@gmail.com>
2024-05-08 12:55:09 +0300

    proxy ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/user/proxy/create/target/ProxyCreateTargetSearch.tsx
+++ components/user/proxy/create/target/ProxyCreateTargetSearch.tsx
@@ -48,35 +48,44 @@ export default function ProxyCreateTargetSearch({
 
   return (
     <Combobox as="div" value={null} onChange={onTargetSelect}>
-      <Combobox.Label className="tw-block tw-text-sm tw-font-medium tw-leading-6 tw-text-gray-900">
+      <Combobox.Label className="mb-0 tw-text-lg tw-font-semibold tw-text-iron-50">
         Assigned to
       </Combobox.Label>
-      <div className="tw-relative tw-mt-2">
+      <div className="tw-relative tw-max-w-md tw-mt-2 lg:tw-mt-4">
         <Combobox.Input
-          className="tw-w-full tw-rounded-md tw-border-0 tw-bg-white tw-py-1.5 tw-pl-3 tw-pr-12 tw-text-gray-900 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-gray-300 focus:tw-ring-2 focus:tw-ring-inset focus:tw-ing-indigo-600 sm:tw-text-sm sm:tw-leading-6"
+          className="tw-form-input tw-block tw-w-full tw-rounded-lg tw-border-0 tw-py-3 tw-pl-11 tw-pr-4 tw-bg-iron-900 tw-text-iron-50 tw-font-normal tw-caret-primary-400 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-iron-700 hover:tw-ring-iron-600 placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-bg-transparent focus:tw-ring-1 focus:tw-ring-inset  focus:tw-ring-primary-400 tw-text-base sm:text-sm tw-transition tw-duration-300 tw-ease-out"
+          placeholder="Search profile"
           onChange={(event) => setQuery(event.target.value)}
           displayValue={(person: any) => person?.handle}
         />
-        <Combobox.Button className="tw-absolute tw-inset-y-0 tw-right-0 tw-flex tw-items-center tw-rounded-r-md tw-px-2 focus:tw-outline-none">
-          chev up icon
-        </Combobox.Button>
-
+        <svg
+          className="tw-pointer-events-none tw-absolute tw-left-4 tw-top-3.5 tw-h-5 tw-w-5 tw-text-iron-300"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
         {profiles && profiles.length > 0 && (
-          <Combobox.Options className="tw-absolute tw-z-10 tw-mt-1 tw-max-h-56 tw-w-full tw-overflow-auto tw-rounded-md tw-bg-white tw-py-1 tw-text-base tw-shadow-lg tw-ring-1 tw-ring-black tw-ring-opacity-5 focus:tw-outline-none sm:tw-text-sm">
+          <Combobox.Options className="tw-list-none tw-px-2 tw-absolute tw-z-10 tw-mt-1 tw-max-h-56 tw-w-full tw-overflow-auto tw-rounded-md tw-bg-iron-800 tw-py-1 tw-text-base tw-shadow-lg tw-ring-1 tw-ring-white/10 tw-ring-opacity-5 focus:tw-outline-none sm:tw-text-sm">
             {profiles.map((profile) => (
               <Combobox.Option
                 key={profile.handle}
                 value={profile}
                 className={({ active }) =>
                   classNames(
-                    "tw-relative tw-cursor-default tw-select-none tw-py-2 tw-pl-3 tw-pr-9",
+                    "tw-relative tw-cursor-pointer tw-select-none tw-rounded-lg tw-py-2 tw-px-2 tw-transition tw-duration-300 tw-ease-out",
                     active
-                      ? "tw-bg-indigo-600 tw-text-white"
-                      : "tw-text-gray-900"
+                      ? "tw-bg-iron-700 tw-text-iron-50 tw-font-medium"
+                      : ""
                   )
                 }
               >
-                {({ active, selected }) => (
+                {({ selected }) => (
                   <>
                     <div className="tw-flex tw-items-center">
                       <img
@@ -93,17 +102,6 @@ export default function ProxyCreateTargetSearch({
                         {profile.handle}
                       </span>
                     </div>
-
-                    {selected && (
-                      <span
-                        className={classNames(
-                          "tw-absolute tw-inset-y-0 tw-right-0 tw-flex tw-items-center tw-pr-4",
-                          active ? "tw-text-white" : "tw-text-indigo-600"
-                        )}
-                      >
-                        check icon
-                      </span>
-                    )}
                   </>
                 )}
               </Combobox.Option>
