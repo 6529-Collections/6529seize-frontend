38321f1b93c9b99b62a4500c17345074310e7b7f
Ragne <ragneinfo@gmail.com>
2024-05-10 11:39:10 +0300

    proxy ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow info@analyticsflow.ee

--- components/user/proxy/proxy/action/utils/credit/ProfileProxyCreditView.tsx
+++ components/user/proxy/proxy/action/utils/credit/ProfileProxyCreditView.tsx
@@ -16,17 +16,19 @@ export default function ProfileProxyCreditView({
     }
   };
   return (
-    <div className="tw-inline-flex">
-      <div className="tw-text-base tw-font-normal tw-text-iron-400">
-        <p className="tw-mb-0 tw-space-x-1 tw-whitespace-nowrap">
-          <span className="tw-text-iron-300 tw-font-medium">
-            {profileProxyAction.credit_amount}
-          </span>
-        </p>
-      </div>
+    <div className="tw-flex">
+      <p className="tw-flex tw-items-center tw-mb-0 tw-gap-x-2 tw-text-base tw-font-normal tw-leading-6 tw-text-iron-500">
+        <span className="tw-text-base tw-font-normal tw-text-iron-500">
+          Credit:
+        </span>
+        <span className="tw-text-iron-300 tw-font-medium">
+          {profileProxyAction.credit_amount}
+        </span>
+      </p>
       {isOwner && (
         <button
-          className="tw-bg-transparent tw-border-none"
+          type="button"
+          className="tw-group tw-bg-transparent tw-border-0 tw-h-8 tw-w-8 tw-flex tw-items-center tw-justify-center tw-rounded-full tw-text-iron-300 hover:tw-text-iron-400 tw-transition tw-duration-300 tw-ease-out"
           onClick={onEditMode}
         >
           <PencilIcon />
--- components/user/proxy/proxy/create-action/config/ProxyCreateActionConfigCreateDropToWave.tsx
+++ components/user/proxy/proxy/create-action/config/ProxyCreateActionConfigCreateDropToWave.tsx
@@ -7,11 +7,19 @@ export default function ProxyCreateActionConfigCreateDropToWave({
 }: {
   readonly endTime: number | null;
   readonly onSubmit: (action: CreateProxyCreateDropToWaveAction) => void;
-  }) {
+}) {
   const handleSubmit = () =>
     onSubmit({
       action_type: ProfileProxyActionType.CreateDropToWave,
       end_time: endTime,
     });
-  return <button onClick={handleSubmit}>SAVE</button>;
+  return (
+    <button
+      type="button"
+      onClick={handleSubmit}
+      className="tw-bg-primary-500 tw-px-3 tw-py-2 tw-text-sm tw-leading-5 tw-font-semibold tw-text-white tw-border tw-border-solid tw-border-primary-500 tw-rounded-lg hover:tw-bg-primary-600 hover:tw-border-primary-600 tw-transition tw-duration-300 tw-ease-out"
+    >
+      Save
+    </button>
+  );
 }
