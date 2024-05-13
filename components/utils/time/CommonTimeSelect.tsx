1c0097809e8556045c24dedfc7d54e50e454ea1b
Simo <info@analyticsflow.ee>
2024-05-13 17:26:04 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- components/utils/time/CommonTimeSelect.tsx
+++ components/utils/time/CommonTimeSelect.tsx
@@ -24,10 +24,12 @@ export default function CommonTimeSelect({
   currentTime,
   onMillis,
   disabled = false,
+  inline = false,
 }: {
   readonly currentTime: number | null;
   readonly onMillis: (millis: number) => void;
   readonly disabled?: boolean;
+  readonly inline?: boolean;
 }) {
   const hours = currentTime
     ? +Time.millis(currentTime).diffFromNow().toHours().toFixed(0)
@@ -100,32 +102,65 @@ export default function CommonTimeSelect({
             setSelected={setTimeMode}
           />
         </div>
+        {inline && (
+          <div
+            className={`${
+              disabled ? "tw-opacity-50 tw-hidden" : ""
+            }  tw-flex tw-items-center tw-gap-x-1.5`}
+          >
+            <svg
+              className="tw-h-4 tw-w-4 tw-text-iron-400"
+              viewBox="0 0 24 24"
+              fill="none"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12 6V12L16 14M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <span className="tw-text-iron-400 tw-text-xs tw-font-medium tw-space-x-1">
+              <span>Expires At:</span>
+              <span className="tw-text-iron-300 tw-font-semibold">
+                {expiresAt}
+              </span>
+            </span>
+          </div>
+        )}
       </div>
-      <div
-        className={`${
-          disabled ? "tw-opacity-50 tw-hidden" : ""
-        }  tw-flex tw-items-center tw-gap-x-1.5`}
-      >
-        <svg
-          className="tw-h-4 tw-w-4 tw-text-iron-400"
-          viewBox="0 0 24 24"
-          fill="none"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
+      {!inline && (
+        <div
+          className={`${
+            disabled ? "tw-opacity-50 tw-hidden" : ""
+          }  tw-flex tw-items-center tw-gap-x-1.5`}
         >
-          <path
-            d="M12 6V12L16 14M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-        <span className="tw-text-iron-400 tw-text-xs tw-font-medium tw-space-x-1">
-          <span>Expires At:</span>
-          <span className="tw-text-iron-300 tw-font-semibold">{expiresAt}</span>
-        </span>
-      </div>
+          <svg
+            className="tw-h-4 tw-w-4 tw-text-iron-400"
+            viewBox="0 0 24 24"
+            fill="none"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 6V12L16 14M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <span className="tw-text-iron-400 tw-text-xs tw-font-medium tw-space-x-1">
+            <span>Expires At:</span>
+            <span className="tw-text-iron-300 tw-font-semibold">
+              {expiresAt}
+            </span>
+          </span>
+        </div>
+      )}
     </div>
   );
 }
