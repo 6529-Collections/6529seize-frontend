3eefcaa7c678118b0f1f7be4e608717bbda7daa2
Simo <info@analyticsflow.ee>
2024-05-13 15:45:53 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/user/rep/modify-rep/UserPageRepModifyModalRaterStats.tsx
+++ components/user/rep/modify-rep/UserPageRepModifyModalRaterStats.tsx
@@ -12,6 +12,7 @@ export default function UserPageRepModifyModalRaterStats({
   readonly giverAvailableRep: number;
 }) {
   const { activeProfileProxy } = useContext(AuthContext);
+  
   return (
     <div className="tw-mt-6 sm:tw-mt-8">
       <div className="tw-flex tw-flex-col tw-space-y-1">
@@ -31,15 +32,36 @@ export default function UserPageRepModifyModalRaterStats({
             {formatNumberWithCommas(giverAvailableRep)}
           </span>
         </span>
-        <span className="tw-text-sm tw-block tw-text-iron-300 tw-font-normal">
-          <span>Your max/min Rep Rating to {repState.category}:</span>
-          <span className="tw-ml-1 tw-font-semibold tw-text-iron-50">
-            +/-{" "}
-            {formatNumberWithCommas(
-              giverAvailableRep + Math.abs(repState.rater_contribution)
-            )}
+        {!!activeProfileProxy ? (
+          <>
+            <span className="tw-text-sm tw-block tw-text-iron-300 tw-font-normal">
+              <span>Your max Rep Rating to {repState.category}:</span>
+              <span className="tw-ml-1 tw-font-semibold tw-text-iron-50">
+                {formatNumberWithCommas(
+                  repState.rater_contribution + giverAvailableRep
+                )}
+              </span>
+            </span>
+            <span className="tw-text-sm tw-block tw-text-iron-300 tw-font-normal">
+              <span>Your min Rep Rating to {repState.category}:</span>
+              <span className="tw-ml-1 tw-font-semibold tw-text-iron-50">
+                {formatNumberWithCommas(
+                  repState.rater_contribution - giverAvailableRep
+                )}
+              </span>
+            </span>
+          </>
+        ) : (
+          <span className="tw-text-sm tw-block tw-text-iron-300 tw-font-normal">
+            <span>Your max/min Rep Rating to {repState.category}:</span>
+            <span className="tw-ml-1 tw-font-semibold tw-text-iron-50">
+              +/-{" "}
+              {formatNumberWithCommas(
+                giverAvailableRep + Math.abs(repState.rater_contribution)
+              )}
+            </span>
           </span>
-        </span>
+        )}
       </div>
     </div>
   );
