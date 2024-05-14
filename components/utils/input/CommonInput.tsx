84531a24132385961ddfc340f6034a95b1b81c3b
Ragne <ragneinfo@gmail.com>
2024-05-14 13:12:18 +0300

    proxy ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/utils/input/CommonInput.tsx
+++ components/utils/input/CommonInput.tsx
@@ -102,7 +102,7 @@ export default function CommonInput(props: NumberInputProps | TextInputProps) {
           disabled ? "tw-opacity-40" : ""
         } ${theme === "dark" ? "tw-bg-iron-900" : "tw-bg-iron-800"} ${
           size === "md" ? "tw-py-2.5" : "tw-py-2"
-        } tw-form-input tw-appearance-none tw-block tw-w-full tw-rounded-lg tw-border-0  tw-pr-3 tw-text-iron-50 tw-font-normal tw-caret-primary-400 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-iron-700 hover:tw-ring-iron-600 placeholder:tw-text-iron-400 focus:tw-outline-none focus:tw-bg-transparent focus:tw-ring-1 focus:tw-ring-inset focus:tw-ring-primary-400 tw-text-base sm:tw-leading-6 tw-transition tw-duration-300 tw-ease-out`}
+        } tw-form-input tw-appearance-none tw-block tw-w-full tw-rounded-lg tw-border-0 tw-pr-3 tw-text-iron-50 tw-font-normal tw-caret-primary-400 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-iron-700 hover:tw-ring-iron-600 placeholder:tw-text-iron-400 focus:tw-outline-none focus:tw-bg-transparent focus:tw-ring-1 focus:tw-ring-inset focus:tw-ring-primary-400 tw-text-base sm:tw-leading-6 tw-transition tw-duration-300 tw-ease-out`}
       />
     </div>
   );
