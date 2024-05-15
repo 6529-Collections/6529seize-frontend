371c168ca4259ea32766bb8b32a35093b66b484a
Ragne <ragneinfo@gmail.com>
2024-05-15 11:38:32 +0300

    proxy ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- components/user/proxy/create/target/ProxyCreateTargetSearch.tsx
+++ components/user/proxy/create/target/ProxyCreateTargetSearch.tsx
@@ -93,9 +93,9 @@ export default function ProxyCreateTargetSearch({
           </div>
         </div>
       ) : (
-        <div className="tw-relative tw-mt-4">
+        <div className="tw-relative tw-mt-4 tw-max-w-xs">
           <Combobox.Input
-            className="tw-form-input tw-block tw-w-72 tw-rounded-lg tw-border-0 tw-py-3 tw-pl-11 tw-pr-4 tw-bg-iron-900 tw-text-iron-50 tw-font-normal tw-caret-primary-400 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-iron-700 hover:tw-ring-iron-600 placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-bg-iron-900 active:tw-fill-none focus:tw-ring-1 focus:tw-ring-inset  focus:tw-ring-primary-400 tw-text-base sm:text-sm tw-transition tw-duration-300 tw-ease-out"
+            className="tw-form-input tw-block tw-w-80 tw-rounded-lg tw-border-0 tw-py-3 tw-pl-11 tw-pr-4 tw-bg-iron-900 tw-text-iron-50 tw-font-normal tw-caret-primary-400 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-iron-700 hover:tw-ring-iron-600 placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-bg-iron-900 active:tw-fill-none focus:tw-ring-1 focus:tw-ring-inset  focus:tw-ring-primary-400 tw-text-base sm:text-sm tw-transition tw-duration-300 tw-ease-out"
             placeholder="Search profile"
             autoComplete="off"
             onChange={(event) => setQuery(event.target.value)}
