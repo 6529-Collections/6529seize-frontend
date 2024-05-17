72e49eb578e3b33f9d5e669ae11614189be9dbea
Simo <info@analyticsflow.ee>
2024-05-17 14:34:30 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

---
+++ helpers/profile-logs.helpers.ts
@@ -0,0 +1,24 @@
+import { ProfileActivityLogType } from "../entities/IProfile";
+
+const DISABLED_LOG_TYPES = [
+  ProfileActivityLogType.DROP_COMMENT,
+  ProfileActivityLogType.DROP_RATING_EDIT,
+  ProfileActivityLogType.DROP_CREATED,
+  ProfileActivityLogType.PROXY_RATING_EDIT,
+];
+export const getProfileLogTypes = ({
+  logTypes,
+  addDisabledLogTypes = [],
+}: {
+  readonly logTypes: ProfileActivityLogType[];
+  readonly addDisabledLogTypes?: ProfileActivityLogType[];
+}): ProfileActivityLogType[] => {
+  const disabledTypes = DISABLED_LOG_TYPES.filter(
+    (logType) => !addDisabledLogTypes.includes(logType)
+  );
+  return logTypes.length
+    ? logTypes
+    : Object.values(ProfileActivityLogType).filter(
+        (logType) => !disabledTypes.includes(logType)
+      );
+};
