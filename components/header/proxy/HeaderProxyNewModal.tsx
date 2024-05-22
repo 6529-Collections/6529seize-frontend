72db9bca73b145ef291ebaba23b30bc7c176210c
analyticsflowee <49652774+analyticsflowee@users.noreply.github.com>
2024-05-22 10:22:11 +0300

    Proxy fixes (#404)
    * navbar user context
    
    Signed-off-by: Simo <info@analyticsflow.ee>
    
    ---------
    
    Signed-off-by: Simo <info@analyticsflow.ee>
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    Co-authored-by: Ragne <ragneinfo@gmail.com>
    Signed-off-by: Analytics Flow info@analyticsflow.ee

---
+++ components/header/proxy/HeaderProxyNewModal.tsx
@@ -0,0 +1,196 @@
+export default function HeaderProxyNewModal() {
+  return (
+    <div className="tw-cursor-default tw-relative tw-z-10">
+      <div className="tw-fixed tw-inset-0 tw-bg-gray-500 tw-bg-opacity-75"></div>
+      <div className="tw-fixed tw-inset-0 tw-z-10 tw-overflow-y-auto">
+        <div className="tw-flex tw-min-h-full tw-items-end tw-justify-center tw-p-4 tw-text-center sm:tw-items-center sm:tw-p-0">
+          <div className="tw-overflow-hidden sm:tw-max-w-xl tw-relative tw-w-full tw-transform tw-rounded-xl tw-bg-iron-950 tw-text-left tw-shadow-xl tw-transition-all tw-duration-500 sm:tw-w-full tw-p-6">
+            <div className="tw-flex tw-justify-between">
+              <div className="tw-max-w-xl sm:tw-flex sm:tw-space-x-4">
+                <div>
+                  <span className="tw-relative tw-inline-flex tw-items-center tw-justify-center tw-rounded-full tw-h-12 tw-w-12 tw-bg-green/20">
+                    <svg
+                      className="tw-flex-shrink-0 tw-h-6 tw-w-6 tw-text-green tw-transition tw-duration-300 tw-ease-out"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                    <div className="tw-absolute">
+                      <svg
+                        width="336"
+                        height="336"
+                        viewBox="0 0 336 336"
+                        fill="none"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <mask
+                          id="mask0_8902_8329"
+                          style={{ maskType: "alpha" }}
+                          maskUnits="userSpaceOnUse"
+                          x="0"
+                          y="0"
+                          width="336"
+                          height="336"
+                        >
+                          <rect
+                            width="336"
+                            height="336"
+                            fill="url(#paint0_radial_8902_8329)"
+                          />
+                        </mask>
+                        <g mask="url(#mask0_8902_8329)">
+                          <circle cx="168" cy="168" r="47.5" stroke="#1F242F" />
+                          <circle cx="168" cy="168" r="47.5" stroke="#1F242F" />
+                          <circle cx="168" cy="168" r="71.5" stroke="#1F242F" />
+                          <circle cx="168" cy="168" r="95.5" stroke="#1F242F" />
+                          <circle
+                            cx="168"
+                            cy="168"
+                            r="119.5"
+                            stroke="#1F242F"
+                          />
+                          <circle
+                            cx="168"
+                            cy="168"
+                            r="143.5"
+                            stroke="#1F242F"
+                          />
+                          <circle
+                            cx="168"
+                            cy="168"
+                            r="167.5"
+                            stroke="#1F242F"
+                          />
+                        </g>
+                        <defs>
+                          <radialGradient
+                            id="paint0_radial_8902_8329"
+                            cx="0"
+                            cy="0"
+                            r="1"
+                            gradientUnits="userSpaceOnUse"
+                            gradientTransform="translate(168 168) rotate(90) scale(168 168)"
+                          >
+                            <stop />
+                            <stop offset="1" stopOpacity="0" />
+                          </radialGradient>
+                        </defs>
+                      </svg>
+                    </div>
+                  </span>
+                </div>
+                <div className="tw-relative tw-mt-3 sm:tw-mt-0 sm:tw-max-w-sm tw-flex tw-flex-col">
+                  <p className="tw-text-lg tw-text-iron-50 tw-font-medium tw-mb-0">
+                    Congrats!
+                  </p>
+                  <p className="tw-mt-1 tw-mb-0 tw-text-md tw-text-iron-400">
+                    Use the arrow next to your profile name in the header to
+                    switch users.
+                  </p>
+                </div>
+              </div>
+              <div className="tw-absolute tw-right-4 tw-top-4 tw-flex tw-justify-between tw-items-center">
+                <button
+                  type="button"
+                  aria-label="Close"
+                  title="Close"
+                  className="tw-p-2.5 tw-flex tw-items-center tw-justify-center tw-rounded-full tw-bg-iron-950 tw-border-0 tw-text-iron-400 hover:tw-text-iron-50 focus:tw-outline-none tw-transition tw-duration-300 tw-ease-out"
+                >
+                  <svg
+                    className="tw-h-6 tw-w-6"
+                    aria-hidden="true"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div className="tw-relative tw-mt-6">
+              <div className="tw-bg-iron-800 tw-rounded-lg tw-p-8">
+                <div className="tailwind-scope">
+                  <div className="tw-flex tw-items-center tw-justify-center tw-rounded-r-lg tw-bg-iron-800 tw-px-2 tw-h-11 tw-border-solid tw-border-l tw-border-y-0 tw-border-r-0 tw-border-iron-700 tw-text-iron-300 tw-shadow-sm hover:tw-bg-iron-700 focus-visible:tw-outline focus-visible:tw-outline-2 focus-visible:tw-outline-offset-2 focus-visible:tw-outline-primary-400 tw-transition tw-duration-300 tw-ease-out">
+                    <svg
+                      className="tw-h-5 tw-w-5 tw-flex-shrink-0"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M6 9L12 15L18 9"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </div>
+                  <div className="tw-mt-1 tw-w-72 tw-rounded-lg tw-shadow-xl tw-bg-iron-800 tw-ring-1 tw-ring-black tw-ring-opacity-5">
+                    <div className="tw-mt-1 tw-overflow-hidden tw-w-full tw-rounded-md tw-bg-iron-800 tw-shadow-2xl tw-ring-1 tw-ring-white/10">
+                      <div className="tw-py-2 tw-flow-root tw-overflow-x-hidden tw-overflow-y-auto">
+                        <div
+                          role="list"
+                          className="tw-flex tw-flex-col tw-gap-y-2 tw-divide-y tw-divide-solid tw-divide-iron-700 tw-divide-x-0"
+                        >
+                          <div className="tw-flex tw-flex-col tw-px-2 tw-gap-y-2 tw-mx-0">
+                            <div className="tw-h-full">oo</div>
+                          </div>
+                          <div className="tw-h-full tw-px-2 tw-pt-2">
+                            <button
+                              type="button"
+                              aria-label="Disconnect"
+                              title="Disconnect"
+                              className="tw-bg-transparent hover:tw-bg-iron-700 tw-py-2.5 tw-w-full tw-h-full tw-border-none tw-text-md tw-font-medium tw-text-left tw-flex tw-items-center tw-gap-x-3 tw-text-iron-300 hover:tw-text-iron-50 tw-rounded-lg tw-relative tw-cursor-pointer tw-select-none tw-px-3 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-primary-400 tw-transition tw-duration-300 tw-ease-out"
+                            >
+                              <svg
+                                className="tw-h-5 tw-w-5 tw-flex-shrink-0"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  d="M16 17L21 12M21 12L16 7M21 12H9M12 17C12 17.93 12 18.395 11.8978 18.7765C11.6204 19.8117 10.8117 20.6204 9.77646 20.8978C9.39496 21 8.92997 21 8 21H7.5C6.10218 21 5.40326 21 4.85195 20.7716C4.11687 20.4672 3.53284 19.8831 3.22836 19.1481C3 18.5967 3 17.8978 3 16.5V7.5C3 6.10217 3 5.40326 3.22836 4.85195C3.53284 4.11687 4.11687 3.53284 4.85195 3.22836C5.40326 3 6.10218 3 7.5 3H8C8.92997 3 9.39496 3 9.77646 3.10222C10.8117 3.37962 11.6204 4.18827 11.8978 5.22354C12 5.60504 12 6.07003 12 7"
+                                  stroke="currentColor"
+                                  strokeWidth="2"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                />
+                              </svg>
+                              <span>Disconnect</span>
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
