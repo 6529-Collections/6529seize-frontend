a8a5e4795c55bf1fc5bc3911ffefd1236958044e
prxt <prxt@6529.io>
2024-05-22 08:26:58 +0300

    Subscriptions public + reset
    Signed-off-by: prxt <prxt@6529.io>

--- components/distribution-plan-tool/review-distribution-plan/table/ReviewDistributionPlanTableSubscription.tsx
+++ components/distribution-plan-tool/review-distribution-plan/table/ReviewDistributionPlanTableSubscription.tsx
@@ -11,8 +11,12 @@ import {
   ReviewDistributionPlanTableItem,
   ReviewDistributionPlanTableItemType,
 } from "./ReviewDistributionPlanTable";
-import { commonApiFetch } from "../../../../services/api/common-api";
+import {
+  commonApiFetch,
+  commonApiPost,
+} from "../../../../services/api/common-api";
 import CircleLoader from "../../common/CircleLoader";
+import { IProfileAndConsolidations } from "../../../../entities/IProfile";
 
 interface WalletResult {
   wallet: string;
@@ -35,69 +39,8 @@ export function SubscriptionLinks(
   const [showConfirm, setShowConfirm] = useState(false);
   const [downloading, setDownloading] = useState(false);
 
-  const isSubscriptionsAdmin = () => {
-    const connectedWallets =
-      connectedProfile?.consolidation.wallets.map(
-        (wallet) => wallet.wallet.address
-      ) ?? [];
-    return connectedWallets.some((w) =>
-      SUBSCRIPTIONS_ADMIN_WALLETS.some((a) => areEqualAddresses(a, w))
-    );
-  };
-
-  function convertToCSV(arr: WalletResult[]): string {
-    const headers = "address,value";
-
-    if (arr.length === 0) {
-      return headers;
-    }
-    const rows = arr.map(({ wallet, amount }) => `${wallet},${amount}`);
-    return [headers, ...rows].join("\n");
-  }
-
-  function downloadCSV(results: WalletResult[], filename: string) {
-    const csv = convertToCSV(results);
-    const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
-    const link = document.createElement("a");
-    link.href = URL.createObjectURL(blob);
-    link.setAttribute(
-      "download",
-      `${props.phase.name.replaceAll(" ", "_").toLowerCase()}_${filename}.csv`
-    );
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }
-
-  const processResults = (results: SubscriptionResult) => {
-    const merged = mergeResults([...results.airdrops, ...results.allowlists]);
-    downloadCSV(merged, "merged");
-    downloadCSV(results.airdrops, "airdrops");
-    downloadCSV(results.allowlists, "allowlists");
-  };
-
-  const download = async (contract: string, tokenId: string) => {
-    setShowConfirm(false);
-    setDownloading(true);
-    try {
-      const response = await commonApiFetch<SubscriptionResult>({
-        endpoint: `subscriptions/allowlists/${contract}/${tokenId}/${props.plan.id}/${props.phase.id}`,
-      });
-
-      processResults(response);
-    } catch (error: any) {
-      console.error("Download failed", error);
-      setToast({
-        type: "error",
-        message: "Something went wrong.",
-      });
-    } finally {
-      setDownloading(false);
-    }
-  };
-
   if (
-    isSubscriptionsAdmin() &&
+    isSubscriptionsAdmin(connectedProfile) &&
     props.phase.type === ReviewDistributionPlanTableItemType.PHASE
   ) {
     return (
@@ -117,22 +60,48 @@ export function SubscriptionLinks(
           )}
         </button>
         <SubscriptionConfirm
+          title="Download Subscriptions"
           plan={props.plan}
           show={showConfirm}
           handleClose={() => setShowConfirm(false)}
-          download={download}
+          onConfirm={async (contract: string, tokenId: string) => {
+            setDownloading(true);
+            try {
+              const downloadResponse = await download(
+                contract,
+                tokenId,
+                props.plan.id,
+                props.phase.id,
+                props.phase.name
+              );
+              setToast({
+                type: downloadResponse.success ? "success" : "error",
+                message: downloadResponse.message,
+              });
+            } catch (error: any) {
+              console.error("Download failed", error);
+              setToast({
+                type: "error",
+                message: "Something went wrong.",
+              });
+            } finally {
+              setShowConfirm(false);
+              setDownloading(false);
+            }
+          }}
         />
       </>
     );
   }
 }
 
-function SubscriptionConfirm(
+export function SubscriptionConfirm(
   props: Readonly<{
+    title: string;
     plan: AllowlistDescription;
     show: boolean;
     handleClose(): void;
-    download(contract: string, tokenId: string): void;
+    onConfirm(contract: string, tokenId: string): void;
   }>
 ) {
   function extractAllNumbers(str: string): number[] {
@@ -155,7 +124,7 @@ function SubscriptionConfirm(
   return (
     <Modal show={props.show} onHide={props.handleClose}>
       <Modal.Header closeButton>
-        <Modal.Title>Confirm Download Info</Modal.Title>
+        <Modal.Title>Confirm {props.title} Info</Modal.Title>
       </Modal.Header>
       <hr className="mb-0 mt-0" />
       <Modal.Body>
@@ -191,7 +160,7 @@ function SubscriptionConfirm(
         <Button
           disabled={!tokenId || isNaN(parseInt(tokenId))}
           variant="primary"
-          onClick={() => props.download(contract, tokenId)}>
+          onClick={() => props.onConfirm(contract, tokenId)}>
           Looks good
         </Button>
       </Modal.Footer>
@@ -210,3 +179,90 @@ const mergeResults = (results: WalletResult[]): WalletResult[] => {
     amount,
   }));
 };
+
+export const resetSubscriptions = async (
+  contract: string,
+  tokenId: string,
+  planId: string
+) => {
+  await commonApiPost({
+    endpoint: `subscriptions/allowlists/${contract}/${tokenId}/${planId}/reset`,
+    body: {},
+  });
+};
+
+export const isSubscriptionsAdmin = (
+  connectedProfile: IProfileAndConsolidations | null
+) => {
+  const connectedWallets =
+    connectedProfile?.consolidation.wallets.map(
+      (wallet) => wallet.wallet.address
+    ) ?? [];
+  return connectedWallets.some((w) =>
+    SUBSCRIPTIONS_ADMIN_WALLETS.some((a) => areEqualAddresses(a, w))
+  );
+};
+
+export const download = async (
+  contract: string,
+  tokenId: string,
+  planId: string,
+  phaseId: string,
+  phaseName: string
+) => {
+  return await commonApiFetch<SubscriptionResult>({
+    endpoint: `subscriptions/allowlists/${contract}/${tokenId}/${planId}/${phaseId}`,
+  })
+    .then((response) => {
+      processResults(phaseName, response);
+      return {
+        success: true,
+        message: "Download successful",
+      };
+    })
+    .catch((error) => {
+      return {
+        success: false,
+        message: `Download failed: ${error}`,
+      };
+    });
+};
+
+const processResults = (phaseName: string, results: SubscriptionResult) => {
+  if (phaseName === "public") {
+    downloadCSV(phaseName, results.airdrops, "airdrops");
+  } else {
+    const merged = mergeResults([...results.airdrops, ...results.allowlists]);
+    downloadCSV(phaseName, merged, "merged");
+    downloadCSV(phaseName, results.airdrops, "airdrops");
+    downloadCSV(phaseName, results.allowlists, "allowlists");
+  }
+};
+
+function convertToCSV(arr: WalletResult[]): string {
+  const headers = "address,value";
+
+  if (arr.length === 0) {
+    return headers;
+  }
+  const rows = arr.map(({ wallet, amount }) => `${wallet},${amount}`);
+  return [headers, ...rows].join("\n");
+}
+
+function downloadCSV(
+  phaseName: string,
+  results: WalletResult[],
+  filename: string
+) {
+  const csv = convertToCSV(results);
+  const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.setAttribute(
+    "download",
+    `${phaseName.replaceAll(" ", "_").toLowerCase()}_${filename}.csv`
+  );
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
