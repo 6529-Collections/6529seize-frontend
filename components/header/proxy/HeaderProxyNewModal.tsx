704c301997549a1a2f12c3e506c0bfa6dc9095ee
Ragne <ragneinfo@gmail.com>
2024-05-22 10:13:30 +0300

    proxy ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/header/proxy/HeaderProxyNewModal.tsx
+++ components/header/proxy/HeaderProxyNewModal.tsx
@@ -126,7 +126,66 @@ export default function HeaderProxyNewModal() {
             </div>
             <div className="tw-relative tw-mt-6">
               <div className="tw-bg-iron-800 tw-rounded-lg tw-p-8">
-                profile name button and dropdown img
+                <div className="tailwind-scope">
+                  <div className="tw-flex tw-items-center tw-justify-center tw-rounded-r-lg tw-bg-iron-800 tw-px-2 tw-h-11 tw-border-solid tw-border-l tw-border-y-0 tw-border-r-0 tw-border-iron-700 tw-text-iron-300 tw-shadow-sm hover:tw-bg-iron-700 focus-visible:tw-outline focus-visible:tw-outline-2 focus-visible:tw-outline-offset-2 focus-visible:tw-outline-primary-400 tw-transition tw-duration-300 tw-ease-out">
+                    <svg
+                      className="tw-h-5 tw-w-5 tw-flex-shrink-0"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M6 9L12 15L18 9"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </div>
+                  <div className="tw-mt-1 tw-w-72 tw-rounded-lg tw-shadow-xl tw-bg-iron-800 tw-ring-1 tw-ring-black tw-ring-opacity-5">
+                    <div className="tw-mt-1 tw-overflow-hidden tw-w-full tw-rounded-md tw-bg-iron-800 tw-shadow-2xl tw-ring-1 tw-ring-white/10">
+                      <div className="tw-py-2 tw-flow-root tw-overflow-x-hidden tw-overflow-y-auto">
+                        <div
+                          role="list"
+                          className="tw-flex tw-flex-col tw-gap-y-2 tw-divide-y tw-divide-solid tw-divide-iron-700 tw-divide-x-0"
+                        >
+                          <div className="tw-flex tw-flex-col tw-px-2 tw-gap-y-2 tw-mx-0">
+                            <div className="tw-h-full">oo</div>
+                          </div>
+                          <div className="tw-h-full tw-px-2 tw-pt-2">
+                            <button
+                              type="button"
+                              aria-label="Disconnect"
+                              title="Disconnect"
+                              className="tw-bg-transparent hover:tw-bg-iron-700 tw-py-2.5 tw-w-full tw-h-full tw-border-none tw-text-md tw-font-medium tw-text-left tw-flex tw-items-center tw-gap-x-3 tw-text-iron-300 hover:tw-text-iron-50 tw-rounded-lg tw-relative tw-cursor-pointer tw-select-none tw-px-3 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-primary-400 tw-transition tw-duration-300 tw-ease-out"
+                            >
+                              <svg
+                                className="tw-h-5 tw-w-5 tw-flex-shrink-0"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  d="M16 17L21 12M21 12L16 7M21 12H9M12 17C12 17.93 12 18.395 11.8978 18.7765C11.6204 19.8117 10.8117 20.6204 9.77646 20.8978C9.39496 21 8.92997 21 8 21H7.5C6.10218 21 5.40326 21 4.85195 20.7716C4.11687 20.4672 3.53284 19.8831 3.22836 19.1481C3 18.5967 3 17.8978 3 16.5V7.5C3 6.10217 3 5.40326 3.22836 4.85195C3.53284 4.11687 4.11687 3.53284 4.85195 3.22836C5.40326 3 6.10218 3 7.5 3H8C8.92997 3 9.39496 3 9.77646 3.10222C10.8117 3.37962 11.6204 4.18827 11.8978 5.22354C12 5.60504 12 6.07003 12 7"
+                                  stroke="currentColor"
+                                  strokeWidth="2"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                />
+                              </svg>
+                              <span>Disconnect</span>
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
