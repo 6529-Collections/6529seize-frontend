5bf496b751c4b60e1b2d08cf9f517c8f288a4f90
Simo <info@analyticsflow.ee>
2024-05-23 10:43:45 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/header/user/HeaderUserContext.tsx
+++ components/header/user/HeaderUserContext.tsx
@@ -1,12 +1,16 @@
+import Link from "next/link";
 import { IProfileAndConsolidations } from "../../../entities/IProfile";
 import HeaderUserProfile from "./HeaderUserProfile";
 import HeaderUserProxy from "./proxy/HeaderUserProxy";
+import { useAccount } from "wagmi";
 
 export default function HeaderUserContext({
   profile,
 }: {
   readonly profile: IProfileAndConsolidations;
 }) {
+  const { address } = useAccount();
+  const haveProfile = !!profile.profile?.handle;
   return (
     <div className="tailwind-scope">
       <div className="tw-flex tw-items-center tw-mr-3">
@@ -17,12 +21,14 @@ export default function HeaderUserContext({
           <HeaderUserProfile profile={profile} />
           <HeaderUserProxy profile={profile} />
         </div>
-        <button
-          type="button"
-          className="tw-ml-3 tw-whitespace-nowrap tw-inline-flex tw-items-center tw-cursor-pointer tw-bg-primary-500 tw-px-3.5 tw-py-2.5 tw-text-sm tw-leading-6 tw-rounded-lg tw-font-semibold tw-text-white tw-border-0 tw-ring-1 tw-ring-inset tw-ring-primary-500 hover:tw-ring-primary-600 placeholder:tw-text-iron-300 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset tw-shadow-sm hover:tw-bg-primary-600 tw-transition tw-duration-300 tw-ease-out"
-        >
-          Create profile
-        </button>
+        {!haveProfile && (
+          <Link
+            href={`/${address}/identity`}
+            className="tw-ml-3 tw-whitespace-nowrap tw-inline-flex tw-items-center tw-cursor-pointer tw-bg-primary-500 tw-px-3.5 tw-py-2.5 tw-text-sm tw-leading-6 tw-rounded-lg tw-font-semibold tw-text-white tw-border-0 tw-ring-1 tw-ring-inset tw-ring-primary-500 hover:tw-ring-primary-600 placeholder:tw-text-iron-300 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset tw-shadow-sm hover:tw-bg-primary-600 tw-transition tw-duration-300 tw-ease-out"
+          >
+            Create profile
+          </Link>
+        )}
         {/* <button
           type="button"
           aria-label="Notifications"
