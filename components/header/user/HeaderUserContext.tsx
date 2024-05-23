b67c364904c3407ec97398fa71f09a520c426424
Ragne <ragneinfo@gmail.com>
2024-05-23 10:37:56 +0300

    create-profile-btn-fix
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/header/user/HeaderUserContext.tsx
+++ components/header/user/HeaderUserContext.tsx
@@ -17,6 +17,12 @@ export default function HeaderUserContext({
           <HeaderUserProfile profile={profile} />
           <HeaderUserProxy profile={profile} />
         </div>
+        <button
+          type="button"
+          className="tw-ml-3 tw-whitespace-nowrap tw-inline-flex tw-items-center tw-cursor-pointer tw-bg-primary-500 tw-px-3.5 tw-py-2.5 tw-text-sm tw-leading-6 tw-rounded-lg tw-font-semibold tw-text-white tw-border-0 tw-ring-1 tw-ring-inset tw-ring-primary-500 hover:tw-ring-primary-600 placeholder:tw-text-iron-300 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset tw-shadow-sm hover:tw-bg-primary-600 tw-transition tw-duration-300 tw-ease-out"
+        >
+          Create profile
+        </button>
         {/* <button
           type="button"
           aria-label="Notifications"
