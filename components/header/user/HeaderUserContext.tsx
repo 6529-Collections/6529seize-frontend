c47d2d289501c1f42ee315d8c34021da49d8cc23
Ragne <ragneinfo@gmail.com>
2024-05-23 11:09:06 +0300

    create-profile-btn-fix
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/header/user/HeaderUserContext.tsx
+++ components/header/user/HeaderUserContext.tsx
@@ -13,7 +13,7 @@ export default function HeaderUserContext({
   const haveProfile = !!profile.profile?.handle;
   return (
     <div className="tailwind-scope">
-      <div className="tw-flex tw-items-center tw-mr-3">
+      <div className="tw-flex lg:tw-mr-3">
         <div
           className="tw-relative tw-inline-flex tw-rounded-lg tw-shadow-sm"
           role="group"
@@ -24,7 +24,7 @@ export default function HeaderUserContext({
         {!haveProfile && (
           <Link
             href={`/${address}/identity`}
-            className="tw-ml-3 tw-whitespace-nowrap tw-inline-flex tw-items-center tw-cursor-pointer tw-bg-primary-500 tw-px-3.5 tw-py-2.5 tw-text-sm tw-leading-6 tw-rounded-lg tw-font-semibold tw-text-white tw-border-0 tw-ring-1 tw-ring-inset tw-ring-primary-500 hover:tw-ring-primary-600 placeholder:tw-text-iron-300 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset tw-shadow-sm hover:tw-bg-primary-600 tw-transition tw-duration-300 tw-ease-out"
+            className="tw-ml-4 sm:tw-ml-3 tw-no-underline tw-whitespace-nowrap tw-inline-flex tw-items-center tw-cursor-pointer tw-bg-primary-500 tw-px-3.5 tw-py-2.5 tw-text-sm tw-leading-6 tw-rounded-lg tw-font-semibold tw-text-white hover:tw-text-white tw-border-0 tw-ring-1 tw-ring-inset tw-ring-primary-500 hover:tw-ring-primary-600 placeholder:tw-text-iron-300 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset tw-shadow-sm hover:tw-bg-primary-600 tw-transition tw-duration-300 tw-ease-out"
           >
             Create profile
           </Link>
