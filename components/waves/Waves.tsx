714f8cfad069075373950c9ec426b10c8cb7f970
Ragne <ragneinfo@gmail.com>
2024-05-27 12:47:38 +0300

    waves ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow info@analyticsflow.ee

--- components/waves/Waves.tsx
+++ components/waves/Waves.tsx
@@ -12,11 +12,11 @@ export default function Waves() {
       <div className="lg:tw-grid lg:tw-grid-cols-12 lg:tw-min-h-screen tw-w-full">
         <WavesMainSteps />
         <div className="tw-relative tw-bg-iron-900 tw-w-full tw-h-full lg:tw-rounded-l-[40px] tw-px-8 tw-pt-12 tw-pb-12 lg:tw-pb-0 lg:tw-col-span-9">
-          {/* <WavesOverview />  */}
+          <WavesOverview />
           {/*  <WavesGroups /> */}
           {/*  <WavesDates /> */}
-          <WavesDrops />
-          {/*   <WavesRating />  */}
+          {/*  <WavesDrops /> */}
+          {/*   <WavesRating /> */}
           {/* <WavesOutcome /> */}
         </div>
       </div>
