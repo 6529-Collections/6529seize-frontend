04c022c654dbbf9b2efacbf57c55cc103deb621d
Ragne <ragneinfo@gmail.com>
2024-05-30 11:29:35 +0300

    waves ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- components/utils/calendar/CommonCalendarDay.tsx
+++ components/utils/calendar/CommonCalendarDay.tsx
@@ -18,7 +18,7 @@ export default function CommonCalendarDay({
     if (isSelected) {
       return "tw-bg-primary-500 tw-text-iron-50 tw-font-semibold hover:tw-border-primary-500";
     }
-    return "tw-bg-iron-800 tw-text-iron-300 hover:tw-border-primary-500";
+    return "tw-font-medium tw-bg-iron-800 tw-text-iron-300 hover:tw-border-primary-500";
   };
 
   const buttonClasses = getButtonClasses();
@@ -28,11 +28,11 @@ export default function CommonCalendarDay({
       type="button"
       onClick={() => setSelectedTimestamp(day.startTimestamp)}
       disabled={!day.isActiveMonth}
-      className={`${buttonClasses} tw-relative  tw-border tw-border-transparent tw-border-solid tw-h-9 tw-w-9   tw-rounded-lg tw-transition tw-duration-300 tw-ease-out focus:tw-z-10 `}
+      className={`${buttonClasses} tw-relative  tw-border tw-border-transparent tw-border-solid tw-h-9 tw-w-9 tw-rounded-lg tw-transition tw-duration-300 tw-ease-out focus:tw-z-10 `}
     >
-      <div className="tw-mx-auto tw-flex tw-h-6 tw-w-6 tw-items-center tw-justify-center tw-rounded-full">
+      <span className="tw-text-sm tw-mx-auto tw-flex tw-items-center tw-justify-center tw-rounded-full">
         {day.date}
-      </div>
+      </span>
     </button>
   );
 }
