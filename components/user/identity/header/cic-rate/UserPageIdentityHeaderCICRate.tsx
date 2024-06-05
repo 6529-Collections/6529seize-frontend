e427655a96efcf9ad2c1fed382204bc1593ac671
Simo <info@analyticsflow.ee>
2024-06-05 12:11:01 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/user/identity/header/cic-rate/UserPageIdentityHeaderCICRate.tsx
+++ components/user/identity/header/cic-rate/UserPageIdentityHeaderCICRate.tsx
@@ -298,8 +298,12 @@ export default function UserPageIdentityHeaderCICRate({
           } tw-flex tw-items-end tw-gap-3`}
         >
           <div className="tw-w-full sm:tw-w-auto">
-            <label className="tw-block tw-text-sm tw-font-normal tw-text-iron-400">
-              Your total CIC Rating of {profile.profile?.handle}:
+            <label
+              className={`${
+                isTooltip ? "tw-max-w-[12rem]" : "tw-w-full sm:tw-w-auto"
+              } tw-block tw-text-sm tw-font-normal tw-text-iron-400 `}
+            >
+              Your total CIC Rating of {profile.input_identity}:
             </label>
             <div className="tw-w-full tw-relative tw-flex tw-mt-1.5">
               <span className="tw-flex tw-flex-col tw-items-center tw-justify-center tw-bg-iron-950 tw-rounded-l-lg tw-border tw-border-solid tw-border-iron-700 tw-px-3">
