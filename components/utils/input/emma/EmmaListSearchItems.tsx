def79b9880177c5b8382f98f8e1d7088f486f3c4
Simo <info@analyticsflow.ee>
2024-06-07 16:56:49 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

---
+++ components/utils/input/emma/EmmaListSearchItems.tsx
@@ -0,0 +1,90 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../auth/Auth";
+import { useQuery } from "@tanstack/react-query";
+import { AllowlistDescription } from "../../../allowlist-tool/allowlist-tool.types";
+import { QueryKey } from "../../../react-query-wrapper/ReactQueryWrapper";
+import { distributionPlanApiFetch } from "../../../../services/distribution-plan-api";
+import { AnimatePresence, motion } from "framer-motion";
+import EmmaListSearchItem from "./EmmaListSearchItem";
+
+export default function EmmaListSearchItems({
+  open,
+  searchCriteria,
+  selectedId,
+  onSelect,
+}: {
+  readonly open: boolean;
+  readonly searchCriteria: string | null;
+  readonly selectedId: string | null;
+  readonly onSelect: (item: AllowlistDescription) => void;
+}) {
+  const { connectedProfile, requestAuth } = useContext(AuthContext);
+  const { data } = useQuery<AllowlistDescription[]>({
+    queryKey: [
+      QueryKey.EMMA_IDENTITY_ALLOWLISTS,
+      { identity: connectedProfile?.profile?.handle },
+    ],
+    queryFn: async () => {
+      const { success } = await requestAuth();
+      if (!success) {
+        return [];
+      }
+      const response = await distributionPlanApiFetch<AllowlistDescription[]>(
+        "/allowlists"
+      );
+      return response.data ?? [];
+    },
+    enabled: !!connectedProfile?.profile?.handle && open,
+  });
+
+  const [items, setItems] = useState<AllowlistDescription[]>([]);
+
+  useEffect(() => {
+    if (!data?.length) {
+      setItems([]);
+      return;
+    }
+    if (!searchCriteria) {
+      setItems(data);
+      return;
+    }
+    const search = searchCriteria.toLowerCase();
+    setItems(data.filter((item) => item.name.toLowerCase().includes(search)));
+  }, [data, searchCriteria]);
+  return (
+    <div className="tw-absolute">
+      <AnimatePresence mode="wait" initial={false}>
+        {open && (
+          <motion.div
+            className="tw-relative tw-z-10 tw-mt-1.5 tw-min-w-[17.4rem] tw-rounded-lg tw-shadow-xl tw-bg-iron-800 tw-ring-1 tw-ring-black tw-ring-opacity-5"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.2 }}
+          >
+            <div className="tw-z-10 tw-overflow-hidden tw-w-full tw-rounded-md tw-bg-iron-800 tw-shadow-2xl tw-ring-1 tw-ring-white/10">
+              <div className="tw-py-1 tw-flow-root tw-overflow-x-hidden tw-overflow-y-auto">
+                <ul className="tw-flex tw-flex-col tw-gap-y-1 tw-px-2 tw-mx-0 tw-mb-0 tw-list-none">
+                  {items.length ? (
+                    items.map((item) => (
+                      <EmmaListSearchItem
+                        key={item.id}
+                        item={item}
+                        selectedId={selectedId}
+                        onSelect={onSelect}
+                      />
+                    ))
+                  ) : (
+                    <li className="tw-py-2 tw-w-full tw-h-full tw-flex tw-items-center tw-justify-between tw-text-sm tw-font-medium tw-text-white tw-rounded-lg tw-relative tw-select-none tw-px-2">
+                      No results
+                    </li>
+                  )}
+                </ul>
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+}
--- components/utils/input/identity/IdentitySearch.tsx
+++ components/utils/input/identity/IdentitySearch.tsx
@@ -5,6 +5,7 @@ import { CommunityMemberMinimal } from "../../../../entities/IProfile";
 import { QueryKey } from "../../../react-query-wrapper/ReactQueryWrapper";
 import { commonApiFetch } from "../../../../services/api/common-api";
 import CommonProfileSearchItems from "../profile-search/CommonProfileSearchItems";
+import { getRandomObjectId } from "../../../../helpers/AllowlistToolHelpers";
 
 const MIN_SEARCH_LENGTH = 3;
 export default function IdentitySearch({
@@ -14,6 +15,7 @@ export default function IdentitySearch({
   readonly identity: string | null;
   readonly setIdentity: (identity: string | null) => void;
 }) {
+  const randomId = getRandomObjectId();
   const [searchCriteria, setSearchCriteria] = useState<string | null>(identity);
   const [debouncedValue, setDebouncedValue] = useState<string | null>(
     searchCriteria
@@ -69,7 +71,7 @@ export default function IdentitySearch({
         onChange={(e) => onSearchCriteriaChange(e.target.value)}
         onFocus={() => onFocusChange(true)}
         onBlur={() => onFocusChange(false)}
-        id="floating_cic_user"
+        id={randomId}
         autoComplete="off"
         className="tw-form-input tw-block tw-py-3 tw-pl-11 tw-pr-4 tw-w-full tw-text-base tw-rounded-lg tw-border-0 tw-appearance-none tw-text-white tw-border-iron-650 focus:tw-border-blue-500 tw-peer
       tw-bg-iron-900 hover:tw-bg-iron-800 focus:tw-bg-iron-900 tw-font-medium tw-caret-primary-300 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-iron-650 placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset focus:tw-ring-primary-400 tw-transition tw-duration-300 tw-ease-out"
@@ -88,7 +90,7 @@ export default function IdentitySearch({
         ></path>
       </svg>
       <label
-        htmlFor="floating_cic_user"
+        htmlFor={randomId}
         className="tw-absolute tw-cursor-text tw-text-base tw-font-medium tw-text-iron-500 tw-duration-300 tw-transform -tw-translate-y-4 tw-scale-75 tw-top-2 tw-z-10 tw-origin-[0] tw-bg-iron-900 tw-rounded-lg group-hover:tw-bg-iron-800 peer-focus:tw-bg-iron-900 tw-ml-8 tw-px-2 peer-focus:tw-px-2 peer-focus:tw-text-primary-400 peer-placeholder-shown:tw-scale-100 
                         peer-placeholder-shown:-tw-translate-y-1/2 peer-placeholder-shown:tw-top-1/2 peer-focus:tw-top-2 peer-focus:tw-scale-75 peer-focus:-tw-translate-y-4 rtl:peer-focus:tw-translate-x-1/4 rtl:peer-focus:tw-left-auto tw-start-1"
       >
