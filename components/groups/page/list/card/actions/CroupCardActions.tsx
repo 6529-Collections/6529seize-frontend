df941e311d89a247811193cf39edc6f5f0a956c7
Ragne <ragneinfo@gmail.com>
2024-06-12 16:06:09 +0300

    groups ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/groups/page/list/card/actions/CroupCardActions.tsx
+++ components/groups/page/list/card/actions/CroupCardActions.tsx
@@ -18,7 +18,12 @@ export default function CroupCardActions({
         <CroupCardActionViewMembers group={group} />
         {!!connectedProfile?.profile?.handle && (
           <>
-            {" "}
+            <button
+              type="button"
+              className="tw-opacity-50 tw-text-iron-600 tw-border-iron-800 tw-inline-flex tw-items-center tw-bg-iron-800 tw-border  tw-border-solid tw-rounded-lg tw-px-3 tw-py-2 tw-text-sm tw-font-semibold tw-shadow-sm focus-visible:tw-outline focus-visible:tw-outline-2 focus-visible:tw-outline-offset-2 focus-visible:tw-outline-iron-700 tw-transition tw-duration-300 tw-ease-out"
+            >
+              Rep all
+            </button>
             <button
               onClick={() => setState(GroupCardState.REP)}
               type="button"
@@ -34,7 +39,7 @@ export default function CroupCardActions({
               CIC all
             </button>
           </>
-        )}{" "}
+        )}
       </div>
       <div className="tw-text-sm tw-inline-flex tw-items-center tw-gap-x-2">
         <svg
