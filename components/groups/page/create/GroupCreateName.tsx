5e3b325b1191c9d78e2a38631fa156d925a7466f
Ragne <ragneinfo@gmail.com>
2024-06-19 18:28:02 +0300

    waves ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow info@analyticsflow.ee

--- components/groups/page/create/GroupCreateName.tsx
+++ components/groups/page/create/GroupCreateName.tsx
@@ -23,7 +23,7 @@ export default function GroupCreateName({
           className="tw-absolute tw-cursor-text tw-text-md tw-font-medium tw-text-iron-500 tw-duration-300 tw-transform -tw-translate-y-4 tw-scale-75 tw-top-2 tw-z-10 tw-origin-[0] tw-bg-iron-900 group-hover:tw-bg-iron-800 peer-focus:tw-bg-iron-900 tw-px-2 peer-focus:tw-px-2 peer-focus:tw-text-primary-400 peer-placeholder-shown:tw-scale-100 tw-rounded-lg
   peer-placeholder-shown:-tw-translate-y-1/2 peer-placeholder-shown:tw-top-1/2 peer-focus:tw-top-2 peer-focus:tw-scale-75 peer-focus:-tw-translate-y-4 rtl:peer-focus:tw-translate-x-1/4 rtl:peer-focus:tw-left-auto tw-start-1"
         >
-          Name*
+          Name
         </label>
       </div>
     </div>
