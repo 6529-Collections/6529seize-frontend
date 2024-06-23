f310dc103a8a7db4615b07a4c7eee0d88e491710
Ragne <ragneinfo@gmail.com>
2024-06-23 13:21:16 +0300

    waves ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

---
+++ components/waves/WavesOutcomeTypes.tsx
@@ -0,0 +1,24 @@
+export default function WavesOutcomeTypes() {
+  return (
+    <div className="tw-grid sm:tw-grid-cols-3 tw-gap-x-4 tw-gap-y-4">
+      <button
+        type="button"
+        className="tw-font-semibold tw-text-iron-300 tw-border-0 tw-ring-iron-700 tw-bg-iron-800 hover:tw-ring-iron-600 tw-flex-1 tw-relative tw-block tw-cursor-pointer tw-rounded-lg tw-ring-1 tw-ring-inset tw-px-5 tw-py-4 tw-shadow-sm focus:tw-outline-none sm:tw-flex sm:tw-items-center tw-gap-x-3 tw-transition tw-duration-300 tw-ease-outt"
+      >
+        Manual
+      </button>
+      <button
+        type="button"
+        className="tw-font-semibold tw-text-iron-300 tw-border-0 tw-ring-iron-700 tw-bg-iron-800 hover:tw-ring-iron-600 tw-flex-1 tw-relative tw-block tw-cursor-pointer tw-rounded-lg tw-ring-1 tw-ring-inset tw-px-5 tw-py-4 tw-shadow-sm focus:tw-outline-none sm:tw-flex sm:tw-items-center tw-gap-x-3 tw-transition tw-duration-300 tw-ease-outt"
+      >
+        Rep
+      </button>
+      <button
+        type="button"
+        className="tw-font-semibold tw-text-iron-300 tw-border-0 tw-ring-iron-700 tw-bg-iron-800 hover:tw-ring-iron-600 tw-flex-1 tw-relative tw-block tw-cursor-pointer tw-rounded-lg tw-ring-1 tw-ring-inset tw-px-5 tw-py-4 tw-shadow-sm focus:tw-outline-none sm:tw-flex sm:tw-items-center tw-gap-x-3 tw-transition tw-duration-300 tw-ease-outt"
+      >
+        CIC
+      </button>
+    </div>
+  );
+}
