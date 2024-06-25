60e6a62792fd9720396cad0d52468776cca31fa9
Ragne <ragneinfo@gmail.com>
2024-06-25 18:14:13 +0300

    waves ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/waves/create-wave/outcomes/winners/CreateWaveOutcomesWinners.tsx
+++ components/waves/create-wave/outcomes/winners/CreateWaveOutcomesWinners.tsx
@@ -39,12 +39,12 @@ export default function CreateWaveOutcomesWinners({
         activeCreditType={winnersConfig.creditValueType}
         setActiveCreditType={onCreditType}
       />
-      <div className="tw-flex tw-flex-col tw-gap-y-2">
+      <div className="tw-flex tw-items-start tw-gap-x-3">
         <div className="tw-relative">
           <input
             type="number"
             autoComplete="off"
-            className="tw-form-input tw-block tw-px-4 tw-pb-3 tw-pt-4 tw-text-base tw-rounded-lg tw-border-0 tw-appearance-none tw-text-white tw-border-iron-600 focus:tw-border-blue-500 tw-peer
+            className="tw-form-input tw-block tw-px-4 tw-pb-3 tw-pt-4 tw-w-full tw-text-base tw-rounded-lg tw-border-0 tw-appearance-none tw-text-white tw-border-iron-600 focus:tw-border-blue-500 tw-peer
 tw-bg-iron-900 focus:tw-bg-iron-900 tw-font-medium tw-caret-primary-400 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-iron-650 placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset focus:tw-ring-primary-400 tw-transition tw-duration-300 tw-ease-out"
             placeholder=" "
           />
@@ -55,11 +55,13 @@ tw-bg-iron-900 focus:tw-bg-iron-900 tw-font-medium tw-caret-primary-400 tw-shado
             Amount
           </label>
         </div>
-        <CreateWaveOutcomesWinnersRows
-          winners={winnersConfig.winners}
-          setWinners={setWinners}
-        />
-        <CreateWaveOutcomesWinnersAddWinner addWinner={addWinner} />
+        <div className="tw-flex tw-flex-col tw-gap-y-2">
+          <CreateWaveOutcomesWinnersRows
+            winners={winnersConfig.winners}
+            setWinners={setWinners}
+          />
+          <CreateWaveOutcomesWinnersAddWinner addWinner={addWinner} />
+        </div>
       </div>
     </div>
   );
