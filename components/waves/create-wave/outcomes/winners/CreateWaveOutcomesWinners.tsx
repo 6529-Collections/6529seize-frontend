d37206cf0e0e429abc719b0f86200b653d1e34a1
Ragne <ragneinfo@gmail.com>
2024-06-27 12:05:03 +0300

    waves ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/waves/create-wave/outcomes/winners/CreateWaveOutcomesWinners.tsx
+++ components/waves/create-wave/outcomes/winners/CreateWaveOutcomesWinners.tsx
@@ -91,38 +91,60 @@ export default function CreateWaveOutcomesWinners({
           setActiveCreditType={onCreditType}
         />
         {percentageError && (
-          <div className="tw-text-sm tw-font-medium tw-text-error">
-            {percentageError}
+          <div className="tw-flex tw-items-center">
+            <svg
+              className="tw-size-5 tw-mr-2 tw-flex-shrink-0 tw-text-error"
+              viewBox="0 0 24 24"
+              fill="none"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12 16V12M12 8H12.01M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <span className="tw-text-xs tw-font-medium tw-text-error">
+              {percentageError}
+            </span>
           </div>
         )}
       </div>
 
       <div className="tw-flex tw-items-start tw-gap-x-3">
         {isPercentageCredit && (
-          <div className="tw-relative">
-            <input
-              type="number"
-              value={winnersConfig.totalAmount}
-              onChange={onTotalAmountChange}
-              autoComplete="off"
-              className={`${
-                totalValueError
-                  ? "tw-ring-error focus:tw-border-error focus:tw-ring-error tw-caret-error tw-text-error"
-                  : "tw-ring-iron-650 focus:tw-border-blue-500 focus:tw-ring-primary-400 tw-caret-primary-400 tw-text-white"
-              } tw-form-input tw-block tw-px-4 tw-pb-3 tw-pt-4 tw-w-full tw-text-base tw-rounded-lg tw-border-0 tw-appearance-none  tw-border-iron-600  tw-peer
+          <div>
+            <div className="tw-relative">
+              <input
+                type="number"
+                value={winnersConfig.totalAmount}
+                onChange={onTotalAmountChange}
+                autoComplete="off"
+                className={`${
+                  totalValueError
+                    ? "tw-ring-error focus:tw-border-error focus:tw-ring-error tw-caret-error tw-text-error"
+                    : "tw-ring-iron-650 focus:tw-border-blue-500 focus:tw-ring-primary-400 tw-caret-primary-400 tw-text-white"
+                } tw-form-input tw-block tw-px-4 tw-pb-3 tw-pt-4 tw-w-full tw-text-base tw-rounded-lg tw-border-0 tw-appearance-none  tw-border-iron-600  tw-peer
 tw-bg-iron-900 focus:tw-bg-iron-900 tw-font-medium tw-shadow-sm tw-ring-1 tw-ring-inset  placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset  tw-transition tw-duration-300 tw-ease-out`}
-              placeholder=" "
-            />
-            <label
-              className={`${
-                totalValueError
-                  ? "peer-focus:tw-text-error tw-text-error"
-                  : "peer-focus:tw-text-primary-400 tw-text-iron-500"
-              } tw-absolute tw-cursor-text tw-text-base tw-font-normal tw-duration-300 tw-transform -tw-translate-y-4 tw-scale-75 tw-top-2 tw-origin-[0] tw-bg-iron-900 peer-focus:tw-bg-iron-900 tw-px-2 peer-focus:tw-px-2 peer-placeholder-shown:tw-scale-100 
+                placeholder=" "
+              />
+              <label
+                className={`${
+                  totalValueError
+                    ? "peer-focus:tw-text-error tw-text-error"
+                    : "peer-focus:tw-text-primary-400 tw-text-iron-500"
+                } tw-absolute tw-cursor-text tw-text-base tw-font-normal tw-duration-300 tw-transform -tw-translate-y-4 tw-scale-75 tw-top-2 tw-origin-[0] tw-bg-iron-900 peer-focus:tw-bg-iron-900 tw-px-2 peer-focus:tw-px-2 peer-placeholder-shown:tw-scale-100 
            peer-placeholder-shown:-tw-translate-y-1/2 peer-placeholder-shown:tw-top-1/2 peer-focus:tw-top-2 peer-focus:tw-scale-75 peer-focus:-tw-translate-y-4 rtl:peer-focus:tw-translate-x-1/4 rtl:peer-focus:tw-left-auto tw-start-1`}
-            >
-              Total Amount
-            </label>
+              >
+                Total Amount
+              </label>
+            </div>
+            <span className="tw-mt-1.5 tw-text-xs tw-font-medium tw-text-error">
+              Error text
+            </span>
           </div>
         )}
         <div className="tw-flex tw-flex-col tw-gap-y-2">
--- components/waves/create-wave/outcomes/winners/CreateWaveOutcomesWinnersRow.tsx
+++ components/waves/create-wave/outcomes/winners/CreateWaveOutcomesWinnersRow.tsx
@@ -34,62 +34,75 @@ export default function CreateWaveOutcomesWinnersRow({
 
   const showRemove = winnersCount > 1;
   return (
-    <div className="tw-flex">
-      <div className="tw-relative">
-        <input
-          type="text"
-          value={winner.value}
-          onChange={onValueChange}
-          autoComplete="off"
-          className={`${
-            totalValueError
-              ? "tw-ring-error focus:tw-border-error focus:tw-ring-error tw-caret-error tw-text-error"
-              : "tw-ring-iron-650 focus:tw-border-blue-500 focus:tw-ring-primary-400 tw-caret-primary-400 tw-text-white"
-          } tw-form-input tw-block tw-px-4 tw-pb-3 tw-pt-4 tw-text-base tw-rounded-lg tw-border-0 tw-appearance-none tw-border-iron-600 tw-peer
+    <div>
+      <div className="tw-flex">
+        <div className="tw-relative">
+          <input
+            type="text"
+            value={winner.value}
+            onChange={onValueChange}
+            autoComplete="off"
+            className={`${
+              totalValueError
+                ? "tw-ring-error focus:tw-border-error focus:tw-ring-error tw-caret-error tw-text-error"
+                : "tw-ring-iron-650 focus:tw-border-blue-500 focus:tw-ring-primary-400 tw-caret-primary-400 tw-text-white"
+            } tw-form-input tw-block tw-px-4 tw-pb-3 tw-pt-4 tw-text-base tw-rounded-lg tw-border-0 tw-appearance-none tw-border-iron-600 tw-peer
 tw-bg-iron-900 focus:tw-bg-iron-900 tw-font-medium tw-shadow-sm tw-ring-1 tw-ring-inset placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset tw-transition tw-duration-300 tw-ease-out`}
-          placeholder=" "
-        />
-        <label
-          className={`${
-            totalValueError
-              ? "peer-focus:tw-text-error tw-text-error"
-              : "peer-focus:tw-text-primary-400 tw-text-iron-500"
-          } tw-absolute tw-cursor-text tw-text-base tw-font-normal tw-duration-300 tw-transform -tw-translate-y-4 tw-scale-75 tw-top-2 tw-z-10 tw-origin-[0] tw-bg-iron-900 peer-focus:tw-bg-iron-900 tw-px-2 peer-focus:tw-px-2  peer-placeholder-shown:tw-scale-100 
+            placeholder=" "
+          />
+          <label
+            className={`${
+              totalValueError
+                ? "peer-focus:tw-text-error tw-text-error"
+                : "peer-focus:tw-text-primary-400 tw-text-iron-500"
+            } tw-absolute tw-cursor-text tw-text-base tw-font-normal tw-duration-300 tw-transform -tw-translate-y-4 tw-scale-75 tw-top-2 tw-origin-[0] tw-bg-iron-900 peer-focus:tw-bg-iron-900 tw-px-2 peer-focus:tw-px-2  peer-placeholder-shown:tw-scale-100 
               peer-placeholder-shown:-tw-translate-y-1/2 peer-placeholder-shown:tw-top-1/2 peer-focus:tw-top-2 peer-focus:tw-scale-75 peer-focus:-tw-translate-y-4 rtl:peer-focus:tw-translate-x-1/4 rtl:peer-focus:tw-left-auto tw-start-1`}
-        >
-          #{i + 1}{" "}
-          {creditValueType ===
-          CreateWaveOutcomeConfigWinnersCreditValueType.PERCENTAGE
-            ? "%"
-            : ""}
-        </label>
-      </div>
-      {showRemove && (
-        <div className="tw-mt-3 tw-ml-3">
-          <button
-            onClick={() => removeWinner(i)}
-            type="button"
-            aria-label="Remove"
-            className="tw-h-8 tw-w-8 tw-text-error tw-flex tw-items-center tw-justify-center tw-bg-transparent tw-border-0 tw-rounded-full hover:tw-bg-iron-800 focus:tw-scale-90 tw-transform tw-transition tw-duration-300 tw-ease-out"
           >
-            <svg
-              className="tw-cursor-pointer tw-h-5 tw-w-5"
-              viewBox="0 0 24 24"
-              fill="none"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M17 7L7 17M7 7L17 17"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </button>
+            #{i + 1}{" "}
+            {creditValueType ===
+            CreateWaveOutcomeConfigWinnersCreditValueType.PERCENTAGE
+              ? "%"
+              : ""}
+          </label>
+          <div className="tw-pointer-events-none tw-absolute tw-inset-y-0 tw-right-0 tw-flex tw-items-center tw-pr-3">
+            <span className="tw-text-iron-500 tw-text-sm tw-font-normal">
+              {creditValueType ===
+              CreateWaveOutcomeConfigWinnersCreditValueType.PERCENTAGE
+                ? "%"
+                : "Rep"}
+            </span>
+          </div>
         </div>
-      )}
+        {showRemove && (
+          <div className="tw-mt-3 tw-ml-3">
+            <button
+              onClick={() => removeWinner(i)}
+              type="button"
+              aria-label="Remove"
+              className="tw-h-8 tw-w-8 hover:tw-bg-iron-800 tw-text-iron-300 tw-flex tw-items-center tw-justify-center tw-bg-transparent tw-border-0 tw-rounded-full focus:tw-scale-90 tw-transform tw-transition tw-duration-300 tw-ease-out"
+            >
+              <svg
+                className="tw-cursor-pointer tw-h-5 tw-w-5"
+                viewBox="0 0 24 24"
+                fill="none"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M17 7L7 17M7 7L17 17"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </button>
+          </div>
+        )}
+      </div>
+      <span className="tw-mt-1.5 tw-text-xs tw-font-medium tw-text-error">
+        Error text
+      </span>
     </div>
   );
 }
