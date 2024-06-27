b4e43e52478ab9f176e25d883dedcd9d7560ba4e
Ragne <ragneinfo@gmail.com>
2024-06-27 12:05:03 +0300

    waves ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- components/waves/create-wave/outcomes/CreateWaveOutcomesManual.tsx
+++ components/waves/create-wave/outcomes/CreateWaveOutcomesManual.tsx
@@ -36,7 +36,7 @@ export default function CreateWaveOutcomesManual({
   return (
     <div className="tw-col-span-full tw-flex tw-flex-col tw-gap-y-2">
       <div className="tw-flex tw-flex-col">
-        <div className="tw-flex">
+        <div>
           <div className="tw-group tw-w-full tw-relative">
             <input
               type="text"
@@ -51,29 +51,50 @@ export default function CreateWaveOutcomesManual({
               tw-bg-iron-900 focus:tw-bg-iron-900 tw-font-medium tw-shadow-sm tw-ring-1 tw-ring-inset placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset tw-transition tw-duration-300 tw-ease-out`}
               placeholder=" "
             />
+            {isInputEmptyError && (
+              <div className="tw-absolute tw-right-3 tw-top-3.5 tw-text-error">
+                <svg
+                  className="tw-size-5"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M12 16V12M12 8H12.01M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
+            )}
             <label
               className={`${
-                isInputEmptyError
-                  ? "peer-focus:tw-text-error tw-text-error"
-                  : "peer-focus:tw-text-primary-400 tw-text-iron-500"
-              } tw-absolute tw-cursor-text tw-text-base tw-font-normal tw-duration-300 tw-transform -tw-translate-y-4 tw-scale-75 tw-top-2 tw-z-10 tw-origin-[0] tw-bg-iron-900 peer-focus:tw-bg-iron-900 tw-px-2 peer-focus:tw-px-2  peer-placeholder-shown:tw-scale-100 
+                isInputEmptyError ? "" : "peer-focus:tw-text-primary-400"
+              }  tw-text-iron-500 tw-absolute tw-cursor-text tw-text-base tw-font-normal tw-duration-300 tw-transform -tw-translate-y-4 tw-scale-75 tw-top-2 tw-z-10 tw-origin-[0] tw-bg-iron-900 peer-focus:tw-bg-iron-900 tw-px-2 peer-focus:tw-px-2  peer-placeholder-shown:tw-scale-100 
               peer-placeholder-shown:-tw-translate-y-1/2 peer-placeholder-shown:tw-top-1/2 peer-focus:tw-top-2 peer-focus:tw-scale-75 peer-focus:-tw-translate-y-4 rtl:peer-focus:tw-translate-x-1/4 rtl:peer-focus:tw-left-auto tw-start-1`}
             >
               Manual action
             </label>
           </div>
+          {isInputEmptyError && (
+            <div className="tw-relative tw-mt-1.5 tw-z-10 tw-text-error tw-text-xs tw-font-medium">
+              Please enter your manual action
+            </div>
+          )}
         </div>
-        <div className="tw-mt-6 tw-flex tw-justify-end tw-gap-x-3">
+        <div className="tw-mt-6 tw-flex tw-justify-end tw-gap-x-3 tw-relative tw-z-50">
           <button
             onClick={onCancel}
             type="button"
-            className="tw-bg-iron-800 tw-border-iron-700 tw-text-iron-300 hover:tw-bg-iron-700 hover:tw-border-iron-700 tw-relative tw-inline-flex tw-items-center tw-justify-center tw-px-4 tw-py-3 tw-text-sm tw-font-semibold tw-border tw-border-solid tw-rounded-lg tw-transition tw-duration-300 tw-ease-out"
+            className="tw-bg-iron-800 tw-border-iron-700 tw-text-iron-300 hover:tw-bg-iron-700 hover:tw-border-iron-700 tw-relative tw-inline-flex tw-items-center tw-justify-center tw-px-4 tw-py-3 tw-text-sm tw-font-semibold tw-border tw-border-solid tw-rounded-lg focus:!tw-outline focus-visible:!tw-outline-2 focus-visible:!tw-outline-offset-2 focus-visible:!tw-outline-primary-400 tw-transition tw-duration-300 tw-ease-out"
           >
             Cancel
           </button>
           <button
             type="button"
-            className="tw-bg-primary-500 tw-border-primary-500 tw-text-white hover:tw-bg-primary-600 hover:tw-border-primary-600 tw-relative tw-inline-flex tw-items-center tw-justify-center tw-px-4 tw-py-3 tw-text-sm tw-font-semibold tw-border tw-border-solid tw-rounded-lg tw-transition tw-duration-300 tw-ease-out"
+            className="tw-bg-primary-500 tw-border-primary-500 tw-text-white hover:tw-bg-primary-600 hover:tw-border-primary-600 tw-relative tw-inline-flex tw-items-center tw-justify-center tw-px-4 tw-py-3 tw-text-sm tw-font-semibold tw-border tw-border-solid tw-rounded-lg focus:!tw-outline focus-visible:!tw-outline-2 focus-visible:!tw-outline-offset-2 focus-visible:!tw-outline-primary-400 tw-transition tw-duration-300 tw-ease-out"
             onClick={onSubmit}
           >
             Save
