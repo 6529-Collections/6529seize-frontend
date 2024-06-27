8eeeb61ce631f58fcf7e147c3e831fef1de4bf11
Ragne <ragneinfo@gmail.com>
2024-06-27 12:05:03 +0300

    waves ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow info@analyticsflow.ee

--- components/utils/input/rep-category/RepCategorySearch.tsx
+++ components/utils/input/rep-category/RepCategorySearch.tsx
@@ -116,69 +116,90 @@ export default function RepCategorySearch({
   }, [data, debouncedValue]);
 
   return (
-    <div className="tw-group tw-w-full tw-relative" ref={wrapperRef}>
-      <input
-        type="text"
-        value={searchCriteria ?? ""}
-        onChange={(e) => onSearchCriteriaChange(e.target.value)}
-        onFocus={() => onFocusChange(true)}
-        onBlur={() => onFocusChange(false)}
-        id={randomId}
-        className={`${INPUT_CLASSES[size]} ${
-          error
-            ? "tw-ring-error focus:tw-border-error focus:tw-ring-error tw-caret-error"
-            : "tw-ring-iron-650 focus:tw-border-blue-500 tw-caret-primary-400 focus:tw-ring-primary-400"
-        } tw-form-input tw-block tw-w-full tw-text-base tw-rounded-lg tw-border-0 tw-appearance-none tw-font-medium tw-border-iron-650  tw-peer tw-pl-10 tw-py-3 tw-pr-4 tw-bg-iron-900 focus:tw-bg-iron-900  tw-shadow-sm tw-ring-1 tw-ring-inset  placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset  tw-transition tw-duration-300 tw-ease-out`}
-        placeholder=" "
-      />
-      <svg
-        className={`${ICON_CLASSES[size]} ${
-          error ? "tw-text-error" : "tw-text-iron-300"
-        } tw-pointer-events-none tw-absolute tw-left-3 tw-h-5 tw-w-5 `}
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        aria-hidden="true"
-      >
-        <path
-          fillRule="evenodd"
-          d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-          clipRule="evenodd"
-        ></path>
-      </svg>
-      {!!category?.length && (
+    <div className="tw-relative tw-w-full" ref={wrapperRef}>
+      <div className="tw-relative tw-group tw-w-full">
+        <input
+          type="text"
+          value={searchCriteria ?? ""}
+          onChange={(e) => onSearchCriteriaChange(e.target.value)}
+          onFocus={() => onFocusChange(true)}
+          onBlur={() => onFocusChange(false)}
+          id={randomId}
+          className={`${INPUT_CLASSES[size]} ${
+            error
+              ? "tw-ring-error focus:tw-border-error focus:tw-ring-error tw-caret-error"
+              : "tw-ring-iron-650 focus:tw-border-blue-500 tw-caret-primary-400 focus:tw-ring-primary-400"
+          } tw-form-input tw-block tw-w-full tw-text-base tw-rounded-lg tw-border-0 tw-appearance-none tw-font-medium tw-border-iron-650  tw-peer tw-pl-10 tw-py-3 tw-pr-4 tw-bg-iron-900 focus:tw-bg-iron-900 tw-shadow-sm tw-ring-1 tw-ring-inset  placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset  tw-transition tw-duration-300 tw-ease-out`}
+          placeholder=" "
+        />
         <svg
-          onClick={() => onValueChange(null)}
-          className={`${ICON_CLASSES[size]} tw-cursor-pointer tw-absolute tw-right-3 tw-h-5 tw-w-5 tw-text-iron-300 hover:tw-text-iron-400 tw-transition tw-duration-300 tw-ease-out`}
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
+          className="tw-text-iron-300 tw-pointer-events-none tw-absolute tw-left-3 tw-top-3.5 tw-h-5 tw-w-5"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
         >
           <path
-            d="M17 7L7 17M7 7L17 17"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
+            fillRule="evenodd"
+            d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+            clipRule="evenodd"
+          ></path>
         </svg>
-      )}
-      <label
-        htmlFor={randomId}
-        className={`${LABEL_CLASSES[size]} ${
-          error
-            ? "peer-focus:tw-text-error tw-text-error"
-            : "peer-focus:tw-text-primary-400 tw-text-iron-500"
-        } tw-absolute tw-cursor-text tw-font-medium  tw-duration-300 tw-transform -tw-translate-y-4 tw-scale-75 tw-top-2 tw-z-10 tw-origin-[0] tw-bg-iron-900 peer-focus:tw-bg-iron-900 tw-ml-7 tw-px-2 peer-focus:tw-px-2  peer-placeholder-shown:tw-scale-100 
+        {!!category?.length && (
+          <svg
+            onClick={() => onValueChange(null)}
+            className={`${ICON_CLASSES[size]} tw-cursor-pointer tw-absolute tw-right-3 tw-h-5 tw-w-5 tw-text-iron-300 hover:tw-text-iron-400 tw-transition tw-duration-300 tw-ease-out`}
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M17 7L7 17M7 7L17 17"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        )}
+        <label
+          htmlFor={randomId}
+          className={`${LABEL_CLASSES[size]} ${
+            error ? "" : "peer-focus:tw-text-primary-400"
+          } tw-text-iron-500 tw-absolute tw-cursor-text tw-font-medium tw-duration-300 tw-transform -tw-translate-y-4 tw-scale-75 tw-top-2 tw-z-10 tw-origin-[0] tw-bg-iron-900 peer-focus:tw-bg-iron-900 tw-ml-7 tw-px-2 peer-focus:tw-px-2 peer-placeholder-shown:tw-scale-100 
         peer-placeholder-shown:-tw-translate-y-1/2 peer-placeholder-shown:tw-top-1/2 peer-focus:tw-top-2 peer-focus:tw-scale-75 peer-focus:-tw-translate-y-4 rtl:peer-focus:tw-translate-x-1/4 rtl:peer-focus:tw-left-auto tw-start-1`}
-      >
-        Rep Category
-      </label>
-      <RepCategorySearchDropdown
-        open={isOpen}
-        selected={category}
-        categories={categories}
-        onSelect={onValueChange}
-      />
+        >
+          Rep Category
+        </label>
+        <RepCategorySearchDropdown
+          open={isOpen}
+          selected={category}
+          categories={categories}
+          onSelect={onValueChange}
+        />
+      </div>
+      {error && (
+        <div className="tw-relative tw-mt-1.5 tw-text-error tw-text-xs tw-font-medium">
+          Please enter rep cateogry
+        </div>
+      )}
+      {error && (
+        <div className="tw-absolute tw-right-3 tw-top-2.5 tw-text-error">
+          <svg
+            className="tw-size-5"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 16V12M12 8H12.01M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
+      )}
     </div>
   );
 }
