32d0acd5d84fb906e00d171075a1ec292e9f1562
Ragne <ragneinfo@gmail.com>
2024-06-27 15:04:06 +0300

    waves ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- components/utils/select-group/SelectGroupModalSearchName.tsx
+++ components/utils/select-group/SelectGroupModalSearchName.tsx
@@ -8,7 +8,7 @@ export default function SelectGroupModalSearchName({
   return (
     <div className="tw-relative">
       <svg
-        className="tw-pointer-events-none tw-absolute tw-left-3 tw-top-3.5 tw-h-5 tw-w-5 tw-text-iron-300"
+        className="tw-pointer-events-none tw-absolute tw-left-3 tw-top-3 tw-h-5 tw-w-5 tw-text-iron-300"
         viewBox="0 0 20 20"
         fill="currentColor"
         aria-hidden="true"
@@ -25,11 +25,11 @@ export default function SelectGroupModalSearchName({
         type="text"
         id="search-group-name"
         placeholder=" "
-        className="tw-pb-3 tw-pt-3 tw-form-input tw-block tw-w-full tw-text-base tw-rounded-lg tw-border-0 tw-appearance-none tw-font-medium tw-border-iron-650 focus:tw-border-blue-500 tw-peer tw-pl-10 tw-pr-4 tw-bg-iron-900 focus:tw-bg-iron-900  tw-caret-primary-400 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-iron-650 placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset focus:tw-ring-primary-400 tw-transition tw-duration-300 tw-ease-out"
+        className="tw-pb-3 tw-pt-3 tw-form-input tw-block tw-w-full tw-text-sm tw-rounded-lg tw-border-0 tw-appearance-none tw-font-medium tw-border-iron-650 focus:tw-border-blue-500 tw-peer tw-pl-10 tw-pr-4 tw-bg-iron-900 focus:tw-bg-iron-900  tw-caret-primary-400 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-iron-650 placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset focus:tw-ring-primary-400 tw-transition tw-duration-300 tw-ease-out"
       />
       <label
         htmlFor="search-group-name"
-        className="tw-text-base tw-absolute tw-cursor-text tw-font-medium tw-text-iron-500 tw-duration-300 tw-transform -tw-translate-y-4 tw-scale-75 tw-top-2 tw-origin-[0] tw-bg-iron-900 tw-rounded-lg peer-focus:tw-bg-iron-900 tw-ml-7 tw-px-2 peer-focus:tw-px-2 peer-focus:tw-text-primary-400 peer-placeholder-shown:tw-scale-100 
+        className="tw-text-sm tw-absolute tw-cursor-text tw-font-medium tw-text-iron-500 tw-duration-300 tw-transform -tw-translate-y-4 tw-scale-75 tw-top-2 tw-origin-[0] tw-bg-iron-900 tw-rounded-lg peer-focus:tw-bg-iron-900 tw-ml-7 tw-px-2 peer-focus:tw-px-2 peer-focus:tw-text-primary-400 peer-placeholder-shown:tw-scale-100 
         peer-placeholder-shown:-tw-translate-y-1/2 peer-placeholder-shown:tw-top-1/2 peer-focus:tw-top-2 peer-focus:tw-scale-75 peer-focus:-tw-translate-y-4 rtl:peer-focus:tw-translate-x-1/4 rtl:peer-focus:tw-left-auto tw-start-1"
       >
         Search by group name
