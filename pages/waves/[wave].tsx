96a54955b23de14758f885ee8adf6ca78e9e733b
Simo <info@analyticsflow.ee>
2024-07-01 17:04:34 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- pages/waves/[wave].tsx
+++ pages/waves/[wave].tsx
@@ -4,6 +4,12 @@ import dynamic from "next/dynamic";
 import HeaderPlaceholder from "../../components/header/HeaderPlaceholder";
 import DropsListItem from "../../components/drops/view/item/DropsListItem";
 import { Drop } from "../../generated/models/Drop";
+import WaveDetailed from "../../components/waves/detailed/WaveDetailed";
+import { useRouter } from "next/router";
+import { useQuery } from "@tanstack/react-query";
+import { QueryKey } from "../../components/react-query-wrapper/ReactQueryWrapper";
+import { Wave } from "../../generated/models/Wave";
+import { commonApiFetch } from "../../services/api/common-api";
 
 const Header = dynamic(() => import("../../components/header/Header"), {
   ssr: false,
@@ -16,6 +22,18 @@ export default function WavePage() {
     { display: "Waves", href: "/waves" },
     { display: "Wave" },
   ];
+
+  const router = useRouter();
+  const wave_id = (router.query.wave as string)?.toLowerCase();
+
+  const { data: wave } = useQuery<Wave>({
+    queryKey: [QueryKey.WAVE, { wave_id }],
+    queryFn: async () =>
+      await commonApiFetch<Wave>({
+        endpoint: `waves/${wave_id}`,
+      }),
+    enabled: !!wave_id,
+  });
 
   return (
     <>
@@ -39,258 +57,7 @@ export default function WavePage() {
           <Header />
           <Breadcrumb breadcrumbs={breadcrumbs} />
         </div>
-        <div className="tailwind-scope tw-bg-iron-950 tw-min-h-screen">
-          <div>
-            <div>
-              <img
-                className="tw-h-32 tw-w-full tw-object-cover lg:tw-h-48"
-                src="https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-                alt=""
-              />
-            </div>
-            <div className="tw-max-w-5xl tw-mx-auto">
-              <div className="-tw-mt-12 sm:tw-flex sm:tw-items-end sm:tw-space-x-5">
-                <div className="tw-flex">
-                  <img
-                    className="tw-h-24 tw-w-24 tw-rounded-full tw-ring-4 tw-ring-iron-950 sm:tw-h-32 sm:tw-w-32 tw-bg-iron-900"
-                    src="#"
-                    alt=""
-                  />
-                </div>
-                <div className="tw-mt-6 sm:tw-flex sm:tw-min-w-0 sm:tw-flex-1 sm:tw-items-center sm:tw-justify-end sm:tw-space-x-6 sm:tw-pb-1">
-                  <div className="tw-mt-6 tw-min-w-0 tw-flex-1 sm:tw-hidden md:tw-block">
-                    <h1 className="tw-truncate tw-text-2xl tw-font-semibold tw-text-white">
-                      Memes Chat
-                    </h1>
-                    <div className="tw-flex tw-items-center">
-                      <div className="tw-flex -tw-space-x-2">
-                        <img
-                          className="tw-inline-block tw-h-6 tw-w-6 tw-rounded-md tw-ring-2 tw-ring-black"
-                          src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
-                        />
-                        <img
-                          className="tw-inline-block tw-h-6 tw-w-6 tw-rounded-md tw-ring-2 tw-ring-black"
-                          src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
-                        />
-                        <img
-                          className="tw-inline-block tw-h-6 tw-w-6 tw-rounded-md tw-ring-2 tw-ring-black"
-                          src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
-                          alt=""
-                        />
-                        <img
-                          className="tw-inline-block tw-h-6 tw-w-6 tw-rounded-md tw-ring-2 tw-ring-black"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
-                        />
-                      </div>
-                      <span className="tw-font-normal tw-ml-2 tw-text-iron-400 tw-text-sm">
-                        <span>+1,235</span> people dropped
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <div className="tw-flex tw-items-center tw-gap-x-3">
-                  <button
-                    type="button"
-                    className="tw-inline-flex tw-items-center tw-gap-x-2 tw-cursor-pointer tw-bg-primary-500 tw-px-3.5 tw-py-2.5 tw-text-sm tw-rounded-lg tw-font-semibold tw-text-white hover:tw-text-white tw-border-0 tw-ring-1 tw-ring-inset tw-ring-primary-500 hover:tw-ring-primary-600 placeholder:tw-text-iron-300 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset tw-shadow-sm hover:tw-bg-primary-600 tw-transition tw-duration-300 tw-ease-out"
-                  >
-                    <svg
-                      className="tw-h-5 tw-w-5 tw-flex-shrink-0"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M12 15.5H7.5C6.10444 15.5 5.40665 15.5 4.83886 15.6722C3.56045 16.06 2.56004 17.0605 2.17224 18.3389C2 18.9067 2 19.6044 2 21M19 21V15M16 18H22M14.5 7.5C14.5 9.98528 12.4853 12 10 12C7.51472 12 5.5 9.98528 5.5 7.5C5.5 5.01472 7.51472 3 10 3C12.4853 3 14.5 5.01472 14.5 7.5Z"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-                    <span>Subscribe</span>
-                  </button>
-                </div>
-              </div>
-              <div className="tw-mt-6 tw-hidden tw-min-w-0 tw-flex-1 sm:tw-block md:tw-hidden">
-                <h1 className="tw-truncate tw-text-2xl tw-font-semibold tw-text-white">
-                  Memes Chat
-                </h1>
-              </div>
-            </div>
-          </div>
-          <div className="tw-mt-12 tw-pb-16 lg:tw-pb-20 tw-max-w-5xl tw-mx-auto">
-            <div className="tw-flex tw-justify-center tw-gap-x-8">
-              <div className="tw-flex-1 tw-flex tw-flex-col tw-gap-y-6">
-                <div className="tw-w-full">
-                  <div className="tw-group">
-                    <div className="tw-bg-iron-900 tw-relative tw-ring-1 tw-ring-inset tw-ring-iron-800 tw-rounded-xl">
-                      <div className="tw-space-y-4 tw-divide-y tw-divide-solid tw-divide-x-0 tw-divide-iron-700">
-                        <div className="tw-px-6 tw-pt-6 tw-flex tw-justify-between tw-items-start tw-gap-x-6">
-                          <p className="tw-mb-0 tw-text-xl tw-text-white tw-font-semibold">
-                            General
-                          </p>
-                        </div>
-                        <div className="tw-px-6 tw-py-6 tw-flex tw-flex-col tw-gap-y-6">
-                          <div className="tw-text-sm tw-flex tw-flex-col tw-gap-2">
-                            <span className="tw-font-medium tw-text-iron-400">
-                              Type
-                            </span>
-                            <span className="tw-font-medium tw-text-white tw-text-base">
-                              Approve
-                            </span>
-                          </div>
-                          <div className="tw-text-sm tw-flex tw-flex-col tw-gap-2">
-                            <span className="tw-font-medium tw-text-iron-400">
-                              Created by
-                            </span>
-                            <div className="tw-flex tw-items-center tw-gap-x-2">
-                              <img
-                                className="tw-h-6 tw-w-6 tw-rounded-lg tw-bg-iron-800"
-                                src="#"
-                                alt="Profile Picture"
-                              />
-                              <span className="tw-font-medium tw-text-white tw-text-base">
-                                punk6529
-                              </span>
-                            </div>
-                          </div>
-                          <div className="tw-text-sm tw-flex tw-flex-col tw-gap-2">
-                            <span className="tw-font-medium tw-text-iron-400">
-                              Created
-                            </span>
-                            <span className="tw-font-medium tw-text-white tw-text-base">
-                              2 monts ago
-                            </span>
-                          </div>
-                          <div className="tw-text-sm tw-flex tw-flex-col tw-gap-2">
-                            <span className="tw-font-medium tw-text-iron-400">
-                              Ending
-                            </span>
-                            <span className="tw-font-medium tw-text-white tw-text-base">
-                              In 7 days
-                            </span>
-                          </div>
-                          <div className="tw-text-sm tw-flex tw-flex-col tw-gap-2">
-                            <span className="tw-font-medium tw-text-iron-400">
-                              View
-                            </span>
-                            <div className="tw-flex tw-items-center tw-gap-x-2">
-                              <img
-                                className="tw-h-6 tw-w-6 tw-rounded-lg tw-bg-iron-800"
-                                src="#"
-                                alt="Profile Picture"
-                              />
-                              <span className="tw-font-medium tw-text-white tw-text-base">
-                                Group name
-                              </span>
-                            </div>
-                          </div>
-
-                          <div className="tw-text-sm tw-flex tw-flex-col tw-gap-2">
-                            <span className="tw-font-medium tw-text-iron-400">
-                              View
-                            </span>
-                            <span className="tw-font-medium tw-text-white tw-text-base">
-                              Anyone
-                            </span>
-                          </div>
-                          <div className="tw-text-sm tw-flex tw-flex-col tw-gap-2">
-                            <span className="tw-font-medium tw-text-iron-400">
-                              Drop
-                            </span>
-                            <div className="tw-flex tw-items-center tw-gap-x-2">
-                              <img
-                                className="tw-h-6 tw-w-6 tw-rounded-lg tw-bg-iron-800"
-                                src="#"
-                                alt="Profile Picture"
-                              />
-                              <span className="tw-font-medium tw-text-white tw-text-base">
-                                Group name
-                              </span>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="tw-w-full">
-                  <div className="tw-group">
-                    <div className="tw-bg-iron-900 tw-relative tw-ring-1 tw-ring-inset tw-ring-iron-800 tw-rounded-xl">
-                      <div className="tw-space-y-4 tw-divide-y tw-divide-solid tw-divide-x-0 tw-divide-iron-700">
-                        <div className="tw-px-6 tw-pt-6 tw-flex tw-justify-between tw-items-start tw-gap-x-6">
-                          <p className="tw-mb-0 tw-text-xl tw-text-white tw-font-semibold">
-                            Outcome
-                          </p>
-                        </div>
-                        <div className="tw-px-6 tw-py-6 tw-flex tw-flex-col tw-gap-y-6">
-                          <div className="tw-text-sm tw-flex tw-flex-col tw-gap-2">
-                            <span className="tw-font-medium tw-text-iron-400">
-                              Threshold
-                            </span>
-                            <span className="tw-font-medium tw-text-white tw-text-base">
-                              200
-                            </span>
-                          </div>
-                          <div className="tw-text-sm tw-flex tw-flex-col tw-gap-2">
-                            <span className="tw-font-medium tw-text-iron-400">
-                              Time
-                            </span>
-                            <span className="tw-font-medium tw-text-white tw-text-base">
-                              2 weeks
-                            </span>
-                          </div>
-                          <div className="tw-text-sm tw-flex tw-flex-col tw-gap-2">
-                            <span className="tw-font-medium tw-text-iron-400">
-                              Winners
-                            </span>
-                            <span className="tw-font-medium tw-text-white tw-text-base">
-                              2 weeks
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="tw-w-[672px]">
-                <DropsListItem
-                  drop={
-                    {
-                      mentioned_users: [],
-                      referenced_nfts: [],
-                      metadata: [],
-                      author: {
-                        id: "1",
-                        handle: "punk6529",
-                        pfp: null,
-                        banner1_color: null,
-                        banner2_color: null,
-                        cic: 0,
-                        rep: 0,
-                        tdh: 0,
-                        level: 0,
-                        archived: false,
-                      },
-                      parts: [
-                        {
-                          content: "my awesome drop",
-                          quoted_drop: null,
-                          media: [],
-                        },
-                      ],
-                    } as any
-                  }
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+        {wave && <WaveDetailed wave={wave} />}
       </main>
     </>
   );
