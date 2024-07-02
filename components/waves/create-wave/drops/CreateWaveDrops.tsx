d404a4c3e344da8badd49f71fdcc743c628040d3
Ragne <ragneinfo@gmail.com>
2024-07-02 14:00:48 +0300

    waves ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/waves/create-wave/drops/CreateWaveDrops.tsx
+++ components/waves/create-wave/drops/CreateWaveDrops.tsx
@@ -38,6 +38,17 @@ export default function CreateWaveDrops({
 
   return (
     <div className="tw-flex tw-flex-col tw-gap-y-6">
+      <div className="tw-flex tw-items-center tw-gap-x-3">
+        <div className="tw-flex tw-h-6 tw-items-center">
+          <input
+            type="checkbox"
+            className="tw-form-checkbox tw-w-5 tw-h-5 tw-rounded tw-bg-iron-800 tw-border-iron-600 tw-border tw-border-solid focus:tw-ring-2 tw-ring-offset-iron-800 tw-text-primary-400 focus:tw-ring-primary-400 tw-cursor-pointer"
+          />
+        </div>
+        <span className="tw-text-sm tw-font-medium tw-text-iron-300">
+          Allow discussion drops
+        </span>
+      </div>
       <CreateWaveDropsTypes
         requiredTypes={drops.requiredTypes}
         onRequiredTypeChange={onRequiredTypeChange}
