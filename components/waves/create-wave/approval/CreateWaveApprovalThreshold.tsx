241487bfd3d177717448dbf8a2899da2695873b4
Ragne <ragneinfo@gmail.com>
2024-07-02 13:32:23 +0300

    waves ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/waves/create-wave/approval/CreateWaveApprovalThreshold.tsx
+++ components/waves/create-wave/approval/CreateWaveApprovalThreshold.tsx
@@ -25,31 +25,32 @@ export default function CreateWaveApprovalThreshold({
           className={`${
             error
               ? "tw-ring-error focus:tw-border-error focus:tw-ring-error tw-caret-error"
-              : "tw-border-iron-650 tw-ring-iron-650 focus:tw-border-blue-500 tw-caret-primary-400 focus:tw-ring-primary-400"
-          } tw-form-input tw-block tw-px-4 tw-pt-4 tw-pb-3 tw-w-full tw-text-base tw-rounded-lg tw-border-0 tw-appearance-none tw-bg-iron-900 focus:tw-bg-iron-900 tw-font-medium tw-shadow-sm tw-ring-1 tw-ring-inset placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset tw-transition tw-duration-300 tw-ease-out ${
-            threshold ? "tw-text-primary-400" : "tw-text-white"
-          } focus:tw-text-white`}
+              : "tw-ring-iron-650 tw-border-iron-650 focus:tw-border-primary-400 focus:tw-ring-primary-400 tw-caret-primary-400"
+          } ${
+            threshold
+              ? "focus:tw-text-white tw-text-primary-400"
+              : "tw-text-white peer-focus:tw-text-white"
+          }  tw-form-input tw-block tw-px-4 tw-pt-4 tw-pb-3 tw-w-full tw-text-base tw-rounded-lg tw-border-0 tw-appearance-none  tw-peer tw-bg-iron-900 focus:tw-bg-iron-900 tw-font-medium tw-shadow-sm tw-ring-1 tw-ring-inset placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset tw-transition tw-duration-300 tw-ease-out  peer-placeholder-shown:tw-text-blue-500`}
           placeholder=" "
         />
         <label
           htmlFor="floating_threshold"
           className={`${
             error
-              ? "peer-focus:tw-text-error tw-text-error"
-              : "peer-focus:tw-text-primary-400 tw-text-iron-500"
-          } tw-absolute tw-cursor-text tw-text-base tw-font-medium  tw-duration-300 tw-transform -tw-translate-y-4 tw-scale-75 tw-top-2 tw-z-10 tw-origin-[0] tw-bg-iron-900 peer-focus:tw-bg-iron-900 tw-px-2 peer-focus:tw-px-2 peer-placeholder-shown:tw-scale-100 peer-placeholder-shown:-tw-translate-y-1/2 peer-placeholder-shown:tw-top-1/2 peer-focus:tw-top-2 peer-focus:tw-scale-75 peer-focus:-tw-translate-y-4 rtl:peer-focus:tw-translate-x-1/4 rtl:peer-focus:tw-left-auto tw-start-1`}
+              ? "peer-focus:tw-text-error"
+              : "peer-focus:tw-text-primary-400"
+          } tw-absolute tw-cursor-text tw-text-base tw-font-normal tw-text-iron-500 tw-duration-300 tw-transform -tw-translate-y-4 tw-scale-75 tw-top-2 tw-z-10 tw-origin-[0] tw-bg-iron-900 peer-focus:tw-bg-iron-900 tw-px-2 peer-focus:tw-px-2 peer-placeholder-shown:tw-scale-100 
+      peer-placeholder-shown:-tw-translate-y-1/2 peer-placeholder-shown:tw-top-1/2 peer-focus:tw-top-2 peer-focus:tw-scale-75 peer-focus:-tw-translate-y-4 rtl:peer-focus:tw-translate-x-1/4 rtl:peer-focus:tw-left-auto tw-start-1`}
         >
           Threshold
         </label>
       </div>
-
       {error && (
         <div className="tw-pt-1.5 tw-relative tw-flex tw-items-center tw-gap-x-2">
           <svg
             className="tw-size-5 tw-flex-shrink-0 tw-text-error"
             viewBox="0 0 24 24"
             fill="none"
-            aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
@@ -60,7 +61,7 @@ export default function CreateWaveApprovalThreshold({
               strokeLinejoin="round"
             />
           </svg>
-          <div className="tw-text-error tw-text-xs tw-font-medium">
+          <div className="tw-relative tw-z-10 tw-text-error tw-text-xs tw-font-medium">
             Please set threshold
           </div>
         </div>
--- components/waves/create-wave/outcomes/winners/CreateWaveOutcomesWinners.tsx
+++ components/waves/create-wave/outcomes/winners/CreateWaveOutcomesWinners.tsx
@@ -145,7 +145,7 @@ export default function CreateWaveOutcomesWinners({
                   totalValueError
                     ? "tw-ring-error focus:tw-border-error focus:tw-ring-error tw-caret-error tw-text-error"
                     : "tw-ring-iron-650 focus:tw-border-blue-500 focus:tw-ring-primary-400 tw-caret-primary-400 tw-text-white"
-                } tw-form-input tw-block tw-px-4 tw-pb-3 tw-pt-4 tw-w-full tw-text-base tw-rounded-lg tw-border-0 tw-appearance-none  tw-border-iron-600  tw-peer
+                }  tw-form-input tw-block tw-px-4 tw-pb-3 tw-pt-4 tw-w-full tw-text-base tw-rounded-lg tw-border-0 tw-appearance-none  tw-border-iron-600  tw-peer
 tw-bg-iron-900 focus:tw-bg-iron-900 tw-font-medium tw-shadow-sm tw-ring-1 tw-ring-inset  placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset  tw-transition tw-duration-300 tw-ease-out`}
                 placeholder=" "
               />
