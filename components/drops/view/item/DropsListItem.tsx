02baad41a7ad2e708ad0ecd1202da151ab29f04a
Simo <info@analyticsflow.ee>
2024-07-04 11:51:48 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>
    
    Signed-off-by: Analytics Flow info@analyticsflow.ee

--- components/drops/view/item/DropsListItem.tsx
+++ components/drops/view/item/DropsListItem.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import DropListItemData from "./data/DropListItemData";
 import DropWrapper from "../../create/utils/DropWrapper";
 import DropListItemContent from "./content/DropListItemContent";
 import DropListItemRateWrapper from "./rate/DropListItemRateWrapper";
 import DropsListItemChallengeBar from "./challenge/DropsListItemChallengeBar";
-import { useRouter } from "next/router";
 import DropListItemCreateQuote from "./quote/DropListItemCreateQuote";
 import { Drop } from "../../../../generated/models/Drop";
 import { getRandomColorWithSeed } from "../../../../helpers/Helpers";
+import DropListItemExternalLink from "./utils/DropListItemExternalLink";
 
 export default function DropsListItem({
   drop,
@@ -20,12 +20,6 @@ export default function DropsListItem({
   readonly showExternalLink?: boolean;
   readonly isWaveDescriptionDrop?: boolean;
 }) {
-  const router = useRouter();
-  const [isTouchScreen, setIsTouchScreen] = useState(false);
-  useEffect(() => {
-    setIsTouchScreen(window.matchMedia("(pointer: coarse)").matches);
-  }, [router.isReady]);
-
   const [quoteModePartId, setQuoteModePartId] = useState<number | null>(null);
   const haveData = !!drop.mentioned_users.length || !!drop.metadata.length;
 
@@ -76,64 +70,10 @@ export default function DropsListItem({
 
           <div className="tw-flex tw-flex-col tw-items-center tw-min-h-full">
             {showExternalLink && (
-              <div className="tw-inline-flex -tw-mt-1.5">
-                <div className="tw-relative">
-                  <button
-                    type="button"
-                    aria-label="Open options"
-                    title="Open options"
-                    className="tw-bg-transparent tw-h-full tw-border-0 tw-block tw-text-iron-500 hover:tw-text-iron-50 tw-transition tw-duration-300 tw-ease-out"
-                    id="options-menu-0-button"
-                    aria-expanded="false"
-                    aria-haspopup="true"
-                  >
-                    <svg
-                      className="tw-h-5 tw-w-5"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path d="M10 3a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM10 8.5a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM11.5 15.5a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0z" />
-                    </svg>
-                  </button>
-                  {/*  <AnimatePresence mode="wait" initial={false}> */}
-                  {/* <div
-                    className="tw-absolute tw-right-0 tw-z-10 tw-mt-2 tw-w-32 tw-origin-top-right tw-rounded-md tw-bg-iron-900 tw-py-2 tw-shadow-lg tw-ring-1 tw-ring-white/5 tw-focus:tw-outline-none"
-                    role="menu"
-                    aria-orientation="vertical"
-                    aria-labelledby="options-menu-0-button"
-                    tabIndex={-1}
-                  >
-                    <div>
-                      <div
-                        className="tw-cursor-pointer tw-block tw-px-3 tw-py-1 tw-text-sm tw-leading-6 tw-text-iron-50 hover:tw-bg-iron-800 tw-transition tw-duration-300 tw-ease-out"
-                        role="menuitem"
-                        tabIndex={-1}
-                        id="options-menu-0-item-0"
-                      >
-                        Copy
-                      </div>
-                    </div>
-                  </div> */}
-                  {/*    </AnimatePresence> */}
-                </div>
-
-                {/*   <Tippy
-                  content="Open"
-                  theme="dark"
-                  placement="top"
-                  disabled={isTouchScreen}
-                >
-                  <a
-                    href={`/brain/${drop.id}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={`tw-block tw-p-2 tw-bg-transparent tw-cursor-pointer tw-text-sm sm:tw-text-base tw-font-semibold tw-text-iron-600 tw-border-0 focus:tw-outline-none tw-transition tw-duration-300 tw-ease-out`}
-                  >
-                    <OutsideLinkIcon />
-                  </a>
-                </Tippy> */}
-              </div>
+              <DropListItemExternalLink
+                drop={drop}
+                isWaveDescriptionDrop={isWaveDescriptionDrop}
+              />
             )}
             <div className="tw-flex-grow tw-flex tw-flex-col tw-justify-center tw-items-center">
               <DropListItemRateWrapper drop={drop} />
--- components/waves/create-wave/outcomes/rep/CreateWaveOutcomesRep.tsx
+++ components/waves/create-wave/outcomes/rep/CreateWaveOutcomesRep.tsx
@@ -1,20 +1,30 @@
-import { CreateWaveOutcomeConfig } from "../../../../../types/waves.types";
+import {
+  CreateWaveDatesConfig,
+  CreateWaveOutcomeConfig,
+} from "../../../../../types/waves.types";
 import { WaveType } from "../../../../../generated/models/WaveType";
 import CreateWaveOutcomesRepRank from "./CreateWaveOutcomesRepRank";
 import CreateWaveOutcomesRepApprove from "./CreateWaveOutcomesRepApprove";
 
 export default function CreateWaveOutcomesRep({
   waveType,
+  dates,
   onOutcome,
   onCancel,
 }: {
   readonly waveType: WaveType;
+  readonly dates: CreateWaveDatesConfig;
   readonly onOutcome: (outcome: CreateWaveOutcomeConfig) => void;
   readonly onCancel: () => void;
 }) {
   const components: Record<WaveType, JSX.Element> = {
     [WaveType.Approve]: (
-      <CreateWaveOutcomesRepApprove onOutcome={onOutcome} onCancel={onCancel} />
+      <CreateWaveOutcomesRepApprove
+        onOutcome={onOutcome}
+        onCancel={onCancel}
+        dates={dates}
+        waveType={waveType}
+      />
     ),
     [WaveType.Rank]: (
       <CreateWaveOutcomesRepRank onOutcome={onOutcome} onCancel={onCancel} />
