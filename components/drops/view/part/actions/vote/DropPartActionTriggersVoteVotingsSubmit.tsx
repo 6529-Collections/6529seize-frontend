6b176945c3dfd7c4aeb943878a973d55c2e57b86
Simo <info@analyticsflow.ee>
2024-07-10 17:09:54 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

---
+++ components/drops/view/part/actions/vote/DropPartActionTriggersVoteVotingsSubmit.tsx
@@ -0,0 +1,32 @@
+import Tippy from "@tippyjs/react";
+import RateClapSolidIcon from "../../../../../utils/icons/RateClapSolidIcon";
+import RateClapOutlineIcon from "../../../../../utils/icons/RateClapOutlineIcon";
+import { Drop } from "../../../../../../generated/models/Drop";
+
+export default function DropPartActionTriggersVoteVotingsSubmit({
+  drop,
+}: {
+  readonly drop: Drop;
+}) {
+  return (
+    <Tippy content="+1">
+      <span>
+        {!!drop.context_profile_context?.rating ? (
+          <RateClapSolidIcon />
+        ) : (
+          <RateClapOutlineIcon />
+        )}
+      </span>
+    </Tippy>
+  );
+}
+
+
+// TODO
+// disabled if not logged in
+// disabled if no profile
+// disabled if logged in as proxy
+// disabled if you are the author
+// disabled if not in "can vote"
+// disabled if no credit left and not voted
+// disabled if no credit left and voted (solid icon)
