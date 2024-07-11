5344509b6d23d535df2e4dba314317f51a58246f
Ragne <ragneinfo@gmail.com>
2024-07-11 14:55:22 +0300

    waves ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow info@analyticsflow.ee

--- components/drops/view/item/data/DropListItemData.tsx
+++ components/drops/view/item/data/DropListItemData.tsx
@@ -7,7 +7,7 @@ export default function DropListItemData({ drop }: { readonly drop: Drop }) {
   const [showMoreOpen, setShowMoreOpen] = useState<boolean>(false);
 
   return (
-    <div className="tw-mt-2 sm:tw-ml-[3.25rem]">
+    <div className="tw-mt-2 sm:tw-ml-16">
       <DropListItemDataTrigger open={showMoreOpen} setOpen={setShowMoreOpen} />
       <DropListItemDataContent open={showMoreOpen} drop={drop} />
     </div>
