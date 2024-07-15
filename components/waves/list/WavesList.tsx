9d86895723f13526f6c95225d545d1cf8bdcbd87
Ragne <ragneinfo@gmail.com>
2024-07-15 16:09:04 +0300

    waves ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- components/waves/list/WavesList.tsx
+++ components/waves/list/WavesList.tsx
@@ -84,33 +84,56 @@ export default function WavesList({
 
   return (
     <div className="tailwind-scope">
-      <div className="tw-max-w-2xl tw-mx-auto tw-py-8 tw-px-4 md:tw-px-0">
-        <div className="tw-w-full tw-flex tw-items-center tw-justify-between">
+      <div className="tw-px-4 min-[992px]:tw-px-3 min-[992px]:tw-max-w-[960px] max-[1100px]:tw-max-w-[950px] min-[1200px]:tw-max-w-[1050px] min-[1300px]:tw-max-w-[1150px] min-[1400px]:tw-max-w-[1250px] min-[1500px]:tw-max-w-[1280px] tw-mx-auto">
+        <div className="tw-mt-6 lg:tw-mt-8">
           <h1>Waves</h1>
-          {showCreateNewWaveButton && (
-            <button
-              onClick={onCreateNewWave}
-              type="button"
-              className="tw-flex tw-items-center tw-whitespace-nowrap tw-border tw-border-solid tw-border-primary-500 tw-rounded-lg tw-bg-primary-500 tw-px-3.5 tw-py-2.5 tw-text-sm tw-font-semibold tw-text-white tw-shadow-sm hover:tw-bg-primary-600 hover:tw-border-primary-600 focus-visible:tw-outline focus-visible:tw-outline-2 focus-visible:tw-outline-offset-2 focus-visible:tw-outline-primary-600 tw-transition tw-duration-300 tw-ease-out"
-            >
+          <div className="tw-flex tw-items-center tw-justify-between tw-w-full tw-gap-x-3">
+            <div className="tw-max-w-sm tw-relative tw-w-full">
               <svg
-                className="tw-size-5 tw-mr-1.5 -tw-ml-1 tw-flex-shrink-0"
-                viewBox="0 0 24 24"
-                fill="none"
+                className="tw-pointer-events-none tw-absolute tw-left-4 tw-top-3 tw-h-5 tw-w-5 tw-text-iron-300"
+                viewBox="0 0 20 20"
+                fill="currentColor"
                 aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  d="M12 5V19M5 12H19"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
+                  fill-rule="evenodd"
+                  d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                  clip-rule="evenodd"
+                ></path>
               </svg>
-              <span>Create New</span>
-            </button>
-          )}
+              <input
+                type="text"
+                placeholder="Search Waves"
+                className="tw-form-input tw-block tw-w-full tw-rounded-lg tw-border-0 tw-py-3 tw-pl-11 tw-pr-4 tw-bg-iron-900 tw-text-iron-50 tw-font-normal tw-caret-primary-400 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-iron-700 hover:tw-ring-iron-600 placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-bg-transparent focus:tw-ring-1 focus:tw-ring-inset  focus:tw-ring-primary-400 tw-text-base sm:tw-text-sm tw-transition tw-duration-300 tw-ease-out"
+              />
+            </div>
+            <div>
+              {showCreateNewWaveButton && (
+                <button
+                  onClick={onCreateNewWave}
+                  type="button"
+                  className="tw-flex tw-items-center tw-whitespace-nowrap tw-border tw-border-solid tw-border-primary-500 tw-rounded-lg tw-bg-primary-500 tw-px-3.5 tw-py-2.5 tw-text-sm tw-font-semibold tw-text-white tw-shadow-sm hover:tw-bg-primary-600 hover:tw-border-primary-600 focus-visible:tw-outline focus-visible:tw-outline-2 focus-visible:tw-outline-offset-2 focus-visible:tw-outline-primary-600 tw-transition tw-duration-300 tw-ease-out"
+                >
+                  <svg
+                    className="tw-size-5 tw-mr-1.5 -tw-ml-1 tw-flex-shrink-0"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M12 5V19M5 12H19"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                  <span>Create New</span>
+                </button>
+              )}
+            </div>
+          </div>
         </div>
         <div className="tw-mt-6">
           {!waves.length && !isFetching && (
@@ -118,6 +141,9 @@ export default function WavesList({
               No Waves to show
             </div>
           )}
+          <span className="tw-tracking-tight tw-text-2xl tw-font-medium">
+            Latest
+          </span>
           <WavesListWrapper
             waves={waves}
             loading={isFetching}
--- components/waves/list/WavesListWrapper.tsx
+++ components/waves/list/WavesListWrapper.tsx
@@ -19,7 +19,73 @@ export default function WavesListWrapper({
 }) {
   return (
     <div className="tw-overflow-hidden">
-      <div className="tw-flex tw-flex-col tw-gap-y-4">
+      <div className="tw-mt-2 tw-grid tw-grid-cols-1 md:tw-grid-cols-2 xl:tw-grid-cols-3 tw-gap-4">
+
+
+
+        <div className="tw-pb-4 tw-relative tw-bg-iron-900 tw-rounded-xl tw-border tw-border-solid tw-border-iron-800">
+          <div className="tw-relative tw-w-full tw-h-10 tw-rounded-t-xl tw-bg-gradient-to-tr tw-from-white tw-to-transparent"></div>
+          <div className="-tw-mt-4 tw-flex-shrink-0 tw-px-4">
+            <div className="tw-h-10 tw-w-10">
+              <img
+                className="tw-flex-shrink-0 tw-object-contain tw-h-full tw-w-full tw-rounded-md tw-bg-iron-700 tw-border-[3px] tw-border-solid tw-border-iron-900"
+                src="#"
+                alt="#"
+              />
+            </div>
+          </div>
+          <div className="tw-flex tw-items-center tw-gap-x-3 tw-justify-between tw-px-4">
+            <span className="tw-text-lg tw-font-semibold tw-text-white">
+              Welcome n00bs
+            </span>
+            <button
+              type="button"
+              className="tw-flex tw-items-center tw-whitespace-nowrap tw-border tw-border-solid tw-border-primary-500 tw-rounded-lg tw-bg-primary-500 tw-px-3.5 tw-py-2.5 tw-text-sm tw-font-semibold tw-text-white tw-shadow-sm hover:tw-bg-primary-600 hover:tw-border-primary-600 focus-visible:tw-outline focus-visible:tw-outline-2 focus-visible:tw-outline-offset-2 focus-visible:tw-outline-primary-600 tw-transition tw-duration-300 tw-ease-out"
+            >
+              <span>Join</span>
+            </button>
+          </div>
+          <div className="tw-mt-4 tw-px-4 tw-flex tw-items-center tw-gap-x-3">
+            <div className="tw-px-4 tw-py-4 tw-w-full tw-rounded-lg tw-bg-iron-800">
+              <div className="tw-flex tw-gap-x-3">
+                <img
+                  src="#"
+                  alt="#"
+                  className="tw-flex-shrink-0 tw-bg-iron-800 tw-h-8 tw-w-8 tw-rounded-lg tw-object-contain"
+                />
+                <div className="tw-flex tw-flex-col tw-gap-y-1">
+                  <div className="tw-flex tw-items-center tw-gap-x-4">
+                    <div className="tw-flex tw-items-center tw-gap-x-2 tw-text-center">
+                      <p className="tw-text-md tw-mb-0 tw-leading-none tw-font-semibold tw-text-iron-50">
+                        <a className="tw-no-underline hover:tw-underline hover:tw-text-iron-500 tw-transition tw-duration-300 tw-ease-out">
+                          simo
+                        </a>
+                      </p>
+                      <div className="tw-relative">
+                        <div className="tw-h-4 tw-w-4 tw-text-[9px] tw-flex tw-items-center tw-justify-center tw-leading-3 tw-font-bold tw-rounded-full tw-ring-2 tw-ring-iron-300 tw-text-iron-300">
+                          6
+                        </div>
+                        <span className="-tw-top-[0.1875rem] tw-h-2 tw-w-2 tw-bg-[#FEDF89] tw-flex-shrink-0 tw-absolute -tw-right-1 tw-block tw-rounded-full"></span>
+                      </div>
+                    </div>
+                    <p className="tw-text-md tw-mb-0 tw-whitespace-nowrap tw-font-normal tw-leading-none tw-text-iron-500">
+                      3 days ago
+                    </p>
+                  </div>
+                  <p className="last:tw-mb-0 tw-text-md tw-leading-5 tw-text-iron-50 tw-font-normal tw-whitespace-pre-wrap tw-break-words word-break tw-text-balance">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Eum cum temporibus tenetur?
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+        {/* 
         {waves.map((wave) => (
           <DropsListItem
             key={wave.id}
@@ -28,7 +94,7 @@ export default function WavesListWrapper({
             showWaveInfo={true}
             availableCredit={availableCredit}
           />
-        ))}
+        ))} */}
       </div>
       {loading && (
         <div className="tw-w-full tw-text-center tw-mt-8">
