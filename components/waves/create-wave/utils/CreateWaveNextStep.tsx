27f90fd90bfbe1659fb287a002c934b522502fe7
Ragne <ragneinfo@gmail.com>
2024-07-22 10:06:10 +0300

    waves ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- components/waves/create-wave/utils/CreateWaveNextStep.tsx
+++ components/waves/create-wave/utils/CreateWaveNextStep.tsx
@@ -53,9 +53,18 @@ export default function CreateWaveNextStep({
           disabled
             ? "tw-bg-iron-800 tw-text-iron-600 tw-border-iron-800"
             : "tw-text-white tw-bg-gradient-to-r tw-from-blue-500 tw-via-blue-600 tw-to-blue-700 hover:tw-bg-gradient-to-b"
-        } tw-relative tw-inline-flex tw-items-center tw-gap-x-2 tw-justify-center tw-px-6 tw-py-3 tw-border-0 tw-text-base tw-font-semibold tw-rounded-lg r tw-transform hover:tw-scale-105 tw-transition tw-duration-300 tw-ease-in-out tw-shadow-lg`}
+        } tw-relative tw-flex tw-items-center tw-justify-center tw-px-6 tw-py-3 tw-border-0 tw-text-base tw-font-semibold tw-rounded-lg r tw-transform hover:tw-scale-105 tw-transition tw-duration-300 tw-ease-in-out tw-shadow-lg`}
       >
-        {submitting ? <CircleLoader /> : <span>Complete</span>}
+        {submitting ? (
+          <>
+            <div className="-tw-ml-2 tw-mr-3">
+              <CircleLoader />
+            </div>
+            <span>Complete</span>
+          </>
+        ) : (
+          <span>Complete</span>
+        )}
       </button>
     ),
   };
--- components/waves/list/WavesList.tsx
+++ components/waves/list/WavesList.tsx
@@ -84,29 +84,10 @@ export default function WavesList({
 
   return (
     <div className="tailwind-scope">
-      <div className="tw-px-4 min-[992px]:tw-px-3 min-[992px]:tw-max-w-[960px] max-[1100px]:tw-max-w-[950px] min-[1200px]:tw-max-w-[1050px] min-[1300px]:tw-max-w-[1150px] min-[1400px]:tw-max-w-[1250px] min-[1500px]:tw-max-w-[1280px] tw-mx-auto">
+      <div className="tw-pb-14 lg:tw-pb-24 tw-px-4 min-[992px]:tw-px-3 min-[992px]:tw-max-w-[960px] max-[1100px]:tw-max-w-[950px] min-[1200px]:tw-max-w-[1050px] min-[1300px]:tw-max-w-[1150px] min-[1400px]:tw-max-w-[1250px] min-[1500px]:tw-max-w-[1280px] tw-mx-auto">
         <div className="tw-mt-6 lg:tw-mt-8">
           <h1>Waves</h1>
-          <div className="tw-flex tw-items-center tw-justify-between tw-w-full tw-gap-x-3">
-            <div className="tw-max-w-sm tw-relative tw-w-full">
-              <svg
-                className="tw-pointer-events-none tw-absolute tw-left-4 tw-top-3 tw-h-5 tw-w-5 tw-text-iron-300"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <input
-                type="text"
-                placeholder="Search Waves"
-                className="tw-form-input tw-block tw-w-full tw-rounded-lg tw-border-0 tw-py-3 tw-pl-11 tw-pr-4 tw-bg-iron-900 tw-text-iron-50 tw-font-normal tw-caret-primary-400 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-iron-700 hover:tw-ring-iron-600 placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-bg-transparent focus:tw-ring-1 focus:tw-ring-inset  focus:tw-ring-primary-400 tw-text-base sm:tw-text-sm tw-transition tw-duration-300 tw-ease-out"
-              />
-            </div>
+          <div className="tw-mt-4 tw-flex tw-items-center tw-gap-x-3">
             <div>
               {showCreateNewWaveButton && (
                 <button
@@ -133,6 +114,25 @@ export default function WavesList({
                 </button>
               )}
             </div>
+            <div className="tw-max-w-sm tw-relative tw-w-full">
+              <svg
+                className="tw-pointer-events-none tw-absolute tw-left-4 tw-top-3 tw-h-5 tw-w-5 tw-text-iron-300"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <input
+                type="text"
+                placeholder="Search Waves"
+                className="tw-form-input tw-block tw-w-full tw-rounded-lg tw-border-0 tw-py-3 tw-pl-11 tw-pr-4 tw-bg-iron-900 tw-text-iron-50 tw-font-normal tw-caret-primary-400 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-iron-700 hover:tw-ring-iron-600 placeholder:tw-text-iron-500 focus:tw-outline-none focus:tw-bg-transparent focus:tw-ring-1 focus:tw-ring-inset  focus:tw-ring-primary-400 tw-text-base sm:tw-text-sm tw-transition tw-duration-300 tw-ease-out"
+              />
+            </div>
           </div>
         </div>
         <div className="tw-mt-6">
@@ -141,7 +141,6 @@ export default function WavesList({
               No Waves to show
             </div>
           )}
-
           <div className="tw-space-y-12">
             <WavesListWrapper waves={waves.slice(0, 3)} label="Latest" />
             <WavesListWrapper
