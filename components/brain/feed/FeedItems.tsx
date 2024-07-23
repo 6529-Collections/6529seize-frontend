f3e674d04d98f2199e8afef3d725c7d575c8a1d8
Ragne <ragneinfo@gmail.com>
2024-07-23 14:49:55 +0300

    waves ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow <info@analyticsflow.ee>

--- components/brain/feed/FeedItems.tsx
+++ components/brain/feed/FeedItems.tsx
@@ -14,8 +14,8 @@ export default function FeedItems({
   availableCredit,
 }: FeedItemsProps) {
   return (
-    <div className="tw-flex tw-gap-x-3">
-      <div className="tw-flex tw-flex-col tw-gap-y-5 tw-max-w-[672px]">
+    <div className="tw-w-full tw-flex tw-gap-x-3">
+      <div className="tw-flex tw-flex-col tw-gap-y-5 tw-w-[672px]">
         {items.map((item, i) => (
           <FeedItem
             key={`feed-item-${item.type}-${i}`}
