3c659e879ce693b81db3dfd7339b6953de798903
Ragne <ragneinfo@gmail.com>
2024-07-23 14:49:55 +0300

    waves ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>
    
    Signed-off-by: Analytics Flow info@analyticsflow.ee

--- components/brain/feed/items/drop-commented/FeedItemDropCommented.tsx
+++ components/brain/feed/items/drop-commented/FeedItemDropCommented.tsx
@@ -12,8 +12,8 @@ export default function FeedItemDropCommented({
   readonly availableCredit: number | null;
 }) {
   return (
-    <div className="tw-flex tw-gap-x-3">
-      <div className="tw-space-y-3">
+    <div className="tw-w-full tw-flex tw-gap-x-3">
+      <div className="tw-w-full tw-flex tw-flex-col tw-space-y-3">
         <div className="tw-inline-flex tw-items-center tw-space-x-2">
           <div className="md:tw-absolute md:-tw-left-10 tw-flex-shrink-0 tw-h-8 tw-w-8 tw-rounded-full tw-bg-iron-800 tw-flex tw-items-center tw-justify-center">
             <svg
