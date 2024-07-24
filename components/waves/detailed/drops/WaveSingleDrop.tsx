f16f57d958c46d7936212e00d987b17f59fab35d
Simo <info@analyticsflow.ee>
2024-07-24 11:08:09 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>
    
    Signed-off-by: Analytics Flow info@analyticsflow.ee

--- components/waves/detailed/drops/WaveSingleDrop.tsx
+++ components/waves/detailed/drops/WaveSingleDrop.tsx
@@ -7,9 +7,11 @@ import DropsListItem from "../../../drops/view/item/DropsListItem";
 export default function WaveSingleDrop({
   dropId,
   availableCredit,
+  onBackToList,
 }: {
   readonly dropId: string;
   readonly availableCredit: number | null;
+  readonly onBackToList: () => void;
 }) {
   const { data: drop } = useQuery<Drop>({
     queryKey: [QueryKey.DROP, { drop_id: dropId }],
@@ -24,7 +26,10 @@ export default function WaveSingleDrop({
   }
   return (
     <div className="tw-space-y-2">
-      <button className="-tw-mt-2 tw-py-2 tw-px-2 -tw-ml-2 tw-flex tw-items-center tw-gap-x-2 tw-justify-center tw-text-sm tw-font-semibold tw-border-0 tw-rounded-lg tw-transition tw-duration-300 tw-ease-out tw-cursor-pointer tw-text-iron-400 tw-bg-transparent hover:tw-text-iron-50">
+      <button
+        onClick={onBackToList}
+        className="-tw-mt-2 tw-py-2 tw-px-2 -tw-ml-2 tw-flex tw-items-center tw-gap-x-2 tw-justify-center tw-text-sm tw-font-semibold tw-border-0 tw-rounded-lg tw-transition tw-duration-300 tw-ease-out tw-cursor-pointer tw-text-iron-400 tw-bg-transparent hover:tw-text-iron-50"
+      >
         <svg
           className="tw-flex-shrink-0 tw-w-5 tw-h-5"
           viewBox="0 0 24 24"
