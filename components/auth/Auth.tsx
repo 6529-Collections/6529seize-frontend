b3348934da376d071eac68a8874bb962ffc65358
Simo <info@analyticsflow.ee>
2024-07-31 14:58:20 +0300

    changed level 7 > 20
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/auth/Auth.tsx
+++ components/auth/Auth.tsx
@@ -45,7 +45,7 @@ type AuthContextType = {
   ) => Promise<void>;
 };
 
-export const WAVES_MIN_ACCESS_LEVEL = 7;
+export const WAVES_MIN_ACCESS_LEVEL = 20;
 
 export const AuthContext = createContext<AuthContextType>({
   connectedProfile: null,
