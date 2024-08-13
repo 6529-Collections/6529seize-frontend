ea5fb6c1245eecf9f549feb92cfc01e03bfc1d8b
Simo <info@analyticsflow.ee>
2024-08-13 15:06:27 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/user/followers/IdentityFollowers.tsx
+++
@@ -1,3 +0,0 @@
-export default function IdentityFollowers(){
-  return <div>123</div>
-}
---
+++ components/user/followers/UserPageFollowers.tsx
@@ -0,0 +1,17 @@
+
+import { IProfileAndConsolidations } from "../../../entities/IProfile";
+
+
+export default function UserPageFollowers({
+  profile,
+}: {
+  readonly profile: IProfileAndConsolidations;
+}) {
+ 
+
+  return (
+    <div className="tailwind-scope">
+      yepyep
+    </div>
+  );
+}
