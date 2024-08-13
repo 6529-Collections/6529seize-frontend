e16f041adee61636457270dbb527520eb84766ef
Ragne <ragneinfo@gmail.com>
2024-08-13 15:06:06 +0300

    replies ui
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/drops/view/part/actions/discussion/DropPartDiscussion.tsx
+++ components/drops/view/part/actions/discussion/DropPartDiscussion.tsx
@@ -7,7 +7,7 @@ export default function DropPartDiscussion({
   drop,
   dropPart,
   availableCredit,
-  closeReplies
+  closeReplies,
 }: {
   readonly drop: Drop;
   readonly dropPart: DropPart;
@@ -15,13 +15,17 @@ export default function DropPartDiscussion({
   readonly closeReplies: () => void;
 }) {
   return (
-    <div className="tw-mt-2 tw-pt-4 tw-pb-2 tw-border-t tw-border-x-0 tw-border-b-0 tw-border-iron-700 tw-border-solid">
-      <DropReplyInputWrapper drop={drop} dropPart={dropPart} onReply={closeReplies}/>
-      <DropPartDiscussionItems
+    <div className="tw-pb-2">
+      <DropReplyInputWrapper
         drop={drop}
         dropPart={dropPart}
-        availableCredit={availableCredit}
+        onReply={closeReplies}
       />
+        <DropPartDiscussionItems
+          drop={drop}
+          dropPart={dropPart}
+          availableCredit={availableCredit}
+        />
     </div>
   );
 }
