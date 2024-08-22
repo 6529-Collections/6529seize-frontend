c65988f3e912d4ae275d2e8c19856a5bdf26affd
prxt6529 <prxt@6529.io>
2024-08-22 15:35:21 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- hooks/isCapacitor.ts
+++ hooks/isCapacitor.ts
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
+import { LocalNotifications } from "@capacitor/local-notifications";
 
 const useIsCapacitor = () => {
   const [isCapacitor, setIsCapacitor] = useState(false);
+  const [isInitialized, setIsInitialized] = useState(false);
 
   useEffect(() => {
     if (typeof window !== "undefined" && (window as any)?.Capacitor) {
       setIsCapacitor(true);
+      if (!isInitialized) {
+        initializePushNotifications();
+        setIsInitialized(true);
+      }
     }
   }, []);
 
@@ -13,3 +19,28 @@ const useIsCapacitor = () => {
 };
 
 export default useIsCapacitor;
+
+export function initializePushNotifications() {
+  LocalNotifications.requestPermissions()
+    .then((permission) => {
+      if (permission.display === "granted") {
+        LocalNotifications.schedule({
+          notifications: [
+            {
+              title: "Notification",
+              body: "You have some activity",
+              id: 1,
+              schedule: { at: new Date(Date.now() + 30000) },
+              actionTypeId: "",
+              extra: null,
+            },
+          ],
+        });
+      } else {
+        console.error("Notifications permission denied");
+      }
+    })
+    .catch((error) => {
+      console.error("Error requesting notifications", error);
+    });
+}
