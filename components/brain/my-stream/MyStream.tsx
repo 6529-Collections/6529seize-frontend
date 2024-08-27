1469c090e0020f1eb13ccbdde5aabc6db69fcd44
Ragne <ragneinfo@gmail.com>
2024-08-27 15:16:07 +0300

    wip
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/brain/my-stream/MyStream.tsx
+++ components/brain/my-stream/MyStream.tsx
@@ -72,11 +72,12 @@ export default function MyStream() {
     });
   return (
     <div className="lg:tw-w-[672px] tw-flex-shrink-0">
+
       {/*  <CreateDropContent /> */}
 
-      <div className="tw-mt-4 tw-flex tw-flex-wrap tw-gap-2 tw-items-center">
+     {/*  <div className="tw-mt-4 tw-flex tw-flex-wrap tw-gap-2 tw-items-center"> */}
         {/*  ACTIVE STATE: tw-ring-primary-400 hover:tw-ring-primary-300 hover:tw-to-iron-900 tw-bg-gradient-to-b tw-from-iron-900 tw-to-iron-950 tw-text-iron-50  */}
-        <button
+       {/*  <button
           type="button"
           className="tw-ring-primary-400 hover:tw-ring-primary-300 hover:tw-to-iron-900 tw-bg-gradient-to-b tw-from-iron-900 tw-to-iron-950 tw-text-iron-50 tw-ring-inset tw-ring-1 tw-border-0 tw-rounded-full tw-bg-iron-950 tw-px-3 tw-py-2.5 0 tw-font-medium tw-text-sm tw-shadow-md tw-transition tw-duration-300 tw-ease-out"
         >
@@ -88,9 +89,9 @@ export default function MyStream() {
             />
             <span className="tw-whitespace-nowrap">Memes Chat</span>
           </div>
-        </button>
+        </button> */}
         {/*  NOT ACTIVE STATE: tw-ring-iron-800 hover:tw-ring-iron-700 hover:tw-bg-iron-900 hover:tw-text-iron-300 tw-text-iron-400  */}
-        <button
+        {/* <button
           type="button"
           className="tw-ring-iron-800 hover:tw-ring-iron-700 hover:tw-bg-iron-900 hover:tw-text-iron-300 tw-text-iron-400 tw-ring-inset tw-ring-1 tw-border-0 tw-rounded-full tw-bg-iron-950 tw-px-3 tw-py-2.5 tw-font-medium tw-text-sm tw-transition tw-duration-300 tw-ease-out"
         >
@@ -102,15 +103,15 @@ export default function MyStream() {
             />
             <span className="tw-whitespace-nowrap">6529 Team</span>
           </div>
-        </button>
-      </div>
+        </button> */}
+      {/* </div> */}
 
       <div>
         {!items.length && !isFetching ? (
           <MyStreamNoItems />
         ) : (
           <>
-            <div className="tw-mt-6">
+            <div>
               <FeedWrapper
                 items={items}
                 loading={isFetching}
