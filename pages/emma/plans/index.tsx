ce7be0b10aaa9f25c2475bc0e8ca7f148c820e63
brookr <11095+brookr@users.noreply.github.com>
2024-08-30 00:38:02 -0700

    Expands width for wide screens
    Signed-off-by: brookr <11095+brookr@users.noreply.github.com>

--- pages/emma/plans/index.tsx
+++ pages/emma/plans/index.tsx
@@ -6,7 +6,7 @@ export default function DistributionPlanToolPlansPage() {
   return (
     <DistributionPlanToolWrapper>
       <div className="tw-flex tw-h-full tw-min-h-screen">
-        <div className="tw-flex-1 tw-pt-8 tw-space-y-8 tw-pb-12 tw-px-4 min-[1000px]:tw-max-w-[850px] min-[1100px]:tw-max-w-[950px] min-[1200px]:tw-max-w-[1050px] min-[1300px]:tw-max-w-[1150px] min-[1400px]:tw-max-w-[1250px] min-[1500px]:tw-max-w-[1280px] tw-mx-auto">
+        <div className="tw-flex-1 tw-pt-8 tw-space-y-8 tw-pb-12 tw-px-4 min-[1000px]:tw-max-w-[850px] min-[1100px]:tw-max-w-[950px] min-[1200px]:tw-max-w-[1050px] min-[1300px]:tw-max-w-[1150px] min-[1400px]:tw-max-w-[1250px] min-[1500px]:tw-max-w-full tw-mx-auto">
           <div className="tw-w-full">
             <div className="tw-w-full tw-flex tw-justify-between tw-items-start">
               <div className="tw-max-w-2xl">
