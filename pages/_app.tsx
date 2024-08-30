bf7f831837d23f5f2b177aa37586dff04d4da4ca
prxt6529 <prxt@6529.io>
2024-08-30 09:55:04 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -274,7 +274,7 @@ export default function App({ Component, ...rest }: AppPropsWithLayout) {
           <Head>
             <meta
               name="viewport"
-              content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+              content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
             />
           </Head>
         )}
