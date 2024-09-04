f58b88ecee8b31cc5fb5c76bbe6ef2ad3022caa0
Simo <info@analyticsflow.ee>
2024-09-04 15:33:14 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/waves/detailed/header/name/WaveHeaderName.tsx
+++ components/waves/detailed/header/name/WaveHeaderName.tsx
@@ -1,12 +1,37 @@
 import Link from "next/link";
 import { Wave } from "../../../../../generated/models/Wave";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../../auth/Auth";
+import WaveHeaderNameEdit from "./WaveHeaderNameEdit";
 
 export default function WaveHeaderName({ wave }: { readonly wave: Wave }) {
+  const { connectedProfile, activeProfileProxy } = useContext(AuthContext);
+  const getShowEdit = () => {
+    if (!connectedProfile?.profile?.handle) {
+      return false;
+    }
+    if (!!activeProfileProxy) {
+      return false;
+    }
+    if (wave.author.handle === connectedProfile.profile.handle) {
+      return true;
+    }
+    if (!!wave.wave.authenticated_user_eligible_for_admin) {
+      return true;
+    }
+    return false;
+  };
+
+  const [showEdit, setShowEdit] = useState(getShowEdit());
+  useEffect(() => setShowEdit(getShowEdit()), [connectedProfile, wave]);
   return (
-    <Link href={`/waves/${wave.id}`} className="tw-no-underline">
-      <h1 className="tw-truncate tw-text-xl sm:tw-text-2xl tw-font-semibold  tw-text-white hover:tw-text-iron-400 tw-transition tw-duration-300 tw-ease-out">
-        {wave.name}
-      </h1>
-    </Link>
+    <div className="tw-group tw-inline-flex tw-space-x-2">
+      <Link href={`/waves/${wave.id}`} className="tw-no-underline">
+        <h1 className="tw-truncate tw-text-xl sm:tw-text-2xl tw-font-semibold  tw-text-white hover:tw-text-iron-400 tw-transition tw-duration-300 tw-ease-out">
+          {wave.name}
+        </h1>
+      </Link>
+      {showEdit && <WaveHeaderNameEdit wave={wave} />}
+    </div>
   );
 }
--- helpers/waves/waves.helpers.ts
+++ helpers/waves/waves.helpers.ts
@@ -1,3 +1,5 @@
+import { CreateNewWave } from "../../generated/models/CreateNewWave";
+import { Wave } from "../../generated/models/Wave";
 import { CreateWaveStepStatus } from "../../types/waves.types";
 
 export const getCreateWaveStepStatus = ({
@@ -15,3 +17,53 @@ export const getCreateWaveStepStatus = ({
   }
   return CreateWaveStepStatus.PENDING;
 };
+
+export const convertWaveToCreateNewWave = (wave: Wave): CreateNewWave => ({
+  name: wave.name,
+  picture: wave.picture,
+  description_drop: {
+    title: wave.description_drop.title,
+    parts: wave.description_drop.parts,
+    referenced_nfts: wave.description_drop.referenced_nfts,
+    mentioned_users: wave.description_drop.mentioned_users,
+    metadata: wave.description_drop.metadata,
+  },
+  voting: {
+    scope: {
+      group_id: wave.voting.scope.group?.id ?? null,
+    },
+    credit_type: wave.voting.credit_type,
+    credit_scope: wave.voting.credit_scope,
+    credit_category: wave.voting.credit_category,
+    creditor_id: wave.voting.creditor?.id ?? null,
+    signature_required: wave.voting.signature_required,
+    period: wave.voting.period,
+  },
+  visibility: {
+    scope: {
+      group_id: wave.visibility.scope.group?.id ?? null,
+    },
+  },
+  participation: {
+    scope: {
+      group_id: wave.participation.scope.group?.id ?? null,
+    },
+    no_of_applications_allowed_per_participant:
+      wave.participation.no_of_applications_allowed_per_participant,
+    required_media: wave.participation.required_media,
+    required_metadata: wave.participation.required_metadata,
+    signature_required: wave.participation.signature_required,
+    period: wave.participation.period,
+  },
+  wave: {
+    type: wave.wave.type,
+    winning_thresholds: wave.wave.winning_thresholds,
+    max_winners: wave.wave.max_winners,
+    time_lock_ms: wave.wave.time_lock_ms,
+    admin_group: {
+      group_id: wave.wave.admin_group.group?.id ?? null,
+    },
+    period: wave.wave.period,
+  },
+  outcomes: wave.outcomes,
+});
