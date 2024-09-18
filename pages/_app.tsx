e4834d6d65fdd7d914ee879ff3db38fe4fc38112
prxt6529 <prxt@6529.io>
2024-09-18 08:36:38 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -111,7 +111,7 @@ import CookiesBanner from "../components/cookies/CookiesBanner";
 import { CookieConsentProvider } from "../components/cookies/CookieConsentContext";
 import { MANIFOLD_NETWORK } from "../hooks/useManifoldClaim";
 import { Capacitor } from "@capacitor/core";
-import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
+import { coinbaseWallet } from "wagmi/connectors";
 
 library.add(
   faArrowUp,
@@ -233,11 +233,10 @@ const connectors: any[] = [];
 
 if (isCapacitor) {
   connectors.push(
-    new CoinbaseWalletSDK({
+    coinbaseWallet({
       appName: "6529 CORE",
-      appLogoUrl:
-        "https://d3lqz0a4bldqgf.cloudfront.net/seize_images/Seize_Logo_Glasses_3.png",
-      appChainIds: chains.map((chain) => chain.id),
+      enableMobileWalletLink: true,
+      version: "3",
     })
   );
 }
