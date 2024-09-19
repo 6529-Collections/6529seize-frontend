a71938876ab9e97b085393a6e390e83c7750c489
Ragne <ragneinfo@gmail.com>
2024-09-19 13:54:36 +0300

    wip
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/waves/detailed/WaveDetailed.tsx
+++ components/waves/detailed/WaveDetailed.tsx
@@ -103,7 +103,7 @@ export default function WaveDetailed({ wave }: WaveDetailedProps) {
   }, [wave, isAuthorAndNotProxy]);
 
   const [activeView, setActiveView] = useState<WaveDetailedView>(
-    WaveDetailedView.CONTENT
+    WaveDetailedView.CHAT
   );
 
   const components: Record<WaveDetailedView, JSX.Element> = {
@@ -120,7 +120,7 @@ export default function WaveDetailed({ wave }: WaveDetailedProps) {
     [WaveDetailedView.FOLLOWERS]: (
       <WaveDetailedFollowers
         wave={wave}
-        onBackClick={() => setActiveView(WaveDetailedView.CONTENT)}
+        onBackClick={() => setActiveView(WaveDetailedView.CHAT)}
       />
     ),
   };
@@ -154,16 +154,6 @@ export default function WaveDetailed({ wave }: WaveDetailedProps) {
         <div className="tw-mb-4 md:tw-hidden">
           <div className="tw-flex tw-gap-x-3 lg:tw-gap-x-4">
             <button
-              onClick={() => setActiveView(WaveDetailedView.CONTENT)}
-              className={`tw-bg-transparent tw-text-base tw-font-semibold tw-border-b-2 tw-border-solid tw-border-x-0 tw-border-t-0 ${
-                activeView === WaveDetailedView.CONTENT
-                  ? "tw-border-primary-400 tw-text-iron-100 tw-whitespace-nowrap tw-font-semibold tw-py-3 tw-px-1"
-                  : "tw-border-transparent tw-text-iron-500 hover:tw-border-gray-300 hover:tw-text-iron-100 tw-whitespace-nowrap tw-border-b-2 tw-py-3 tw-px-1 tw-transition tw-duration-300 tw-ease-out"
-              }`}
-            >
-              About
-            </button>
-            <button
               onClick={() => setActiveView(WaveDetailedView.CHAT)}
               className={`tw-bg-transparent tw-text-base tw-font-semibold tw-border-solid tw-border-x-0 tw-border-t-0 tw-border-b-2 ${
                 activeView === WaveDetailedView.CHAT
@@ -173,14 +163,36 @@ export default function WaveDetailed({ wave }: WaveDetailedProps) {
             >
               Chat
             </button>
+            <button
+              onClick={() => setActiveView(WaveDetailedView.CONTENT)}
+              className={`tw-bg-transparent tw-text-base tw-font-semibold tw-border-b-2 tw-border-solid tw-border-x-0 tw-border-t-0 ${
+                activeView === WaveDetailedView.CONTENT
+                  ? "tw-border-primary-400 tw-text-iron-100 tw-whitespace-nowrap tw-font-semibold tw-py-3 tw-px-1"
+                  : "tw-border-transparent tw-text-iron-500 hover:tw-border-gray-300 hover:tw-text-iron-100 tw-whitespace-nowrap tw-border-b-2 tw-py-3 tw-px-1 tw-transition tw-duration-300 tw-ease-out"
+              }`}
+            >
+              About
+            </button>
           </div>
         </div>
         <div className="tw-flex tw-items-start tw-justify-center tw-gap-x-4">
-          <div className={`tw-h-[calc(100vh-160px)] tw-overflow-y-auto no-scrollbar md:tw-max-w-[20.5rem] tw-w-full ${activeView !== WaveDetailedView.CONTENT ? 'tw-hidden md:tw-block' : ''}`}>
+          <div
+            className={`tw-h-[calc(100vh-160px)] tw-overflow-y-auto no-scrollbar md:tw-max-w-[20.5rem] tw-w-full ${
+              activeView !== WaveDetailedView.CONTENT
+                ? "tw-hidden md:tw-block"
+                : ""
+            }`}
+          >
             {renderLeftColumn()}
           </div>
 
-          <div className={`tw-flex-1 ${activeView === WaveDetailedView.CONTENT ? 'tw-hidden md:tw-block' : ''}`}>
+          <div
+            className={`tw-flex-1 ${
+              activeView === WaveDetailedView.CONTENT
+                ? "tw-hidden md:tw-block"
+                : ""
+            }`}
+          >
             <div
               ref={contentWrapperRef}
               className="tw-rounded-xl tw-overflow-hidden tw-bg-iron-950 tw-ring-1 tw-ring-iron-800"
