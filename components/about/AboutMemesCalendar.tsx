3dd5269c8510fa94788577230389f8cb9e611367
prxt6529 <117092621+prxt6529@users.noreply.github.com>
2024-09-19 12:18:16 +0000

    Implement Memes Seasonal Calendar page (#526)
    Signed-off-by: prxt6529 <prxt@6529.io>

--- components/about/AboutMemesCalendar.tsx
+++ components/about/AboutMemesCalendar.tsx
@@ -1,63 +1,73 @@
-import { Col, Container, Row } from "react-bootstrap";
+import styles from "./About.module.scss";
+import { Col, Container, Row, Table } from "react-bootstrap";
 import { MEMES_CALENDARS } from "../../helpers/meme_calendar.helpers";
+import { Time } from "../../helpers/time";
+import { Fragment } from "react";
 
 export default function AboutMemesCalendar() {
+  function isActiveSection(start: Time, end: Time) {
+    const now = Time.now();
+    return start.lte(now) && end.gte(now);
+  }
+
   return (
     <Container>
       <Row>
-        <Col>
+        <Col sm={12} md={{ span: 10, offset: 1 }}>
           <h1>
             <span className="font-lightest">Memes</span> Seasonal Calendar
           </h1>
         </Col>
       </Row>
       {MEMES_CALENDARS.map((calendar) => (
-        <Row key={calendar.year}>
-          <Col sm={12} md={{ span: 8, offset: 2 }}>
-            <table
-              className="table table-bordered"
-              style={{
-                borderColor: "white",
-              }}
-            >
-              <thead>
-                <tr>
-                  <th
-                    colSpan={2}
-                    style={{ paddingTop: "25px", paddingBottom: "25px" }}
-                  >
-                    {calendar.year}: {calendar.title}
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {calendar.blocks.map((block) => (
-                  <tr key={block.title}>
-                    <td
-                      style={{
-                        border: "1px solid white",
-                        width: "50%",
-                        padding: "10px",
-                      }}
-                    >
-                      {block.start.toMonthAndDayString()} to{" "}
-                      {block.end.toMonthAndDayString()}
-                    </td>
-                    <td
-                      style={{
-                        border: "1px solid white",
-                        width: "50%",
-                        padding: "10px",
-                      }}
-                    >
-                      {block.title}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </Col>
-        </Row>
+        <Fragment key={calendar.year}>
+          <Row className="pt-3">
+            <Col
+              sm={12}
+              md={{ span: 10, offset: 1 }}
+              className="font-bolder font-larger">
+              {calendar.year}: {calendar.title}
+            </Col>
+          </Row>
+          <Row className="pt-2">
+            <Col sm={12} md={{ span: 10, offset: 1 }}>
+              <Table bordered={false} className={styles.calendarTable}>
+                <tbody>
+                  {calendar.blocks.map((block) => (
+                    <tr
+                      key={block.title}
+                      className={
+                        isActiveSection(block.start, block.end)
+                          ? styles.activeSection
+                          : ""
+                      }>
+                      <td>
+                        <Container className="no-padding">
+                          <Row>
+                            <Col sm={12} md={4}>
+                              <b>{block.title}</b>
+                            </Col>
+                            <Col sm={12} md={8}>
+                              {block.start.toMonthAndDayString()}
+                              <span
+                                style={{
+                                  paddingLeft: "10px",
+                                  paddingRight: "10px",
+                                }}>
+                                -
+                              </span>
+                              {block.end.toMonthAndDayString()}
+                            </Col>
+                          </Row>
+                        </Container>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </Fragment>
       ))}
     </Container>
   );
