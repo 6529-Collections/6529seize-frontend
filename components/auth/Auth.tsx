f324c4859e90a47a6502b2895117fab362db05cb
Simo <info@analyticsflow.ee>
2024-09-24 16:54:22 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/auth/Auth.tsx
+++ components/auth/Auth.tsx
@@ -61,8 +61,8 @@ type AuthContextType = {
   readonly title: string;
 };
 
-// TODO: Set back to 10
-export const WAVES_MIN_ACCESS_LEVEL = 0;
+
+export const WAVES_MIN_ACCESS_LEVEL = 10;
 const DEFAULT_TITLE = "6529 SEIZE";
 
 export const AuthContext = createContext<AuthContextType>({
