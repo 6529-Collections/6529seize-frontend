6d08936635ab748f427c9d6b2f2dfec3e8e5cf41
Simo <info@analyticsflow.ee>
2024-10-03 15:53:35 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/waves/detailed/WaveDetailedFollowingWaves.tsx
+++ components/waves/detailed/WaveDetailedFollowingWaves.tsx
@@ -11,14 +11,14 @@ import Link from "next/link";
 import { useIntersectionObserver } from "../../../hooks/useIntersectionObserver";
 import { WaveDropsFeed } from "../../../generated/models/WaveDropsFeed";
 import { useRouter } from "next/router";
-import { AnimatePresence, motion } from "framer-motion";
+import WaveDetailedFollowingWavesSort from "./WaveDetailedFollowingWavesSort";
 
 const PAGE_SIZE = 20;
 
 const WaveDetailedFollowingWaves: React.FC = () => {
   const queryClient = useQueryClient();
   const router = useRouter();
-  const dropdownRef = useRef<HTMLDivElement>(null);
+
   const queryKey = [
     QueryKey.DROPS,
     {
@@ -68,44 +68,12 @@ const WaveDetailedFollowingWaves: React.FC = () => {
   };
 
   const [waves, setWaves] = useState<Wave[]>(getWaves());
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState<string | null>(null);
-
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
-
-  const handleOptionSelect = (option: string) => {
-    if (selectedOption === option) {
-      setSelectedOption(null);
-      setIsDropdownOpen(false);
-    } else {
-      setSelectedOption(option);
-      setIsDropdownOpen(false);
-    }
-  };
 
   useEffect(() => {
     if (data) {
       setWaves(getWaves());
     }
   }, [data]);
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        dropdownRef.current &&
-        !dropdownRef.current.contains(event.target as Node)
-      ) {
-        setIsDropdownOpen(false);
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
 
   const onHover = (waveId: string) => {
     queryClient.prefetchQuery({
@@ -162,83 +130,7 @@ const WaveDetailedFollowingWaves: React.FC = () => {
               Waves you follow
             </p>
           </div>
-          <div className="tw-relative" ref={dropdownRef}>
-            <div className="tw-group">
-              <button
-                type="button"
-                onClick={toggleDropdown}
-                className="tw-border-0 tw-flex tw-items-center tw-gap-x-2 tw-justify-between tw-whitespace-nowrap tw-text-xs tw-font-medium tw-text-iron-400 hover:tw-text-primary-400 tw-bg-iron-950 tw-rounded-lg focus:tw-outline-none focus-visible:tw-ring-2 focus-visible:tw-ring-primary-400 focus-visible:tw-border-primary-400 tw-transition-colors tw-duration-300 tw-ease-out tw-px-2 tw-py-2 -tw-mr-2"
-              >
-                <span>{selectedOption || "Sort by"}</span>
-                <svg
-                  className="tw-size-4 tw-flex-shrink-0"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  aria-hidden="true"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m19.5 8.25-7.5 7.5-7.5-7.5"
-                  />
-                </svg>
-              </button>
-              <AnimatePresence>
-                {isDropdownOpen && (
-                  <motion.div
-                    initial={{ opacity: 0, y: -10 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    exit={{ opacity: 0, y: -10 }}
-                    transition={{ duration: 0.2 }}
-                    className="tw-absolute tw-z-10 tw-w-56 tw-right-0 tw-bottom-full tw-mb-1 tw-bg-iron-900 tw-ring-1 tw-ring-inset tw-ring-iron-800 tw-rounded-lg tw-shadow-lg tw-shadow-iron-950/50"
-                  >
-                    <div className="tw-py-2 tw-px-2 tw-space-y-1.5">
-                      {[
-                        "Latest Drops",
-                        "Most Subscribed",
-                        "Most Dropped",
-                        "Most Dropped by You",
-                        "Recently Dropped",
-                        "Recently Dropped by You",
-                      ].map((option) => (
-                        <div
-                          key={option}
-                          onClick={() => handleOptionSelect(option)}
-                          className={`tw-px-4 tw-py-2 tw-text-xs tw-rounded-xl tw-transition-colors tw-duration-300 tw-cursor-pointer tw-whitespace-nowrap tw-flex tw-justify-between tw-items-center ${
-                            selectedOption === option
-                              ? "tw-text-iron-100 tw-bg-iron-700"
-                              : "tw-text-iron-200 hover:tw-bg-iron-800 hover:tw-text-iron-100"
-                          }`}
-                        >
-                          {option}
-                          {selectedOption === option && (
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth="1.5"
-                              aria-hidden="true"
-                              stroke="currentColor"
-                              className="tw-w-4 tw-h-4"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M6 18L18 6M6 6l12 12"
-                              />
-                            </svg>
-                          )}
-                        </div>
-                      ))}
-                    </div>
-                  </motion.div>
-                )}
-              </AnimatePresence>
-            </div>
-          </div>
+          <WaveDetailedFollowingWavesSort />
         </div>
         <div className="tw-mt-2 tw-max-h-60 tw-overflow-y-auto tw-scrollbar-thin tw-scrollbar-thumb-iron-700 tw-scrollbar-track-iron-900">
           <div className="tw-flex tw-flex-col">
