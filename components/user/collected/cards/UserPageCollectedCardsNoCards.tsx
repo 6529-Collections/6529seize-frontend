b2a328895cff2678b8e5f502ee7383a72682f48d
prxt6529 <prxt@6529.io>
2024-10-04 12:20:35 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- components/user/collected/cards/UserPageCollectedCardsNoCards.tsx
+++ components/user/collected/cards/UserPageCollectedCardsNoCards.tsx
@@ -31,6 +31,7 @@ export default function UserPageCollectedCardsNoCards({
           case MEMES_SEASON.SZN6:
           case MEMES_SEASON.SZN7:
           case MEMES_SEASON.SZN8:
+          case MEMES_SEASON.SZN9:
             return `Congratulations, ${filters.szn} full setter!`;
           default:
             assertUnreachable(filters.szn);
