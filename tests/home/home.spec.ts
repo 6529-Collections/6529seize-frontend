c91669a4c61d78ebbda6190a7619b88dcdbecdb6
brookr <11095+brookr@users.noreply.github.com>
2024-10-07 14:35:28 -0700

    Fixes quadratic regex backtracking concerns
    Signed-off-by: brookr <11095+brookr@users.noreply.github.com>

--- tests/home/home.spec.ts
+++ tests/home/home.spec.ts
@@ -2,16 +2,18 @@ import { test, expect } from "@playwright/test";
 import { login } from "../testHelpers";
 
 test.describe("Home Page", () => {
-  test.beforeEach(async ({ page, baseURL }) => {
-    await login(page, baseURL!);
+  test.beforeEach(async ({ page }, testInfo) => {
+    // Avoid hammering the server (especially if running against staging):
+    await page.waitForTimeout(testInfo.project.metadata.testDelay);
+    await page.goto("/");
   });
 
   test("should display the Latest Drop section", async ({ page }) => {
     await page.goto("/");
-    await expect(page).toHaveTitle(/6529 SEIZE/);
+    await expect(page).toHaveTitle("6529 SEIZE");
 
-    const latestDropHeading = page.locator("h1", { hasText: "Latest Drop" });
-    await expect(latestDropHeading).toBeVisible();
+    const heading = page.locator("h1", { hasText: "Latest Drop" });
+    await expect(heading).toBeVisible();
 
     const memeTitle = page.locator('h3 a[href^="/the-memes/"]');
     await expect(memeTitle).toBeVisible();
