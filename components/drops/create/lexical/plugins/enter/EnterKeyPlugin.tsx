bafb938000c8a39a97f06d4106321be1cdfbd926
Simo <info@analyticsflow.ee>
2024-10-08 17:36:16 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/drops/create/lexical/plugins/enter/EnterKeyPlugin.tsx
+++ components/drops/create/lexical/plugins/enter/EnterKeyPlugin.tsx
@@ -1,7 +1,12 @@
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import { COMMAND_PRIORITY_HIGH, KEY_ENTER_COMMAND } from "lexical";
+import {
+  $getSelection,
+  $isRangeSelection,
+  COMMAND_PRIORITY_HIGH,
+  KEY_ENTER_COMMAND,
+} from "lexical";
 import { useEffect } from "react";
-
+import { $isListItemNode, $isListNode } from "@lexical/list";
 export default function EnterKeyPlugin({
   disabled,
   handleSubmit,
@@ -20,6 +25,20 @@ export default function EnterKeyPlugin({
         if (disabled || !canSubmitWithEnter()) {
           // Let the mention plugin handle the Enter key
           return false; // Allows the mention plugin to process the Enter key
+        }
+        const selection = $getSelection();
+        if ($isRangeSelection(selection)) {
+          const anchorNode = selection.anchor.getNode();
+          let parentNode = anchorNode.getParent();
+          if (parentNode === null) {
+            parentNode = anchorNode.getTopLevelElement();
+          }
+
+          // Check if the cursor is inside a list item
+          if ($isListItemNode(parentNode) || $isListNode(parentNode)) {
+            // Inside a list item: Let Lexical handle the 'Enter' key as usual
+            return false; // Returning false allows default Lexical behavior
+          }
         }
 
         if (event?.shiftKey) {
