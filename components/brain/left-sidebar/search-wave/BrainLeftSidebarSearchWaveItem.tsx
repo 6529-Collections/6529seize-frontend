cd5a57f93c6755c29df358dfe2a13e2c0a4f6c9b
Ragne <ragneinfo@gmail.com>
2024-11-01 16:45:27 +0200

    wip
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/brain/left-sidebar/search-wave/BrainLeftSidebarSearchWaveItem.tsx
+++ components/brain/left-sidebar/search-wave/BrainLeftSidebarSearchWaveItem.tsx
@@ -90,22 +90,20 @@ const BrainLeftSidebarSearchWaveItem: React.FC<
       <Tippy content="Go to wave">
         <Link
           href={`/waves/${wave.id}`}
-          className="tw-size-7 sm:tw-size-6 desktop-hover:hover:tw-bg-iron-700 tw-bg-transparent tw-flex tw-items-center tw-justify-center tw-border-0 tw-flex-shrink-0 tw-rounded-full tw-text-iron-400 desktop-hover:hover:tw-text-iron-50 tw-transition tw-duration-300 tw-ease-out focus-visible:tw-outline-none focus-visible:tw-ring-1 focus-visible:tw-ring-primary-400"
-          aria-label="Open chat"
+          className={`tw-mt-1.5 tw-no-underline tw-rounded-lg tw-bg-iron-800 tw-flex tw-items-center tw-justify-center tw-border-0 tw-flex-shrink-0 tw-text-iron-400 desktop-hover:hover:tw-text-iron-50 tw-transition tw-duration-300 tw-ease-out hover:tw-bg-iron-700 focus-visible:tw-outline-none focus-visible:tw-ring-1 focus-visible:tw-ring-primary-400 tw-size-6`}
         >
           <svg
+            className="tw-size-4 tw-flex-shrink-0"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             strokeWidth="1.5"
             stroke="currentColor"
-            aria-hidden="true"
-            className="tw-size-5 sm:tw-size-4 tw-flex-shrink-0"
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+              d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
             />
           </svg>
         </Link>
