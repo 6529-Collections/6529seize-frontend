8607afbe15b93832f287863aff29cba932157698
prxt6529 <prxt@6529.io>
2024-11-20 16:17:24 +0200

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,16 +1,93 @@
 /** @type {import("next-sitemap").IConfig} */
+
+const fetchPaginatedData = async (url) => {
+  let results = [];
+  let nextPage = url;
+
+  while (nextPage) {
+    const response = await fetch(nextPage);
+    try {
+      const result = await response.json();
+      results = [...results, ...result.data];
+      nextPage = result.next;
+    } catch (error) {
+      console.error(`error for ${nextPage}`);
+      throw error;
+    }
+  }
+
+  return results;
+};
+
+const getVariationsPaths = async (sitePath, apiPath) => {
+  const nftPaths = (
+    await fetchPaginatedData(`https://api.staging.seize.io/sitemap/${apiPath}`)
+  ).flatMap((id) => {
+    const basePath = `${sitePath}/${id}`;
+    return getNftVariations(basePath);
+  });
+  return nftPaths;
+};
+
+const getPlainPaths = async (sitePath, apiPath) => {
+  const plainPaths = (
+    await fetchPaginatedData(`https://api.staging.seize.io/sitemap/${apiPath}`)
+  ).map((id) => ({
+    loc: `/${sitePath}/${id}`,
+    changefreq: "daily",
+    priority: 0.7,
+  }));
+  return plainPaths;
+};
+
+const getNftVariations = (basePath) => {
+  const queryVariations = [
+    "live",
+    "your-cards",
+    "the-art",
+    "collectors",
+    "activity",
+    "timeline",
+  ];
+
+  return [
+    { loc: basePath, changefreq: "daily", priority: 0.7 },
+    ...queryVariations.map((focus) => ({
+      loc: `${basePath}?focus=${focus}`,
+      changefreq: "daily",
+      priority: 0.7,
+    })),
+  ];
+};
+
 module.exports = {
   siteUrl: "https://seize.io",
   generateRobotsTxt: true,
   changefreq: "daily",
   exclude: ["/access", "/restricted", "/nextgen/manager"],
   additionalPaths: async (config) => {
-    const additionalPaths = AboutSections.map((section) => ({
-      loc: `/about/${section}`, // Required
+    const aboutPaths = AboutSections.map((section) => ({
+      loc: `/about/${section}`,
       changefreq: "daily",
       priority: 0.7,
     }));
-    return additionalPaths;
+
+    const memesPaths = await getVariationsPaths("/the-memes", "memes");
+    const gradientPaths = await getPlainPaths("/6529-gradient", "gradient");
+    const memeLabPaths = await getVariationsPaths("/meme-lab", "meme-lab");
+
+    const nextgenTokensPaths = await getPlainPaths(
+      "/nextgen/token",
+      "nextgen/tokens"
+    );
+
+    return [
+      ...aboutPaths,
+      ...memesPaths,
+      ...gradientPaths,
+      ...memeLabPaths,
+      ...nextgenTokensPaths,
+    ];
   },
   transform: async (config, path) => {
     return {
