0d18c643be995bb6b2ada05a2371e99512b3d53e
analyticsflowee <49652774+analyticsflowee@users.noreply.github.com>
2024-12-09 15:03:12 +0200

    wip (#597)
    * required metadata only for drops
    
    Signed-off-by: Simo <info@analyticsflow.ee>
    
    ---------
    
    Signed-off-by: Simo <info@analyticsflow.ee>

---
+++ components/waves/detailed/utils/getMissingRequirements.ts
@@ -0,0 +1,48 @@
+import { ApiWaveParticipationRequirement } from "../../../../generated/models/ApiWaveParticipationRequirement";
+import { CreateDropMetadataType } from "../CreateDropContent";
+
+export interface MissingRequirements {
+  metadata: string[];
+  media: ApiWaveParticipationRequirement[];
+}
+
+const isRequiredMetadataMissing = (item: CreateDropMetadataType): boolean => {
+  return item.required && (item.value === null || item.value === undefined || item.value === "");
+};
+
+const isMediaTypeMatching = (file: File, mediaType: ApiWaveParticipationRequirement): boolean => {
+  switch (mediaType) {
+    case ApiWaveParticipationRequirement.Image:
+      return file.type.startsWith("image/");
+    case ApiWaveParticipationRequirement.Audio:
+      return file.type.startsWith("audio/");
+    case ApiWaveParticipationRequirement.Video:
+      return file.type.startsWith("video/");
+    default:
+      return false;
+  }
+};
+
+export const getMissingRequirements = (
+  isDropMode: boolean,
+  metadata: CreateDropMetadataType[],
+  files: File[],
+  requiredMedia: ApiWaveParticipationRequirement[]
+): MissingRequirements => {
+  if (!isDropMode) {
+    return {
+      metadata: [],
+      media: [],
+    };
+  }
+  const getMissingMetadata = () =>
+    metadata.filter(isRequiredMetadataMissing).map((item) => item.key);
+
+  const getMissingMedia = () =>
+    requiredMedia.filter((media) => !files.some((file) => isMediaTypeMatching(file, media)));
+
+  return {
+    metadata: getMissingMetadata(),
+    media: getMissingMedia(),
+  };
+}; 
