88a4ac7cf8f3971fedc5f2b113f5c7f4479f4948
Ragne <ragneinfo@gmail.com>
2024-12-12 16:02:42 +0200

    wip
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/brain/content/BrainContent.tsx
+++ components/brain/content/BrainContent.tsx
@@ -3,6 +3,7 @@ import BrainContentPinnedWaves from "./BrainContentPinnedWaves";
 import BrainContentInput from "./input/BrainContentInput";
 import { useWaveData } from "../../../hooks/useWaveData";
 import { ActiveDropState } from "../../waves/detailed/chat/WaveChat";
+import FilterDrops from "../../common/FilterDrops";
 
 interface BrainContentProps {
   readonly children: React.ReactNode;
@@ -30,15 +31,20 @@ const BrainContent: React.FC<BrainContentProps> = ({
         onCancelReplyQuote={onCancelReplyQuote}
       />
       <div className="tw-flex-1">
-        {!!wave && (
-          <div className="tw-flex tw-items-center tw-gap-x-2 tw-mb-2 tw-mt-2">
-            <h2 className="tw-mb-0 tw-text-sm tw-font-medium tw-text-iron-200">
-              <span>{wave.name}</span>{" "}
-              <span className="tw-text-iron-400">Stream</span>
-            </h2>
-            <div className="tw-flex-1 tw-h-px tw-bg-iron-900" />
-          </div>
-        )}
+        <div className="tw-flex tw-items-center tw-gap-x-2 tw-mb-2 tw-mt-2">
+          {!!wave ? (
+            <>
+              <h2 className="tw-mb-0 tw-text-sm tw-font-medium tw-text-iron-200">
+                <span>{wave.name}</span>{" "}
+                <span className="tw-text-iron-400">Stream</span>
+              </h2>
+              <div className="tw-flex-1 tw-h-px tw-bg-iron-900" />
+              <FilterDrops />
+            </>
+          ) : (
+            <FilterDrops />
+          )}
+        </div>
         <div>{children}</div>
       </div>
     </div>
--- components/waves/detailed/WaveDetailedDesktopTabs.tsx
+++ components/waves/detailed/WaveDetailedDesktopTabs.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { WaveDetailedView } from "./WaveDetailed";
 import { TabToggle } from "../../common/TabToggle";
-import Link from "next/link";
+import FilterDrops from "../../common/FilterDrops";
 
 interface WaveDetailedDesktopTabsProps {
   readonly activeTab: WaveDetailedView;
   readonly setActiveTab: (tab: WaveDetailedView) => void;
 }
 
-export const WaveDetailedDesktopTabs: React.FC<WaveDetailedDesktopTabsProps> = ({
-  activeTab,
-  setActiveTab,
-}) => {
+export const WaveDetailedDesktopTabs: React.FC<
+  WaveDetailedDesktopTabsProps
+> = ({ activeTab, setActiveTab }) => {
   const options = [
     { key: WaveDetailedView.CHAT, label: "Chat" },
     { key: WaveDetailedView.LEADERBOARD, label: "Leaderboard" },
@@ -19,21 +18,18 @@ export const WaveDetailedDesktopTabs: React.FC<WaveDetailedDesktopTabsProps> = (
   ] as const;
 
   return (
-   <div className="tw-flex tw-items-center tw-gap-4">
-     <TabToggle
-      options={options}
-      activeKey={activeTab}
-      onSelect={(key) => setActiveTab(key as WaveDetailedView)}
-    />
-     <div className="tw-flex tw-items-center tw-text-xs tw-text-iron-300">
-       <span>
-         Rank is in testing mode. Please report bugs in the{" "}
-         <Link href="/waves/dc6e0569-e4a3-4122-bc20-ee66c76981f5" className="tw-underline tw-text-iron-300 tw-transition-all tw-duration-300 hover:tw-text-white">
-           Rank Alpha Debugging
-         </Link>{" "}
-         wave.
-       </span>
-     </div>
-   </div>
+    <div className="tw-flex tw-items-center tw-gap-4 tw-justify-between tw-w-full">
+      <TabToggle
+        options={options}
+        activeKey={activeTab}
+        onSelect={(key) => setActiveTab(key as WaveDetailedView)}
+      />
+
+      {activeTab === WaveDetailedView.CHAT && (
+        <div className="tw-pr-4">
+          <FilterDrops />
+        </div>
+      )}
+    </div>
   );
 };
