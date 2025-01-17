6b1abbc060cc808a6b7bf58789867c66091ecea4
Ragne <ragneinfo@gmail.com>
2025-01-17 14:07:41 +0200

    wip
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- services/auth/auth.utils.ts
+++ services/auth/auth.utils.ts
@@ -28,13 +28,27 @@ export const setAuthJwt = (
   }
 };
 
+
+export const getAuthJwt = () => {
+  return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQ1Y2I4YzlkLTExNDQtNDlkMi1iMTUzLTk3NGIyZTY4ZWVkOSIsInN1YiI6IjB4Y2U4ZDcyOWEyYmZhZGQ2OTRjM2EyZDA0YzQ3ZDJjODk2OTBjNzNmMSIsImlhdCI6MTczNzEwOTMwOCwiZXhwIjoxNzM3Mjc1NzA4fQ.xluO-2_vResBuI-rurDsu0K14RoQCizrBk11xirz-I8"
+};
+
+export const getRefreshToken = () => {
+  return "a6460fa0c4fe0f66ea54f0d1cf54718afcc8497d797388933e5aa0b1fb179a73260817d07489281eedc39672e17465bd247d8d29d21bdb4aae3aba6f0073a375"
+};
+
+export const getWalletAddress = () => {
+  return "0xce8d729a2bfadd694c3a2d04c47d2c89690c73f1"
+};
+
+
 export const getWalletType = () => {
   return Cookies.get(WALLET_TYPE_COOKIE) ?? null;
 };
 
-export const getAuthJwt = () => {
+/* export const getAuthJwt = () => {
   return Cookies.get(WALLET_AUTH_COOKIE) ?? null;
-};
+}; 
 
 export const getRefreshToken = () => {
   return Cookies.get(WALLET_REFRESH_TOKEN_COOKIE) ?? null;
@@ -43,6 +57,7 @@ export const getRefreshToken = () => {
 export const getWalletAddress = () => {
   return Cookies.get(WALLET_ADDRESS_COOKIE) ?? null;
 };
+*/
 
 export const getWalletRole = () => {
   return Cookies.get(WALLET_ROLE_COOKIE) ?? null;
