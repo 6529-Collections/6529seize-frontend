abcefe3d449d524f997f328b952f87e7c98daef9
prxt6529 <prxt@6529.io>
2025-01-30 11:48:00 +0200

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- components/header/qr/HeaderQRScanner.tsx
+++ components/header/qr/HeaderQRScanner.tsx
@@ -7,6 +7,7 @@ import { useState, useEffect } from "react";
 import useCapacitor from "../../../hooks/useCapacitor";
 import { useAuth } from "../../auth/Auth";
 import { DeepLinkScope } from "../capacitor/CapacitorWidget";
+import Image from "next/image";
 
 export default function HeaderQRScanner() {
   const appScheme = process.env.MOBILE_APP_SCHEME ?? "mobile6529";
@@ -116,20 +117,12 @@ export default function HeaderQRScanner() {
 
 function HeaderQRScannerIcon() {
   return (
-    <svg
-      height="20"
-      width="20"
-      fill="currentColor"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg">
-      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-      <g
-        id="SVGRepo_tracerCarrier"
-        strokeLinecap="round"
-        strokeLinejoin="round"></g>
-      <g id="SVGRepo_iconCarrier">
-        <path d="M4,4h6v6H4V4M20,4v6H14V4h6M14,15h2V13H14V11h2v2h2V11h2v2H18v2h2v3H18v2H16V18H13v2H11V16h3V15m2,0v3h2V15H16M4,20V14h6v6H4M6,6V8H8V6H6M16,6V8h2V6H16M6,16v2H8V16H6M4,11H6v2H4V11m5,0h4v4H11V13H9V11m2-5h2v4H11V6M2,2V6H0V2A2,2,0,0,1,2,0H6V2H2M22,0a2,2,0,0,1,2,2V6H22V2H18V0h4M2,18v4H6v2H2a2,2,0,0,1-2-2V18H2m20,4V18h2v4a2,2,0,0,1-2,2H18V22Z"></path>{" "}
-      </g>
-    </svg>
+    <Image
+      src="/barcode-scanner.png"
+      alt="QR Scanner"
+      width={20}
+      height={20}
+      className="tw-h-5 tw-w-5 tw-flex-shrink-0"
+    />
   );
 }
