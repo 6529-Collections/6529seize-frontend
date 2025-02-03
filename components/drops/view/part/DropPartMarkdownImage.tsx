fb06aaa830c68bea9f95358ce2a63710c143ba0a
Ragne <ragneinfo@gmail.com>
2025-02-03 18:02:11 +0200

    changed twitter card styles in small screen so it fits better
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/drops/view/part/DropPartMarkdownImage.tsx
+++ components/drops/view/part/DropPartMarkdownImage.tsx
@@ -112,7 +112,7 @@ const DropPartMarkdownImage: React.FC<DropPartMarkdownImageProps> = ({
 
   return (
     <>
-      <div className="tw-relative tw-w-full tw-max-w-lg tw-mt-3 tw-flex">
+      <div className="tw-relative tw-w-full sm:tw-max-w-lg tw-mt-3 tw-flex">
         {isLoading && (
           <div
             className="tw-absolute tw-inset-0 tw-bg-iron-800 tw-animate-pulse tw-rounded-xl"
