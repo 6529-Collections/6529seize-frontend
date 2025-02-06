c5cdec9b3e8a73bceae7e3d7c6cfd50e827a47d9
Ragne <135017495+ragnep@users.noreply.github.com>
2025-02-06 14:11:31 +0200

    remove-scroll (#656)
    Removed extra scroll from the left sidebar in Brain

--- components/brain/left-sidebar/BrainLeftSidebar.tsx
+++ components/brain/left-sidebar/BrainLeftSidebar.tsx
@@ -12,8 +12,7 @@ const BrainLeftSidebar: React.FC<BrainLeftSidebarProps> = ({
 }) => {
   return (
     <div
-      className="tw-flex-shrink-0 tw-relative tw-flex tw-flex-col h-screen lg:tw-h-[calc(100vh-5.5rem)] tw-overflow-y-auto lg:tw-w-[20.5rem] tw-w-full tw-scrollbar-thin tw-scrollbar-thumb-iron-600 tw-scrollbar-track-iron-800 
-        hover:tw-scrollbar-thumb-iron-300 lg:tw-pr-2"
+      className="tw-flex-shrink-0 tw-relative tw-flex tw-flex-col h-screen lg:tw-h-[calc(100vh-5.5rem)] tw-overflow-y-auto lg:tw-w-[20.5rem] tw-w-full no-scrollbar"
     >
       <div className="tw-pt-4 tw-pb-4 tw-flex-1 tw-px-4 md:tw-px-2 lg:tw-px-0 tw-gap-y-4 tw-flex-col tw-flex">
         <BrainLeftSidebarViewChange />
