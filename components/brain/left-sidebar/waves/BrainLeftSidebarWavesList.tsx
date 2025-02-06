a19e9d824fc303f76c286bb536a3f4e80dde76d0
Ragne <ragneinfo@gmail.com>
2025-02-06 12:46:49 +0200

    wip
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/brain/left-sidebar/waves/BrainLeftSidebarWavesList.tsx
+++ components/brain/left-sidebar/waves/BrainLeftSidebarWavesList.tsx
@@ -55,7 +55,7 @@ const BrainLeftSidebarWavesList: React.FC<BrainLeftSidebarWavesListProps> = ({
 
   return (
     <div className="tw-mb-4">
-      <div className="tw-h-full tw-bg-iron-950 tw-rounded-xl tw-ring-1 tw-ring-inset tw-ring-iron-800 tw-pt-5 tw-pb-2">
+      <div className="tw-h-full tw-bg-iron-950 tw-rounded-xl tw-ring-1 tw-ring-inset tw-ring-iron-800 tw-py-4">
         <div className="tw-flex tw-flex-col tw-gap-y-1.5 tw-px-5">
           <div className="tw-flex tw-justify-between tw-items-center">
             <p className="tw-mb-0 tw-text-lg sm:tw-text-xl tw-font-semibold tw-text-iron-200 tw-tracking-tight">
@@ -76,7 +76,7 @@ const BrainLeftSidebarWavesList: React.FC<BrainLeftSidebarWavesListProps> = ({
             />
           </div>
         </div>
-        <div className="tw-mt-2 tw-max-h-96 tw-overflow-y-auto tw-scrollbar-thin tw-scrollbar-thumb-iron-600 tw-scrollbar-track-iron-900">
+        <div className="tw-mt-2 tw-max-h-96 tw-pb-2 tw-overflow-y-auto tw-scrollbar-thin tw-scrollbar-thumb-iron-600 tw-scrollbar-track-iron-900">
           <div className="tw-flex tw-flex-col">
             {memoizedWaves.map((wave) => (
               <BrainLeftSidebarWave
@@ -94,7 +94,7 @@ const BrainLeftSidebarWavesList: React.FC<BrainLeftSidebarWavesListProps> = ({
             <div ref={intersectionElementRef}></div>
           </div>
         </div>
-        <div className="tw-px-4 tw-pt-2 tw-border-t tw-border-iron-800 tw-border-x-0 tw-border-b-0 tw-border-solid">
+        <div className="tw-px-4 tw-mt-2">
           <BrainLeftSidebarCreateAWaveButton />
         </div>
       </div>
