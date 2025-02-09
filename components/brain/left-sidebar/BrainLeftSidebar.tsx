39a1e7227c448a269839ff36a1c0e19ad25da72f
Ragne <ragneinfo@gmail.com>
2025-02-09 17:58:04 +0200

    wip
    Signed-off-by: Ragne <ragneinfo@gmail.com>

--- components/brain/left-sidebar/BrainLeftSidebar.tsx
+++ components/brain/left-sidebar/BrainLeftSidebar.tsx
@@ -12,9 +12,6 @@ const BrainLeftSidebar: React.FC<BrainLeftSidebarProps> = ({
 }) => {
   return (
     <div className="tw-flex-shrink-0 tw-relative tw-flex tw-flex-col h-screen lg:tw-h-[calc(100vh-5.5rem)] tw-overflow-y-auto lg:tw-w-80 tw-w-full no-scrollbar">
-    <div
-      className="tw-flex-shrink-0 tw-relative tw-flex tw-flex-col h-screen lg:tw-h-[calc(100vh-5.5rem)] tw-overflow-y-auto lg:tw-w-[20.5rem] tw-w-full no-scrollbar"
-    >
       <div className="tw-pt-4 tw-pb-4 tw-flex-1 tw-px-4 md:tw-px-2 lg:tw-px-0 tw-gap-y-4 tw-flex-col tw-flex">
         <BrainLeftSidebarViewChange />
         <BrainLeftSidebarSearchWave />
