eefc7b408fa03b3c293c100834425b6e83b50283
prxt6529 <prxt@6529.io>
2025-02-10 12:00:38 +0200

    Disable background tasks in capacitor
    Signed-off-by: prxt6529 <prxt@6529.io>

--- hooks/useUnreadNotifications.ts
+++ hooks/useUnreadNotifications.ts
@@ -1,12 +1,29 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { ApiNotificationsResponse } from "../generated/models/ApiNotificationsResponse";
-
 import { commonApiFetch } from "../services/api/common-api";
 import { QueryKey } from "../components/react-query-wrapper/ReactQueryWrapper";
+import useCapacitor from "./useCapacitor";
 
 export function useUnreadNotifications(handle: string | undefined) {
-  const { data: notifications } = useQuery<ApiNotificationsResponse>({
+  const { isCapacitor } = useCapacitor();
+
+  const [refetchEnabled, setRefetchEnabled] = useState(
+    !document.hidden || !isCapacitor
+  );
+  const refetchIntervalRef = useRef<number | undefined>(30000);
+
+  useEffect(() => {
+    const handleVisibilityChange = () => {
+      setRefetchEnabled(!document.hidden || !isCapacitor);
+    };
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+    return () =>
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+  }, []);
+
+  const { data: notifications, refetch } = useQuery<ApiNotificationsResponse>({
     queryKey: [
       QueryKey.IDENTITY_NOTIFICATIONS,
       { identity: handle, limit: "1" },
@@ -19,11 +36,11 @@ export function useUnreadNotifications(handle: string | undefined) {
         },
       }),
     enabled: !!handle,
-    refetchInterval: 30000,
+    refetchInterval: refetchEnabled ? refetchIntervalRef.current : false,
     refetchOnWindowFocus: true,
     refetchOnMount: true,
     refetchOnReconnect: true,
-    refetchIntervalInBackground: true,
+    refetchIntervalInBackground: false,
   });
 
   const [haveUnreadNotifications, setHaveUnreadNotifications] = useState(false);
