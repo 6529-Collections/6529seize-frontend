967c571947182c0ab71baeef6caa4b15292147dc
prxt6529 <prxt@6529.io>
2025-03-11 15:21:22 +0200

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- hooks/useWaveNotificationSubscription.ts
+++ hooks/useWaveNotificationSubscription.ts
@@ -2,13 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import { commonApiFetch } from "../services/api/common-api";
 import { ApiWave } from "../generated/models/ApiWave";
 import { useSeizeSettings } from "../contexts/SeizeSettingsContext";
+import { GetWaveSubscription200Response } from "../generated/models/GetWaveSubscription200Response";
 
 export function useWaveNotificationSubscription(wave: ApiWave) {
   const seizeSettings = useSeizeSettings();
   return useQuery({
     queryKey: ["wave-notification-subscription", wave.id],
     queryFn: () => {
-      return commonApiFetch<{ subscribed: boolean }>({
+      return commonApiFetch<GetWaveSubscription200Response>({
         endpoint: `notifications/wave-subscription/${wave.id}`,
       });
     },
