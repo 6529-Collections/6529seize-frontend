7c0cb06186a31adc678e310ab6be86058f77c98c
prxt6529 <prxt@6529.io>
2025-03-17 16:40:05 +0200

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- components/brain/mobile/BrainMobileAbout.tsx
+++ components/brain/mobile/BrainMobileAbout.tsx
@@ -39,7 +39,7 @@ const BrainMobileAbout: React.FC<BrainMobileAboutProps> = ({
   const containerClassName = `tw-h-[calc(100vh-9.5rem)] tw-px-2 sm:tw-px-4 md:tw-px-6 tw-overflow-y-auto no-scrollbar tw-divide-y tw-divide-solid tw-divide-iron-800 tw-divide-x-0${
     capacitor.isIos
       ? "tw-pb-[calc(4rem+80px)]"
-      : capacitor.isAndroid
+      : capacitor.isAndroid && !capacitor.keyboardVisible
       ? "tw-pb-[70px]"
       : ""
   }`;
