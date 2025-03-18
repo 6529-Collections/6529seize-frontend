1bacb66bb2aacc0bb8b52da1e1d88b704ba36d40
prxt6529 <prxt@6529.io>
2025-03-18 13:09:09 +0200

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- components/brain/my-stream/layout/MyStreamLayout.tsx
+++ components/brain/my-stream/layout/MyStreamLayout.tsx
@@ -30,11 +30,11 @@ export default function MyStreamLayout({
   useEffect(() => setTitle({ title: "My Stream | 6529 SEIZE" }), []);
 
   const capacitor = useCapacitor();
-  let containerClassName = `lg:tw-pt-4 tw-relative tw-flex tw-flex-col tw-h-[calc(100vh-9.5rem)] lg:tw-h-full lg:tw-flex-1 tailwind-scope`;
+  let containerClassName = `tw-relative tw-flex tw-flex-col tw-flex-1 tailwind-scope`;
   if (capacitor.isIos) {
-    containerClassName = `${containerClassName} tw-pb-[calc(4rem+80px)]`;
+    containerClassName = `${containerClassName} tw-h-[calc(100vh-9.5rem)] tw-pb-[calc(4rem+80px)]`;
   } else if (capacitor.isAndroid && !capacitor.keyboardVisible) {
-    containerClassName = `${containerClassName} tw-pb-[70px]`;
+    containerClassName = `${containerClassName} tw-h-[calc(100vh-9.5rem)] tw-pb-[70px]`;
   }
 
   return (
