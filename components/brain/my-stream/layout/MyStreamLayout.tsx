f41ff35fb67c4a8689db8d970eb112c7d0c1fa31
prxt6529 <prxt@6529.io>
2025-03-18 15:09:03 +0200

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- components/brain/my-stream/layout/MyStreamLayout.tsx
+++ components/brain/my-stream/layout/MyStreamLayout.tsx
@@ -33,7 +33,7 @@ export default function MyStreamLayout({
   let containerClassName = `tw-relative tw-flex tw-flex-col tw-flex-1 tailwind-scope`;
   if (capacitor.isIos) {
     containerClassName = `${containerClassName} tw-h-[calc(100vh-9.5rem)] tw-pb-[calc(4rem+80px)]`;
-  } else if (capacitor.isAndroid) {
+  } else if (capacitor.isAndroid && !capacitor.keyboardVisible) {
     containerClassName = `${containerClassName} tw-h-[calc(100vh-9.5rem)] tw-pb-[calc(4rem+10px)]`;
   }
 
