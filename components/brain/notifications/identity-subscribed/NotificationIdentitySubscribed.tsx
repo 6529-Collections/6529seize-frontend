f9bf6af92b244891d4195249c197d98c5478ea49
ragnep <ragneinfo@gmail.com>
2025-03-21 13:09:12 +0200

    wip
    Signed-off-by: ragnep <ragneinfo@gmail.com>

--- components/brain/notifications/identity-subscribed/NotificationIdentitySubscribed.tsx
+++ components/brain/notifications/identity-subscribed/NotificationIdentitySubscribed.tsx
@@ -15,7 +15,7 @@ export default function NotificationIdentitySubscribed({
   readonly notification: INotificationIdentitySubscribed;
 }) {
   return (
-    <div className="tw-w-full tw-flex tw-items-start tw-gap-x-2 tw-flex-wrap">
+    <div className="tw-w-full tw-flex tw-items-start sm:tw-items-center tw-gap-x-3 tw-flex-wrap">
       <div className="tw-h-7 tw-w-7 tw-flex-shrink-0">
         {notification.related_identity.pfp ? (
           <img
@@ -30,27 +30,27 @@ export default function NotificationIdentitySubscribed({
           <div className="tw-flex-shrink-0 tw-object-contain tw-h-full tw-w-full tw-rounded-md tw-bg-iron-800 tw-ring-1 tw-ring-iron-700" />
         )}
       </div>
-      
-      <div className="tw-flex tw-flex-1 tw-min-w-0 tw-items-center tw-flex-wrap tw-gap-x-2">
-        <span className="tw-text-sm tw-font-normal tw-text-iron-50">
-          <Link
-            href={`/${notification.related_identity.handle}`}
-            className="tw-no-underline tw-font-semibold">
-            {notification.related_identity.handle}
-          </Link>{" "}
-          started following you
-        </span>
+      <div className="tw-flex tw-flex-col sm:tw-flex-row sm:tw-items-center sm:tw-flex-1 sm:tw-justify-between tw-gap-x-3 tw-gap-y-1">
+        <div className="tw-flex tw-flex-col sm:tw-flex-row sm:tw-items-center tw-gap-x-1 tw-gap-y-1">
+          <span className="tw-text-sm tw-font-normal tw-text-iron-400">
+            <Link
+              href={`/${notification.related_identity.handle}`}
+              className="tw-no-underline tw-font-semibold">
+              {notification.related_identity.handle}
+            </Link>{" "}
+            started following you
+          </span>
+          <span className="tw-size-[3px] tw-bg-iron-600 tw-rounded-full tw-flex-shrink-0 tw-hidden sm:tw-block"></span>
+          <span className="tw-text-sm tw-text-iron-500 tw-font-normal tw-whitespace-nowrap">
+            {getTimeAgoShort(notification.created_at)}
+          </span>
+        </div>
 
-        <div className="tw-w-1 tw-h-1 tw-rounded-full tw-bg-iron-600 tw-flex-shrink-0"></div>
-        <span className="tw-text-sm tw-text-iron-500 tw-font-normal tw-whitespace-nowrap">
-          {getTimeAgoShort(notification.created_at)}
-        </span>
+        <UserFollowBtn
+          handle={notification.related_identity.handle}
+          size={UserFollowBtnSize.SMALL}
+        />
       </div>
-      
-      <UserFollowBtn
-        handle={notification.related_identity.handle}
-        size={UserFollowBtnSize.SMALL}
-      />
     </div>
   );
 }
