aaa6b319a1d0ac46e83c9d4b1b008e05ffa37fde
prxt6529 <117092621+prxt6529@users.noreply.github.com>
2025-03-24 18:34:54 +0200

    Notification Filter padding fix (#684)
    Signed-off-by: prxt6529 <prxt@6529.io>

--- components/brain/notifications/NotificationsCauseFilter.tsx
+++ components/brain/notifications/NotificationsCauseFilter.tsx
@@ -92,7 +92,7 @@ export default function NotificationsCauseFilter({
   const isActive = (filter: NotificationFilter) => activeFilter === filter;
 
   return (
-    <div className="tw-p-2 tw-w-full">
+    <div className="tw-pb-2 tw-w-full">
       <div
         ref={containerRef}
         className="tw-relative tw-flex tw-nowrap tw-items-center tw-gap-1 tw-h-10 tw-bg-iron-950 tw-border tw-border-solid tw-border-iron-800 tw-rounded-lg tw-overflow-x-auto">
