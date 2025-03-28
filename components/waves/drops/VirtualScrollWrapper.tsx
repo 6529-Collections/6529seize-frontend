70630a9e61cc29cc35251acab3dc9d17b4b47e78
analyticsflowee <49652774+analyticsflowee@users.noreply.github.com>
2025-03-28 18:32:59 +0200

    added tweet to novirtual scroll (#706)
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/waves/drops/VirtualScrollWrapper.tsx
+++ components/waves/drops/VirtualScrollWrapper.tsx
@@ -49,7 +49,7 @@ interface VirtualScrollWrapperProps {
  * </VirtualScrollWrapper>
  */
 export default function VirtualScrollWrapper({
-  delay = 2000,
+  delay = 10000,
   scrollContainerRef,
   children,
   drop,
@@ -58,7 +58,11 @@ export default function VirtualScrollWrapper({
     const haveQuoteOrMedia = drop.parts.some(
       (part) => !!part.quoted_drop || !!part.media.length
     );
-    return haveQuoteOrMedia;
+    const haveReply = !!drop.reply_to;
+    const haveEmbeddedTweet = drop.parts.some(
+      (part) => !!part.content?.match(/https:\/\/(?:twitter\.com|x\.com)\/(?:#!\/)?(\w+)\/status(es)?\/(\d+)/)
+    );
+    return haveQuoteOrMedia || haveReply || haveEmbeddedTweet;
   };
 
   const [shouldAlwaysRender, setShouldAlwaysRender] = useState<boolean>(
@@ -203,7 +207,7 @@ export default function VirtualScrollWrapper({
         // For a reversed layout, we need a large margin at both top and bottom
         // This ensures elements are detected well before they enter/leave the viewport
         // Using a large value for both directions ensures smooth operation in both regular and reversed layouts
-        rootMargin: "500px 0px 500px 0px",
+        rootMargin: "1500px 0px 15000px 0px",
         threshold: 0.0,
         root: scrollContainerRef.current,
       }
