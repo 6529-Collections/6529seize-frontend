ea1d4226077d97bad898ac2f93d253b30e190725
Simo <info@analyticsflow.ee>
2025-04-03 11:54:38 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

--- components/drops/view/part/DropPartMarkdown.tsx
+++ components/drops/view/part/DropPartMarkdown.tsx
@@ -286,7 +286,7 @@ function DropPartMarkdown({
     href: string,
     props: AnchorHTMLAttributes<HTMLAnchorElement> & ExtraProps
   ) => {
-    const baseEndpoint = process.env.BASE_ENDPOINT || "";
+    const baseEndpoint = process.env.BASE_ENDPOINT ?? "";
     const isExternalLink = baseEndpoint && !href.startsWith(baseEndpoint);
 
     if (isExternalLink) {
