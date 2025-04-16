b7c2aba1bbe4fc6f85db6bc81a05a590fd212116
prxt6529 <prxt@6529.io>
2025-04-16 16:32:52 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- pages/open-mobile.tsx
+++ pages/open-mobile.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/router";
 import { ShareMobileApp } from "../components/header/share/HeaderShareMobileApps";
+import useCapacitor from "../hooks/useCapacitor";
 
 const OpenMobilePage = () => {
   const router = useRouter();
   const { path = "/" } = router.query;
+
+  const capacitor = useCapacitor();
 
   const [decodedPath, setDecodedPath] = useState<string | null>(null);
 
@@ -23,6 +26,24 @@ const OpenMobilePage = () => {
     window.location.href = `${window.location.origin}${decodedPath}`;
   };
 
+  const printShareMobileApps = useMemo(() => {
+    const shareIos = <ShareMobileApp platform="ios" target="_self" />;
+    const shareAndroid = <ShareMobileApp platform="android" target="_self" />;
+
+    if (capacitor.isIos) {
+      return shareIos;
+    } else if (capacitor.isAndroid) {
+      return shareAndroid;
+    }
+
+    return (
+      <>
+        {shareIos}
+        {shareAndroid}
+      </>
+    );
+  }, [capacitor]);
+
   return (
     <div className="tailwind-scope tw-flex tw-flex-col tw-items-center tw-justify-center tw-h-screen tw-text-center tw-p-4 tw-gap-10">
       <p className="tw-text-2xl tw-font-bold tw-animate-pulse">
@@ -31,8 +52,7 @@ const OpenMobilePage = () => {
       <div className="tw-flex tw-flex-col tw-items-center tw-justify-center tw-gap-1">
         <p className="tw-text-xl tw-font-bold">Get 6529 Mobile</p>
         <div className="tw-flex tw-flex-wrap tw-items-center tw-justify-center tw-gap-4">
-          <ShareMobileApp platform="ios" target="_self" />
-          <ShareMobileApp platform="android" target="_self" />
+          {printShareMobileApps()}
         </div>
       </div>
       <div
