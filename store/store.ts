a8af2992d1e8c94da379244ab93f3c113b2aa203
Simo <info@analyticsflow.ee>
2025-04-23 10:22:15 +0300

    clean unused files
    Signed-off-by: Simo <info@analyticsflow.ee>

--- store/store.ts
+++ store/store.ts
@@ -1,12 +1,10 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
-import { counterSlice } from "./counterSlice";
 import { groupSlice } from "./groupSlice";
 
 const makeStore = () =>
   configureStore({
     reducer: {
-      [counterSlice.name]: counterSlice.reducer,
       [groupSlice.name]: groupSlice.reducer,
     },
     devTools: true,
