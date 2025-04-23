49f049e85a558818e69fc20fd6e64cf60980b470
ragnep <ragneinfo@gmail.com>
2025-04-23 10:24:18 +0300

    fixed some layout issues
    Signed-off-by: ragnep <ragneinfo@gmail.com>

--- components/brain/my-stream/layout/MyStreamLayout.tsx
+++ components/brain/my-stream/layout/MyStreamLayout.tsx
@@ -108,10 +108,9 @@ function MyStreamLayoutContent({ children }: { readonly children: ReactNode }) {
           content={`${process.env.BASE_ENDPOINT}/6529io.png`}
         />
         <meta property="og:description" content="6529.io" />
-        <style>{`body { overflow: hidden !important; }`}</style>
+       {/*  <style>{`body { overflow: hidden !important; }`}</style> */}
       </Head>
-
-      <div className="tailwind-scope tw-flex tw-flex-col tw-bg-black tw-overflow-hidden">
+      <div className="tailwind-scope tw-flex tw-flex-col tw-bg-black tw-overflow-hidden tw-min-h-[80dvh]">
         <ClientOnly>{content}</ClientOnly>
       </div>
     </>
