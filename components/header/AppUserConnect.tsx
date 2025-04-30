4be7689848223dc440902541e4ce8ec0acf78391
ragnep <ragneinfo@gmail.com>
2025-04-30 15:47:27 +0300

    connect button bug fix
    Signed-off-by: ragnep <ragneinfo@gmail.com>

--- components/header/AppUserConnect.tsx
+++ components/header/AppUserConnect.tsx
@@ -9,10 +9,10 @@ export default function AppUserConnect({
 }: {
   readonly onNavigate: () => void;
 }) {
-  const { isConnected, seizeConnect, seizeDisconnectAndLogout } =
+  const { address, seizeConnect, seizeDisconnectAndLogout } =
     useSeizeConnectContext();
 
-  if (!isConnected) {
+  if (!address) {
     return (
       <button
         onClick={() => {
@@ -22,7 +22,7 @@ export default function AppUserConnect({
         type="button"
         className="tw-whitespace-nowrap tw-flex tw-w-full tw-items-center tw-justify-center tw-cursor-pointer tw-bg-primary-500 tw-px-4 tw-py-2.5 tw-text-sm tw-leading-6 tw-rounded-lg tw-font-semibold tw-text-white tw-border-0 tw-ring-1 tw-ring-inset tw-ring-primary-500 hover:tw-ring-primary-600 placeholder:tw-text-iron-300 focus:tw-outline-none focus:tw-ring-1 focus:tw-ring-inset tw-shadow-sm hover:tw-bg-primary-600 tw-transition tw-duration-300 tw-ease-out"
       >
-        <span>Connect Wallet</span>
+        <span>Connect</span>
       </button>
     );
   }
