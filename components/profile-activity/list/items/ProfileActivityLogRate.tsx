9fbcbf442cb3a446e7e5480c3b2873d6aa5ba3f8
Ragne <135017495+ragnep@users.noreply.github.com>
2025-05-08 09:42:26 +0300

    added system adjustment pill to activity log (#812)
    Added system adjustment pill in activity log

--- components/profile-activity/list/items/ProfileActivityLogRate.tsx
+++ components/profile-activity/list/items/ProfileActivityLogRate.tsx
@@ -9,6 +9,7 @@ import { formatNumberWithCommas } from "../../../../helpers/Helpers";
 import { UserPageTabType } from "../../../user/layout/UserPageTabs";
 import CommonProfileLink from "../../../user/utils/CommonProfileLink";
 import Link from "next/link";
+import { SystemAdjustmentPill } from "../../../common/SystemAdjustmentPill";
 
 enum ProfileActivityLogRateType {
   ADDED = "ADDED",
@@ -97,7 +98,8 @@ export default function ProfileActivityLogRate({
       {!!proxyHandle && isSelfProxy && (
         <Link
           href={`/${proxyHandle}`}
-          className="tw-no-underline tw-whitespace-nowrap tw-text-xs tw-text-iron-400 tw-font-medium">
+          className="tw-no-underline tw-whitespace-nowrap tw-text-xs tw-text-iron-400 tw-font-medium"
+        >
           (Proxy for {proxyHandle})
         </Link>
       )}
@@ -105,11 +107,13 @@ export default function ProfileActivityLogRate({
       <span
         className={`${
           isChangePositive ? "tw-text-green" : "tw-text-red"
-        } tw-text-base tw-font-medium`}>
+        } tw-text-base tw-font-medium`}
+      >
         {changeStr}
       </span>
       <span
-        className={`${getTotalRatingClass()} tw-whitespace-nowrap tw-text-base tw-font-medium`}>
+        className={`${getTotalRatingClass()} tw-whitespace-nowrap tw-text-base tw-font-medium`}
+      >
         (total {newRatingStr})
       </span>
       {log.contents.rating_matter === RateMatter.REP && (
@@ -131,22 +135,13 @@ export default function ProfileActivityLogRate({
       {!!proxyHandle && !isSelfProxy && (
         <Link
           href={`/${proxyHandle}`}
-          className="tw-no-underline tw-whitespace-nowrap tw-text-xs tw-text-iron-400 tw-font-medium">
+          className="tw-no-underline tw-whitespace-nowrap tw-text-xs tw-text-iron-400 tw-font-medium"
+        >
           (Proxy: {proxyHandle})
         </Link>
       )}
 
-      {isSystemAdjustment && (
-        <span className="tw-whitespace-nowrap tw-inline-flex tw-items-center tw-gap-x-1.5 tw-rounded-md tw-px-2 tw-py-1 tw-text-sm tw-font-medium tw-text-iron-300 tw-ring-1 tw-ring-inset tw-ring-iron-700">
-          <svg
-            className="tw-flex-shrink-0 tw-h-1.5 tw-w-1.5 tw-fill-red"
-            viewBox="0 0 6 6"
-            aria-hidden="true">
-            <circle cx="3" cy="3" r="3" />
-          </svg>
-          <span>System Adjustment</span>
-        </span>
-      )}
+      {isSystemAdjustment && <SystemAdjustmentPill />}
     </>
   );
 }
