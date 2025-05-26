1b4119e82bd14fffef899ce5ae41a1ff1f3253ac
analyticsflowee <49652774+analyticsflowee@users.noreply.github.com>
2025-05-26 16:14:30 +0300

    test: fix path and ts errors in layout tests (#993)

--- __tests__/components/header/user/HeaderUserConnecting.test.tsx
+++ __tests__/components/header/user/HeaderUserConnecting.test.tsx
@@ -1,8 +1,8 @@
 import { render } from "@testing-library/react";
 import HeaderUserConnecting from "../../../../components/header/user/HeaderUserConnecting";
 
-const mockLoader = jest.fn((props?: any) => <div data-testid="loader" {...props} />);
-const mockLoader = jest.fn((props: any) => <div data-testid="loader" />);
+const mockLoader = jest.fn();
+mockLoader.mockImplementation(() => <div data-testid="loader" />);
 
 jest.mock("../../../../components/distribution-plan-tool/common/CircleLoader", () => ({
   __esModule: true,
