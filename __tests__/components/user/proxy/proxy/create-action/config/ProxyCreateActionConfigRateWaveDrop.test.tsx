0f8520182888c87e96f3eff4141640c007525e71
analyticsflowee <49652774+analyticsflowee@users.noreply.github.com>
2025-05-27 18:37:47 +0300

    test: add coverage for user components (#1102)

---
+++ __tests__/components/user/proxy/proxy/create-action/config/ProxyCreateActionConfigRateWaveDrop.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import ProxyCreateActionConfigRateWaveDrop from '../../../../../../../components/user/proxy/proxy/create-action/config/ProxyCreateActionConfigRateWaveDrop';
+import { ApiProfileProxyActionType } from '../../../../../../../generated/models/ApiProfileProxyActionType';
+
+describe('ProxyCreateActionConfigRateWaveDrop', () => {
+  it('submits rate wave drop action', async () => {
+    const onSubmit = jest.fn();
+    render(
+      <ProxyCreateActionConfigRateWaveDrop endTime={42} onSubmit={onSubmit} onCancel={() => {}} />
+    );
+    await userEvent.click(screen.getByRole('button', { name: /save/i }));
+    expect(onSubmit).toHaveBeenCalledWith({
+      action_type: ApiProfileProxyActionType.RateWaveDrop,
+      end_time: 42,
+    });
+  });
+
+  it('handles cancel click', async () => {
+    const onCancel = jest.fn();
+    render(
+      <ProxyCreateActionConfigRateWaveDrop endTime={null} onSubmit={() => {}} onCancel={onCancel} />
+    );
+    await userEvent.click(screen.getByRole('button', { name: /cancel/i }));
+    expect(onCancel).toHaveBeenCalled();
+  });
+});
