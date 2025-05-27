314d5ae9f699eff1cd645cf3790e0b7dc6eacde0
analyticsflowee <49652774+analyticsflowee@users.noreply.github.com>
2025-05-27 14:11:06 +0300

    test: add coverage for HeaderSearchModalItem (#1056)

--- jest.setup.js
+++ jest.setup.js
@@ -3,4 +3,6 @@ import { TextEncoder, TextDecoder } from 'util';
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
-require('@testing-library/jest-dom'); 
+require('@testing-library/jest-dom');
+// Default API endpoint needed for service tests
+process.env.API_ENDPOINT = process.env.API_ENDPOINT || 'http://example.com';
