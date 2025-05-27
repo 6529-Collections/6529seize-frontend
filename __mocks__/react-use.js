7845cd2419656b8b89baf8770fe7ab9528900bea
analyticsflowee <49652774+analyticsflowee@users.noreply.github.com>
2025-05-27 09:31:36 +0300

    test: add group create config coverage (#1034)

--- __mocks__/react-use.js
+++ __mocks__/react-use.js
@@ -1,4 +1,9 @@
 // __mocks__/react-use.js
 module.exports = {
   useCss: jest.fn(() => ['mocked-classname-from-react-use-mock', jest.fn()]),
+  useClickAway: jest.fn(),
+  useKeyPressEvent: jest.fn(),
+  createBreakpoint: jest.fn(() => () => 'LG'),
+  useIntersection: jest.fn(() => ({ isIntersecting: true })),
+  useDebounce: jest.fn((value) => value),
 };
