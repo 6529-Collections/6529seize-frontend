06ebf036b792722b86e06615354de398c7b9c0e6
analyticsflowee <49652774+analyticsflowee@users.noreply.github.com>
2025-05-28 08:33:39 +0300

    test: add coverage for user subscriptions mode (#1131)

--- __tests__/components/user/utils/user-cic-type/tooltip/UserCICTypeIconTooltip.test.tsx
+++ __tests__/components/user/utils/user-cic-type/tooltip/UserCICTypeIconTooltip.test.tsx
@@ -1,8 +1,8 @@
 // @ts-nocheck
 import { render, screen } from '@testing-library/react';
 import UserCICTypeIconTooltip from '../../../../../../components/user/utils/user-cic-type/tooltip/UserCICTypeIconTooltip';
-import { CICType } from '../../../../../entities/IProfile';
-import { ApiIdentity } from '../../../../../generated/models/ApiIdentity';
+import { CICType } from '../../../../../../entities/IProfile';
+import { ApiIdentity } from '../../../../../../generated/models/ApiIdentity';
 
 jest.mock('../../../../../../components/user/utils/user-cic-type/UserCICTypeIcon', () => () => <div data-testid="icon" />);
 jest.mock('../../../../../../components/user/utils/user-cic-type/tooltip/UserCICTypeIconTooltipHeaders', () => () => <div data-testid="headers" />);
