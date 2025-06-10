d677e57ccd3411bacb726d9bbb9fb28e5d07fa9c
ragnep <ragneinfo@gmail.com>
2025-06-10 17:14:58 +0300

    wip
    Signed-off-by: ragnep <ragneinfo@gmail.com>

--- components/drops/view/item/content/media/DropListItemContentMediaVideo.tsx
+++ components/drops/view/item/content/media/DropListItemContentMediaVideo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useInView } from "../../../../../../hooks/useInView";
 import useDeviceInfo from "../../../../../../hooks/useDeviceInfo";
 import { useOptimizedVideo } from "../../../../../../hooks/useOptimizedVideo";
