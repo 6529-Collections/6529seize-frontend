da6e5dffcc4898ad25e742e5c94b97dc2818f28d
prxt6529 <prxt@6529.io>
2025-06-11 14:22:04 +0300

    MEME CALENDAR SZN12
    Signed-off-by: prxt6529 <prxt@6529.io>

--- __tests__/components/user/utils/user-cic-type/tooltip/UserCICTypeIconTooltipHeaders.test.tsx
+++ __tests__/components/user/utils/user-cic-type/tooltip/UserCICTypeIconTooltipHeaders.test.tsx
@@ -1,10 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import UserCICTypeIconTooltipHeaders from '../../../../../../components/user/utils/user-cic-type/tooltip/UserCICTypeIconTooltipHeaders';
+import { render, screen } from "@testing-library/react";
+import UserCICTypeIconTooltipHeaders from "../../../../../../components/user/utils/user-cic-type/tooltip/UserCICTypeIconTooltipHeaders";
 
-describe('UserCICTypeIconTooltipHeaders', () => {
-  it('displays tooltip header text', () => {
+describe("UserCICTypeIconTooltipHeaders", () => {
+  it("displays tooltip header text", () => {
     render(<UserCICTypeIconTooltipHeaders />);
-    expect(screen.getByText('Community ID Check')).toBeInTheDocument();
+    expect(screen.getByText("Network ID Check")).toBeInTheDocument();
     expect(
       screen.getByText(/Does the community believe this profile/i)
     ).toBeInTheDocument();
