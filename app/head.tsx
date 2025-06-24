8acfbc3233094ad38db63241cc1147fbdf5970f1
prxt6529 <prxt@6529.io>
2025-06-24 14:30:48 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- app/head.tsx
+++
@@ -1,8 +0,0 @@
-export default function Head() {
-  return (
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
-    />
-  );
-}
