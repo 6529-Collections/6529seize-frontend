729055b81bda6f4fc1901456770b38e6fd33c1ea
prxt6529 <prxt@6529.io>
2025-07-07 09:28:02 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- app/error-page.tsx
+++ app/error-page.tsx
@@ -1,12 +1,12 @@
 import { Suspense } from "react";
 import styles from "@/styles/Home.module.scss";
-import Error from "@/components/error/Error";
+import ErrorComponent from "@/components/error/Error";
 
 export default function ErrorPage() {
   return (
     <main className={styles.main}>
       <Suspense fallback={null}>
-        <Error />
+        <ErrorComponent />
       </Suspense>
     </main>
   );
