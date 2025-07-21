96e89b8c2ce98f02cde572487e9c421fc5dc3bc0
prxt6529 <prxt@6529.io>
2025-07-21 12:16:35 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- app/om/OM/page.tsx
+++ app/om/OM/page.tsx
@@ -3,15 +3,13 @@ import { getAppMetadata } from "@/components/providers/metadata";
 import type { Metadata } from "next";
 
 const IndexPage = () => (
-  <>
-    <div>
-      <title>Redirecting...</title>
-      <meta httpEquiv="refresh" content="0;url=/om/" />
-      <p>
-        You are being redirected to <a href="/om/">/om/</a>
-      </p>
-    </div>
-  </>
+  <div>
+    <title>Redirecting...</title>
+    <meta httpEquiv="refresh" content="0;url=/om/" />
+    <p>
+      You are being redirected to <a href="/om/">/om/</a>
+    </p>
+  </div>
 );
 
 export default IndexPage;
@@ -19,4 +17,3 @@ export default IndexPage;
 export async function generateMetadata(): Promise<Metadata> {
   return getAppMetadata({ title: "Redirecting" });
 }
-
