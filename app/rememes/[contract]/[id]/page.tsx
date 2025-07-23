231c3d7cba42159c5746985e76fbfe898f4365ae
prxt6529 <prxt@6529.io>
2025-07-23 12:18:03 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- app/rememes/[contract]/[id]/page.tsx
+++ app/rememes/[contract]/[id]/page.tsx
@@ -1,4 +1,4 @@
-import styles from "@/styles/Home.module.css";
+import styles from "@/styles/Home.module.scss";
 import RememePage from "@/components/rememes/RememePage";
 import { fetchUrl } from "@/services/6529api";
 import { formatAddress } from "@/helpers/Helpers";
