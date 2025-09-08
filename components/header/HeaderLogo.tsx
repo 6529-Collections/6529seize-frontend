08f6f66256a1e470d02435b1f9c6c1c1f52eb3d0
Simo <info@analyticsflow.ee>
2025-09-08 15:58:27 +0300

    wip
    Signed-off-by: Simo <info@analyticsflow.ee>

---
+++ components/header/HeaderLogo.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Header.module.scss";
+
+interface HeaderLogoProps {
+  isSmall?: boolean;
+  isCapacitor: boolean;
+  isMobile: boolean;
+}
+
+export default function HeaderLogo({
+  isSmall,
+  isCapacitor,
+  isMobile,
+}: HeaderLogoProps) {
+  const logoSrc: string = "/6529.png";
+  let logoWidth: number = 50;
+  let logoHeight: number = 50;
+  
+  if (isCapacitor || isMobile) {
+    logoWidth = 40;
+    logoHeight = 40;
+  }
+
+  const logoClassName = isSmall || isCapacitor || isMobile
+    ? styles.logoIconSmall
+    : styles.logoIcon;
+
+  return (
+    <Link href="/">
+      <Image
+        unoptimized
+        loading="eager"
+        priority
+        className={logoClassName}
+        src={logoSrc}
+        alt="6529Seize"
+        width={logoWidth}
+        height={logoHeight}
+      />
+    </Link>
+  );
+}
