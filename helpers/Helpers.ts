5c81e71f7028f90c437b6ee037d2542284c148b3
prxt6529 <prxt@6529.io>
2025-09-15 13:54:47 +0300

    Fix Coinbase wallet for mobile app
    Signed-off-by: prxt6529 <prxt@6529.io>

--- helpers/Helpers.ts
+++ helpers/Helpers.ts
@@ -1,4 +1,11 @@
-import { mainnet, sepolia, goerli } from "wagmi/chains";
+import {
+  NEXTGEN_CHAIN_ID,
+  NEXTGEN_CORE,
+} from "@/components/nextGen/nextgen_contracts";
+import {
+  USER_PAGE_TAB_META,
+  UserPageTabType,
+} from "@/components/user/layout/UserPageTabs";
 import {
   GRADIENT_CONTRACT,
   MEMELAB_CONTRACT,
@@ -6,20 +13,13 @@ import {
   NULL_ADDRESS,
   NULL_DEAD_ADDRESS,
   ROYALTIES_PERCENTAGE,
-} from "../constants";
-import { BaseNFT, VolumeType } from "../entities/INFT";
-import { DateIntervalsSelection } from "../enums";
-import { CICType } from "../entities/IProfile";
-import {
-  USER_PAGE_TAB_META,
-  UserPageTabType,
-} from "../components/user/layout/UserPageTabs";
-import {
-  NEXTGEN_CHAIN_ID,
-  NEXTGEN_CORE,
-} from "../components/nextGen/nextgen_contracts";
+} from "@/constants";
+import { BaseNFT, VolumeType } from "@/entities/INFT";
+import { CICType } from "@/entities/IProfile";
+import { DateIntervalsSelection } from "@/enums";
+import { ApiIdentity } from "@/generated/models/ApiIdentity";
+import { goerli, mainnet, sepolia } from "wagmi/chains";
 import { PageSSRMetadata, Period } from "./Types";
-import { ApiIdentity } from "../generated/models/ApiIdentity";
 
 export const MAX_DROP_UPLOAD_FILES = 8;
 
@@ -782,6 +782,10 @@ export const getMetadataForUserPage = (
   profile: ApiIdentity,
   path?: string
 ): PageSSRMetadata => {
+  let display = profile.handle ?? formatAddress(profile.display);
+  if (!display) {
+    display = profile.query ?? "";
+  }
   return {
     title: profile.handle + (path ? ` | ${path}` : ""),
     ogImage: profile.pfp ?? "",
