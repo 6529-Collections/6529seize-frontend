82c784280f24dd09019ad8ef8e60b0e9bcab3c5b
Simo <simo@6529.io>
2025-10-01 15:46:11 +0300

    wip
    Signed-off-by: Simo <simo@6529.io>

--- components/nft-picker/AllTokensSelectedCard.tsx
+++ components/nft-picker/AllTokensSelectedCard.tsx
@@ -1,11 +1,12 @@
-import { CheckCircleIcon } from "@heroicons/react/20/solid";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import type { MutableRefObject, RefObject } from "react";
 
 type ButtonRef = RefObject<HTMLButtonElement | null> | MutableRefObject<HTMLButtonElement | null>;
 
 interface AllTokensSelectedCardProps {
-  onDeselect: () => void;
-  buttonRef?: ButtonRef;
+  readonly onDeselect: () => void;
+  readonly buttonRef?: ButtonRef;
 }
 
 export function AllTokensSelectedCard({
@@ -20,7 +21,7 @@ export function AllTokensSelectedCard({
       className="tw-flex tw-w-full tw-flex-col tw-gap-3 tw-rounded-lg tw-border-2 tw-border-green-500/60 tw-bg-green-950/30 tw-p-4 tw-transition-all tw-duration-200 tw-ease-in-out @md:tw-flex-row @md:tw-items-center"
     >
       <div className="tw-flex tw-h-10 tw-w-10 tw-shrink-0 tw-items-center tw-justify-center tw-rounded-full tw-bg-green-600">
-        <CheckCircleIcon className="tw-h-6 tw-w-6 tw-text-white" aria-hidden="true" />
+        <FontAwesomeIcon icon={faCheckCircle} className="tw-h-6 tw-w-6 tw-text-white" aria-hidden="true" />
       </div>
 
       <div className="tw-flex-1 tw-min-w-0">
@@ -37,7 +38,7 @@ export function AllTokensSelectedCard({
         aria-label="Deselect all tokens and return to manual selection"
         className="tw-flex tw-w-full tw-items-center tw-justify-center tw-gap-2 tw-rounded-md tw-bg-green-600 tw-px-4 tw-py-2.5 tw-font-medium tw-text-white tw-transition-all hover:tw-bg-green-700 focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-green-500 focus:tw-ring-offset-2 focus:tw-ring-offset-iron-900 active:tw-scale-95 @md:tw-w-auto @md:tw-shrink-0"
       >
-        <CheckCircleIcon className="tw-h-5 tw-w-5 tw-text-white" aria-hidden="true" />
+        <FontAwesomeIcon icon={faCheckCircle} className="tw-h-5 tw-w-5 tw-text-white" aria-hidden="true" />
         Deselect All
       </button>
     </div>
