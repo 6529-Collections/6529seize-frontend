0e8eb723dd9bfd351855c2ffa6f2556c764aea3d
Simo <simo@6529.io>
2025-10-14 12:51:28 +0200

    wip
    Signed-off-by: Simo <simo@6529.io>

--- components/utils/input/profile-search/CommonProfileSearchItems.tsx
+++ components/utils/input/profile-search/CommonProfileSearchItems.tsx
@@ -29,7 +29,7 @@ export default function CommonProfileSearchItems({
           exit={{ opacity: 0, y: -20 }}
           transition={{ duration: 0.2 }}
         >
-          <div className="tw-absolute tw-z-50 tw-overflow-hidden tw-w-full tw-rounded-md tw-bg-iron-800 tw-shadow-2xl tw-ring-1 tw-ring-white/10">
+          <div className="tw-absolute tw-overflow-hidden tw-w-full tw-rounded-md tw-bg-iron-800 tw-shadow-2xl tw-ring-1 tw-ring-white/10">
             <div className="tw-py-1 tw-flow-root tw-overflow-x-hidden tw-overflow-y-auto">
               <ul className="tw-flex tw-flex-col tw-gap-y-1 tw-px-2 tw-mx-0 tw-mb-0 tw-list-none">
                 {profiles.length ? (
