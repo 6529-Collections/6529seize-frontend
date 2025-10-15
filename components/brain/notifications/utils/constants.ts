b68d3511756663201c9c82bf84d4076043d74915
simo6529 <simo@6529.io>
2025-10-15 13:34:42 +0200

    Notifications component cleanup (#1545)
    * Notifications component cleanup
    
    Signed-off-by: Simo <simo@6529.io>
    
    ---------
    
    Signed-off-by: Simo <simo@6529.io>

---
+++ components/brain/notifications/utils/constants.ts
@@ -0,0 +1,6 @@
+export const STICK_TO_BOTTOM_SCROLL_THRESHOLD_PX = 32;
+export const LOAD_TIMEOUT_MS = 15000;
+export const DEFAULT_ERROR_MESSAGE =
+  "Failed to load notifications. Please try again.";
+export const LOAD_TIMEOUT_MESSAGE =
+  "Loading notifications is taking longer than expected. Please try again.";
