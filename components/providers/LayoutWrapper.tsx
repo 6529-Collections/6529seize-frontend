ce718723a32a6125fadb9adebc7494e150909707
ragnep <ragneinfo@gmail.com>
2025-10-23 10:49:05 +0300

    wip
    Signed-off-by: ragnep <ragneinfo@gmail.com>

--- components/providers/LayoutWrapper.tsx
+++ components/providers/LayoutWrapper.tsx
@@ -76,16 +76,16 @@ export default function LayoutWrapper({
     LayoutComponent = SmallScreenLayout;
   }
 
-  const content = (
-    <>
-      {children}
-      <FooterWrapper />
-    </>
-  );
-
   if (isAccessOrRestricted) {
     return <>{children}</>;
   }
 
-  return <LayoutComponent>{content}</LayoutComponent>;
+  return (
+    <LayoutComponent>
+      <>
+        {children}
+        <FooterWrapper />
+      </>
+    </LayoutComponent>
+  );
 }
