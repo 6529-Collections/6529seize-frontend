552157c0cab19c55120d49bba1c2314424ddcf34
Simo <simo@6529.io>
2025-10-29 12:02:18 +0200

    wip
    Signed-off-by: Simo <simo@6529.io>

--- components/waves/memes/traits/index.ts
+++ components/waves/memes/traits/index.ts
@@ -1,7 +1,2 @@
-export * from './TextTrait';
-export * from './NumberTrait';
-export * from './DropdownTrait';
-export * from './BooleanTrait';
 export * from './Section';
 export * from './TraitField';
-export * from './types';
--- components/waves/memes/traits/types.ts
+++ components/waves/memes/traits/types.ts
@@ -15,39 +15,3 @@ export interface TextTraitProps extends BaseTraitProps {
   readonly traits: TraitsData;
   readonly updateText: (field: keyof TraitsData, value: string) => void;
 }
-
-export interface NumberTraitProps extends BaseTraitProps {
-  readonly readOnly?: boolean;
-  readonly min: number;
-  readonly max: number;
-  readonly traits: TraitsData;
-  readonly updateNumber: (field: keyof TraitsData, value: number) => void;
-}
-
-export interface DropdownTraitProps extends BaseTraitProps {
-  readonly options: readonly string[];
-  readonly traits: TraitsData;
-  readonly updateText: (field: keyof TraitsData, value: string) => void;
-}
-
-export interface BooleanTraitProps extends BaseTraitProps {
-  readonly traits: TraitsData;
-  readonly updateBoolean: (field: keyof TraitsData, value: boolean) => void;
-}
-
-export interface SectionProps {
-  readonly title: string;
-  readonly children: React.ReactNode;
-  readonly className?: string;
-}
-
-export interface TraitWrapperProps {
-  readonly label: string;
-  readonly readOnly?: boolean;
-  readonly children: React.ReactNode;
-  readonly isBoolean?: boolean;
-  readonly className?: string;
-  readonly error?: string | null;
-  readonly id?: string;
-  readonly isFieldFilled?: boolean;
-}
