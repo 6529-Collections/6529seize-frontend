dbf0be52a89e445909829239b352dabb79b44742
GelatoGenesis <tarmokalling@gmail.com>
2025-11-28 15:27:34 +0200

    Added Sentry to FE
    Signed-off-by: GelatoGenesis <tarmokalling@gmail.com>

---
+++ instrumentation-client.ts
@@ -0,0 +1,39 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+import {publicEnv} from "@/config/env";
+
+const sentryEnabled = !!publicEnv.SENTRY_DSN;
+const isProduction = publicEnv.NODE_ENV === "production";
+const dsn = publicEnv.SENTRY_DSN;
+
+Sentry.init({
+  dsn: sentryEnabled ? dsn : undefined,
+  enabled: sentryEnabled,
+
+  // Add optional integrations for additional features
+  integrations: [
+    Sentry.replayIntegration(),
+  ],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Define how likely Replay events are sampled.
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: sentryEnabled && isProduction ? 0.1 : 0,
+
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: sentryEnabled && isProduction ? 1.0 : 0,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
