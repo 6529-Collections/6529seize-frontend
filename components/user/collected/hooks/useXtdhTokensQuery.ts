574252a70f91fca75b93ca4ab54a156d6573b6bd
Simo <simo@6529.io>
2025-12-04 12:24:21 +0100

    wip
    Signed-off-by: Simo <simo@6529.io>

--- components/user/collected/hooks/useXtdhTokensQuery.ts
+++ components/user/collected/hooks/useXtdhTokensQuery.ts
@@ -1,4 +1,5 @@
 import { QueryKey } from "@/components/react-query-wrapper/ReactQueryWrapper";
+import { CollectionSort } from "@/entities/IProfile";
 import { ApiXTdhTokensPage } from "@/generated/models/ApiXTdhTokensPage";
 import { commonApiFetch } from "@/services/api/common-api";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
@@ -27,17 +28,25 @@ export const useXtdhTokensQuery = ({
       sort,
       order,
     ],
-    queryFn: async () =>
-      await commonApiFetch<ApiXTdhTokensPage>({
+    queryFn: async () => {
+      let apiSort = sort;
+      if (sort === CollectionSort.XTDH) {
+        apiSort = "xtdh";
+      } else if (sort === CollectionSort.XTDH_DAY) {
+        apiSort = "xtdh_rate";
+      }
+
+      return await commonApiFetch<ApiXTdhTokensPage>({
         endpoint: "xtdh/tokens",
         params: {
           identity,
           page: page.toString(),
           page_size: pageSize.toString(),
-          sort,
+          sort: apiSort,
           order,
         },
-      }),
+      });
+    },
     placeholderData: keepPreviousData,
   });
 };
