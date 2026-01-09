f3a31b1876027aabeda067f3959e2df470f58f44
prxt6529 <prxt@6529.io>
2026-01-09 08:53:59 +0200

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- components/network/NetworkPageLayout.tsx
+++ components/network/NetworkPageLayout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import type { ReactNode } from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
 import { selectActiveGroupId, setActiveGroupId } from "@/store/groupSlice";
@@ -18,17 +18,19 @@ export default function NetworkPageLayout({
   const dispatch = useDispatch();
   const { isApp } = useDeviceInfo();
 
-  const [init, setInit] = useState(false);
+  const initRef = useRef(false);
+  const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
-    if (!init) {
+    if (!initRef.current) {
+      initRef.current = true;
       const group = searchParams?.get("group");
       if (group && group !== activeGroupId) {
         dispatch(setActiveGroupId(group));
       }
-      setInit(true);
+      setIsReady(true);
     }
-  }, []);
+  }, [searchParams, activeGroupId, dispatch]);
 
   if (isApp) {
     return <NetworkPageLayoutApp>{children}</NetworkPageLayoutApp>;
@@ -39,7 +41,7 @@ export default function NetworkPageLayout({
       <div className="tailwind-scope tw-bg-black tw-min-h-dvh tw-mt-6 lg:tw-mt-8 tw-pb-6 tw-mx-auto">
         <div className="tw-w-full">
           <div className="tw-mt-4 tw-px-3 lg:tw-px-6 xl:tw-px-8">
-            {init && children}
+            {isReady && children}
           </div>
         </div>
       </div>
