aec31b20ecbd8b42c60da5828ce1b417b3c9dddc
prxt6529 <prxt@6529.io>
2026-01-14 10:51:39 +0200

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- components/providers/Providers.tsx
+++ components/providers/Providers.tsx
@@ -1,27 +1,27 @@
-import Auth from "@/components/auth/Auth";
-import ReactQueryWrapper from "@/components/react-query-wrapper/ReactQueryWrapper";
-import { CookieConsentProvider } from "@/components/cookies/CookieConsentContext";
-import { NotificationsProvider } from "@/components/notifications/NotificationsContext";
-import { SeizeConnectProvider } from "@/components/auth/SeizeConnectContext";
-import { IpfsProvider } from "@/components/ipfs/IPFSContext";
-import { EULAConsentProvider } from "@/components/eula/EULAConsentContext";
 import { AppWalletsProvider } from "@/components/app-wallets/AppWalletsContext";
-import { SeizeSettingsProvider } from "@/contexts/SeizeSettingsContext";
-import { EmojiProvider } from "@/contexts/EmojiContext";
-import { AppWebSocketProvider } from "@/services/websocket/AppWebSocketProvider";
-import { HeaderProvider } from "@/contexts/HeaderContext";
+import Auth from "@/components/auth/Auth";
+import { SeizeConnectProvider } from "@/components/auth/SeizeConnectContext";
+import { CookieConsentProvider } from "@/components/cookies/CookieConsentContext";
+import { EULAConsentProvider } from "@/components/eula/EULAConsentContext";
+import { IpfsProvider } from "@/components/ipfs/IPFSContext";
+import { NotificationsProvider } from "@/components/notifications/NotificationsContext";
+import ReactQueryWrapper from "@/components/react-query-wrapper/ReactQueryWrapper";
 import NewVersionToast from "@/components/utils/NewVersionToast";
-import WagmiSetup from "./WagmiSetup";
+import { EmojiProvider } from "@/contexts/EmojiContext";
+import { HeaderProvider } from "@/contexts/HeaderContext";
+import { NavigationHistoryProvider } from "@/contexts/NavigationHistoryContext";
+import { ScrollPositionProvider } from "@/contexts/ScrollPositionContext";
+import { SeizeSettingsProvider } from "@/contexts/SeizeSettingsContext";
+import { TitleProvider } from "@/contexts/TitleContext";
+import { MyStreamProvider } from "@/contexts/wave/MyStreamContext";
+import { WaveEligibilityProvider } from "@/contexts/wave/WaveEligibilityContext";
+import { AppWebSocketProvider } from "@/services/websocket/AppWebSocketProvider";
+import { LayoutProvider } from "../brain/my-stream/layout/LayoutContext";
+import { ViewProvider } from "../navigation/ViewContext";
 import CapacitorSetup from "./CapacitorSetup";
 import IpfsImageSetup from "./IpfsImageSetup";
 import QueryClientSetup from "./QueryClientSetup";
-import { NavigationHistoryProvider } from "@/contexts/NavigationHistoryContext";
-import { ScrollPositionProvider } from "@/contexts/ScrollPositionContext";
-import { LayoutProvider } from "../brain/my-stream/layout/LayoutContext";
-import { ViewProvider } from "../navigation/ViewContext";
-import { MyStreamProvider } from "@/contexts/wave/MyStreamContext";
-import { TitleProvider } from "@/contexts/TitleContext";
-import { WaveEligibilityProvider } from "@/contexts/wave/WaveEligibilityContext";
+import WagmiSetup from "./WagmiSetup";
 
 export default function Providers({
   children,
