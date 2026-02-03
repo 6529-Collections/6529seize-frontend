36917030d19646719b9c4ce989ce78d54cc23c5a
prxt6529 <117092621+prxt6529@users.noreply.github.com>
2026-02-03 14:49:41 +0200

    Adding preview image for 'My Votes', adding drop media type where applicable (#1872)
    Signed-off-by: prxt6529 <prxt@6529.io>

--- components/waves/winners/drops/header/WaveWinnersDropHeaderAuthorPfp.tsx
+++ components/waves/winners/drops/header/WaveWinnersDropHeaderAuthorPfp.tsx
@@ -2,27 +2,32 @@ import type { ApiWaveDecisionWinner } from "@/generated/models/ApiWaveDecisionWi
 
 interface WaveWinnersDropHeaderAuthorPfpProps {
   readonly winner: ApiWaveDecisionWinner;
+  readonly size?: "sm" | "md";
 }
 
 export default function WaveWinnersDropHeaderAuthorPfp({
   winner,
+  size = "md",
 }: WaveWinnersDropHeaderAuthorPfpProps) {
+  const sizeClasses = size === "sm" ? "tw-h-8 tw-w-8" : "tw-h-12 tw-w-12";
   return (
-    <div className="tw-h-11 tw-w-11 tw-bg-iron-900 tw-relative tw-flex-shrink-0 tw-rounded-lg">
+    <div
+      className={`tw-relative tw-flex-shrink-0 tw-rounded-lg tw-bg-iron-900 ${sizeClasses}`}
+    >
       {winner.drop.author.pfp ? (
-        <div className="tw-rounded-lg tw-h-full tw-w-full">
-          <div className="tw-h-full tw-w-full tw-max-w-full tw-rounded-lg tw-overflow-hidden tw-bg-iron-900 tw-ring-1 tw-ring-white/10">
-            <div className="tw-h-full tw-text-center tw-flex tw-items-center tw-justify-center tw-rounded-lg tw-overflow-hidden">
+        <div className="tw-h-full tw-w-full tw-rounded-lg">
+          <div className="tw-h-full tw-w-full tw-max-w-full tw-overflow-hidden tw-rounded-lg tw-bg-iron-900 tw-ring-1 tw-ring-white/10">
+            <div className="tw-flex tw-h-full tw-items-center tw-justify-center tw-overflow-hidden tw-rounded-lg tw-text-center">
               <img
                 src={winner.drop.author.pfp}
                 alt="Profile picture"
-                className="tw-bg-transparent tw-max-w-full tw-max-h-full tw-h-auto tw-w-auto tw-mx-auto tw-object-contain"
+                className="tw-mx-auto tw-h-auto tw-max-h-full tw-w-auto tw-max-w-full tw-bg-transparent tw-object-contain"
               />
             </div>
           </div>
         </div>
       ) : (
-        <div className="tw-h-full tw-w-full tw-bg-iron-900 tw-ring-1 tw-ring-inset tw-ring-white/10 tw-rounded-lg"></div>
+        <div className="tw-h-full tw-w-full tw-rounded-lg tw-bg-iron-900 tw-ring-1 tw-ring-inset tw-ring-white/10"></div>
       )}
     </div>
   );
