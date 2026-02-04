03453dcb7225167d17638089cc623a064ec50f45
ragnep <ragneinfo@gmail.com>
2026-02-04 13:12:41 +0200

    fix for level
    Signed-off-by: ragnep <ragneinfo@gmail.com>

--- components/brain/my-stream/votes/MyStreamWaveMyVote.tsx
+++ components/brain/my-stream/votes/MyStreamWaveMyVote.tsx
@@ -147,12 +147,10 @@ const MyStreamWaveMyVote: React.FC<MyStreamWaveMyVoteProps> = ({
                 {drop.author.handle}
               </Link>
             </UserProfileTooltipWrapper>
-            {!!drop.author.level && (
-              <UserCICAndLevel
-                level={drop.author.level}
-                size={UserCICAndLevelSize.SMALL}
-              />
-            )}
+            <UserCICAndLevel
+              level={drop.author.level || 0}
+              size={UserCICAndLevelSize.SMALL}
+            />
           </div>
           <div className="tw-mt-3.5 tw-flex tw-flex-col tw-justify-between tw-gap-4 @lg:tw-flex-col @[42rem]:tw-flex-row xl:tw-mt-3">
             <div className="tw-flex tw-items-center tw-gap-x-6">
