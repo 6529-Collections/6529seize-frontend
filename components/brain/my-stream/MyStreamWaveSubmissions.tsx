4b12861f46dbf24c2d8a27cd39723b66f3ddb27a
Simo <simo@6529.io>
2026-04-14 11:00:16 +0300

    wip
    Signed-off-by: Simo <simo@6529.io>

--- components/brain/my-stream/MyStreamWaveSubmissions.tsx
+++ components/brain/my-stream/MyStreamWaveSubmissions.tsx
@@ -86,7 +86,7 @@ const MyStreamWaveSubmissions: React.FC<MyStreamWaveSubmissionsProps> = ({
         return;
       }
 
-      void fetchNextPage();
+      fetchNextPage();
     },
     [fetchNextPage, hasNextPage, isFetching, isFetchingNextPage]
   );
