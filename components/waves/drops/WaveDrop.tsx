551323b5c831db8fd4a67293b63476180a7d4ccc
prxt6529 <prxt@6529.io>
2026-05-21 16:08:03 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- components/waves/drops/WaveDrop.tsx
+++ components/waves/drops/WaveDrop.tsx
@@ -182,8 +182,8 @@ const getAuthorHighlightBase = (level: number) => {
 const getStableHash = (value: string): number => {
   let hash = 0;
 
-  for (let i = 0; i < value.length; i++) {
-    hash = (hash * 31 + value.charCodeAt(i)) >>> 0;
+  for (const character of value) {
+    hash = (hash * 31 + character.codePointAt(0)!) >>> 0;
   }
 
   return hash;
