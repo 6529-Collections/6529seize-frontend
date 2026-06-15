c365d51f23dce5a2af38c7bf6f6a6868282d6888
GelatoGenesis <tarmokalling@gmail.com>
2026-06-16 00:15:02 +0300

    Poll option where only chatters can respond
    Signed-off-by: GelatoGenesis <tarmokalling@gmail.com>

--- generated/models/ApiWavePoll.ts
+++ generated/models/ApiWavePoll.ts
@@ -26,6 +26,7 @@ export class ApiWavePoll {
     'voted': Array<number>;
     'multichoice': boolean;
     'anonymous': boolean;
+    'only_droppers_can_respond': boolean;
     'closing_time': number;
     'is_open': boolean;
 
@@ -79,6 +80,12 @@ export class ApiWavePoll {
         {
             "name": "anonymous",
             "baseName": "anonymous",
+            "type": "boolean",
+            "format": ""
+        },
+        {
+            "name": "only_droppers_can_respond",
+            "baseName": "only_droppers_can_respond",
             "type": "boolean",
             "format": ""
         },
