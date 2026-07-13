7f47a44cbe5fd0e2d2c4c92e9bea6694ff239b55
ragnep <ragneinfo@gmail.com>
2026-07-13 20:53:07 +0300

    Fix mobile About navigation overlap
    Signed-off-by: ragnep <ragneinfo@gmail.com>

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -50,21 +50,8 @@ export default function About({ section }: { readonly section: AboutSection }) {
     >
       <Row>
         <Col>
-          <AboutContentsDropdown
-            className={
-              section === AboutSection.SUBSCRIPTIONS
-                ? "max-sm:!tw-top-2 max-sm:-tw-mt-14 max-sm:tw-mb-0 max-sm:tw-py-1"
-                : undefined
-            }
-            currentSection={section}
-          />
-          <div
-            className={
-              section === AboutSection.SUBSCRIPTIONS
-                ? "tw-w-full max-sm:tw-pt-8"
-                : "tw-w-full"
-            }
-          >
+          <AboutContentsDropdown currentSection={section} />
+          <div className="tw-w-full">
             <AboutSectionContent section={section} />
           </div>
         </Col>
