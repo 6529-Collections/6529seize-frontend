4fc2650c90040c2fedbc8d0692e243ae31bab581
ragnep <ragneinfo@gmail.com>
2026-07-23 11:09:20 +0300

    Align About The Memes navigation
    Signed-off-by: ragnep <ragneinfo@gmail.com>

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -45,9 +45,11 @@ export default function About({ section }: { readonly section: AboutSection }) {
   if (section === AboutSection.MEMES) {
     return (
       <div className="tw-min-h-[calc(100vh-100px)] tw-bg-[#0D0D0F]">
-        <div className="tw-mx-auto tw-w-full tw-max-w-[1400px] tw-px-4 tw-pt-4 sm:tw-px-6 lg:tw-px-8">
-          <AboutContentsDropdown currentSection={section} />
-        </div>
+        <AboutContentsDropdown
+          className="!tw-mb-0 tw-mx-auto tw-w-full tw-max-w-[1400px] !tw-bg-[#0D0D0F] tw-px-4 sm:tw-px-6 lg:tw-px-8"
+          currentSection={section}
+          withDivider
+        />
         <AboutMemes />
       </div>
     );
