e4672dc7ba104783b470d215c29be2002d7b24ff
ragnep <ragneinfo@gmail.com>
2026-08-05 10:40:52 +0300

    Increase desktop table heading size
    Signed-off-by: ragnep <ragneinfo@gmail.com>

--- components/leaderboard/NFTLeaderboard.tsx
+++ components/leaderboard/NFTLeaderboard.tsx
@@ -16,7 +16,10 @@ import { t } from "@/i18n/messages";
 import { formatInteger } from "@/i18n/format";
 import { QueryKey } from "@/components/react-query-wrapper/ReactQueryWrapper";
 import Button from "@/components/utils/button/Button";
-import { DATA_TABLE_INTERACTIVE_ROW_CLASS_NAME } from "@/components/utils/table/tableRowStyles";
+import {
+  DATA_TABLE_HEADER_TEXT_CLASS_NAME,
+  DATA_TABLE_INTERACTIVE_ROW_CLASS_NAME,
+} from "@/components/utils/table/tableRowStyles";
 import Pagination from "../pagination/Pagination";
 import { SortDirection } from "@/entities/ISort";
 import type { CICType } from "@/entities/IProfile";
@@ -34,8 +37,7 @@ import {
   faSquareCaretDown,
 } from "@fortawesome/free-solid-svg-icons";
 
-const tableHeaderClassName =
-  "tw-whitespace-nowrap tw-border-0 tw-border-b tw-border-solid tw-border-iron-800 tw-px-2 tw-py-2 tw-text-center tw-text-xs tw-font-semibold tw-leading-4 tw-text-iron-400 md:tw-px-4 md:tw-py-3";
+const tableHeaderClassName = `tw-whitespace-nowrap tw-border-0 tw-border-b tw-border-solid tw-border-iron-800 tw-px-2 tw-py-2 tw-text-center tw-font-semibold tw-text-iron-400 md:tw-px-4 md:tw-py-3 ${DATA_TABLE_HEADER_TEXT_CLASS_NAME}`;
 
 const tableCellClassName =
   "tw-whitespace-nowrap tw-border-0 tw-border-b tw-border-solid tw-border-iron-800 tw-px-2 tw-py-2 tw-text-center tw-text-xs tw-font-medium tw-leading-5 tw-text-iron-100 md:tw-px-4 md:tw-py-3 md:tw-text-sm";
@@ -661,14 +663,14 @@ export default function NFTLeaderboard(props: Readonly<Props>) {
               <th
                 colSpan={3}
                 scope="colgroup"
-                className="tw-whitespace-nowrap tw-border-0 tw-border-b tw-border-l tw-border-solid tw-border-iron-800 tw-px-4 tw-py-3 tw-text-center tw-text-xs tw-font-semibold tw-leading-4 tw-text-iron-300"
+                className={`tw-whitespace-nowrap tw-border-0 tw-border-b tw-border-l tw-border-solid tw-border-iron-800 tw-px-4 tw-py-3 tw-text-center tw-font-semibold tw-text-iron-300 ${DATA_TABLE_HEADER_TEXT_CLASS_NAME}`}
               >
                 This Card
               </th>
               <th
                 colSpan={3}
                 scope="colgroup"
-                className="tw-whitespace-nowrap tw-border-0 tw-border-b tw-border-l tw-border-solid tw-border-iron-800 tw-px-4 tw-py-3 tw-text-center tw-text-xs tw-font-semibold tw-leading-4 tw-text-iron-300"
+                className={`tw-whitespace-nowrap tw-border-0 tw-border-b tw-border-l tw-border-solid tw-border-iron-800 tw-px-4 tw-py-3 tw-text-center tw-font-semibold tw-text-iron-300 ${DATA_TABLE_HEADER_TEXT_CLASS_NAME}`}
               >
                 Total
               </th>
@@ -676,13 +678,13 @@ export default function NFTLeaderboard(props: Readonly<Props>) {
             <tr>
               <th
                 scope="col"
-                className="tw-w-12 tw-whitespace-nowrap tw-border-0 tw-border-b tw-border-solid tw-border-iron-800 tw-px-2 tw-py-2 tw-text-center tw-text-xs tw-font-semibold tw-leading-4 tw-text-iron-400 md:tw-w-20 md:tw-px-4 md:tw-py-3"
+                className={`tw-w-12 tw-whitespace-nowrap tw-border-0 tw-border-b tw-border-solid tw-border-iron-800 tw-px-2 tw-py-2 tw-text-center tw-font-semibold tw-text-iron-400 md:tw-w-20 md:tw-px-4 md:tw-py-3 ${DATA_TABLE_HEADER_TEXT_CLASS_NAME}`}
               >
                 Rank
               </th>
               <th
                 scope="col"
-                className="tw-min-w-[11rem] tw-whitespace-nowrap tw-border-0 tw-border-b tw-border-solid tw-border-iron-800 tw-px-2 tw-py-2 tw-text-left tw-text-xs tw-font-semibold tw-leading-4 tw-text-iron-400 md:tw-min-w-[18rem] md:tw-px-4 md:tw-py-3"
+                className={`tw-min-w-[11rem] tw-whitespace-nowrap tw-border-0 tw-border-b tw-border-solid tw-border-iron-800 tw-px-2 tw-py-2 tw-text-left tw-font-semibold tw-text-iron-400 md:tw-min-w-[18rem] md:tw-px-4 md:tw-py-3 ${DATA_TABLE_HEADER_TEXT_CLASS_NAME}`}
               >
                 Collector{" "}
                 {totalResults > 0 && `x${formatInteger(locale, totalResults)}`}
--- components/utils/table/tableRowStyles.ts
+++ components/utils/table/tableRowStyles.ts
@@ -1,2 +1,5 @@
 export const DATA_TABLE_INTERACTIVE_ROW_CLASS_NAME =
   "odd:tw-bg-transparent even:tw-bg-iron-900/70 hover:tw-bg-iron-900 focus-within:tw-bg-iron-900";
+
+export const DATA_TABLE_HEADER_TEXT_CLASS_NAME =
+  "tw-text-xs tw-leading-4 md:tw-text-sm md:tw-leading-5";
