35eb83592a913e735ffd5cd2ba6aa58a5072374b
ragnep <ragneinfo@gmail.com>
2026-08-06 11:22:21 +0300

    Refine usage and tech page styling
    Signed-off-by: ragnep <ragneinfo@gmail.com>

---
+++ app/open-data/6529bot/loading.tsx
@@ -0,0 +1,11 @@
+import {
+  ReviewbotUsageLoading,
+} from "@/components/reviewbot-usage/ReviewbotUsageDashboard";
+
+export default function Loading() {
+  return (
+    <main className="tw-min-h-screen">
+      <ReviewbotUsageLoading />
+    </main>
+  );
+}
