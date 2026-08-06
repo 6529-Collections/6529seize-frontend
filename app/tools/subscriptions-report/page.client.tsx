a502fbbfd4c19906cf3488eac929d4af221d069b
ragnep <ragneinfo@gmail.com>
2026-08-06 13:31:11 +0300

    Strengthen subscriptions report divider
    Signed-off-by: ragnep <ragneinfo@gmail.com>

--- app/tools/subscriptions-report/page.client.tsx
+++ app/tools/subscriptions-report/page.client.tsx
@@ -6,7 +6,7 @@ import styles from "@/styles/Home.module.css";
 import clsx from "clsx";
 
 const SUBSCRIPTIONS_REPORT_MAIN_CLASS =
-  "tailwind-scope tw-min-h-[100dvh] tw-border-0 tw-border-solid tw-border-iron-800 !tw-bg-[#0D0D0F] tw-text-iron-300 md:tw-border-l";
+  "tailwind-scope tw-min-h-[100dvh] tw-border-0 tw-border-solid tw-border-iron-700 !tw-bg-[#0D0D0F] tw-text-iron-300 md:tw-border-l";
 
 export default function SubscriptionsReport() {
   useSetTitle("Subscriptions Report | Tools");
