6a5f32352876853ccf75c040f553a582871810d8
ragnep <ragneinfo@gmail.com>
2026-08-17 13:08:16 +0300

    Correct block picker selection semantics
    Signed-off-by: ragnep <ragneinfo@gmail.com>

--- components/block-picker/BlockPickerTimeWindowSelectList.tsx
+++ components/block-picker/BlockPickerTimeWindowSelectList.tsx
@@ -17,10 +17,10 @@ export default function BlockPickerTimeWindowSelectList({
       className="tw-absolute tw-z-10 tw-mt-1 tw-max-h-60 tw-w-full tw-list-none tw-overflow-auto tw-rounded-lg tw-bg-iron-900 tw-py-2 tw-pl-1.5 tw-pr-1.5 tw-text-base tw-text-iron-200 tw-shadow-xl tw-ring-1 tw-ring-inset tw-ring-iron-700/60 focus:tw-outline-none sm:tw-text-sm"
       role="listbox"
       aria-labelledby="listbox-label"
-      aria-activedescendant="listbox-option-3"
     >
       {options.map((option) => (
         <li
+          id={`block-picker-time-window-option-${option.value}`}
           key={option.value}
           onClick={(e) => {
             e.stopPropagation();
@@ -28,7 +28,7 @@ export default function BlockPickerTimeWindowSelectList({
           }}
           className="tw-group tw-relative tw-cursor-pointer tw-select-none tw-rounded-lg tw-py-2.5 tw-pl-3 tw-pr-9 tw-text-iron-200 tw-transition tw-duration-300 tw-ease-out hover:tw-bg-iron-800 hover:tw-text-iron-50"
           role="option"
-          aria-selected="true"
+          aria-selected={timeWindow === option.value}
         >
           <div className="tw-flex tw-w-full tw-items-center tw-justify-between tw-gap-x-4 tw-pr-4">
             <div className="tw-flex-1">
