dac51999a6427df93096308dc22a051450c84016
ragnep <ragneinfo@gmail.com>
2026-09-04 13:35:43 +0300

    Keep storm part action label compact
    Signed-off-by: ragnep <ragneinfo@gmail.com>

--- components/waves/create-drop-content/CreateDropComposer.tsx
+++ components/waves/create-drop-content/CreateDropComposer.tsx
@@ -100,7 +100,7 @@ export default function CreateDropComposer({
     } else if (canAddPart || (drop?.parts.length ?? 0) === 0) {
       submitLabel = t(locale, "waves.stormComposer.addPart");
     } else {
-      submitLabel = t(locale, "waves.stormComposer.postStorm");
+      submitLabel = t(locale, "waves.stormComposer.addPart");
     }
   }
   return (
