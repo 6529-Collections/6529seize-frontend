8b84cfde6c0f9d9e24f3194e0c76ac955e6e2768
ragnep <ragneinfo@gmail.com>
2026-09-10 14:49:27 +0300

    Refine empty podium placeholders
    Signed-off-by: ragnep <ragneinfo@gmail.com>

--- components/waves/winners/podium/WaveWinnersPodiumPlaceholder.tsx
+++ components/waves/winners/podium/WaveWinnersPodiumPlaceholder.tsx
@@ -20,25 +20,22 @@ export const WaveWinnersPodiumPlaceholder: React.FC<
         className={`${styles.pfpOverlap} tw-relative tw-z-10 tw-flex tw-flex-shrink-0 tw-flex-col tw-items-center`}
       >
         <div
-          className={`${styles.pfpSize} tw-flex-shrink-0 tw-rounded-full tw-bg-iron-800 tw-ring-1 tw-ring-white/[0.04]`}
+          className={`${styles.pfpSize} tw-flex-shrink-0 tw-rounded-full tw-bg-iron-800/80`}
         />
-        <div className="tw-absolute tw-inset-x-0 tw-bottom-0 tw-flex tw-translate-y-1 tw-justify-center sm:tw-translate-y-0">
-          <div className="tw-bg-iron-850 tw-h-5 tw-w-10 tw-rounded-full tw-border tw-border-solid tw-border-white/[0.03] sm:tw-h-6 sm:tw-w-12" />
-        </div>
       </div>
 
       <div
         className={`${podiumSurfaceClassName} ${styles.height} !tw-border-white/[0.025] !tw-from-iron-900/55 !tw-via-iron-900/20 !tw-to-transparent`}
       >
         <div className="tw-flex tw-w-full tw-flex-1 tw-flex-col tw-items-center tw-justify-center tw-gap-2 tw-pb-3 tw-pt-7 sm:tw-gap-3 sm:tw-pb-4 sm:tw-pt-9">
-          <div className="tw-h-3 tw-w-3/4 tw-max-w-28 tw-rounded-full tw-bg-iron-800" />
-          <div className="tw-h-2 tw-w-1/2 tw-max-w-20 tw-rounded-full tw-bg-iron-800" />
-          <div className="tw-h-3 tw-w-14 tw-rounded-full tw-bg-iron-800" />
+          <div className="tw-h-3 tw-w-3/4 tw-max-w-28 tw-rounded-full tw-bg-iron-800/80" />
+          <div className="tw-h-2 tw-w-1/2 tw-max-w-20 tw-rounded-full tw-bg-iron-800/80" />
+          <div className="tw-h-3 tw-w-14 tw-rounded-full tw-bg-iron-800/80" />
         </div>
         <div className="tw-flex tw-w-full tw-flex-col tw-items-center tw-gap-2 sm:tw-gap-3">
           <div className="tw-flex tw-w-full tw-flex-col tw-items-center tw-justify-center tw-gap-1 @[42rem]/podium:tw-flex-row @[42rem]/podium:tw-gap-1.5">
-            <div className="tw-h-7 tw-w-14 tw-rounded-lg tw-bg-iron-800 sm:tw-h-8 sm:tw-w-16" />
-            <div className="tw-h-7 tw-w-14 tw-rounded-lg tw-bg-iron-800 sm:tw-h-8 sm:tw-w-16" />
+            <div className="tw-h-7 tw-w-14 tw-rounded-lg tw-bg-iron-800/80 sm:tw-h-8 sm:tw-w-16" />
+            <div className="tw-h-7 tw-w-14 tw-rounded-lg tw-bg-iron-800/80 sm:tw-h-8 sm:tw-w-16" />
           </div>
         </div>
       </div>
