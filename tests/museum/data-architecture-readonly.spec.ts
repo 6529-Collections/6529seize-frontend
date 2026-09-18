2eb4537815fb33de49306fcaa19f9f88f2175feb
prxt6529 <117092621+prxt6529@users.noreply.github.com>
2026-09-18 11:23:48 +0300

    Isolate local Museum checks from runner IP geolocation
    Signed-off-by: prxt6529 <117092621+prxt6529@users.noreply.github.com>

--- tests/museum/data-architecture-readonly.spec.ts
+++ tests/museum/data-architecture-readonly.spec.ts
@@ -11,6 +11,7 @@ import {
   assertNoFailedResponses,
   attachPageDiagnostics,
 } from "../support/pageAssertions";
+import { installLocalMuseumCountryCheck } from "../support/localMuseumCountryCheck";
 import { gotoDocumentWithTransientRetry } from "../support/routeReadiness";
 import { MUSEUM_SETTINGS_FETCH_ERROR_PATTERN } from "../support/museumConsoleDiagnostics";
 import {
@@ -131,7 +132,8 @@ test.describe("Museum data architecture @surface @readonly", () => {
   test.describe.configure({ mode: "serial" });
   test.setTimeout(300_000);
 
-  test.beforeEach(async ({ page }, testInfo) => {
+  test.beforeEach(async ({ page, baseURL }, testInfo) => {
+    await installLocalMuseumCountryCheck(page, baseURL);
     if (testInfo.project.name === MOBILE_PROJECT) {
       await page.setViewportSize(MOBILE_VIEWPORT);
       expect(page.viewportSize()).toEqual(MOBILE_VIEWPORT);
